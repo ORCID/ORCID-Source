326269ee745b16c6f725269c027c9051f1eb5469
rcpeters <info@rcpeters.com>
2013-07-16 09:01:26 -0700

    context switching

---
+++ orcid-core/src/main/java/org/orcid/core/locale/LocaleChangeInterceptor.java
@@ -0,0 +1,123 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2013 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orcid.core.locale;
+
+import java.security.Principal;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.orcid.core.manager.OrcidProfileManager;
+import org.orcid.jaxb.model.message.OrcidProfile;
+import org.springframework.context.ApplicationContext;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+/**
+ * Interceptor that allows for changing the current locale on every request,
+ * via a configurable request parameter.
+ *
+ * @author rcpeters
+ * @see org.springframework.web.servlet.LocaleResolver
+ */
+public class LocaleChangeInterceptor extends HandlerInterceptorAdapter {
+    
+        
+        @Resource
+        protected OrcidProfileManager orcidProfileManager;
+        
+        /**
+         * Default name of the locale specification parameter: "locale".
+         */
+        public static final String DEFAULT_PARAM_NAME = "locale";
+
+        private String paramName = DEFAULT_PARAM_NAME;
+
+
+        /**
+         * Set the name of the parameter that contains a locale specification
+         * in a locale change request. Default is "locale".
+         */
+        public void setParamName(String paramName) {
+                this.paramName = paramName;
+        }
+
+        /**
+         * Return the name of the parameter that contains a locale specification
+         * in a locale change request.
+         */
+        public String getParamName() {
+                return this.paramName;
+        }
+
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+                        throws ServletException {
+
+                String newLocale = request.getParameter(this.paramName);
+                Principal principal = request.getUserPrincipal();
+                if (newLocale == null && principal != null) {
+                    // make sure the local is the correct settings.
+                    LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+                    Locale currentLocale = localeResolver.resolveLocale(request);
+                     
+                    String orcidId =  principal.getName();
+                    OrcidProfile orcidProfile = orcidProfileManager.retrieveOrcidProfile(orcidId);
+                    if (orcidProfile.getOrcidInternal() != null 
+                            && orcidProfile.getOrcidInternal().getPreferences() != null
+                            && orcidProfile.getOrcidInternal().getPreferences().getLocale() != null 
+                            && orcidProfile.getOrcidInternal().getPreferences().getLocale().value() != null) {
+                        if (currentLocale.toString() != orcidProfile.getOrcidInternal().getPreferences().getLocale().value()) {
+                            String preferedLocale = orcidProfile.getOrcidInternal().getPreferences().getLocale().value();
+                            localeResolver.setLocale(request, response, StringUtils.parseLocaleString(preferedLocale));
+                        }
+                    }
+                    
+                }
+                if (newLocale != null) {
+                        LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+                        if (localeResolver == null) {
+                                throw new IllegalStateException("No LocaleResolver found: not in a DispatcherServlet request?");
+                        }
+                        localeResolver.setLocale(request, response, StringUtils.parseLocaleString(newLocale));
+                }
+                // Proceed in any case.
+                return true;
+        }
+
+}
