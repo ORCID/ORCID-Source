083e3e100682526b27d434396a31aae3b81cedcd
rcpeters <info@rcpeters.com>
2013-09-06 14:44:29 -0700

    change run times

--- orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/EmailEventType.java
+++ orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/EmailEventType.java
@@ -24,6 +24,6 @@ package org.orcid.persistence.jpa.entities;
 public enum EmailEventType {
     
     VERIFY_EMAIL_7_DAYS_SENT,
-    VERIFY_EMAIL_7_DAYS_SENT_SKIPPED /* we are going to skip notifing email address that where already in the system before lauching this */
+    VERIFY_EMAIL_7_DAYS_SENT_SKIPPED /* we are going to skip notifying email address that where already in the system before launching this */
     ;
 }
