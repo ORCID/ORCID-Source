b402f39498b710dfc18bbd67eba6cb18758335ef
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2013-10-02 21:35:37 -0600

    Adding message converter from v21 to v22

---
+++ orcid-core/src/main/java/org/orcid/core/version/impl/OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.java
@@ -0,0 +1,85 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2013 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.core.version.impl;
+
+import org.orcid.core.version.OrcidMessageVersionConverter;
+import org.orcid.jaxb.model.message.OrcidActivities;
+import org.orcid.jaxb.model.message.OrcidMessage;
+import org.orcid.jaxb.model.message.OrcidProfile;
+import org.orcid.jaxb.model.message.OrcidWork;
+import org.orcid.jaxb.model.message.OrcidWorks;
+
+/**
+ * 
+ * @author Will Simpson
+ * 
+ */
+public class OrcidMessageVersionConverterImplV1_0_21ToV1_0_22 implements OrcidMessageVersionConverter {
+
+    private static final String FROM_VERSION = "1.0.21";
+    private static final String TO_VERSION = "1.0.22";
+
+    @Override
+    public String getFromVersion() {
+        return FROM_VERSION;
+    }
+
+    @Override
+    public String getToVersion() {
+        return TO_VERSION;
+    }
+
+    @Override
+    public OrcidMessage downgradeMessage(OrcidMessage orcidMessage) {
+        if (orcidMessage == null) {
+            return null;
+        }
+        orcidMessage.setMessageVersion(FROM_VERSION);
+        // Remove affiliations from new place
+        OrcidProfile orcidProfile = orcidMessage.getOrcidProfile();
+        if(orcidProfile != null){
+	        OrcidActivities orcidActivities = orcidProfile.getOrcidActivities();	        
+	        if(orcidActivities != null){
+	        	OrcidWorks orcidWorks = orcidActivities.getOrcidWorks();
+	        	if(orcidWorks != null){
+	        		for(OrcidWork orcidWork : orcidWorks.getOrcidWork()){
+	        			// Remove the translated title
+	        			if(orcidWork.getWorkTitle() != null){
+	        				orcidWork.getWorkTitle().setTranslatedTitle(null);
+	        			}
+	        			
+	        			// Remove the language
+	        			orcidWork.setLanguageCode(null);
+	        		}
+	        	}
+	        }
+        }
+        
+        
+        return orcidMessage;
+    }
+
+    @Override
+    public OrcidMessage upgradeMessage(OrcidMessage orcidMessage) {
+        if (orcidMessage == null) {
+            return null;
+        }
+        orcidMessage.setMessageVersion(TO_VERSION);
+        return orcidMessage;
+    }
+
+}
