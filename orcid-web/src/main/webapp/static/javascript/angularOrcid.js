7d19eb716182df511c5e7ed532c775fc0c279530
rcpeters <info@rcpeters.com>
2013-10-07 16:14:07 -0700

    removed code no longer used

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -1386,40 +1386,6 @@ function WorkCtrl($scope, $compile, worksSrvc){
     	$('#translatedTitle').toggle();
     	$.colorbox.resize();
 	};		
-
-	$scope.showDetailModal = function(idx){
-		var dw = $scope.works[idx];
-		
-		// filter out blank contributors 
-		for (idx in dw.contributors) {
-			if (dw.contributors[idx].contributorSequence == null
-				&& dw.contributors[idx].email == null
-				&& dw.contributors[idx].orcid == null
-				&& dw.contributors[idx].creditName == null
-				&& dw.contributors[idx].contributorRole == null
-				&& dw.contributors[idx].creditNameVisibility == null)
-				delete dw.contributors.splice(idx,1);
-		}
-		
-		$scope.detailWork = dw;
-		$scope.showBibtex = false;
-		if ($scope.detailWork.citation && $scope.detailWork.citation.citationType.value == 'bibtex') {
-			try {
-				$scope.detailWork.bibtexCitation = bibtexParse.toJSON($scope.detailWork.citation.citation.value);
-				console.log($scope.detailWork.bibtexCitation);
-				$scope.showBibtex = true;
-			} catch (err) {
-				console.log("couldn't parse bibtex: " + $scope.detailWork.citation.citation.value);
-			}
-		}
-	    $.colorbox({        	
-	        html: $compile($('#detail-work-modal').html())($scope),
-	        scrolling: true,
-	        onComplete: function() {
-	        	$.colorbox.resize();
-	        }
-	    });  
-    };
     
     $scope.bibtexShowToggle = function () {
     	$scope.showBibtex = !($scope.showBibtex);
