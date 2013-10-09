69cc3eb1dc3a773b4020c470b2cff783a021a89e
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2013-10-09 09:50:54 -0600

    Fixed version used by public api

---
+++ orcid-pub-web/src/main/java/org/orcid/api/t1/server/T1OrcidApiServiceImplV1_0_21.java
@@ -0,0 +1,29 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2013 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.api.t1.server;
+
+import javax.ws.rs.Path;
+
+/**
+ * 
+ * @author Will Simpson
+ * 
+ */
+@Path("/v1.0.21")
+public class T1OrcidApiServiceImplV1_0_21 extends T1OrcidApiServiceImplBase {
+
+}
