7c8ff00727c1a75f7d70801786cd8d186cf15d0e
Will Simpson <will@wjrs.co.uk>
2013-10-22 11:27:00 +0100

    Added ability to process zip file directly. Also added processing of deleted IDs (just checking and logging for now).

--- orcid-core/src/main/java/org/orcid/core/cli/LoadRinggoldData.java
+++ orcid-core/src/main/java/org/orcid/core/cli/LoadRinggoldData.java
@@ -20,9 +20,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.args4j.CmdLineException;
@@ -33,6 +37,7 @@ import org.orcid.jaxb.model.message.Iso3166Country;
 import org.orcid.persistence.dao.OrgDisambiguatedDao;
 import org.orcid.persistence.jpa.entities.OrgDisambiguatedEntity;
 import org.orcid.persistence.jpa.entities.OrgEntity;
+import org.orcid.utils.NullUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -47,25 +52,38 @@ import au.com.bytecode.opencsv.CSVReader;
  */
 public class LoadRinggoldData {
 
+    private static final String RINGGOLD_CHARACTER_ENCODING = "UTF-8";
     private static final String RINGGOLD_SOURCE_TYPE = "RINGGOLD";
     private static final Logger LOGGER = LoggerFactory.getLogger(LoadRinggoldData.class);
-    @Option(name = "-f", usage = "Path to file to load into DB", required = true)
+    @Option(name = "-f", usage = "Path to CSV file containing Ringgold parents to load into DB")
     private File fileToLoad;
+    @Option(name = "-d", usage = "Path to CSV file containing Ringgold deleted IDs to process")
+    private File deletedIdsFile;
+    @Option(name = "-z", usage = "Path to zip file containing Ringgold data to process")
+    private File zipFile;
     private OrgDisambiguatedDao orgDisambiguatedDao;
     private OrgManager orgManager;
     private int numAdded;
     private int numUpdated;
+    private int numDeleted;
 
     public static void main(String[] args) {
         LoadRinggoldData loadRinggoldData = new LoadRinggoldData();
         CmdLineParser parser = new CmdLineParser(loadRinggoldData);
         try {
             parser.parseArgument(args);
+            loadRinggoldData.validateArgs(parser);
             loadRinggoldData.init();
             loadRinggoldData.execute();
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
             parser.printUsage(System.err);
+        }
+    }
+
+    private void validateArgs(CmdLineParser parser) throws CmdLineException {
+        if (NullUtils.allNull(fileToLoad, deletedIdsFile, zipFile)) {
+            throw new CmdLineException(parser, "At least one of -f | -d | -z must be specificed");
         }
     }
 
@@ -76,21 +94,67 @@ public class LoadRinggoldData {
     }
 
     public void execute() {
+        if (fileToLoad != null) {
+            processParentsCsv();
+        }
+        if (deletedIdsFile != null) {
+            processDeletedIds();
+        } else {
+            processZip();
+        }
+        LOGGER.info("Finished loading Ringgold data");
+    }
+
+    private void processDeletedIds() {
+        try (Reader reader = openFile(deletedIdsFile)) {
+            processDeletedIdsReader(reader);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading csv file", e);
+        } finally {
+            LOGGER.info("Number deleted={}", new Object[] { numDeleted });
+        }
+
+    }
+
+    private void processZip() {
+        try (ZipFile zip = new ZipFile(zipFile)) {
+            for (ZipEntry entry : Collections.list(zip.entries())) {
+                String entryName = entry.getName();
+                if (entryName.endsWith("_parents.csv")) {
+                    LOGGER.info("Found parents file: " + entryName);
+                    Reader reader = getReader(zip, entry);
+                    processReader(reader);
+                }
+                if (entryName.endsWith("deleted_ids.csv")) {
+                    LOGGER.info("Found deleted ids file: " + entryName);
+                    Reader reader = getReader(zip, entry);
+                    processDeletedIdsReader(reader);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading zip file", e);
+        }
+    }
+
+    private Reader getReader(ZipFile zip, ZipEntry entry) throws IOException, UnsupportedEncodingException {
+        InputStream is = zip.getInputStream(entry);
+        Reader reader = new InputStreamReader(is, RINGGOLD_CHARACTER_ENCODING);
+        return reader;
+    }
+
+    private void processParentsCsv() {
         try (Reader reader = openFile(fileToLoad)) {
             processReader(reader);
         } catch (IOException e) {
             throw new RuntimeException("Error reading csv file", e);
-        } finally {
-            LOGGER.info("Number added={}, number updated={}, total={}", new Object[] { numAdded, numUpdated, numAdded + numUpdated });
         }
-        LOGGER.info("Finished loading Ringgold data");
     }
 
     private Reader openFile(File fileToLoad) {
         Reader reader = null;
         try {
             FileInputStream fis = new FileInputStream(fileToLoad);
-            reader = new InputStreamReader(fis, "UTF-8");
+            reader = new InputStreamReader(fis, RINGGOLD_CHARACTER_ENCODING);
         } catch (FileNotFoundException e) {
             if (!fileToLoad.exists()) {
                 throw new IllegalArgumentException("Input file does not exist: " + fileToLoad);
@@ -106,12 +170,18 @@ public class LoadRinggoldData {
     }
 
     private void processReader(Reader reader) throws IOException {
-        try (CSVReader csvReader = new CSVReader(reader, ',', '"', 1)) {
+        try (CSVReader csvReader = createCSVReader(reader)) {
             String[] line;
             while ((line = csvReader.readNext()) != null) {
                 processLine(line);
             }
+        } finally {
+            LOGGER.info("Number added={}, number updated={}, total={}", new Object[] { numAdded, numUpdated, numAdded + numUpdated });
         }
+    }
+
+    private CSVReader createCSVReader(Reader reader) {
+        return new CSVReader(reader, ',', '"', 1);
     }
 
     private void processLine(String[] line) {
@@ -184,6 +254,27 @@ public class LoadRinggoldData {
             countryString = "CA";
         }
         return Iso3166Country.valueOf(countryString);
+    }
+
+    private void processDeletedIdsReader(Reader reader) throws IOException {
+        try (CSVReader csvReader = createCSVReader(reader)) {
+            String[] line;
+            while ((line = csvReader.readNext()) != null) {
+                processDeletedIdsLine(line);
+            }
+        } finally {
+            LOGGER.info("Number deleted={}", numDeleted);
+        }
+    }
+
+    private void processDeletedIdsLine(String[] line) {
+        String deletedId = line[0];
+        OrgDisambiguatedEntity entity = orgDisambiguatedDao.findBySourceIdAndSourceType(deletedId, RINGGOLD_SOURCE_TYPE);
+        if (entity != null) {
+            LOGGER.info("Deleted ID exists in DB, id={}", deletedId);
+            // Should actually delete, or flag status?
+            numDeleted++;
+        }
     }
 
 }
