9df4ea0884c68e8f08c6f647aabae066fb40baa3
Will Simpson <will@wjrs.co.uk>
2013-10-24 19:46:19 +0100

    Added Kosovo.

--- orcid-core/src/main/java/org/orcid/core/version/impl/OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.java
+++ orcid-core/src/main/java/org/orcid/core/version/impl/OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.java
@@ -16,8 +16,14 @@
  */
 package org.orcid.core.version.impl;
 
+import java.util.Iterator;
+
 import org.apache.commons.lang3.StringUtils;
 import org.orcid.core.version.OrcidMessageVersionConverter;
+import org.orcid.jaxb.model.message.Affiliation;
+import org.orcid.jaxb.model.message.AffiliationAddress;
+import org.orcid.jaxb.model.message.Affiliations;
+import org.orcid.jaxb.model.message.Iso3166Country;
 import org.orcid.jaxb.model.message.OrcidActivities;
 import org.orcid.jaxb.model.message.OrcidMessage;
 import org.orcid.jaxb.model.message.OrcidProfile;
@@ -35,7 +41,7 @@ public class OrcidMessageVersionConverterImplV1_0_21ToV1_0_22 implements OrcidMe
 
     private static final String FROM_VERSION = "1.0.21";
     private static final String TO_VERSION = "1.0.22";
-    
+
     private static final String EMPTY_TITLE = "NOT_DEFINED";
 
     @Override
@@ -59,29 +65,53 @@ public class OrcidMessageVersionConverterImplV1_0_21ToV1_0_22 implements OrcidMe
         if (orcidProfile != null) {
             OrcidActivities orcidActivities = orcidProfile.getOrcidActivities();
             if (orcidActivities != null) {
-                OrcidWorks orcidWorks = orcidActivities.getOrcidWorks();
-                if (orcidWorks != null) {
-                    for (OrcidWork orcidWork : orcidWorks.getOrcidWork()) {
-                        // Remove the translated title
-                        if (orcidWork.getWorkTitle() != null) {
-                            orcidWork.getWorkTitle().setTranslatedTitle(null);
-                        }
-
-                        // Remove the language code
-                        orcidWork.setLanguageCode(null);
-
-                        // Remove the location
-                        orcidWork.setCountry(null);
-                        
-                        // If title is NOT_DEFINED, change it to an empty string
-                        if(OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.EMPTY_TITLE.equals(orcidWork.getWorkTitle().getTitle()))
-                            orcidWork.getWorkTitle().getTitle().setContent("");
-                    }
-                }
+                downgradeAffiliations(orcidActivities);
+                downgradeWorks(orcidActivities);
             }
         }
 
         return orcidMessage;
+    }
+
+    private void downgradeAffiliations(OrcidActivities orcidActivities) {
+        Affiliations affiliations = orcidActivities.getAffiliations();
+        if (affiliations != null) {
+            for (Iterator<Affiliation> affiliationIterator = affiliations.getAffiliation().iterator(); affiliationIterator.hasNext();) {
+                Affiliation affiliation = affiliationIterator.next();
+                AffiliationAddress address = affiliation.getAffiliationAddress();
+                if (address != null) {
+                    Iso3166Country country = address.getAffiliationCountry().getValue();
+                    if (Iso3166Country.XK.equals(country)) {
+                        // The country code is not valid in the earlier version
+                        // of the schema, so unfortunately we have to
+                        // omit the affiliation
+                        affiliationIterator.remove();
+                    }
+                }
+            }
+        }
+    }
+
+    private void downgradeWorks(OrcidActivities orcidActivities) {
+        OrcidWorks orcidWorks = orcidActivities.getOrcidWorks();
+        if (orcidWorks != null) {
+            for (OrcidWork orcidWork : orcidWorks.getOrcidWork()) {
+                // Remove the translated title
+                if (orcidWork.getWorkTitle() != null) {
+                    orcidWork.getWorkTitle().setTranslatedTitle(null);
+                }
+
+                // Remove the language code
+                orcidWork.setLanguageCode(null);
+
+                // Remove the location
+                orcidWork.setCountry(null);
+
+                // If title is NOT_DEFINED, change it to an empty string
+                if (OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.EMPTY_TITLE.equals(orcidWork.getWorkTitle().getTitle()))
+                    orcidWork.getWorkTitle().getTitle().setContent("");
+            }
+        }
     }
 
     @Override
@@ -90,8 +120,9 @@ public class OrcidMessageVersionConverterImplV1_0_21ToV1_0_22 implements OrcidMe
             return null;
         }
         orcidMessage.setMessageVersion(TO_VERSION);
-        
-        // Title cannot be empty, so, for backwards compatibility, set the string
+
+        // Title cannot be empty, so, for backwards compatibility, set the
+        // string
         // NOT_DEFINED to empty titles
         OrcidProfile orcidProfile = orcidMessage.getOrcidProfile();
         if (orcidProfile != null) {
@@ -101,15 +132,15 @@ public class OrcidMessageVersionConverterImplV1_0_21ToV1_0_22 implements OrcidMe
                 if (orcidWorks != null) {
                     for (OrcidWork orcidWork : orcidWorks.getOrcidWork()) {
                         WorkTitle workTitle = orcidWork.getWorkTitle();
-                        if(workTitle == null){
+                        if (workTitle == null) {
                             workTitle = new WorkTitle();
                             Title title = new Title(OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.EMPTY_TITLE);
                             workTitle.setTitle(title);
                             orcidWork.setWorkTitle(workTitle);
-                        } else if(workTitle.getTitle() == null ){
+                        } else if (workTitle.getTitle() == null) {
                             Title title = new Title(OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.EMPTY_TITLE);
                             workTitle.setTitle(title);
-                        } else if(StringUtils.isEmpty(workTitle.getTitle().getContent())){
+                        } else if (StringUtils.isEmpty(workTitle.getTitle().getContent())) {
                             workTitle.getTitle().setContent(OrcidMessageVersionConverterImplV1_0_21ToV1_0_22.EMPTY_TITLE);
                         }
                     }
