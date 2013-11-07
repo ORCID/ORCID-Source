547844cb53e988ca99844649a01a1e32b8e7cff1
rcpeters <info@rcpeters.com>
2013-11-07 10:54:52 -0800

    change timeout for popup checker

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -1698,7 +1698,7 @@ function WorkCtrl($scope, $compile, worksSrvc) {
 		        //Second Checking Condition Works For Chrome
 		        window.location.href = url;
 		    } 
-		}, 25);
+		}, 250);
 		$.colorbox.close();
 		
 	};
