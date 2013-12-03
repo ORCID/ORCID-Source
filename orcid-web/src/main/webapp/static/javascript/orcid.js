b8eac561ba1dafed29a7475bf09e906e969c55c9
hexplus <jframonge@gmail.com>
2013-12-03 14:41:20 -0600

    Removing console output

--- orcid-web/src/main/webapp/static/javascript/orcid.js
+++ orcid-web/src/main/webapp/static/javascript/orcid.js
@@ -148,8 +148,7 @@
         
             $(window).bind('resize', function() {
                 if(navigator.appVersion.indexOf("MSIE 7.") == -1){ //Not IE7
-                    ww = document.body.clientWidth;
-                    console.log(ww);
+                    ww = document.body.clientWidth;                    
                     if (ww > 750){ //Tablet ~ PC                
                         $(".container .header .search form input[type='search']").blur(); /* To let "else" statement work properly*/
                         restoreDesktopUI();
