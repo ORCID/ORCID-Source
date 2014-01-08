aa3d8ccaf7e13c03637a9296df25fe6ac635e13d
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-01-08 14:47:50 -0600

    Finding resources to load

--- orcid-core/src/main/java/org/orcid/core/cli/LoadFundRefData.java
+++ orcid-core/src/main/java/org/orcid/core/cli/LoadFundRefData.java
@@ -16,21 +16,26 @@
  */
 package org.orcid.core.cli;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.orcid.core.manager.OrgManager;
 import org.orcid.persistence.dao.OrgDisambiguatedDao;
-import org.orcid.utils.NullUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.util.FileManager;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * 
@@ -39,7 +44,7 @@ import com.hp.hpl.jena.util.FileManager;
  */
 public class LoadFundRefData {
 	@Option(name = "-f", usage = "Path to RDF file containing FundRef info to load into DB")
-    private String fileToLoad;
+    private File fileToLoad;
 	private OrgDisambiguatedDao orgDisambiguatedDao;
     private OrgManager orgManager;
 	
@@ -54,11 +59,12 @@ public class LoadFundRefData {
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
             parser.printUsage(System.err);
-        }
+        }     
+                
     }
 
     private void validateArgs(CmdLineParser parser) throws CmdLineException {
-        if (NullUtils.allNull(fileToLoad)) {
+        if (fileToLoad == null) {
             throw new CmdLineException(parser, "-f parameter must be specificed");
         }
     }
@@ -70,17 +76,53 @@ public class LoadFundRefData {
     }
     
     private void execute() {
-    	Model model = ModelFactory.createDefaultModel();
-    	InputStream in = FileManager.get().open(fileToLoad);
-        if (in == null) {
-            throw new IllegalArgumentException( "File: " + fileToLoad + " not found");
-        }
-        
-        // read the RDF/XML file
-        model.read(in, "");
-        
-        model.write(System.out);
+    	try {
+			FileInputStream file = new FileInputStream(fileToLoad);			
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();			
+			DocumentBuilder builder =  builderFactory.newDocumentBuilder();			
+			Document xmlDocument = builder.parse(file);		
+			XPath xPath =  XPathFactory.newInstance().newXPath();
+			// Get the list of resources
+			String expression = "/RDF/ConceptScheme/hasTopConcept";
+			NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				NamedNodeMap attrs = nodeList.item(i).getAttributes();
+				Node node = attrs.getNamedItem("rdf:resource");
+				System.out.println(node.toString() + " -> " + node.getNodeValue());
+			}
+    	} catch(Exception e) {
+    		
+    	}
         
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
