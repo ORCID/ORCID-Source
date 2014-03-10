1104d7349a659f83ee83f3ef91402c566674baf8
rcpeters <info@rcpeters.com>
2014-03-10 11:56:30 -0700

    turn on ko for java

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -3260,10 +3260,10 @@ function languageCtrl($scope, $cookies){
 	        	"value": 'fr',
 	    		"label": 'Français'
 	        },	        
-//	        {
-//	        	"value": 'ko',
-//	    		"label": '한국어'
-//	        },
+	        {
+	        	"value": 'ko',
+	    		"label": '한국어'
+	        },
 	        {
 		        "value": 'zh_CN',
 			    "label": '简体中文'
