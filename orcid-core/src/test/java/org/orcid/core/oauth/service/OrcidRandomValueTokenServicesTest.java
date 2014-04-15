98cfc69a7b123c4f7bc4a24091af16c2e755ebb0
Will Simpson <will@wjrs.co.uk>
2014-04-15 11:24:07 +0100

    Fixed so that expiration is updated when a token is reissued.

--- orcid-core/src/test/java/org/orcid/core/oauth/service/OrcidRandomValueTokenServicesTest.java
+++ orcid-core/src/test/java/org/orcid/core/oauth/service/OrcidRandomValueTokenServicesTest.java
@@ -16,6 +16,7 @@
  */
 package org.orcid.core.oauth.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
@@ -138,6 +139,60 @@ public class OrcidRandomValueTokenServicesTest extends DBUnitTest {
         Date refreshTokenExpiration = tokenDetail.getRefreshTokenExpiration();
         assertFalse(refreshTokenExpiration.before(earliestRefreshExpiry));
         assertFalse(refreshTokenExpiration.after(latestRefreshExpiry));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testReissuedAccessTokenHasUpdatedExpiration() throws InterruptedException {
+        Date earliestExpiry = oneHoursTime();
+        Date earliestRefreshExpiry = earliestExpiry;
+
+        Map<String, String> authorizationParameters = new HashMap<>();
+        String clientId = "4444-4444-4444-4441";
+        authorizationParameters.put(AuthorizationRequest.CLIENT_ID, clientId);
+        authorizationParameters.put(AuthorizationRequest.SCOPE, "/orcid-works/create");
+        AuthorizationRequest request = new DefaultAuthorizationRequest(authorizationParameters);
+        ClientDetailsEntity clientDetails = clientDetailsDao.find(clientId);
+        Authentication userAuthentication = new OrcidOauth2ClientAuthentication(clientDetails);
+        OAuth2Authentication authentication = new OAuth2Authentication(request, userAuthentication);
+        OAuth2AccessToken oauth2AccessToken = tokenServices.createAccessToken(authentication);
+
+        Date latestExpiry = oneHoursTime();
+        Date latestRefreshExpiry = latestExpiry;
+
+        assertNotNull(oauth2AccessToken);
+        assertFalse(oauth2AccessToken.getExpiration().before(earliestExpiry));
+        assertFalse(oauth2AccessToken.getExpiration().after(latestExpiry));
+
+        OrcidOauth2TokenDetail tokenDetail = orcidOauthTokenDetailService.findByRefreshTokenValue(oauth2AccessToken.getRefreshToken().getValue());
+        assertNotNull(tokenDetail);
+        Date refreshTokenExpiration = tokenDetail.getRefreshTokenExpiration();
+        assertFalse(refreshTokenExpiration.before(earliestRefreshExpiry));
+        assertFalse(refreshTokenExpiration.after(latestRefreshExpiry));
+
+        Thread.sleep(1000);
+        earliestExpiry = oneHoursTime();
+        earliestRefreshExpiry = earliestExpiry;
+
+        OAuth2AccessToken reissuedOauth2AccessToken = tokenServices.createAccessToken(authentication);
+
+        latestExpiry = oneHoursTime();
+        latestRefreshExpiry = latestExpiry;
+
+        assertNotNull(reissuedOauth2AccessToken);
+        // Check is the same token - should be as token has not be used, and has
+        // not expired.
+        assertEquals(oauth2AccessToken.getValue(), reissuedOauth2AccessToken.getValue());
+        assertFalse(reissuedOauth2AccessToken.getExpiration().before(earliestExpiry));
+        assertFalse(reissuedOauth2AccessToken.getExpiration().after(latestExpiry));
+
+        OrcidOauth2TokenDetail reissuedTokenDetail = orcidOauthTokenDetailService.findByRefreshTokenValue(reissuedOauth2AccessToken.getRefreshToken().getValue());
+        assertNotNull(reissuedTokenDetail);
+        Date reissuedRefreshTokenExpiration = reissuedTokenDetail.getRefreshTokenExpiration();
+        assertFalse(reissuedRefreshTokenExpiration.before(earliestRefreshExpiry));
+        assertFalse(reissuedRefreshTokenExpiration.after(latestRefreshExpiry));
+
     }
 
     private Date twentyYearsTime() {
