e0159d5de26bc8be075e8b2640ddb67eb08a445a
Will Simpson <will@wjrs.co.uk>
2014-04-29 11:57:14 +0100

    Fixed problem where works PUT does not update works if there are no existing activities.

--- orcid-core/src/main/java/org/orcid/core/manager/impl/OrcidProfileManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/OrcidProfileManagerImpl.java
@@ -780,6 +780,24 @@ public class OrcidProfileManagerImpl implements OrcidProfileManager {
         if (existingProfile == null) {
             return null;
         }
+        OrcidActivities updatedActivities = updatedOrcidProfile.getOrcidActivities();
+        if (updatedActivities == null) {
+            return null;
+        }
+        OrcidWorks updatedOrcidWorks = updatedActivities.getOrcidWorks();
+        if (updatedOrcidWorks == null) {
+            return null;
+        }
+        OrcidActivities existingActivities = existingProfile.getOrcidActivities();
+        if (existingActivities == null) {
+            existingActivities = new OrcidActivities();
+            existingProfile.setOrcidActivities(existingActivities);
+        }
+        OrcidWorks existingOrcidWorks = existingActivities.getOrcidWorks();
+        if (existingOrcidWorks == null) {
+            existingOrcidWorks = new OrcidWorks();
+            existingActivities.setOrcidWorks(existingOrcidWorks);
+        }
         orcidJaxbCopyManager.copyUpdatedWorksPreservingVisbility(existingProfile.retrieveOrcidWorks(), updatedOrcidProfile.retrieveOrcidWorks());
         return updateOrcidProfile(existingProfile);
     }
@@ -978,14 +996,13 @@ public class OrcidProfileManagerImpl implements OrcidProfileManager {
         }
     }
 
-    @Override 
+    @Override
     @Transactional
     public void updateCountry(OrcidProfile orcidProfile) {
-        profileDao.updateCountry(orcidProfile.getOrcidIdentifier().getPath()
-                , orcidProfile.getOrcidBio().getContactDetails().getAddress().getCountry().getValue()
-                , orcidProfile.getOrcidBio().getContactDetails().getAddress().getCountry().getVisibility());        
+        profileDao.updateCountry(orcidProfile.getOrcidIdentifier().getPath(), orcidProfile.getOrcidBio().getContactDetails().getAddress().getCountry().getValue(),
+                orcidProfile.getOrcidBio().getContactDetails().getAddress().getCountry().getVisibility());
     }
-    
+
     @Override
     @Transactional
     public void updatePreferences(String orcid, Preferences preferences) {
