611b1e797a202dc34d2fd717151dce52c4b9e26c
Will Simpson <will@wjrs.co.uk>
2014-05-14 12:08:37 +0100

    Airplane mode.

--- orcid-web/src/main/java/org/orcid/frontend/web/forms/validate/OrcidDomainValidator.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/forms/validate/OrcidDomainValidator.java
@@ -19,6 +19,10 @@ package org.orcid.frontend.web.forms.validate;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.apache.commons.validator.routines.DomainValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 
  * @author Will Simpson
@@ -26,13 +30,32 @@ import java.net.UnknownHostException;
  */
 public class OrcidDomainValidator {
 
+    private DomainValidator standardDomainValidator = DomainValidator.getInstance();
+
+    private static final String DNS_TEST_DOMAIN = "google.com";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrcidDomainValidator.class);
+
     public boolean isValid(String hostLocation) {
+        if (isDnsOk()) {
+            return isValidInDns(hostLocation);
+        } else {
+            LOGGER.warn("DNS is not OK, so validating in offline mode");
+            return standardDomainValidator.isValid(hostLocation);
+        }
+    }
+
+    private boolean isValidInDns(String hostLocation) {
         try {
             InetAddress.getByName(hostLocation);
             return true;
         } catch (UnknownHostException e) {
             return false;
         }
+    }
+
+    private boolean isDnsOk() {
+        return isValidInDns(DNS_TEST_DOMAIN);
     }
 
     public static void main(String[] args) {
