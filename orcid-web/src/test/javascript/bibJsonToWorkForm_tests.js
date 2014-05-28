ec40e438de11590a4542bdee96ff2d5022697bfe
rcpeters <info@rcpeters.com>
2014-05-28 16:00:35 -0700

    Just initing a test and learning karma

--- orcid-web/src/test/javascript/bibJsonToWorkForm_tests.js
+++ orcid-web/src/test/javascript/bibJsonToWorkForm_tests.js
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-describe("Test that functoin exist", function() {
+describe("Test that function exist", function() {
 	// simple test to get things started
 	it("test method exist", function() {
 		expect(typeof(populateWorkAjaxForm) == "function").toBe(true);
