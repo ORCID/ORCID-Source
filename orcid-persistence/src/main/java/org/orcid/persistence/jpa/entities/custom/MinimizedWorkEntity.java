ff614505f26b7c11fe21785345f5468286d1ebbd
rcpeters <info@rcpeters.com>
2014-06-03 16:46:03 -0700

    add external identifiers to minimized works

--- orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/custom/MinimizedWorkEntity.java
+++ orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/custom/MinimizedWorkEntity.java
@@ -29,98 +29,106 @@ import org.orcid.jaxb.model.message.Visibility;
 public class MinimizedWorkEntity implements Serializable {
     private static final long serialVersionUID = -6961089690820823167L;
 
-        private Long id;
-	private String title;
-	private String subtitle;
-	private String description;
-	private int publicationDay;
-	private int publicationMonth;
-	private int publicationYear;
-	private Visibility visibility;
+    private Long id;
+    private String title;
+    private String subtitle;
+    private String description;
+    private int publicationDay;
+    private int publicationMonth;
+    private int publicationYear;
+    private Visibility visibility;
+    private String externalIdentifiersJson;
 
-	public MinimizedWorkEntity() {
-		super();
-	}
+    public MinimizedWorkEntity() {
+        super();
+    }
 
-	public MinimizedWorkEntity(Long id, String title, String subtitle,
-			String description, Integer publicationDay,
-			Integer publicationMonth, Integer publicationYear,
-			Visibility visibility) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.subtitle = subtitle;
-		this.description = description;
-		if (publicationDay != null)
-			this.publicationDay = publicationDay;
-		if (publicationMonth != null)
-			this.publicationMonth = publicationMonth;
-		if (publicationYear != null)
-			this.publicationYear = publicationYear;
-		this.visibility = visibility;
-	}
+    public MinimizedWorkEntity(Long id, String title, String subtitle, String description, Integer publicationDay, Integer publicationMonth, Integer publicationYear,
+            Visibility visibility, String externalIdentifiersJson) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+        if (publicationDay != null)
+            this.publicationDay = publicationDay;
+        if (publicationMonth != null)
+            this.publicationMonth = publicationMonth;
+        if (publicationYear != null)
+            this.publicationYear = publicationYear;
+        this.visibility = visibility;
+        this.externalIdentifiersJson = externalIdentifiersJson;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getSubtitle() {
-		return subtitle;
-	}
+    public String getSubtitle() {
+        return subtitle;
+    }
 
-	public void setSubtitle(String subtitle) {
-		this.subtitle = subtitle;
-	}
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public int getPublicationDay() {
-		return publicationDay;
-	}
+    public int getPublicationDay() {
+        return publicationDay;
+    }
 
-	public void setPublicationDay(int publicationDay) {
-		this.publicationDay = publicationDay;
-	}
+    public void setPublicationDay(int publicationDay) {
+        this.publicationDay = publicationDay;
+    }
 
-	public int getPublicationMonth() {
-		return publicationMonth;
-	}
+    public int getPublicationMonth() {
+        return publicationMonth;
+    }
 
-	public void setPublicationMonth(int publicationMonth) {
-		this.publicationMonth = publicationMonth;
-	}
+    public void setPublicationMonth(int publicationMonth) {
+        this.publicationMonth = publicationMonth;
+    }
 
-	public int getPublicationYear() {
-		return publicationYear;
-	}
+    public int getPublicationYear() {
+        return publicationYear;
+    }
 
-	public void setPublicationYear(int publicationYear) {
-		this.publicationYear = publicationYear;
-	}
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
 
-	public Visibility getVisibility() {
-		return visibility;
-	}
+    public Visibility getVisibility() {
+        return visibility;
+    }
 
-	public void setVisibility(Visibility visibility) {
-		this.visibility = visibility;
-	}
+    public void setVisibility(Visibility visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getExternalIdentifiersJson() {
+        return externalIdentifiersJson;
+    }
+
+    public void setExternalIdentifiersJson(String externalIdentifiersJson) {
+        this.externalIdentifiersJson = externalIdentifiersJson;
+    }
 }
