d7aeb385906392fbd05ce65ba44381224582d773
Will Simpson <will@wjrs.co.uk>
2014-06-27 18:13:39 +0100

    Initial JPA<->JAXB mappings for notifications (using Orika).

---
+++ orcid-core/src/main/java/org/orcid/core/adapter/impl/JpaJaxbNotificationAdapterImpl.java
@@ -0,0 +1,54 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2013 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.core.adapter.impl;
+
+import ma.glasnost.orika.MapperFacade;
+
+import org.orcid.core.adapter.JpaJaxbNotificationAdapter;
+import org.orcid.jaxb.model.notification.Notification;
+import org.orcid.persistence.jpa.entities.NotificationEntity;
+
+/**
+ * 
+ * @author Will Simpson
+ * 
+ */
+public class JpaJaxbNotificationAdapterImpl implements JpaJaxbNotificationAdapter {
+
+    private MapperFacade mapperFacade;
+
+    public void setMapperFacade(MapperFacade mapperFacade) {
+        this.mapperFacade = mapperFacade;
+    }
+
+    @Override
+    public NotificationEntity toNotificationEntity(Notification notification) {
+        if (notification == null) {
+            return null;
+        }
+        return mapperFacade.map(notification, NotificationEntity.class);
+    }
+
+    @Override
+    public Notification toNotification(NotificationEntity notificationEntity) {
+        if (notificationEntity == null) {
+            return null;
+        }
+        return mapperFacade.map(notificationEntity, Notification.class);
+    }
+
+}
---
+++ orcid-core/src/test/java/org/orcid/core/adapter/JpaJaxbNotificationAdapterTest.java
@@ -0,0 +1,73 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2013 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.core.adapter;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.orcid.jaxb.model.notification.Notification;
+import org.orcid.jaxb.model.notification.NotificationType;
+import org.orcid.persistence.jpa.entities.NotificationEntity;
+import org.orcid.utils.DateUtils;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 
+ * @author Will Simpson
+ * 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:orcid-core-context.xml" })
+public class JpaJaxbNotificationAdapterTest {
+
+    @Resource
+    private JpaJaxbNotificationAdapter jpaJaxbNotificationAdapter;
+
+    @Test
+    public void testToNotificationEntity() {
+        Notification notification = new Notification();
+        notification.setNotificationType(NotificationType.RECORD_UPDATED_BY_MEMBER);
+        notification.setSubject("Test subject");
+
+        NotificationEntity notificationEntity = jpaJaxbNotificationAdapter.toNotificationEntity(notification);
+
+        assertNotNull(notificationEntity);
+        assertEquals(NotificationType.RECORD_UPDATED_BY_MEMBER, notificationEntity.getNotificationType());
+        assertEquals("Test subject", notification.getSubject());
+    }
+
+    @Test
+    public void testToNotification() {
+        NotificationEntity notificationEntity = new NotificationEntity();
+        notificationEntity.setId(123L);
+        notificationEntity.setNotificationType(NotificationType.RECORD_UPDATED_BY_MEMBER);
+        notificationEntity.setSubject("Test subject");
+        notificationEntity.setReadDate(DateUtils.convertToDate("2014-03-04T17:43:06"));
+
+        Notification notification = jpaJaxbNotificationAdapter.toNotification(notificationEntity);
+
+        assertNotNull(notification);
+        assertEquals(NotificationType.RECORD_UPDATED_BY_MEMBER, notification.getNotificationType());
+        assertEquals("Test subject", notification.getSubject());
+        assertEquals("2014-03-04T17:43:06.000Z", notification.getReadDate().toXMLFormat());
+    }
+
+}
