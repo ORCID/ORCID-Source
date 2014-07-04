5e4b342b4012adee914723274b1411db56f9108d
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-07-03 18:18:20 -0600

    Adding integration test, need to work on it

--- orcid-api-web/src/test/java/org/orcid/api/t2/integration/T2OrcidOAuthApiClientReadPrivateDataIntegrationTest.java
+++ orcid-api-web/src/test/java/org/orcid/api/t2/integration/T2OrcidOAuthApiClientReadPrivateDataIntegrationTest.java
@@ -1,180 +1,175 @@
 package org.orcid.api.t2.integration;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.annotation.Resource;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.orcid.core.manager.OrcidClientGroupManager;
-import org.orcid.jaxb.model.clientgroup.OrcidClient;
-import org.orcid.jaxb.model.clientgroup.OrcidClientGroup;
-import org.orcid.jaxb.model.message.Affiliation;
-import org.orcid.jaxb.model.message.AffiliationType;
-import org.orcid.jaxb.model.message.Affiliations;
-import org.orcid.jaxb.model.message.Funding;
-import org.orcid.jaxb.model.message.FundingList;
-import org.orcid.jaxb.model.message.FundingTitle;
-import org.orcid.jaxb.model.message.FundingType;
-import org.orcid.jaxb.model.message.Iso3166Country;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.orcid.api.t2.T2OAuthAPIService;
 import org.orcid.jaxb.model.message.OrcidMessage;
 import org.orcid.jaxb.model.message.OrcidWork;
-import org.orcid.jaxb.model.message.OrcidWorks;
-import org.orcid.jaxb.model.message.Organization;
-import org.orcid.jaxb.model.message.OrganizationAddress;
-import org.orcid.jaxb.model.message.ScopePathType;
-import org.orcid.jaxb.model.message.Title;
-import org.orcid.jaxb.model.message.WorkType;
-import org.orcid.pojo.ajaxForm.PojoUtil;
+import org.orcid.persistence.dao.ProfileDao;
+import org.orcid.persistence.jpa.entities.ProfileEntity;
+import org.orcid.test.DBUnitTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
+/**
+ * 
+ * @author Angel Montenegro
+ * 
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
-public class T2OrcidOAuthApiClientReadPrivateDataIntegrationTest extends BaseT2OrcidOAuthApiClientIntegrationTest {
+@ContextConfiguration(locations = { "classpath:test-oauth-orcid-api-client-context.xml" })
+public class T2OrcidOAuthApiClientReadPrivateDataIntegrationTest extends DBUnitTest {
+    private static final int DEFAULT_TIMEOUT_SECONDS = 30;
+    
+    private static final Pattern AUTHORIZATION_CODE_PATTERN = Pattern.compile("code=(.+)");
+    
+    private static final String READ_PRIVATE_WORKS_CLIENT_ID = "9999-9999-9999-9991";
+    private static final String READ_PRIVATE_AFFILIATIONS_CLIENT_ID = "9999-9999-9999-9992";
+    private static final String READ_PRIVATE_FUNDING_CLIENT_ID = "9999-9999-9999-9993";
+    private static final String READ_ONLY_LIMITED_INFO_CLIENT_ID = "9999-9999-9999-9994";
+    private static final String REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
+    private static final List<String> DATA_FILES = Arrays.asList("/group_client_data/EmptyEntityData.xml", "/group_client_data/ProfileEntityData.xml", "/group_client_data/WorksEntityData.xml",
+            "/group_client_data/OrgsEntityData.xml", "/group_client_data/ClientDetailsEntityData.xml", "/group_client_data/ProfileWorksEntityData.xml", "/group_client_data/OrgAffiliationEntityData.xml", "/group_client_data/ProfileFundingEntityData.xml");
+
+    private WebDriver webDriver;
+    
+    @Value("${org.orcid.web.base.url:http://localhost:8080/orcid-web}")
+    private String webBaseUrl;
+    
     @Resource
-    private OrcidClientGroupManager orcidClientGroupManager;
-        
-    private String redirectUriString = "https://developers.google.com/oauthplayground";
+    private ProfileDao profileDao;
     
-    OrcidClientGroup group = null;
-    OrcidClient readPrivateWorksClient = null;
-    OrcidClient readPrivateFundingClient = null;
-    OrcidClient readPrivateAffiliationsClient = null;
-    OrcidClient cantReadPrivateDataClient = null;
-    String userOrcid = null;
+    @Resource(name = "t2OAuthClient")
+    private T2OAuthAPIService<ClientResponse> oauthT2Client;
     
-    String client1token = null;
-    String client2token = null;
-    String client3token = null;
-    String client4token = null;
-    
-    /**
-     * TODO Create integration tests that test the following:
-     * 1. Can read his private works, but not other private works
-     * 2. Can read his private affiliations, but not other private affiliations
-     * 3. Can read his private funding, but not other pirvate funding
-     * */
-    
-    
-    
-    @Override
-    @Before
-    public void createClientCredentialsAndAccessToken() {
-        group = orcidClientDataHelper.createAndPersistGroupWithMultipleClients();
-        
-        assertNotNull(group);
-        assertNotNull(group.getOrcidClient());
-        assertEquals(4, group.getOrcidClient().size());
-        
-        readPrivateWorksClient = group.getOrcidClient().get(0);
-        readPrivateFundingClient = group.getOrcidClient().get(1); 
-        readPrivateAffiliationsClient = group.getOrcidClient().get(2); 
-        cantReadPrivateDataClient = group.getOrcidClient().get(3);
-        
-        try{
-            //Create the user profile
-            clientId = cantReadPrivateDataClient.getClientId();
-            clientSecret = cantReadPrivateDataClient.getClientSecret();
-            client4token = createAccessTokenFromCredentials(ScopePathType.ORCID_PROFILE_CREATE.value() + ' ' + ScopePathType.ORCID_PROFILE_READ_LIMITED.value());
-            OrcidMessage profile = orcidClientDataHelper.createFromXML(OrcidClientDataHelper.USER_TO_TEST_PRIVATE_DATA_VISIBILITY);
-            ClientResponse clientResponse = oauthT2Client.createProfileXML(profile, client4token);
-            // assign orcid any time it's created for use in tear-down
-            userOrcid = orcidClientDataHelper.extractOrcidFromResponseCreated(clientResponse);            
-            assertNotNull(this.accessToken);
-            assertFalse(PojoUtil.isEmpty(userOrcid));
-
-            //Organization used by funding and affiliation
-            Organization organization = new Organization();
-            organization.setName("Org Name");
-            OrganizationAddress address = new OrganizationAddress();
-            address.setCity("Some city");
-            address.setCountry(Iso3166Country.CR);
-            address.setRegion("Some region");
-            organization.setAddress(address);
-            
-            //Add works to the new profile
-            clientId = readPrivateWorksClient.getClientId();
-            clientSecret = readPrivateWorksClient.getClientSecret();
-            client1token = createAccessTokenFromCredentials(ScopePathType.ORCID_PROFILE_READ_LIMITED.value() + ' ' + ScopePathType.ORCID_WORKS_CREATE.value() + ' ' + ScopePathType.ORCID_WORKS_UPDATE.value() + ' ' + ScopePathType.ORCID_WORKS_READ_LIMITED.value());
-            
-            OrcidWorks orcidWorks = new OrcidWorks();
-            OrcidWork orcidWork = orcidClientDataHelper.createWork("Private work for client1");
-            orcidWork.setWorkType(WorkType.ARTISTIC_PERFORMANCE);
-            orcidWorks.getOrcidWork().add(orcidWork);
-            profile.getOrcidProfile().setOrcidWorks(orcidWorks);
-            
-            clientResponse = oauthT2Client.addWorksXml(userOrcid, profile, client1token);
-            assertEquals(201, clientResponse.getStatus());
-            
-            //Add funding to the new profile
-            clientId = readPrivateFundingClient.getClientId();
-            clientSecret = readPrivateFundingClient.getClientSecret();
-            client2token = createAccessTokenFromCredentials(ScopePathType.ORCID_PROFILE_READ_LIMITED.value() + ' ' + ScopePathType.FUNDING_CREATE.value() + ' ' + ScopePathType.FUNDING_UPDATE.value() + ' ' + ScopePathType.FUNDING_READ_LIMITED.value());
-            
-            FundingList fundingList = new FundingList();
-            Funding funding = new Funding();
-            funding.setType(FundingType.AWARD);
-            FundingTitle title = new FundingTitle();
-            title.setTitle(new Title("Private funding for client2"));
-            funding.setTitle(title);            
-            funding.setOrganization(organization);
-            fundingList.getFundings().add(funding);
-            profile.getOrcidProfile().setFundings(fundingList);
-            
-            clientResponse = oauthT2Client.addFundingXml(userOrcid, profile, client2token);
-            assertEquals(201, clientResponse.getStatus());
-            
-            //Add affiliations to the new profile
-            clientId = readPrivateAffiliationsClient.getClientId();
-            clientSecret = readPrivateAffiliationsClient.getClientSecret();
-            client3token = createAccessTokenFromCredentials(ScopePathType.ORCID_PROFILE_READ_LIMITED.value() + ' ' + ScopePathType.AFFILIATIONS_CREATE.value() + ' ' + ScopePathType.AFFILIATIONS_UPDATE.value() + ' ' + ScopePathType.AFFILIATIONS_READ_LIMITED.value());
-            
-            Affiliations affiliations = new Affiliations();
-            Affiliation affiliation = new Affiliation();
-            affiliation.setType(AffiliationType.EDUCATION);
-            affiliation.setDepartmentName("Private affiliation for client3");
-            affiliation.setOrganization(organization);
-            affiliations.getAffiliation().add(affiliation);            
-            profile.getOrcidProfile().setAffiliations(affiliations);
-            
-            clientResponse = oauthT2Client.addAffiliationsXml(userOrcid, profile, client3token);
-            assertEquals(201, clientResponse.getStatus());
-            
-            
-        } catch(Exception e) {
-            fail();
-        }
+    @BeforeClass
+    public static void initDBUnitData() throws Exception {
+        initDBUnitData(DATA_FILES, null);
     }
     
-    @After
-    public void clearOrcid() throws Exception {
-        // remove any client data if it exists
-        orcidClientDataHelper.deleteClientId(readPrivateWorksClient.getClientId());
-        orcidClientDataHelper.deleteClientId(readPrivateFundingClient.getClientId());
-        orcidClientDataHelper.deleteClientId(readPrivateAffiliationsClient.getClientId());
-        orcidClientDataHelper.deleteClientId(cantReadPrivateDataClient.getClientId());
-        orcidClientDataHelper.deleteOrcidProfile(group.getGroupOrcid());
+    @Before
+    @Transactional
+    public void before() {
+        webDriver = new FirefoxDriver();
         
-        readPrivateWorksClient = null;
-        readPrivateFundingClient = null;
-        readPrivateAffiliationsClient = null;
-        cantReadPrivateDataClient = null;
-        group = null;
+        webDriver.get(webBaseUrl + "/signout");
+        // Update last modified to force cache eviction (because DB unit deletes
+        // a load of stuff from the DB, but reinserts profiles with older last
+        // modified date)
+        for (ProfileEntity profile : profileDao.getAll()) {
+            profileDao.updateLastModifiedDateWithoutResult(profile.getId());
+        }
+    }
+
+    @After
+    public void after() {
+        webDriver.quit();
     }
     
     @Test
-    public void testViewPrivateWorks() {
-        ClientResponse clientResponse = oauthT2Client.viewBioDetailsXml(userOrcid, client4token);
-        assertNotNull(clientResponse);
-        assertEquals(200, clientResponse.getStatus());        
-        OrcidMessage orcidMessage = clientResponse.getEntity(OrcidMessage.class);
+    public void testGetProfileWithOnlyReadLimitedScope() throws JSONException, InterruptedException {
+        String scopes = "/orcid-profile/read-limited";
+        String authorizationCode = obtainAuthorizationCode(READ_ONLY_LIMITED_INFO_CLIENT_ID, scopes, REDIRECT_URI);
+        String accessToken = obtainAccessToken(READ_ONLY_LIMITED_INFO_CLIENT_ID, authorizationCode, REDIRECT_URI, scopes);
+
+        ClientResponse fullResponse1 = oauthT2Client.viewFullDetailsXml("9999-9999-9999-9989", accessToken);
+        assertEquals(200, fullResponse1.getStatus());
+        OrcidMessage orcidMessage = fullResponse1.getEntity(OrcidMessage.class);
         assertNotNull(orcidMessage);
+        assertNotNull(orcidMessage.getOrcidProfile());
+        assertNotNull(orcidMessage.getOrcidProfile().getOrcidActivities());
+        assertNotNull(orcidMessage.getOrcidProfile().getOrcidActivities().getOrcidWorks());
+        assertNotNull(orcidMessage.getOrcidProfile().getOrcidActivities().getOrcidWorks().getOrcidWork());
+        assertEquals(2, orcidMessage.getOrcidProfile().getOrcidActivities().getOrcidWorks().getOrcidWork().size());
+        
+        List<OrcidWork> works = orcidMessage.getOrcidProfile().getOrcidActivities().getOrcidWorks().getOrcidWork();
+        for(OrcidWork work : works) {
+            String putCode = work.getPutCode();
+            if(putCode.equals("1") || putCode.equals("4")){
+                fail("This client should not have access to work: " + putCode);
+            }
+        }                
     }
+    
+    private String obtainAuthorizationCode(String orcid, String scopes, String redirectUri) throws InterruptedException {
+        webDriver.get(String.format("%s/oauth/authorize?client_id=%s&response_type=code&scope=%s&redirect_uri=%s", webBaseUrl, orcid, scopes, redirectUri));
+        WebElement userId = webDriver.findElement(By.id("userId"));
+        userId.sendKeys("michael@bentine.com");
+        WebElement password = webDriver.findElement(By.id("password"));
+        password.sendKeys("password");
+        password.submit();
+        (new WebDriverWait(webDriver, DEFAULT_TIMEOUT_SECONDS)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                if (d.getCurrentUrl().contains("authorize")) {
+                    WebElement authorizeButton = d.findElement(By.name("authorize"));
+                    if (authorizeButton != null) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        WebElement authorizeButton = webDriver.findElement(By.name("authorize"));
+        Thread.sleep(3000);
+        authorizeButton.submit();
+        (new WebDriverWait(webDriver, DEFAULT_TIMEOUT_SECONDS)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().equals("ORCID Playground");
+            }
+        });
+        String currentUrl = webDriver.getCurrentUrl();
+        Matcher matcher = AUTHORIZATION_CODE_PATTERN.matcher(currentUrl);
+        assertTrue(matcher.find());
+        String authorizationCode = matcher.group(1);
+        assertNotNull(authorizationCode);
+        return authorizationCode;
+    }
+    
+    private String obtainAccessToken(String clientId, String authorizationCode, String redirectUri, String scopes) throws JSONException {
+        MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+        params.add("client_id", clientId);
+        params.add("client_secret", "client-secret");
+        params.add("grant_type", "authorization_code");
+        params.add("scope", scopes);
+        params.add("redirect_uri", redirectUri);
+        params.add("code", authorizationCode);
+        ClientResponse tokenResponse = oauthT2Client.obtainOauth2TokenPost("client_credentials", params);
+        assertEquals(200, tokenResponse.getStatus());
+        String body = tokenResponse.getEntity(String.class);
+        JSONObject jsonObject = new JSONObject(body);
+        String accessToken = (String) jsonObject.get("access_token");
+        assertNotNull(accessToken);
+        return accessToken;
+    }
+
 }
