2bf0c28e2a6e9097991e5cb7c1b8d3be5dea96cb
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-07-04 16:38:06 -0600

    Working on the hackaton project

---
+++ orcid-web/src/main/java/org/orcid/frontend/web/social/twitter/TwitterConnector.java
@@ -0,0 +1,41 @@
+package org.orcid.frontend.web.social.twitter;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class TwitterConnector {
+
+    static String consumerKeyStr = "soCTKWWByfjq91SxuaQRh4Gnk";
+    static String consumerSecretStr = "sjtMHV2myGQ6qZAoKROoKaNfvRFvyDtIuGn0cKdy5h0RQ55NPM";
+    // These two should be saved on database
+    static String accessTokenStr = "2604224184-JEW89PiNZrEjVCyHOvOuYjilPxU2criQbQtxMaA";
+    static String accessTokenSecretStr = "CaWVUpeQI8f2NMH5AKluOsPMzewlz1WMfSWtghz6t6BHa";
+
+    
+    public static void postToTwitter() {
+        try {
+            Twitter twitter = new TwitterFactory().getInstance();
+
+            twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
+            AccessToken accessToken = new AccessToken(accessTokenStr, accessTokenSecretStr);
+
+            twitter.setOAuthAccessToken(accessToken);
+
+            twitter.updateStatus("Post using Twitter4J Again " + System.currentTimeMillis());
+
+            System.out.println("Successfully updated the status in Twitter.");
+        } catch (TwitterException te) {
+            te.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 0; i < 100; i++) {
+            TwitterConnector.postToTwitter();
+            Thread.sleep(5000);
+        }        
+    }
+
+}
