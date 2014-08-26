cdd4336d7c3b5bbe787cac16794411bb7ccb04e8
rcpeters <info@rcpeters.com>
2014-08-26 14:22:16 -0700

    fix issue where loading work types overwrites specified bibtex type

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -3389,7 +3389,6 @@ function WorkCtrl($scope, $compile, $filter, worksSrvc, workspaceSrvc) {
 	$scope.moreInfo = {};
 	$scope.editSources = {};
 	$scope.bibtexParsingError = false;
-	$scope.edittingWork = false;
 	$scope.bibtexCancelLink = false;
 	$scope.bibtextWork = false;
 	$scope.bibtextWorkIndex = null;	
@@ -3439,7 +3438,7 @@ function WorkCtrl($scope, $compile, $filter, worksSrvc, workspaceSrvc) {
     $scope.addWorkFromBibtex = function(work) {    	
     	$scope.bibtextWorkIndex = $scope.worksFromBibtex.indexOf(work);
     	$scope.bibtextWork = true;    	
-    	$scope.addWorkModalFromBibTex($scope.worksFromBibtex[$scope.bibtextWorkIndex]);
+    	$scope.addWorkModal($scope.worksFromBibtex[$scope.bibtextWorkIndex]);
     };
    
     $scope.openBibTextWizard = function () {
@@ -3476,7 +3475,7 @@ function WorkCtrl($scope, $compile, $filter, worksSrvc, workspaceSrvc) {
 		$scope.editWork.contributors.splice(index,1);
 	};
 
-	$scope.showAddModal = function(){
+	$scope.showAddWorkModal = function(){
 		$scope.editTranslatedTitle = false;
 		$scope.types = null;
 	    $.colorbox({	    	
@@ -3507,30 +3506,22 @@ function WorkCtrl($scope, $compile, $filter, worksSrvc, workspaceSrvc) {
             onComplete: function() {$.colorbox.resize();}
         });
 	};
-	
-	$scope.addWorkModalFromBibTex = function(data){
-		$scope.loadWorkTypes();		 
-		$scope.edittingWork = false;
-		$scope.editWork = data;
-		$scope.showAddModal();		
-	};
-	
-	$scope.addWorkModal = function(data){		
 		
-		$scope.loadWorkTypes();
+	$scope.addWorkModal = function(data){		
 		if (data == undefined) { 
-			$scope.edittingWork = false;
 			worksSrvc.getBlankWork(function(data) {
 				$scope.editWork = data;
-				$scope.$apply(function() {					
-					$scope.showAddModal();
+				$scope.$apply(function() {
+					$scope.loadWorkTypes();
+					$scope.showAddWorkModal();
 				});			
 			});
 		} else {
-			$scope.edittingWork = true;
 			$scope.editWork = data;
-			$scope.showAddModal();
+            $scope.loadWorkTypes();
+			$scope.showAddWorkModal();
 		}
+		
 	};
 	
     $scope.openEditWork = function(putCode){
@@ -3553,7 +3544,8 @@ function WorkCtrl($scope, $compile, $filter, worksSrvc, workspaceSrvc) {
 	        		$scope.closeAllMoreInfo();
 	        		$.colorbox.close(); 
 	        		$scope.addingWork = false;
-	        		$scope.worksSrvc.loadAbbrWorks(worksSrvc.constants.access_type.USER);	        		
+	        		$scope.worksSrvc.loadAbbrWorks(worksSrvc.constants.access_type.USER);
+	        		
 	        		if($scope.bibtextWork == true){
 	        			$scope.worksFromBibtex.splice($scope.bibtextWorkIndex, 1);
 	        			$scope.bibtextWork = false;	        			
@@ -3607,22 +3599,21 @@ function WorkCtrl($scope, $compile, $filter, worksSrvc, workspaceSrvc) {
 		return info;
 	};
 			
-	$scope.loadWorkTypes = function(){			
+	$scope.loadWorkTypes = function(){	
 		var workCategory = "";
 		if($scope.editWork != null && $scope.editWork.workCategory != null && $scope.editWork.workCategory.value != null && $scope.editWork.workCategory.value != "")
 			workCategory = $scope.editWork.workCategory.value;
-					
 		$.ajax({
 	        url: getBaseUri() + '/works/loadWorkTypes.json?workCategory=' + workCategory,
 	        type: 'POST',	        
 	        contentType: 'application/json;charset=UTF-8',
 	        dataType: 'json',
 	        success: function(data) {
-	        	
 	        	$scope.$apply(function() {
-		        	$scope.types = data;
+		        	$scope.types = data;		
 		        	if($scope.editWork != null && $scope.editWork.workCategory != null) {
-		        		if(!$scope.edittingWork) {
+		        		if($scope.editWork.workType.value == null || $scope.editWork.workType.value == '' ) {
+		        			$scope.editWork.workType.value;
 		        			switch ($scope.editWork.workCategory.value){
 			                case "conference":
 			                	$scope.editWork.workType.value="conference-paper";		                	
