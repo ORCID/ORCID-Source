7e8d374b490a1ccb4241034a6436b26baf95e897
Fran Ramírez Monge <jframonge@gmail.com>
2014-09-24 18:05:10 -0600

    Fixing show Bibtex in HTML for the Public Profile

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -3571,7 +3571,7 @@ function PublicFundingCtrl($scope, $compile, $filter, fundingSrvc){
 function PublicWorkCtrl($scope, $compile, $filter, worksSrvc, actSortSrvc) {
 	actSortSrvc.initScope($scope, GroupedActivities.ABBR_WORK);
 	$scope.worksSrvc = worksSrvc;
-	$scope.showBibtex = false;
+	$scope.showBibtex = {};
 	$scope.moreInfoOpen = false;
 	$scope.moreInfo = {};
 	$scope.displayWorks = true;
@@ -3580,8 +3580,8 @@ function PublicWorkCtrl($scope, $compile, $filter, worksSrvc, actSortSrvc) {
 		actSortSrvc.sort(key,$scope);
 	};
 	
-    $scope.bibtexShowToggle = function () {
-    	$scope.showBibtex = !($scope.showBibtex);
+	$scope.bibtexShowToggle = function (putCode) {
+        $scope.showBibtex[putCode] = !($scope.showBibtex[putCode]);     
     };   
 	  
 	$scope.renderTranslatedTitleInfo = function(putCode) {		
