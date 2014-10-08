993a6df950b8c38306e4f9b4a5b08205c5f5b3b1
Will Simpson <will@wjrs.co.uk>
2014-10-08 16:09:46 +0100

    Formatted.

--- orcid-integration-test/src/test/java/org/orcid/api/t2/integration/OauthSignInPersistentParametersTests.java
+++ orcid-integration-test/src/test/java/org/orcid/api/t2/integration/OauthSignInPersistentParametersTests.java
@@ -53,7 +53,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * 
  * @author Angel Montenegro
- *
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-oauth-orcid-api-client-context.xml" })
@@ -65,15 +65,15 @@ public class OauthSignInPersistentParametersTests extends DBUnitTest {
     private static final Pattern OTHER_PATTERN = Pattern.compile("other=(.+)");
     private static final Pattern MADE_UP_PATTERN = Pattern.compile("made_up_param_not_passed=(.+)");
     private static final String DEFAULT = "default";
-    
+
     private WebDriver webDriver;
 
     @Resource
     private ClientRedirectDao clientRedirectDao;
-    
+
     @Resource
     private ClientDetailsManager clientDetailsManager;
-    
+
     @Resource
     private ProfileDao profileDao;
 
@@ -103,7 +103,7 @@ public class OauthSignInPersistentParametersTests extends DBUnitTest {
         if (clientRedirectDao.find(clientRedirectUriPk) == null) {
             clientDetailsManager.addClientRedirectUri(CLIENT_ID, redirectUri);
         }
-        
+
         webDriver.get(webBaseUrl + "/signout");
 
         // Update last modified to force cache eviction (because DB unit deletes
@@ -120,20 +120,23 @@ public class OauthSignInPersistentParametersTests extends DBUnitTest {
     }
 
     @Test
-    public void stateParamIsPersistentAndReturnedTest() throws InterruptedException {        
-        webDriver.get(String.format("%s/oauth/authorize?client_id=9999-9999-9999-9994&response_type=code&scope=/orcid-profile/read-limited&redirect_uri=%s&state=MyState&made_up_param_not_passed=true&other=present", webBaseUrl, redirectUri));
-        //Switch to the login form
+    public void stateParamIsPersistentAndReturnedTest() throws InterruptedException {
+        webDriver
+                .get(String
+                        .format("%s/oauth/authorize?client_id=9999-9999-9999-9994&response_type=code&scope=/orcid-profile/read-limited&redirect_uri=%s&state=MyState&made_up_param_not_passed=true&other=present",
+                                webBaseUrl, redirectUri));
+        // Switch to the login form
         WebElement switchFromLink = webDriver.findElement(By.id("in-register-switch-form"));
         switchFromLink.click();
         Thread.sleep(500);
-        //Fill the form
+        // Fill the form
         WebElement userId = webDriver.findElement(By.id("userId"));
         userId.sendKeys("user_to_test@user.com");
         WebElement password = webDriver.findElement(By.id("password"));
         password.sendKeys("password");
-        WebElement submitButton = webDriver.findElement(By.id("authorize-button")); 
+        WebElement submitButton = webDriver.findElement(By.id("authorize-button"));
         submitButton.click();
-        
+
         (new WebDriverWait(webDriver, DEFAULT_TIMEOUT_SECONDS)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return d.getTitle().equals("ORCID Playground");
@@ -141,17 +144,17 @@ public class OauthSignInPersistentParametersTests extends DBUnitTest {
         });
         String currentUrl = webDriver.getCurrentUrl();
         Matcher matcher = AUTHORIZATION_CODE_PATTERN.matcher(currentUrl);
-        //Check the auth code is present
+        // Check the auth code is present
         assertTrue(matcher.find());
         String authorizationCode = matcher.group(1);
         assertNotNull(authorizationCode);
-        //Check the state param is present
+        // Check the state param is present
         matcher = STATE_PATTERN.matcher(currentUrl);
         assertTrue(matcher.find());
         String stateParam = matcher.group(1);
         assertNotNull(stateParam);
         assertEquals("MyState", stateParam);
-        
+
         matcher = OTHER_PATTERN.matcher(currentUrl);
         assertFalse(matcher.find());
         matcher = MADE_UP_PATTERN.matcher(currentUrl);
