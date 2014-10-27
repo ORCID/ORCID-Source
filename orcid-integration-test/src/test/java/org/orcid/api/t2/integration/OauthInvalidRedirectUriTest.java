c6218fd3c018e195aa39685ee6969f724f8db8f5
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-10-27 15:44:28 -0600

    Fix an issue where the user cant register/login on the oauth screen if the redirect uri is wrong

---
+++ orcid-integration-test/src/test/java/org/orcid/api/t2/integration/OauthInvalidRedirectUriTest.java
@@ -0,0 +1,148 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.api.t2.integration;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.orcid.core.manager.ClientDetailsManager;
+import org.orcid.persistence.dao.ClientRedirectDao;
+import org.orcid.persistence.dao.ProfileDao;
+import org.orcid.persistence.jpa.entities.ProfileEntity;
+import org.orcid.persistence.jpa.entities.keys.ClientRedirectUriPk;
+import org.orcid.test.DBUnitTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 
+ * @author Angel Montenegro
+ * 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-oauth-orcid-api-client-context.xml" })
+public class OauthInvalidRedirectUriTest extends DBUnitTest {
+    private static final int DEFAULT_TIMEOUT_SECONDS = 30;
+    private static final String CLIENT_ID = "9999-9999-9999-9994";
+    private static final String DEFAULT = "default";
+    private static final String BAD_REDIRECT_URI = "http://page/error";
+    private WebDriver webDriver;
+
+    @Resource
+    private ClientRedirectDao clientRedirectDao;
+
+    @Resource
+    private ClientDetailsManager clientDetailsManager;
+
+    @Resource
+    private ProfileDao profileDao;
+
+    @Value("${org.orcid.web.base.url:http://localhost:8080/orcid-web}")
+    private String webBaseUrl;
+
+    private String redirectUri;
+
+    private static final List<String> DATA_FILES = Arrays.asList("/group_client_data/EmptyEntityData.xml", "/group_client_data/SecurityQuestionEntityData.xml",
+            "/group_client_data/ProfileEntityData.xml", "/group_client_data/WorksEntityData.xml", "/group_client_data/OrgsEntityData.xml",
+            "/group_client_data/ClientDetailsEntityData.xml", "/group_client_data/ProfileWorksEntityData.xml", "/group_client_data/OrgAffiliationEntityData.xml",
+            "/group_client_data/ProfileFundingEntityData.xml");
+
+    @BeforeClass
+    public static void initDBUnitData() throws Exception {
+        initDBUnitData(DATA_FILES, null);
+    }
+
+    @Before
+    @Transactional
+    public void before() {
+        webDriver = new FirefoxDriver();
+        redirectUri = webBaseUrl + "/oauth/playground";
+
+        // Set redirect uris if needed
+        ClientRedirectUriPk clientRedirectUriPk = new ClientRedirectUriPk(CLIENT_ID, redirectUri, DEFAULT);
+        if (clientRedirectDao.find(clientRedirectUriPk) == null) {
+            clientDetailsManager.addClientRedirectUri(CLIENT_ID, redirectUri);
+        }
+
+        webDriver.get(webBaseUrl + "/signout");
+
+        // Update last modified to force cache eviction (because DB unit deletes
+        // a load of stuff from the DB, but reinserts profiles with older last
+        // modified date)
+        for (ProfileEntity profile : profileDao.getAll()) {
+            profileDao.updateLastModifiedDateWithoutResult(profile.getId());
+        }
+    }
+
+    @After
+    public void after() {
+        webDriver.quit();
+    }
+    
+    @Test
+    public void invalidRedirectUriAllowsLoginThenShowErrorTest() throws InterruptedException {
+        webDriver
+        .get(String
+                .format("%s/oauth/authorize?client_id=9999-9999-9999-9994&response_type=code&scope=/orcid-profile/read-limited&redirect_uri=%s&state=MyState&made_up_param_not_passed=true&other=present",
+                        webBaseUrl, BAD_REDIRECT_URI));
+        // Switch to the login form
+        By switchFromLinkLocator = By.id("in-register-switch-form");
+        (new WebDriverWait(webDriver, DEFAULT_TIMEOUT_SECONDS)).until(ExpectedConditions.presenceOfElementLocated(switchFromLinkLocator));
+        WebElement switchFromLink = webDriver.findElement(switchFromLinkLocator);
+        switchFromLink.click();
+
+        // Fill the form
+        By userIdElementLocator = By.id("userId");
+        (new WebDriverWait(webDriver, DEFAULT_TIMEOUT_SECONDS)).until(ExpectedConditions.presenceOfElementLocated(userIdElementLocator));
+        WebElement userIdElement = webDriver.findElement(userIdElementLocator);
+        userIdElement.sendKeys("user_to_test@user.com");
+        WebElement passwordElement = webDriver.findElement(By.id("password"));
+        passwordElement.sendKeys("password");
+        WebElement submitButton = webDriver.findElement(By.id("authorize-button"));
+        submitButton.click();
+        Thread.sleep(1500);
+        (new WebDriverWait(webDriver, DEFAULT_TIMEOUT_SECONDS)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().equals("ORCID");
+            }
+        });
+        String currentUrl = webDriver.getCurrentUrl();
+        assertTrue(currentUrl.contains("/oauth/error/redirect-uri-mismatch"));
+        assertTrue(currentUrl.contains("client_id=9999-9999-9999-9994"));
+        assertTrue(currentUrl.contains("response_type=code"));
+        assertTrue(currentUrl.contains("redirect_uri=" + BAD_REDIRECT_URI));
+        assertTrue(currentUrl.contains("scope=/orcid-profile/read-limited"));
+    }
+}
