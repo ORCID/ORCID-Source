2d367050d47589d19133dc56b94c85a93373b003
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-11-06 14:39:49 -0600

    Upgrade JUnit to a newest one and creating a profile to run the integration tests

---
+++ orcid-integration-test/src/test/java/org/orcid/api/t1/integration/JustATest.java
@@ -0,0 +1,33 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.api.t1.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:orcid-t1-client-context.xml" })
+public class JustATest {
+
+    @Test
+    public void thisIsATest() {
+       System.out.println("This is just a test");
+    }
+    
+}
