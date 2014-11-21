5164a801a1a2cee8ca0e3328dd17108d32fd2e09
Will Simpson <will@wjrs.co.uk>
2014-11-21 19:14:36 +0000

    Organized imports.

--- orcid-model/src/main/java/org/orcid/jaxb/model/notification/Notification.java
+++ orcid-model/src/main/java/org/orcid/jaxb/model/notification/Notification.java
@@ -28,10 +28,8 @@ import java.io.Serializable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlTransient;
-import javax.xml.bind.annotation.XmlType;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.orcid.jaxb.model.common.PutCode;
