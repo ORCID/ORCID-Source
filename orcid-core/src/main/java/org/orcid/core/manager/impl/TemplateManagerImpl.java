cf95ff0b71fa6e5aa42aa298d522820b4cdfad53
Will Simpson <will@wjrs.co.uk>
2014-11-28 13:48:26 +0000

    Placeholder for add activities notification text.

--- orcid-core/src/main/java/org/orcid/core/manager/impl/TemplateManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/TemplateManagerImpl.java
@@ -20,6 +20,9 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.orcid.core.locale.LocaleManager;
 import org.orcid.core.manager.TemplateManager;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -31,18 +34,19 @@ public class TemplateManagerImpl implements TemplateManager, InitializingBean {
 
     private Configuration freeMarkerConfiguration;
 
+    @Resource
+    private LocaleManager localeManager;
+
     @Override
     public String processTemplate(String templateName, Map<String, Object> params) {
         try {
-            Template template = freeMarkerConfiguration.getTemplate(templateName);
+            Template template = freeMarkerConfiguration.getTemplate(templateName, localeManager.getLocale());
             StringWriter result = new StringWriter();
             template.process(params, result);
             return result.toString();
         } catch (IOException e) {
-            // XXX
             throw new IllegalArgumentException(e);
         } catch (TemplateException e) {
-            // XXX
             throw new IllegalArgumentException(e);
         }
     }
