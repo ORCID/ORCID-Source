80e5fbf7c19adc76a0db2ea8c47b32f5dc7fa8e2
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2014-12-29 12:38:55 -0600

    Done, checking unit tests

---
+++ orcid-persistence/src/main/java/org/orcid/persistence/dao/ClientScopeDao.java
@@ -0,0 +1,9 @@
+package org.orcid.persistence.dao;
+
+import org.orcid.persistence.jpa.entities.ClientScopeEntity;
+import org.orcid.persistence.jpa.entities.keys.ClientScopePk;
+
+public interface ClientScopeDao extends GenericDao<ClientScopeEntity, ClientScopePk> {
+
+    boolean deleteScope(String clientId, String scopeType);
+}
---
+++ orcid-persistence/src/main/java/org/orcid/persistence/dao/impl/ClientScopeDaoImpl.java
@@ -0,0 +1,32 @@
+package org.orcid.persistence.dao.impl;
+
+import javax.persistence.Query;
+
+import org.orcid.persistence.dao.ClientScopeDao;
+import org.orcid.persistence.jpa.entities.ClientScopeEntity;
+import org.orcid.persistence.jpa.entities.keys.ClientScopePk;
+import org.springframework.transaction.annotation.Transactional;
+
+public class ClientScopeDaoImpl extends GenericDaoImpl<ClientScopeEntity, ClientScopePk> implements ClientScopeDao {    
+
+    public ClientScopeDaoImpl() {
+        super(ClientScopeEntity.class);
+    }
+
+    /**
+     * Removes a client secret key
+     * 
+     * @param clientId
+     * @param clientSecret
+     * @return true if a entity is removed
+     * */
+    @Override
+    @Transactional
+    public boolean deleteScope(String clientId, String scopeType) {
+        Query deleteQuery = entityManager.createNativeQuery("delete from client_scope where client_details_id=:clientId and scope_type=:scopeType");
+        deleteQuery.setParameter("clientId", clientId);
+        deleteQuery.setParameter("scopeType", scopeType);
+        return deleteQuery.executeUpdate() > 0;
+    }
+
+}
