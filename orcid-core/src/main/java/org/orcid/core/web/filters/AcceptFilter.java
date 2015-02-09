9fde4ff44a85c45f23e1d0fb3af0c11ca9366a12
rcpeters <info@rcpeters.com>
2015-02-09 13:23:55 -0800

    add filter that sets accept if left out and content is set

---
+++ orcid-core/src/main/java/org/orcid/core/web/filters/AcceptFilter.java
@@ -0,0 +1,56 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.core.web.filters;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * 
+ * @author Robert Peters (rcpeters)
+ * 
+ */
+
+public class AcceptFilter extends OncePerRequestFilter {
+
+    private static Log log = LogFactory.getLog(AcceptFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accept = request.getHeader("accept");
+        String contentType = request.getHeader("Content-Type");
+        
+        if ((accept == null 
+                || accept.equals("*/*")) && contentType != null ) {
+            HttpServletRequestWrapper requestWrapper = new AcceptHeaderRequestWrapper(request, contentType);
+            filterChain.doFilter(requestWrapper, response);
+        } else {
+             filterChain.doFilter(request, response);
+        }
+    }
+}
