2068a1ee8e1808489633d2291578f606b77e2261
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-02-19 17:32:11 -0600

    Removing spaces from other clas

--- orcid-api-web/src/main/java/org/orcid/api/memberV2/server/MemberV2ApiServiceImplBase.java
+++ orcid-api-web/src/main/java/org/orcid/api/memberV2/server/MemberV2ApiServiceImplBase.java
@@ -126,10 +126,6 @@ abstract public class MemberV2ApiServiceImplBase {
         return serviceDelegator.viewFunding(orcid, putCode);
     }
     
-    
-    
-    
-    
     @GET
     @Produces(value = { VND_ORCID_XML, ORCID_XML, MediaType.APPLICATION_XML, VND_ORCID_JSON, ORCID_JSON, MediaType.APPLICATION_JSON })
     @Path(FUNDING_SUMMARY + PUTCODE)
@@ -137,17 +133,6 @@ abstract public class MemberV2ApiServiceImplBase {
         return serviceDelegator.viewFundingSummary(orcid, putCode);
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
     @POST
     @Produces(value = { VND_ORCID_XML, ORCID_XML, MediaType.APPLICATION_XML, VND_ORCID_JSON, ORCID_JSON, MediaType.APPLICATION_JSON })
     @Consumes(value = { VND_ORCID_XML, ORCID_XML, MediaType.APPLICATION_XML, VND_ORCID_JSON, ORCID_JSON, MediaType.APPLICATION_JSON })
