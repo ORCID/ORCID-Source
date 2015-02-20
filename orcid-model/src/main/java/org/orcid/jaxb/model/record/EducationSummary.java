b083df5866d783cb36c78665fea873f237e1829c
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-02-20 09:37:53 -0600

    Adding missing header

--- orcid-model/src/main/java/org/orcid/jaxb/model/record/EducationSummary.java
+++ orcid-model/src/main/java/org/orcid/jaxb/model/record/EducationSummary.java
@@ -1,10 +1,33 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
 package org.orcid.jaxb.model.record;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = { "departmentName", "roleTitle", "startDate", "endDate", "source", "createdDate", "lastModifiedDate" })
+@XmlRootElement(name = "educationSummary", namespace = "http://www.orcid.org/ns/education")
 public class EducationSummary implements VisibilityType, Activity, Serializable {
             
     private static final long serialVersionUID = 6988028860521476955L;
