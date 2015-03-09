18a93f78fa07f4a425a4c34e8f7be1dab7ace11b
Will Simpson <will@wjrs.co.uk>
2015-03-09 15:57:06 +0000

    Organized imports.

--- orcid-model/src/main/java/org/orcid/jaxb/model/record/summary/ActivitiesSummary.java
+++ orcid-model/src/main/java/org/orcid/jaxb/model/record/summary/ActivitiesSummary.java
@@ -32,7 +32,6 @@ import java.util.Map;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
