e0eff9383215518b091a8ddce3668196d8a69e7c
Will Simpson <will@wjrs.co.uk>
2015-03-17 19:02:36 +0000

    Organized imports.

--- orcid-model/src/main/java/org/orcid/jaxb/model/notification/addactivities/Activities.java
+++ orcid-model/src/main/java/org/orcid/jaxb/model/notification/addactivities/Activities.java
@@ -33,8 +33,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import org.codehaus.jackson.annotate.JsonValue;
-
 /**
  * <p>
  * Java class for anonymous complex type.
