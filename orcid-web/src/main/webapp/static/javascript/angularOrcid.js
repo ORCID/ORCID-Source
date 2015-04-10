a9a5a62faa56dcb2936dc1b32db16162baa07403
rcpeters <info@rcpeters.com>
2015-04-09 23:28:02 -0700

    fix issue where scope.worksSrvc.loadAbbrWorks was being called to early

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -4096,26 +4096,21 @@ orcidNgModule.controller('WorkCtrl', ['$scope', '$compile', '$filter', 'worksSrv
     };
     
     $scope.saveAllFromBibtex = function(){
-    	var works = $scope.worksFromBibtex;
-    	var prom = [];
-    	angular.forEach( works, function( work, key ) {    		
-    		if (work.errors.length == 0) {
-    			prom.push(
-    			     worksSrvc.putWork(work,function(data) {
-    			         if (data.errors.length == 0) {
-                            index = works.indexOf(work);
-                            works.splice(index, 1);
-                            $scope.worksFromBibtex = works;
-                            $scope.$apply();
-    			         }
-                     })
-    			);
-    		};    		
-    	}); 
-    	
-    	$q.all(prom).then(function(){
-    		$scope.worksSrvc.loadAbbrWorks(worksSrvc.constants.access_type.USER);    		
-    	});
+        var warksToSave =  new Array();
+        angular.forEach($scope.worksFromBibtex, function( work, key ) {
+            if (work.errors.length == 0) warksToSave.push(work);
+        });
+        var numToSave = warksToSave.length;
+        angular.forEach( warksToSave, function( work, key ) {
+            worksSrvc.putWork(work,function(data) {
+                    index = $scope.worksFromBibtex.indexOf(work);
+                    $scope.worksFromBibtex.splice(index, 1);
+                    $scope.$apply();
+                    numToSave--;
+                    if (numToSave == 0)
+                        $scope.worksSrvc.loadAbbrWorks(worksSrvc.constants.access_type.USER);
+            });
+        });
     };
 
     $scope.openBibTextWizard = function () {
