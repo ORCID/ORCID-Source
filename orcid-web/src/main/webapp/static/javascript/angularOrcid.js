f9694131c5fdd94aca45846932166114824b4a69
Elizabeth Krznarich <ekrznarich@Elizabeths-MacBook-Pro.local>
2015-07-24 08:34:40 -0400

    add italian to testing languages list

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -5998,6 +5998,10 @@ orcidNgModule.controller('languageCtrl',['$scope', '$cookies', function ($scope,
                 "label": 'Français'
             },
             {
+                "value": 'it',
+                "label": 'Italiano'
+            },
+            {
                 "value": 'ja',
                 "label": '日本語'
             },
