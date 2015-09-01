da9da3f8aa94232f7fc851ee638749a604a71d21
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-09-01 12:07:40 -0600

    Initial commit

---
+++ orcid-internal-api/README.md
@@ -0,0 +1,5 @@
+# ORCID-INTERNAL-API (Internal API)
+
+#License
+See [LICENSE.md](https://github.com/ORCID/ORCID-Work-in-Progress/blob/master/LICENSE.md)
+
---
+++ orcid-internal-api/src/main/java/org/orcid/internal/server/delegator/InternalOrcidApiServiceDelegator.java
@@ -0,0 +1,8 @@
+package org.orcid.internal.server.delegator;
+
+import javax.ws.rs.core.Response;
+
+public interface InternalOrcidApiServiceDelegator {
+
+    Response viewStatusText();
+}
