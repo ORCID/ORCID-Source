7a9bffba06c5a0e89d0e164bc80d3cd9f64b22f2
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-09-16 08:58:00 -0600

    Modifying the pattern to filter the paths

--- orcid-core/src/main/java/org/orcid/core/web/filters/CrossDomainWebManger.java
+++ orcid-core/src/main/java/org/orcid/core/web/filters/CrossDomainWebManger.java
@@ -33,7 +33,7 @@ public class CrossDomainWebManger {
 
     private static final String LOCALHOST = "localhost";
 
-    Pattern p = Pattern.compile("/public/.*|/public_widgets/.*|.*/userStatus\\.json|.*/lang\\.json");
+    Pattern p = Pattern.compile("^/public/.*|^/public_widgets/(?:\\d{4}-){3,}\\d{3}[\\dX]/.+/info\\.json|^/userStatus\\.json|^/lang\\.json");
 
     @Value("${org.orcid.security.cors.allowed_domains:qa.orcid.org,sandbox.orcid.org,orcid.org}")
     private String allowedDomains;
@@ -48,12 +48,7 @@ public class CrossDomainWebManger {
             // If it is a valid domain, allow
             if (validateDomain(request.getHeader("origin"))) {
                 return true;
-            } else {
-                // If it is and invalid domain, validate the path
-                if (validatePath(path)) {
-                    return true;
-                }
-            }
+            } 
         } else {
             // Check referer header for localhost
             if (!PojoUtil.isEmpty(request.getHeader("referer"))) {
@@ -65,6 +60,11 @@ public class CrossDomainWebManger {
             }
         }
 
+        // If it is and invalid domain, validate the path
+        if (validatePath(path)) {
+            return true;
+        }
+        
         return false;
     }
 
