982f5f7bba543dd9a3079140cd1e77995c3b8684
Will Simpson <will@wjrs.co.uk>
2015-09-17 13:20:59 +0100

    Fix validation so allows just path in the authorization uri.

--- orcid-core/src/main/java/org/orcid/core/manager/impl/NotificationValidationManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/NotificationValidationManagerImpl.java
@@ -19,7 +19,6 @@ package org.orcid.core.manager.impl;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.commons.lang3.StringUtils;
 import org.orcid.core.exception.OrcidValidationException;
 import org.orcid.core.manager.NotificationValidationManager;
 import org.orcid.jaxb.model.notification.permission.AuthorizationUrl;
@@ -31,13 +30,12 @@ public class NotificationValidationManagerImpl implements NotificationValidation
     public void validateNotificationPermission(NotificationPermission notification) {
         AuthorizationUrl authorizationUrl = notification.getAuthorizationUrl();
         String uriString = authorizationUrl.getUri();
-        if (StringUtils.isBlank(uriString)) {
-            throw new OrcidValidationException("Authorization uri must not be blank");
-        }
-        try {
-            new URI(uriString);
-        } catch (URISyntaxException e) {
-            throw new OrcidValidationException("Bad authorization uri", e);
+        if (uriString != null) {
+            try {
+                new URI(uriString);
+            } catch (URISyntaxException e) {
+                throw new OrcidValidationException("Bad authorization uri", e);
+            }
         }
     }
 
