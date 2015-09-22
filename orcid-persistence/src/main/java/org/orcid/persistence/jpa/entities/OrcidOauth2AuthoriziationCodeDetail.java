bfec5cde7eeb13685ba7eced95597d92edbba17c
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-09-22 12:02:22 -0600

    Change the way we load the client details form the oauth2_authoriziation_code_detail table

--- orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/OrcidOauth2AuthoriziationCodeDetail.java
+++ orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/OrcidOauth2AuthoriziationCodeDetail.java
@@ -171,7 +171,7 @@ public class OrcidOauth2AuthoriziationCodeDetail extends BaseEntity<String> impl
         this.state = state;
     }
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "client_details_id")
     public ClientDetailsEntity getClientDetailsEntity() {
         return clientDetailsEntity;
