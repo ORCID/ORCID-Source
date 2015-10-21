6e40d0c1c748d38c7bb712768a8eaa844c8e8c81
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-10-21 09:46:31 -0600

    Done

--- orcid-persistence/src/main/java/org/orcid/persistence/dao/impl/StatisticsGeneratorDaoImpl.java
+++ orcid-persistence/src/main/java/org/orcid/persistence/dao/impl/StatisticsGeneratorDaoImpl.java
@@ -19,15 +19,17 @@ package org.orcid.persistence.dao.impl;
 import java.math.BigInteger;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.orcid.persistence.dao.StatisticsGeneratorDao;
 
 public class StatisticsGeneratorDaoImpl implements StatisticsGeneratorDao {
 
-    @PersistenceContext(unitName = "orcid")
     protected EntityManager entityManager;
+    
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     public long getLiveIds() {
         Query query = entityManager.createNativeQuery("select count(*) from profile where profile_deactivation_date is null and record_locked = false");
