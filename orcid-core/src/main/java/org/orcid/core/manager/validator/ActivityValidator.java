27c04c15757dcd1335dcfce53c4b03d5159dde01
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-10-27 11:15:31 -0600

    Pathching error where ext id type is null

--- orcid-core/src/main/java/org/orcid/core/manager/validator/ActivityValidator.java
+++ orcid-core/src/main/java/org/orcid/core/manager/validator/ActivityValidator.java
@@ -42,7 +42,7 @@ import org.orcid.persistence.jpa.entities.SourceEntity;
 
 public class ActivityValidator {
 
-	public static void validateWork(Work work, boolean createFlag, SourceEntity sourceEntity) {
+    public static void validateWork(Work work, boolean createFlag, SourceEntity sourceEntity) {
         WorkTitle title = work.getWorkTitle();
         if (title == null || title.getTitle() == null || StringUtils.isEmpty(title.getTitle().getContent())) {
             throw new ActivityTitleValidationException();
@@ -50,110 +50,88 @@ public class ActivityValidator {
 
         if (work.getWorkExternalIdentifiers() == null || work.getWorkExternalIdentifiers().getWorkExternalIdentifier() == null
                 || work.getWorkExternalIdentifiers().getWorkExternalIdentifier().isEmpty()) {
-        	throw new ActivityIdentifierValidationException();
+            throw new ActivityIdentifierValidationException();
         }
-        
+
         if (work.getPutCode() != null && createFlag) {
-        	Map<String, String> params = new HashMap<String, String>();
-        	if(sourceEntity != null) {
-        		params.put("clientName", sourceEntity.getSourceName());
-        	}
+            Map<String, String> params = new HashMap<String, String>();
+            if (sourceEntity != null) {
+                params.put("clientName", sourceEntity.getSourceName());
+            }
             throw new InvalidPutCodeException(params);
         }
     }
-    
+
     public static void validateFunding(Funding funding, SourceEntity sourceEntity, boolean createFlag) {
-    	FundingTitle title = funding.getTitle();
+        FundingTitle title = funding.getTitle();
         if (title == null || title.getTitle() == null || StringUtils.isEmpty(title.getTitle().getContent())) {
             throw new ActivityTitleValidationException();
         }
 
         if (funding.getExternalIdentifiers() == null || funding.getExternalIdentifiers().getExternalIdentifier() == null
                 || funding.getExternalIdentifiers().getExternalIdentifier().isEmpty()) {
-        	throw new ActivityIdentifierValidationException();
-        }
-        
-        if (funding.getPutCode() != null && createFlag) {
-        	Map<String, String> params = new HashMap<String, String>();
-        	if(sourceEntity != null) {
-        		params.put("clientName", sourceEntity.getSourceName());
-        	}
-            throw new InvalidPutCodeException(params);
-        }
-    }
-    
-    public static void validateEmployment(Employment employment, SourceEntity sourceEntity) {
-        if (employment.getPutCode() != null) {
-        	Map<String, String> params = new HashMap<String, String>();
-        	if(sourceEntity != null) {
-        		params.put("clientName", sourceEntity.getSourceName());
-        	}
-            throw new InvalidPutCodeException(params);
-        }
-    }
-    
-    public static void validateEducation(Education education, SourceEntity sourceEntity) {
-        if (education.getPutCode() != null) {
-        	Map<String, String> params = new HashMap<String, String>();
-        	if(sourceEntity != null) {
-        		params.put("clientName", sourceEntity.getSourceName());
-        	}
-            throw new InvalidPutCodeException(params);
-        }
-    }
-    
-    public static void validatePeerReview(PeerReview peerReview, SourceEntity sourceEntity) {
-        if(peerReview.getExternalIdentifiers() == null || peerReview.getExternalIdentifiers().getExternalIdentifier().isEmpty()) {
             throw new ActivityIdentifierValidationException();
         }
-        
-        if (peerReview.getPutCode() != null) {
-        	Map<String, String> params = new HashMap<String, String>();
-        	params.put("clientName", sourceEntity.getSourceName());
+
+        if (funding.getPutCode() != null && createFlag) {
+            Map<String, String> params = new HashMap<String, String>();
+            if (sourceEntity != null) {
+                params.put("clientName", sourceEntity.getSourceName());
+            }
             throw new InvalidPutCodeException(params);
         }
     }
-    
+
+    public static void validateEmployment(Employment employment, SourceEntity sourceEntity) {
+        if (employment.getPutCode() != null) {
+            Map<String, String> params = new HashMap<String, String>();
+            if (sourceEntity != null) {
+                params.put("clientName", sourceEntity.getSourceName());
+            }
+            throw new InvalidPutCodeException(params);
+        }
+    }
+
+    public static void validateEducation(Education education, SourceEntity sourceEntity) {
+        if (education.getPutCode() != null) {
+            Map<String, String> params = new HashMap<String, String>();
+            if (sourceEntity != null) {
+                params.put("clientName", sourceEntity.getSourceName());
+            }
+            throw new InvalidPutCodeException(params);
+        }
+    }
+
+    public static void validatePeerReview(PeerReview peerReview, SourceEntity sourceEntity) {
+        if (peerReview.getExternalIdentifiers() == null || peerReview.getExternalIdentifiers().getExternalIdentifier().isEmpty()) {
+            throw new ActivityIdentifierValidationException();
+        }
+
+        if (peerReview.getPutCode() != null) {
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("clientName", sourceEntity.getSourceName());
+            throw new InvalidPutCodeException(params);
+        }
+    }
+
     public static void validateCreateGroupRecord(GroupIdRecord groupIdRecord, SourceEntity sourceEntity) {
         if (groupIdRecord.getPutCode() != null) {
-        	Map<String, String> params = new HashMap<String, String>();
-        	if(sourceEntity != null) {
-        		params.put("clientName", sourceEntity.getSourceName());
-        	}
+            Map<String, String> params = new HashMap<String, String>();
+            if (sourceEntity != null) {
+                params.put("clientName", sourceEntity.getSourceName());
+            }
             throw new InvalidPutCodeException(params);
         }
     }
 
-    public static void checkExternalIdentifiers(WorkExternalIdentifiers newExtIds,
-    		WorkExternalIdentifiers existingExtIds, Source existingSource, SourceEntity sourceEntity) {
-        if(existingExtIds != null && newExtIds != null) {            
-            for(WorkExternalIdentifier existingId : existingExtIds.getExternalIdentifier()) {
-                for(WorkExternalIdentifier newId : newExtIds.getExternalIdentifier()) {
-                    if(existingId.getRelationship().equals(Relationship.SELF) 
-                    		&& newId.getRelationship().equals(Relationship.SELF)
-                    		&& newId.getWorkExternalIdentifierId().getContent().equals(existingId.getWorkExternalIdentifierId().getContent())
-                    		&& newId.getWorkExternalIdentifierType().equals(existingId.getWorkExternalIdentifierType())
-                    		&& sourceEntity.getSourceId().equals(getExistingSource(existingSource))){
-                    	Map<String, String> params = new HashMap<String, String>();
-                        params.put("clientName", sourceEntity.getSourceName());
-                        throw new OrcidDuplicatedActivityException(params);
-                    }
-                }
-            }
-        }
-    }
-    
-    public static void checkFundingExternalIdentifiers(FundingExternalIdentifiers newExtIds,
-    		FundingExternalIdentifiers existingExtIds, Source existingSource, SourceEntity sourceEntity) {
-        if(existingExtIds != null && newExtIds != null) {            
-            for(FundingExternalIdentifier existingId : existingExtIds.getExternalIdentifier()) {
-                for(FundingExternalIdentifier newId : newExtIds.getExternalIdentifier()) {
-                    if(existingId.getRelationship().equals(Relationship.SELF) 
-                    		&& newId.getRelationship().equals(Relationship.SELF)
-                    		&& newId.getValue().equals(existingId.getValue())
-                    		&& newId.getType().equals(existingId.getType())
-                    		&& sourceEntity.getSourceId().equals(getExistingSource(existingSource))){
-                    	Map<String, String> params = new HashMap<String, String>();
+    public static void checkExternalIdentifiers(WorkExternalIdentifiers newExtIds, WorkExternalIdentifiers existingExtIds, Source existingSource,
+            SourceEntity sourceEntity) {
+        if (existingExtIds != null && newExtIds != null) {
+            for (WorkExternalIdentifier existingId : existingExtIds.getExternalIdentifier()) {
+                for (WorkExternalIdentifier newId : newExtIds.getExternalIdentifier()) {
+                    if (isDupRelationship(newId, existingId) && isDupValue(newId, existingId) && isDupType(newId, existingId)
+                            && sourceEntity.getSourceId().equals(getExistingSource(existingSource))) {
+                        Map<String, String> params = new HashMap<String, String>();
                         params.put("clientName", sourceEntity.getSourceName());
                         throw new OrcidDuplicatedActivityException(params);
                     }
@@ -162,12 +140,44 @@ public class ActivityValidator {
         }
     }
 
-	@SuppressWarnings("deprecation")
-	private static String getExistingSource(Source source) {
-		if(source != null) {
-			return (source.getSourceClientId() != null) ? 
-					source.getSourceClientId().getPath() : source.getSourceOrcid().getPath();
-		}
-		return null;
-	}
+    private static boolean isDupRelationship(WorkExternalIdentifier newId, WorkExternalIdentifier existingId) {
+        return existingId.getRelationship() != null && existingId.getRelationship().equals(Relationship.SELF) && newId.getRelationship() != null
+                && newId.getRelationship().equals(Relationship.SELF);
+    }
+
+    private static boolean isDupValue(WorkExternalIdentifier newId, WorkExternalIdentifier existingId) {
+        return existingId.getWorkExternalIdentifierId() != null && existingId.getWorkExternalIdentifierId().getContent() != null
+                && newId.getWorkExternalIdentifierId() != null && newId.getWorkExternalIdentifierId().getContent() != null
+                && newId.getWorkExternalIdentifierId().getContent().equals(existingId.getWorkExternalIdentifierId().getContent());
+    }
+
+    private static boolean isDupType(WorkExternalIdentifier newId, WorkExternalIdentifier existingId) {
+        return existingId.getWorkExternalIdentifierType() != null && newId.getWorkExternalIdentifierType() != null
+                && newId.getWorkExternalIdentifierType().equals(existingId.getWorkExternalIdentifierType());
+    }
+
+    public static void checkFundingExternalIdentifiers(FundingExternalIdentifiers newExtIds, FundingExternalIdentifiers existingExtIds, Source existingSource,
+            SourceEntity sourceEntity) {
+        if (existingExtIds != null && newExtIds != null) {
+            for (FundingExternalIdentifier existingId : existingExtIds.getExternalIdentifier()) {
+                for (FundingExternalIdentifier newId : newExtIds.getExternalIdentifier()) {
+                    if (existingId.getRelationship().equals(Relationship.SELF) && newId.getRelationship().equals(Relationship.SELF)
+                            && newId.getValue().equals(existingId.getValue()) && newId.getType().equals(existingId.getType())
+                            && sourceEntity.getSourceId().equals(getExistingSource(existingSource))) {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("clientName", sourceEntity.getSourceName());
+                        throw new OrcidDuplicatedActivityException(params);
+                    }
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private static String getExistingSource(Source source) {
+        if (source != null) {
+            return (source.getSourceClientId() != null) ? source.getSourceClientId().getPath() : source.getSourceOrcid().getPath();
+        }
+        return null;
+    }
 }
