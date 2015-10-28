962489ade2370e2756e55e34722fb064872fec31
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-10-28 15:40:03 -0600

    Updating the equals so it works as it should be

--- orcid-model/src/main/java/org/orcid/jaxb/model/record/WorkExternalIdentifier.java
+++ orcid-model/src/main/java/org/orcid/jaxb/model/record/WorkExternalIdentifier.java
@@ -32,14 +32,17 @@ import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 
 /**
- * <p>Java class for anonymous complex type.
+ * <p>
+ * Java class for anonymous complex type.
  * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- *  
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType( propOrder = { "workExternalIdentifierType", "workExternalIdentifierId" })
+@XmlType(propOrder = { "workExternalIdentifierType", "workExternalIdentifierId" })
 @XmlRootElement(name = "workExternalIdentifier")
 public class WorkExternalIdentifier extends ExternalIdentifierBase implements Serializable {
 
@@ -50,16 +53,14 @@ public class WorkExternalIdentifier extends ExternalIdentifierBase implements Se
     protected WorkExternalIdentifierId workExternalIdentifierId;
 
     public WorkExternalIdentifier() {
-        
+
     }
-    
+
     /**
      * Gets the value of the workExternalIdentifierType property.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
+     * @return possible object is {@link String }
+     * 
      */
     public WorkExternalIdentifierType getWorkExternalIdentifierType() {
         return workExternalIdentifierType;
@@ -69,9 +70,8 @@ public class WorkExternalIdentifier extends ExternalIdentifierBase implements Se
      * Sets the value of the workExternalIdentifierType property.
      * 
      * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
+     *            allowed object is {@link String }
+     * 
      */
     public void setWorkExternalIdentifierType(WorkExternalIdentifierType value) {
         this.workExternalIdentifierType = value;
@@ -80,10 +80,8 @@ public class WorkExternalIdentifier extends ExternalIdentifierBase implements Se
     /**
      * Gets the value of the workExternalIdentifierId property.
      * 
-     * @return
-     *     possible object is
-     *     {@link WorkExternalIdentifierId }
-     *     
+     * @return possible object is {@link WorkExternalIdentifierId }
+     * 
      */
     public WorkExternalIdentifierId getWorkExternalIdentifierId() {
         return workExternalIdentifierId;
@@ -93,74 +91,86 @@ public class WorkExternalIdentifier extends ExternalIdentifierBase implements Se
      * Sets the value of the workExternalIdentifierId property.
      * 
      * @param value
-     *     allowed object is
-     *     {@link WorkExternalIdentifierId }
-     *     
+     *            allowed object is {@link WorkExternalIdentifierId }
+     * 
      */
     public void setWorkExternalIdentifierId(WorkExternalIdentifierId value) {
         this.workExternalIdentifierId = value;
     }
-    
+
     @Override
     public String toString() {
         String result = "";
-        if(workExternalIdentifierType != null)
+        if (workExternalIdentifierType != null)
             result += workExternalIdentifierType.toString() + "(";
-        if(workExternalIdentifierId != null)
+        if (workExternalIdentifierId != null)
             result += workExternalIdentifierId.getContent();
-        if(workExternalIdentifierType != null)
-            result += ")";        
+        if (workExternalIdentifierType != null)
+            result += ")";
         return result;
     }
-    
+
     @Override
     public boolean passGroupingValidation() {
-        //Perform general validations
-        if(!super.passGroupingValidation()) 
-            return false;        
-        
-        //Dont groups works where the external id is empty
-        if(workExternalIdentifierType == null && (workExternalIdentifierId == null || workExternalIdentifierId.getContent() == null || workExternalIdentifierId.getContent().isEmpty()))
+        // Perform general validations
+        if (!super.passGroupingValidation())
             return false;
-        
-        //Dont work works by ISSN identifier
-        if(WorkExternalIdentifierType.ISSN.equals(workExternalIdentifierType))
+
+        // Dont groups works where the external id is empty
+        if (workExternalIdentifierType == null
+                && (workExternalIdentifierId == null || workExternalIdentifierId.getContent() == null || workExternalIdentifierId.getContent().isEmpty()))
             return false;
-        
+
+        // Dont work works by ISSN identifier
+        if (WorkExternalIdentifierType.ISSN.equals(workExternalIdentifierType))
+            return false;
+
         return true;
-    }
+    }    
 
     @Override
     public int hashCode() {
         final int prime = 31;
-        int result = prime + ((workExternalIdentifierId == null) ? 0 : workExternalIdentifierId.hashCode());
+        int result = super.hashCode();
+        result = prime * result + ((workExternalIdentifierId == null) ? 0 : workExternalIdentifierId.hashCode());
         result = prime * result + ((workExternalIdentifierType == null) ? 0 : workExternalIdentifierType.hashCode());
         return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (getClass() != obj.getClass())
+        }
+        if (!super.equals(obj)) {
             return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
         WorkExternalIdentifier other = (WorkExternalIdentifier) obj;
+        if(super.equals(other)) {
+            return false;
+        }
         if (workExternalIdentifierId == null) {
-            if (other.workExternalIdentifierId != null)
+            if (other.workExternalIdentifierId != null) {
                 return false;
-        } else if (!workExternalIdentifierId.equals(other.workExternalIdentifierId))
+            }
+        } else if (!workExternalIdentifierId.equals(other.workExternalIdentifierId)) {
             return false;
-        if (workExternalIdentifierType != other.workExternalIdentifierType)
+        }
+        if (workExternalIdentifierType != other.workExternalIdentifierType) {
             return false;
+        }
         return true;
-    }     
-    
-    public static WorkExternalIdentifier fromMessageExtId(org.orcid.jaxb.model.message.WorkExternalIdentifier oldExtId) {        
+    }
+
+    public static WorkExternalIdentifier fromMessageExtId(org.orcid.jaxb.model.message.WorkExternalIdentifier oldExtId) {
         WorkExternalIdentifier newExtId = new WorkExternalIdentifier();
-        if(oldExtId.getWorkExternalIdentifierId() != null) {
+        if (oldExtId.getWorkExternalIdentifierId() != null) {
             newExtId.setWorkExternalIdentifierId(new WorkExternalIdentifierId(oldExtId.getWorkExternalIdentifierId().getContent()));
         }
-        if(oldExtId.getWorkExternalIdentifierType() != null) {
+        if (oldExtId.getWorkExternalIdentifierType() != null) {
             newExtId.setWorkExternalIdentifierType(WorkExternalIdentifierType.fromValue(oldExtId.getWorkExternalIdentifierType().value()));
         }
         return newExtId;
