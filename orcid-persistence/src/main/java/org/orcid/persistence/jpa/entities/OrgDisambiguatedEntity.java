fa8b9f03683efcb63b44b7d5adb2aac5ea92d01b
Will Simpson <will@wjrs.co.uk>
2015-12-11 17:33:07 +0000

    Formatted.

--- orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/OrgDisambiguatedEntity.java
+++ orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/OrgDisambiguatedEntity.java
@@ -29,7 +29,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-    import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedNativeQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
