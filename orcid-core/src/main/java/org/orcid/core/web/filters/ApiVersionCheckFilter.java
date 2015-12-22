c778dfc43f7d0ead0b1eddf6dee4cc01ffc39309
Will Simpson <will@wjrs.co.uk>
2015-12-22 17:57:39 +0000

    Formatted.

--- orcid-core/src/main/java/org/orcid/core/web/filters/ApiVersionCheckFilter.java
+++ orcid-core/src/main/java/org/orcid/core/web/filters/ApiVersionCheckFilter.java
@@ -14,24 +14,24 @@ import com.sun.jersey.spi.container.ContainerRequestFilter;
 @Provider
 public class ApiVersionCheckFilter implements ContainerRequestFilter {
 
-	@Value("${org.orcid.core.v1_x.disabled}")
-	private boolean v1_1Disabled;
-	
-	private static final Pattern VERSION_PATTERN = Pattern.compile("v(\\d.*?)/");
-	
-	@Override
-	public ContainerRequest filter(ContainerRequest request) {
-		String path = request.getPath();
+    @Value("${org.orcid.core.v1_x.disabled}")
+    private boolean v1_1Disabled;
+
+    private static final Pattern VERSION_PATTERN = Pattern.compile("v(\\d.*?)/");
+
+    @Override
+    public ContainerRequest filter(ContainerRequest request) {
+        String path = request.getPath();
         Matcher matcher = VERSION_PATTERN.matcher(path);
         String version = null;
         if (matcher.lookingAt()) {
             version = matcher.group(1);
         }
-        
-        if(version != null && version.startsWith("1.1") && v1_1Disabled) {
-        	throw new OrcidBadRequestException("API Version 1.1 is not enabled");
-    	}
-        
+
+        if (version != null && version.startsWith("1.1") && v1_1Disabled) {
+            throw new OrcidBadRequestException("API Version 1.1 is not enabled");
+        }
+
         return request;
     }
 }
