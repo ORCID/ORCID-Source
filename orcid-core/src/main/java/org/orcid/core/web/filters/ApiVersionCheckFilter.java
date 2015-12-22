4da8143c6a5a63a0d420a921a77f5b823d6b77d5
Will Simpson <will@wjrs.co.uk>
2015-12-22 17:58:17 +0000

    Inject property into Jersey filter.

--- orcid-core/src/main/java/org/orcid/core/web/filters/ApiVersionCheckFilter.java
+++ orcid-core/src/main/java/org/orcid/core/web/filters/ApiVersionCheckFilter.java
@@ -6,16 +6,16 @@ import java.util.regex.Pattern;
 import javax.ws.rs.ext.Provider;
 
 import org.orcid.core.exception.OrcidBadRequestException;
-import org.springframework.beans.factory.annotation.Value;
 
+import com.sun.jersey.api.core.InjectParam;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 
 @Provider
 public class ApiVersionCheckFilter implements ContainerRequestFilter {
 
-    @Value("${org.orcid.core.v1_x.disabled}")
-    private boolean v1_1Disabled;
+    @InjectParam("v1xDisabled")
+    private Boolean v1xDisabled;
 
     private static final Pattern VERSION_PATTERN = Pattern.compile("v(\\d.*?)/");
 
@@ -28,7 +28,7 @@ public class ApiVersionCheckFilter implements ContainerRequestFilter {
             version = matcher.group(1);
         }
 
-        if (version != null && version.startsWith("1.1") && v1_1Disabled) {
+        if (version != null && version.startsWith("1.1") && v1xDisabled) {
             throw new OrcidBadRequestException("API Version 1.1 is not enabled");
         }
 
