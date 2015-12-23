480c2091bf94bdc33b3726ab965f5fe022f3d206
Shobhit Tyagi <nitw.shobhit@gmail.com>
2015-12-23 12:12:57 -0500

    Moved error message to the properties file

--- orcid-core/src/main/java/org/orcid/core/web/filters/ApiVersionCheckFilter.java
+++ orcid-core/src/main/java/org/orcid/core/web/filters/ApiVersionCheckFilter.java
@@ -3,9 +3,11 @@ package org.orcid.core.web.filters;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.annotation.Resource;
 import javax.ws.rs.ext.Provider;
 
 import org.orcid.core.exception.OrcidBadRequestException;
+import org.orcid.core.locale.LocaleManager;
 
 import com.sun.jersey.api.core.InjectParam;
 import com.sun.jersey.spi.container.ContainerRequest;
@@ -16,6 +18,10 @@ public class ApiVersionCheckFilter implements ContainerRequestFilter {
 
     @InjectParam("v1xDisabled")
     private Boolean v1xDisabled;
+    
+    @InjectParam("localeManager")
+    @Resource
+    private LocaleManager localeManager;
 
     private static final Pattern VERSION_PATTERN = Pattern.compile("v(\\d.*?)/");
 
@@ -29,7 +35,7 @@ public class ApiVersionCheckFilter implements ContainerRequestFilter {
         }
 
         if (version != null && version.startsWith("1.1") && v1xDisabled) {
-            throw new OrcidBadRequestException("API Version 1.1 is not enabled");
+            throw new OrcidBadRequestException(localeManager.resolveMessage("apiError.badrequest_version_disabled.exception"));
         }
 
         return request;
