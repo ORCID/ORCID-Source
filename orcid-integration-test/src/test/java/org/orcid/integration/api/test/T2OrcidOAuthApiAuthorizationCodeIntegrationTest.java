9daf186074460a500b860f87877f93a9856ec15f
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2016-01-12 17:26:49 -0600

    Fix integration tests to use https site

--- orcid-integration-test/src/test/java/org/orcid/integration/api/test/T2OrcidOAuthApiAuthorizationCodeIntegrationTest.java
+++ orcid-integration-test/src/test/java/org/orcid/integration/api/test/T2OrcidOAuthApiAuthorizationCodeIntegrationTest.java
@@ -157,7 +157,7 @@ public class T2OrcidOAuthApiAuthorizationCodeIntegrationTest extends DBUnitTest 
     @Resource
     OrcidSSOManager ssoManager;
 
-    @Value("${org.orcid.web.base.url:http://localhost:8080/orcid-web}")
+    @Value("${org.orcid.web.base.url:https://localhost:8443/orcid-web}")
     private String webBaseUrl;
 
     private String redirectUri;
