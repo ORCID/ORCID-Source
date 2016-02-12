f6c153ae184cda6c62106fcc0b5bbb88b78b5d3d
Shobhit Tyagi <nitw.shobhit@gmail.com>
2016-02-12 09:53:38 -0500

    correct formatting 2

--- orcid-pub-web/src/main/java/org/orcid/api/publicV2/server/delegator/impl/PublicV2ApiServiceVersionedDelegatorImpl.java
+++ orcid-pub-web/src/main/java/org/orcid/api/publicV2/server/delegator/impl/PublicV2ApiServiceVersionedDelegatorImpl.java
@@ -33,267 +33,233 @@ import org.orcid.persistence.jpa.entities.ProfileEntity;
 import org.orcid.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Value;
 
-public class PublicV2ApiServiceVersionedDelegatorImpl
-		implements
-		PublicV2ApiServiceDelegator<Object, Object, Object, Object, Object, Object, Object, Object, Object> {
+public class PublicV2ApiServiceVersionedDelegatorImpl implements PublicV2ApiServiceDelegator<Object, Object, Object, Object, Object, Object, Object, Object, Object> {
 
-	@Resource
-	private PublicV2ApiServiceDelegator<Object, Object, Object, Object, Object, Object, Object, Object, Object> publicV2ApiServiceDelegator;
+    @Resource
+    private PublicV2ApiServiceDelegator<Object, Object, Object, Object, Object, Object, Object, Object, Object> publicV2ApiServiceDelegator;
 
-	private String externalVersion;
+    private String externalVersion;
 
-	@Resource
-	private V2VersionConverterChain v2VersionConverterChain;
+    @Resource
+    private V2VersionConverterChain v2VersionConverterChain;
 
-	@Resource
-	private ProfileDao profileDao;
+    @Resource
+    private ProfileDao profileDao;
 
-	@Resource
-	private ProfileEntityManager profileEntityManager;
+    @Resource
+    private ProfileEntityManager profileEntityManager;
 
-	@Value("${org.orcid.core.baseUri}")
-	private String baseUrl;
+    @Value("${org.orcid.core.baseUri}")
+    private String baseUrl;
 
-	@Override
-	public Response viewStatusText() {
-		return downgradeResponse(publicV2ApiServiceDelegator.viewStatusText());
-	}
+    @Override
+    public Response viewStatusText() {
+        return downgradeResponse(publicV2ApiServiceDelegator.viewStatusText());
+    }
 
-	@Override
-	public Response viewActivities(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewActivities(orcid));
-	}
+    @Override
+    public Response viewActivities(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewActivities(orcid));
+    }
 
-	@Override
-	public Response viewWork(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewWork(orcid,
-				putCode));
-	}
+    @Override
+    public Response viewWork(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewWork(orcid, putCode));
+    }
 
-	@Override
-	public Response viewWorkCitation(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		// DO not downgrade non-orcid schema responses (this is citeproc);
-		return publicV2ApiServiceDelegator.viewWorkCitation(orcid, putCode);
-		// return
-		// downgradeResponse(publicV2ApiServiceDelegator.viewWorkCitation(orcid,
-		// putCode), orcid);
-	}
+    @Override
+    public Response viewWorkCitation(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        // DO not downgrade non-orcid schema responses (this is citeproc);
+        return publicV2ApiServiceDelegator.viewWorkCitation(orcid, putCode);
+        // return
+        // downgradeResponse(publicV2ApiServiceDelegator.viewWorkCitation(orcid,
+        // putCode), orcid);
+    }
 
-	@Override
-	public Response viewWorkSummary(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewWorkSummary(
-				orcid, putCode));
-	}
+    @Override
+    public Response viewWorkSummary(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewWorkSummary(orcid, putCode));
+    }
 
-	@Override
-	public Response viewFunding(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewFunding(orcid,
-				putCode));
-	}
+    @Override
+    public Response viewFunding(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewFunding(orcid, putCode));
+    }
 
-	@Override
-	public Response viewFundingSummary(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewFundingSummary(orcid, putCode));
-	}
+    @Override
+    public Response viewFundingSummary(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewFundingSummary(orcid, putCode));
+    }
 
-	@Override
-	public Response viewEducation(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewEducation(
-				orcid, putCode));
-	}
+    @Override
+    public Response viewEducation(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewEducation(orcid, putCode));
+    }
 
-	@Override
-	public Response viewEducationSummary(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewEducationSummary(orcid, putCode));
-	}
+    @Override
+    public Response viewEducationSummary(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewEducationSummary(orcid, putCode));
+    }
 
-	@Override
-	public Response viewEmployment(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewEmployment(
-				orcid, putCode));
-	}
+    @Override
+    public Response viewEmployment(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewEmployment(orcid, putCode));
+    }
 
-	@Override
-	public Response viewEmploymentSummary(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewEmploymentSummary(orcid, putCode));
-	}
+    @Override
+    public Response viewEmploymentSummary(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewEmploymentSummary(orcid, putCode));
+    }
 
-	@Override
-	public Response viewPeerReview(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewPeerReview(
-				orcid, putCode));
-	}
+    @Override
+    public Response viewPeerReview(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewPeerReview(orcid, putCode));
+    }
 
-	@Override
-	public Response viewPeerReviewSummary(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewPeerReviewSummary(orcid, putCode));
-	}
+    @Override
+    public Response viewPeerReviewSummary(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewPeerReviewSummary(orcid, putCode));
+    }
 
-	@Override
-	public Response viewGroupIdRecord(Long putCode) {
-		return publicV2ApiServiceDelegator.viewGroupIdRecord(putCode);
-	}
+    @Override
+    public Response viewGroupIdRecord(Long putCode) {
+        return publicV2ApiServiceDelegator.viewGroupIdRecord(putCode);
+    }
 
-	@Override
-	public Response viewGroupIdRecords(String pageSize, String pageNum) {
-		return publicV2ApiServiceDelegator
-				.viewGroupIdRecords(pageSize, pageNum);
-	}
+    @Override
+    public Response viewGroupIdRecords(String pageSize, String pageNum) {
+        return publicV2ApiServiceDelegator.viewGroupIdRecords(pageSize, pageNum);
+    }
 
-	@Override
-	public Response viewResearcherUrls(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewResearcherUrls(orcid));
-	}
+    @Override
+    public Response viewResearcherUrls(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewResearcherUrls(orcid));
+    }
 
-	@Override
-	public Response viewResearcherUrl(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewResearcherUrl(
-				orcid, putCode));
-	}
+    @Override
+    public Response viewResearcherUrl(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewResearcherUrl(orcid, putCode));
+    }
 
-	@Override
-	public Response viewEmails(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewEmails(orcid));
-	}
+    @Override
+    public Response viewEmails(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewEmails(orcid));
+    }
 
-	@Override
-	public Response viewOtherNames(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewOtherNames(orcid));
-	}
+    @Override
+    public Response viewOtherNames(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewOtherNames(orcid));
+    }
 
-	@Override
-	public Response viewOtherName(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewOtherName(
-				orcid, putCode));
-	}
+    @Override
+    public Response viewOtherName(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewOtherName(orcid, putCode));
+    }
 
-	@Override
-	public Response viewPersonalDetails(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewPersonalDetails(orcid));
-	}
+    @Override
+    public Response viewPersonalDetails(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewPersonalDetails(orcid));
+    }
 
-	@Override
-	public Response viewExternalIdentifiers(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewExternalIdentifiers(orcid));
-	}
+    @Override
+    public Response viewExternalIdentifiers(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewExternalIdentifiers(orcid));
+    }
 
-	@Override
-	public Response viewExternalIdentifier(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewExternalIdentifier(orcid, putCode));
-	}
+    @Override
+    public Response viewExternalIdentifier(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewExternalIdentifier(orcid, putCode));
+    }
 
-	@Override
-	public Response viewBiography(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewBiography(orcid));
-	}
+    @Override
+    public Response viewBiography(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewBiography(orcid));
+    }
 
-	@Override
-	public Response viewKeywords(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewKeywords(orcid));
-	}
+    @Override
+    public Response viewKeywords(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewKeywords(orcid));
+    }
 
-	@Override
-	public Response viewKeyword(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewKeyword(orcid,
-				putCode));
-	}
+    @Override
+    public Response viewKeyword(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewKeyword(orcid, putCode));
+    }
 
-	@Override
-	public Response viewAddresses(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator
-				.viewAddresses(orcid));
-	}
+    @Override
+    public Response viewAddresses(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewAddresses(orcid));
+    }
 
-	@Override
-	public Response viewAddress(String orcid, Long putCode) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewAddress(orcid,
-				putCode));
-	}
+    @Override
+    public Response viewAddress(String orcid, Long putCode) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewAddress(orcid, putCode));
+    }
 
-	@Override
-	public Response viewPerson(String orcid) {
-		checkProfileStatus(orcid);
-		return downgradeResponse(publicV2ApiServiceDelegator.viewPerson(orcid));
-	}
+    @Override
+    public Response viewPerson(String orcid) {
+        checkProfileStatus(orcid);
+        return downgradeResponse(publicV2ApiServiceDelegator.viewPerson(orcid));
+    }
 
-	private Response downgradeResponse(Response response) {
-		Object entity = response.getEntity();
-		V2Convertible result = null;
-		if (entity != null) {
-			result = v2VersionConverterChain.downgrade(new V2Convertible(
-					entity, PublicV2ApiServiceDelegator.LATEST_V2_VERSION),
-					externalVersion);
-		}
-		return Response.fromResponse(response)
-				.entity(result.getObjectToConvert()).build();
-	}
+    private Response downgradeResponse(Response response) {
+        Object entity = response.getEntity();
+        V2Convertible result = null;
+        if (entity != null) {
+            result = v2VersionConverterChain.downgrade(new V2Convertible(entity, PublicV2ApiServiceDelegator.LATEST_V2_VERSION), externalVersion);
+        }
+        return Response.fromResponse(response).entity(result.getObjectToConvert()).build();
+    }
 
-	private void checkProfileStatus(String orcid) {
-		ProfileEntity entity = profileEntityManager.findByOrcid(orcid);
-		if (profileDao.isProfileDeprecated(orcid)) {
-			StringBuffer primary = new StringBuffer(baseUrl).append("/")
-					.append(entity.getPrimaryRecord().getId());
-			Map<String, String> params = new HashMap<String, String>();
-			params.put(OrcidDeprecatedException.ORCID, primary.toString());
-			if (entity.getDeprecatedDate() != null) {
-				XMLGregorianCalendar calendar = DateUtils
-						.convertToXMLGregorianCalendar(entity
-								.getDeprecatedDate());
-				params.put(OrcidDeprecatedException.DEPRECATED_DATE,
-						calendar.toString());
-			}
-			throw new OrcidDeprecatedException(params);
-		}
-	}
+    private void checkProfileStatus(String orcid) {
+        ProfileEntity entity = profileEntityManager.findByOrcid(orcid);
+        if (profileDao.isProfileDeprecated(orcid)) {
+            StringBuffer primary = new StringBuffer(baseUrl).append("/").append(entity.getPrimaryRecord().getId());
+            Map<String, String> params = new HashMap<String, String>();
+            params.put(OrcidDeprecatedException.ORCID, primary.toString());
+            if (entity.getDeprecatedDate() != null) {
+                XMLGregorianCalendar calendar = DateUtils.convertToXMLGregorianCalendar(entity.getDeprecatedDate());
+                params.put(OrcidDeprecatedException.DEPRECATED_DATE, calendar.toString());
+            }
+            throw new OrcidDeprecatedException(params);
+        }
+    }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void setMemberV2ApiServiceDelegator(
-			PublicV2ApiServiceDelegator memberV2ApiServiceDelegator) {
-		this.publicV2ApiServiceDelegator = memberV2ApiServiceDelegator;
-	}
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void setMemberV2ApiServiceDelegator(PublicV2ApiServiceDelegator memberV2ApiServiceDelegator) {
+        this.publicV2ApiServiceDelegator = memberV2ApiServiceDelegator;
+    }
 
-	public String getExternalVersion() {
-		return externalVersion;
-	}
+    public String getExternalVersion() {
+        return externalVersion;
+    }
 
-	public void setExternalVersion(String externalVersion) {
-		this.externalVersion = externalVersion;
-	}
+    public void setExternalVersion(String externalVersion) {
+        this.externalVersion = externalVersion;
+    }
 }
