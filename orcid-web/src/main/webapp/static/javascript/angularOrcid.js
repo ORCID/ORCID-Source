ae8bf31df0cd6ce0b3435072d70b080de1047017
Shobhit Tyagi <nitw.shobhit@gmail.com>
2016-02-15 13:54:33 -0500

    Deactivate batch profiles

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -7240,6 +7240,35 @@ orcidNgModule.controller('profileDeactivationAndReactivationCtrl',['$scope', '$c
     };
 }]);
 
+orcidNgModule.controller('DeactivateProfileCtrl', ['$scope', function ($scope) {
+	$scope.orcidsToDeactivate = "";
+	$scope.showSection = false;
+
+    $scope.toggleSection = function(){
+        $scope.showSection = !$scope.showSection;
+        $('#deactivation_modal').toggle();
+    };
+
+	
+	$scope.deactivateOrcids = function() {
+		$.ajax({
+            url: getBaseUri()+'/admin-actions/deactivate-profiles.json',
+            type: 'POST',
+            dataType: 'json',
+            data: $scope.orcidsToDeactivate,
+            contentType: 'application/json;charset=UTF-8',
+            success: function(data){
+            	$scope.$apply(function(){
+            		$scope.result = data;
+                });
+            }
+        }).fail(function(error) {
+            // something bad is happening!
+            console.log("Error re-sending claim emails");
+        });
+	}
+}]);
+
 orcidNgModule.controller('profileDeprecationCtrl',['$scope','$compile', function profileDeprecationCtrl($scope,$compile){
     $scope.deprecated_verified = false;
     $scope.primary_verified = false;
