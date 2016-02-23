f66ee065f6b65a14627af5a0c32de1594e8ad3ae
Shobhit Tyagi <nitw.shobhit@gmail.com>
2016-02-22 19:52:20 -0500

    Removing bad changes

--- orcid-core/src/main/java/org/orcid/core/manager/impl/ClientDetailsManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/ClientDetailsManagerImpl.java
@@ -201,8 +201,6 @@ public class ClientDetailsManagerImpl implements ClientDetailsManager {
      */
     @Override
     public ClientDetailsEntity createClientDetails(ClientDetailsEntity clientDetailsEntity) {
-        clientDetailsEntity.getClientRegisteredRedirectUris().iterator().next().setUriActType(null);
-        clientDetailsEntity.getClientRegisteredRedirectUris().iterator().next().setUriGeoArea(null);
         persist(clientDetailsEntity);
         return findByClientId(clientDetailsEntity.getId());
     }
