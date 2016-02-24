be55b1d7b619c6fcca6859ddc43db81d909c67df
rcpeters <info@rcpeters.com>
2016-02-24 15:20:49 -0800

    my coding suggestion for checking for https connections

--- orcid-core/src/main/java/org/orcid/core/manager/impl/OrcidUrlManager.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/OrcidUrlManager.java
@@ -23,10 +23,14 @@ import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 public class OrcidUrlManager {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrcidUrlManager.class);
 
     static Pattern fileNamePattern = Pattern.compile("https{0,1}:\\/\\/[^\\/]*(.*){0,1}");
 
@@ -180,7 +184,9 @@ public class OrcidUrlManager {
     public static String getscheme(HttpServletRequest request) {
         String forwardedProto = request.getHeader("X-Forwarded-Proto");
         String scheme = forwardedProto != null ? forwardedProto : request.getScheme();
-        return scheme == null ? null : scheme.toLowerCase();
+        if (scheme == null) 
+            LOGGER.error("WHAT THE HELL is going on? Request scheme is null.", request);
+        return scheme.toLowerCase();
     }
     
     public static boolean isSecure(HttpServletRequest request) {
