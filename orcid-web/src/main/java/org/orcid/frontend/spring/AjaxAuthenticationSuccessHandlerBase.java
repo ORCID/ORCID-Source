0cf2cd6e579ee28c15986a5a867fd8b4f1382276
Will Simpson <will@wjrs.co.uk>
2016-03-10 17:34:58 +0000

    Formatted.

--- orcid-web/src/main/java/org/orcid/frontend/spring/AjaxAuthenticationSuccessHandlerBase.java
+++ orcid-web/src/main/java/org/orcid/frontend/spring/AjaxAuthenticationSuccessHandlerBase.java
@@ -39,48 +39,48 @@ public class AjaxAuthenticationSuccessHandlerBase extends SimpleUrlAuthenticatio
 
     @Resource
     private OrcidUrlManager orcidUrlManager;
-    
+
     @Resource
     protected ProfileDao profileDao;
-    
+
     @Resource
     protected InternalSSOManager internalSSOManager;
-    
+
     @Resource
     protected SourceManager sourceManager;
-    
+
     @Resource
     protected OrcidProfileManager orcidProfileManager;
-    
+
     @Resource
     protected UserConnectionDao userConnectionDao;
-    
+
     protected String getTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-    	String targetUrl = determineFullTargetUrlFromSavedRequest(request, response);
+        String targetUrl = determineFullTargetUrlFromSavedRequest(request, response);
         if (authentication != null) {
             String orcidId = authentication.getName();
             checkLocale(request, response, orcidId);
-            if(internalSSOManager.enableCookie()) {
+            if (internalSSOManager.enableCookie()) {
                 internalSSOManager.writeCookie(orcidId, request, response);
-            }            
+            }
             profileDao.updateIpAddress(orcidId, OrcidRequestUtil.getIpAddress(request));
         }
         if (targetUrl == null) {
             targetUrl = determineFullTargetUrl(request, response);
         }
-        
+
         return targetUrl;
     }
-    
+
     protected OrcidProfile getRealProfile() {
         String realOrcid = getRealUserOrcid();
         return realOrcid == null ? null : orcidProfileManager.retrieveOrcidProfile(realOrcid);
     }
-    
+
     private String getRealUserOrcid() {
         return sourceManager.retrieveRealUserOrcid();
     }
-    
+
     // new method - persist which ever local they logged in with
     private void checkLocale(HttpServletRequest request, HttpServletResponse response, String orcidId) {
         Locale lastKnownLocale = profileDao.retrieveLocale(orcidId);
@@ -101,7 +101,7 @@ public class AjaxAuthenticationSuccessHandlerBase extends SimpleUrlAuthenticatio
             }
         }
     }
-    
+
     private String determineFullTargetUrlFromSavedRequest(HttpServletRequest request, HttpServletResponse response) {
         SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
         String url = null;
@@ -111,10 +111,10 @@ public class AjaxAuthenticationSuccessHandlerBase extends SimpleUrlAuthenticatio
 
         // this next section is a hack, we should refactor to us
         // some of kind of configuration file
-        String contextPath =  request.getContextPath();
+        String contextPath = request.getContextPath();
         if (url != null) {
-        	if (orcidUrlManager.getBasePath().equals("/") && !contextPath.equals("/"))
-        		url = url.replaceFirst(contextPath.replace("/", "\\/"), "");
+            if (orcidUrlManager.getBasePath().equals("/") && !contextPath.equals("/"))
+                url = url.replaceFirst(contextPath.replace("/", "\\/"), "");
             if (url.contains("/signin/auth"))
                 url = null;
             else if (url.contains(".json"))
