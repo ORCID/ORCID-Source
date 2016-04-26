f0e4a21b237e66731c58681298e08a4ab242b5f2
Tom Demeranville <tom@demeranville.com>
2016-04-26 14:59:28 +0100

    added IdentifierType persistence layer and tests

---
+++ orcid-persistence/src/main/java/org/orcid/persistence/dao/impl/IdentifierTypeDaoImpl.java
@@ -0,0 +1,71 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.persistence.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.orcid.persistence.dao.IdentifierTypeDao;
+import org.orcid.persistence.jpa.entities.IdentifierTypeEntity;
+import org.orcid.persistence.jpa.entities.WorkEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+public class IdentifierTypeDaoImpl extends GenericDaoImpl<IdentifierTypeEntity, Long> implements IdentifierTypeDao{
+
+    public IdentifierTypeDaoImpl() {
+        super(IdentifierTypeEntity.class);
+    }
+
+    @Override
+    @Transactional
+    public IdentifierTypeEntity addIdentifierType(IdentifierTypeEntity identifierType) {
+        this.persist(identifierType);
+        this.flush();
+        return identifierType;
+    }
+
+    /** Updates id_resolution_prefix, id_validation_regex, id_deprecated
+     * 
+     * Ignores name (cannot be modified)
+     * 
+     */
+    @Override
+    @Transactional
+    public IdentifierTypeEntity updateIdentifierType(IdentifierTypeEntity identifierType) {
+        IdentifierTypeEntity id = this.find(identifierType.getId());
+        id.setResolutionPrefix(identifierType.getResolutionPrefix());
+        id.setValidationRegex(identifierType.getValidationRegex());
+        id.setIsDeprecated(identifierType.getIsDeprecated());
+        id = this.merge(id);
+        return id;
+    }
+
+    @Override
+    public IdentifierTypeEntity getEntities(String idName) {
+        TypedQuery<IdentifierTypeEntity> query = entityManager.createQuery("FROM IdentifierTypeEntity WHERE id_name = :idName", IdentifierTypeEntity.class);        
+        query.setParameter("idName", idName);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public List<IdentifierTypeEntity> getEntities() {
+        TypedQuery<IdentifierTypeEntity> query = entityManager.createQuery("FROM IdentifierTypeEntity order by id_name", IdentifierTypeEntity.class);        
+        return query.getResultList();
+    }
+
+}
---
+++ orcid-persistence/src/test/java/org/orcid/persistence/dao/IdentifierTypeDaoTest.java
@@ -0,0 +1,117 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.persistence.dao;
+
+import javax.annotation.Resource;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.orcid.persistence.jpa.entities.ClientDetailsEntity;
+import org.orcid.persistence.jpa.entities.IdentifierTypeEntity;
+import org.orcid.test.DBUnitTest;
+import org.orcid.test.OrcidJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@RunWith(OrcidJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:orcid-persistence-context.xml" })
+public class IdentifierTypeDaoTest extends DBUnitTest{
+
+    @Resource
+    private IdentifierTypeDao idTypeDao;
+    
+    @Resource
+    private ClientDetailsDao clientDetailsDao;
+
+    
+    @BeforeClass
+    public static void initDBUnitData() throws Exception {
+        initDBUnitData(Arrays.asList("/data/SecurityQuestionEntityData.xml", "/data/SubjectEntityData.xml", "/data/SourceClientDetailsEntityData.xml",
+                "/data/ProfileEntityData.xml", "/data/RecordNameEntityData.xml","/data/ClientDetailsEntityData.xml"));
+    }
+
+    @AfterClass
+    public static void removeDBUnitData() throws Exception {
+        
+    }
+
+    @Test
+    public void testAddID(){
+        IdentifierTypeEntity e1 = new IdentifierTypeEntity();
+        e1.setName("name");
+        e1.setResolutionPrefix("http://whatever.com/{id}");
+        e1.setValidationRegex("blah");        
+        e1.setSourceClient(clientDetailsDao.findByClientId("APP-6666666666666666",new Date().getTime()));  
+        System.out.println(e1.getSourceClient().getClientId());
+        IdentifierTypeEntity e2 = idTypeDao.addIdentifierType(e1);
+        assertNotNull(e2.getId());
+    }
+    
+    @Test
+    public void testFetchID(){
+        IdentifierTypeEntity e1 = idTypeDao.getEntities("name");
+        assertEquals("name",e1.getName());
+        assertEquals("http://whatever.com/{id}",e1.getResolutionPrefix());
+        assertEquals("blah",e1.getValidationRegex());
+        assertFalse(e1.getIsDeprecated());
+        assertNotNull(e1.getId());
+        assertEquals("APP-6666666666666666",e1.getSourceClient().getClientId());
+        assertTrue((new Date()).after(e1.getDateCreated()));
+        assertTrue((new Date()).after(e1.getLastModified()));
+        assertEquals("APP-6666666666666666",e1.getSourceClient().getId());
+    }
+
+    @Test
+    public void testUpdateID(){
+        IdentifierTypeEntity e1 = idTypeDao.getEntities("name");
+        Date last = e1.getLastModified();
+        e1.setResolutionPrefix("http://whatever2.com/{id}");
+        e1.setValidationRegex("blah2");
+        e1.setIsDeprecated(true);
+        e1 = idTypeDao.updateIdentifierType(e1);
+        assertEquals("name",e1.getName());
+        assertEquals("http://whatever2.com/{id}",e1.getResolutionPrefix());
+        assertEquals("blah2",e1.getValidationRegex());
+        assertTrue(e1.getIsDeprecated());
+        assertNotNull(e1.getId());
+        assertEquals("APP-6666666666666666",e1.getSourceClient().getClientId());
+        assertTrue((new Date()).after(e1.getDateCreated()));
+        assertTrue(last.before(e1.getLastModified()));        
+    }
+    
+    @Test
+    public void testFetchIDList(){
+        IdentifierTypeEntity e1 = new IdentifierTypeEntity();
+        e1.setName("aaa");
+        ClientDetailsEntity sourceClient = new ClientDetailsEntity();
+        sourceClient.setId("APP-6666666666666666");
+        e1.setSourceClient(sourceClient);
+        idTypeDao.addIdentifierType(e1);
+        List<IdentifierTypeEntity> list = idTypeDao.getEntities();
+        assertEquals(2, list.size());
+        assertEquals("aaa",list.get(0).getName());        
+        assertEquals("name",list.get(1).getName());        
+    }
+    
+}
