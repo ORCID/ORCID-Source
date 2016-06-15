6d82e19193683446b25d0903c267ba1bdb35cb37
rcpeters <info@rcpeters.com>
2016-06-14 17:24:33 -0700

    don't call retrieveLastModifiedDate in the DAO

--- orcid-core/src/main/java/org/orcid/core/manager/impl/ProfileEntityManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/ProfileEntityManagerImpl.java
@@ -609,17 +609,16 @@ public class ProfileEntityManagerImpl implements ProfileEntityManager {
         if (sra != null)
             lastMod = (Date)sra.getAttribute(ProfileLastModifiedAspect.REQUEST_PROFILE_LAST_MODIFIED, ServletRequestAttributes.SCOPE_REQUEST);
         if (lastMod == null) {
-            return lastMod = updateLastModifed(orcid);
+            lastMod = profileDao.retrieveLastModifiedDate(orcid);
+            sra.setAttribute(ProfileLastModifiedAspect.REQUEST_PROFILE_LAST_MODIFIED, lastMod,ServletRequestAttributes.SCOPE_REQUEST);
         }
         return lastMod;
     }
 
     @Override
     public Date updateLastModifed(String orcid) {
-        Date lastMod = null;
-        if (lastMod == null) {
-            lastMod = profileDao.updateLastModifiedDateAndIndexingStatus(orcid);
-        }
+        profileDao.updateLastModifiedDateAndIndexingStatus(orcid);
+        Date lastMod  = profileDao.retrieveLastModifiedDate(orcid);
         ServletRequestAttributes sra = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
         if (sra != null)
             sra.setAttribute(ProfileLastModifiedAspect.REQUEST_PROFILE_LAST_MODIFIED, lastMod,ServletRequestAttributes.SCOPE_REQUEST);
