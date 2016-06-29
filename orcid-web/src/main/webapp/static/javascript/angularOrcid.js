44930f986ce726f2c58ee88e5ba208a5f901ab57
Will Simpson <will@wjrs.co.uk>
2016-06-29 16:48:39 +0100

    Only load notification on inbox page.

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -1600,7 +1600,6 @@ orcidNgModule.factory("notificationsSrvc", ['$rootScope', '$q', function ($rootS
             
         }
     };
-    serv.getNotifications();    
     return serv;
 }]);
 
@@ -7498,6 +7497,7 @@ orcidNgModule.controller('NotificationsCtrl',['$scope', '$compile', 'notificatio
         }
     });
 
+    notificationsSrvc.getNotifications();
         
 }]);
 
