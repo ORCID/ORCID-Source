a74b2085d13b7e9bc011fc8173b76d854f2cf9c8
Will Simpson <will@wjrs.co.uk>
2016-07-25 14:22:31 +0100

    Added example Idp XML with multiple languages.

--- orcid-core/src/test/java/org/orcid/core/manager/IdentityProviderManagerTest.java
+++ orcid-core/src/test/java/org/orcid/core/manager/IdentityProviderManagerTest.java
@@ -25,6 +25,22 @@ public class IdentityProviderManagerTest {
     private IdentityProviderManager identityProviderManager = new IdentityProviderManagerImpl();
 
     @Test
+    public void testCreateEntityFromXml() throws SAXException, IOException, ParserConfigurationException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        dbFactory.setNamespaceAware(true);
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(getClass().getResourceAsStream("example_idp.xml"));
+        Element idpElement = doc.getDocumentElement();
+        IdentityProviderEntity result = identityProviderManager.createEntityFromXml(idpElement);
+        assertNotNull(result);
+        assertEquals("https://idp.example.ch/idp/shibboleth", result.getProviderid());
+        assertEquals("Universität Example Display", result.getDisplayName());
+        assertEquals("support@example.ch", result.getSupportEmail());
+        assertNull(result.getAdminEmail());
+        assertEquals("technical@example.ch", result.getTechEmail());
+    }
+
+    @Test
     public void testCreateEntityFromXmlWithLegacyNames() throws SAXException, IOException, ParserConfigurationException {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         dbFactory.setNamespaceAware(true);
@@ -34,7 +50,8 @@ public class IdentityProviderManagerTest {
         IdentityProviderEntity result = identityProviderManager.createEntityFromXml(idpElement);
         assertNotNull(result);
         assertEquals("https://registry.shibboleth.example.ac.uk/idp", result.getProviderid());
-        //assertEquals("University of Example Display", result.getDisplayName());
+        // assertEquals("University of Example Display",
+        // result.getDisplayName());
         assertEquals("help@it.example.ac.uk", result.getSupportEmail());
         assertEquals("admin@it.example.ac.uk", result.getAdminEmail());
         assertEquals("sysdev@it.example.ac.uk", result.getTechEmail());
