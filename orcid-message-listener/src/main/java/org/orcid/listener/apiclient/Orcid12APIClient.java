ac7d4549d617fe51be7c0aec9182f00c1c4f2abf
Tom <tom@Toms-MacBook-Pro-2.local>
2016-07-27 11:57:30 +0100

    created simple jersey client for 1.2 API

--- orcid-message-listener/src/main/java/org/orcid/listener/apiclient/Orcid12APIClient.java
+++ orcid-message-listener/src/main/java/org/orcid/listener/apiclient/Orcid12APIClient.java
@@ -1,5 +1,7 @@
 package org.orcid.listener.apiclient;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -9,31 +11,52 @@ import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.orcid.jaxb.model.message.LastModifiedDate;
 import org.orcid.jaxb.model.message.OrcidHistory;
+import org.orcid.jaxb.model.message.OrcidMessage;
 import org.orcid.jaxb.model.message.OrcidProfile;
+import org.orcid.listener.LastUpdatedListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 @Component
 public class Orcid12APIClient {
 
+    Logger LOG = LoggerFactory.getLogger(LastUpdatedListener.class);
+    protected final Client jerseyClient;    
+    protected final URI baseUri;
+    
+    @Autowired
+    public Orcid12APIClient(@Value("${org.orcid.message-lisener.api12BaseURI}") String baseUri) throws URISyntaxException{
+        LOG.info("Creating Orcid12APIClient with baseUri = "+ baseUri);
+        ClientConfig config = new DefaultClientConfig();
+        config.getClasses().add(JacksonJaxbJsonProvider.class);
+        jerseyClient = Client.create(config);
+        this.baseUri = new URI(baseUri);
+    }
+    
     /** Fetches the profile from the ORCID public API
+     *  VERY basic implementation
      * 
      * @param orcid
      * @return
      */
     public OrcidProfile fetchPublicProfile(String orcid){
-        OrcidProfile p = new OrcidProfile();
-        p.setOrcid(orcid);
-        p.setOrcidHistory(new OrcidHistory());
-        p.getOrcidHistory().setLastModifiedDate(new LastModifiedDate());
-        GregorianCalendar c = new GregorianCalendar();
-        c.setTime(new Date("1/1/1999"));
-        XMLGregorianCalendar date2;
-        try {
-            date2 = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
-        } catch (DatatypeConfigurationException e) {
-            throw new RuntimeException(e);
+        WebResource webResource = jerseyClient.resource(baseUri);
+        ClientResponse response = webResource.path(orcid+"/orcid-profile").accept("application/xml").get(ClientResponse.class);
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
         }
-        p.getOrcidHistory().getLastModifiedDate().setValue(date2);
-        return p;
+        OrcidMessage output = response.getEntity(OrcidMessage.class);
+        LOG.info(output.toString());
+        return output.getOrcidProfile();
     }
 }
