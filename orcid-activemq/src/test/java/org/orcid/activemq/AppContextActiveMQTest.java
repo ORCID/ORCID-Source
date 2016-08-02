65afd66be5c27d09332702de255ee2f68e04404a
Tom <tom@Toms-MacBook-Pro-2.local>
2016-08-02 12:51:53 +0100

    added unit test for activemq broker

---
+++ orcid-activemq/src/test/java/org/orcid/activemq/AppContextActiveMQTest.java
@@ -0,0 +1,42 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.activemq;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.apache.activemq.xbean.BrokerFactoryBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.orcid.test.OrcidJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(OrcidJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:orcid-activemq-web-context.xml" })
+public class AppContextActiveMQTest {
+
+    @Resource
+    private BrokerFactoryBean activeMQ;
+    
+    @Test
+    public void testServerRunning() throws Exception {
+        assertNotNull(activeMQ);
+        assertTrue(activeMQ.getBroker().isStarted());
+        assertNull(activeMQ.getBroker().getStartException());
+    }
+}
