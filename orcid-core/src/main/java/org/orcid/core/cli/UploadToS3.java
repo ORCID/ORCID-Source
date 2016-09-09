d521faffc7924ec51258714bbcb01bf11e2f3ef8
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2016-09-09 16:23:02 -0600

    Working on a CLI to upload records to the S3 buckets

---
+++ orcid-core/src/main/java/org/orcid/core/cli/UploadToS3.java
@@ -0,0 +1,23 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.core.cli;
+
+public class UploadToS3 {
+    //# 1: get a batch of orcid's that are not in the amazon_s3_migration_process table
+    //# 2: upload them to S3
+    //# 3: update the amazon_s3_migration_process table
+}
