9182986b47b17ed49bde51d1adc96c99cf171101
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2016-10-25 15:14:49 -0600

    Remove unused method

--- orcid-message-listener/src/main/java/org/orcid/listener/clients/Orcid20APIClient.java
+++ orcid-message-listener/src/main/java/org/orcid/listener/clients/Orcid20APIClient.java
@@ -56,10 +56,6 @@ public class Orcid20APIClient {
         this.baseUri = new URI(baseUri);
         this.host = hostHeaderOverride;
     }
-
-    private Client createJerseyClient() {
-     return null;   
-    }
     
     /**
      * Fetches the profile from the ORCID public API v1.2
