fbb10e87098ad6f9e35bdd8f98f7e175f8528f9e
Will Simpson <will@wjrs.co.uk>
2016-10-26 17:54:00 +0100

    Changes missed in last commit.

--- orcid-message-listener/src/main/java/org/orcid/listener/jersey/OrcidJerseyClientHandler.java
+++ orcid-message-listener/src/main/java/org/orcid/listener/jersey/OrcidJerseyClientHandler.java
@@ -16,27 +16,37 @@
  */
 package org.orcid.listener.jersey;
 
+import java.util.Map;
+import java.util.Set;
+
+import org.orcid.util.DevJerseyClientConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class OrcidJerseyClientHandler {
-    /**
-     * Create a default client with client configuration.
-     *
-     * @param cc the client configuration.
-     * @return a default client.
-     */
-    public static Client create(ClientConfig cc, boolean isLocalHost) {
-        Client client = null;
-        if(isLocalHost) {
-            client = Client.create(cc);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrcidJerseyClientHandler.class);
+
+    public static Client create(boolean isDevelopmentMode, Map<String, Object> properties) {
+        ClientConfig config = null;
+        if (isDevelopmentMode) {
+            // DANGER!!! Trust all certs
+            LOGGER.info("TRUSTING ALL SSL CERTS IN DEV MODE!!!");
+            config = new DevJerseyClientConfig();
         } else {
-            ClientConfig config = new DefaultClientConfig();
-            config.getClasses().add(JacksonJaxbJsonProvider.class);
-            client = Client.create(config);
+            config = new DefaultClientConfig();
         }
-        return client;
-    }    
+        Set<String> keyset = properties.keySet();
+        for (String key : keyset) {
+            config.getProperties().put(key, properties.get(key));
+        }
+        config.getClasses().add(JacksonJaxbJsonProvider.class);
+        return Client.create(config);
+    }
+
 }
