14f812e24b5289029bfcbe28ea8a57f1f44d5671
Liz Krznarich <ejkrznarich@gmail.com>
2016-11-23 08:24:52 -0600

    remove code that strips /email/read-limited scope

--- orcid-web/src/main/webapp/static/javascript/script.js
+++ orcid-web/src/main/webapp/static/javascript/script.js
@@ -770,20 +770,6 @@ $(function() {
     
 });
 
-//request private email oauth
-$("#allowEmailAccess").change(function() {
-    var url = window.location.href;
-    if($(this).not(":checked")) {
-        if(url.indexOf('%20/email/read-private') > -1){
-            search = '%20/email/read-private';
-        }else if(url.indexOf('/email/read-private') > -1){
-            search = '/email/read-private';
-        }
-        url = url.replace(search,'');
-        location.replace(url);
-     }
-});
-
 /* START: Bibjson to work AjaxForm */
 /*
  * { "errors":[], "publicationDate": {
