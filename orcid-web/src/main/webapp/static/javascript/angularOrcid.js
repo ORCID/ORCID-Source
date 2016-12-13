202f8ba93cad153b0079fc2286ffbe8ac88da65e
Manuel Calvo <m.calvo@ost.orcid.org>
2016-12-13 17:03:46 -0600

    [3132] Directive Modal Unverified update: Fixing bug with email sent function. Updating strings

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -11737,15 +11737,18 @@ orcidNgModule.directive(
         '$timeout',
         function( $compile, $rootScope, $timeout ) {
 
+            var emailVerifiedObj = null;
+
             var closeModal = function(){
                 $.colorbox.remove();
                 $('modal-email-un-verified').html('<div id="modal-email-unverified-container"></div>');
             }
 
             var openModal = function( scope, data ){
+                emailVerifiedObj = data;
                 $.colorbox(
                     {
-                        html : $compile($('#modal-email-unverified-container').html('<div class="lightbox-container" id="modal-email-unverified"><div class="row"><div class="col-md-12 col-xs-12 col-sm-12"><h4>' + om.get("orcid.frontend.workspace.your_primary_email") + '</h4><p>' + om.get("orcid.frontend.workspace.ensure_future_access") + '</p><p>' + om.get("orcid.frontend.workspace.ensure_future_access2") + '<br /><a href="mailto:' + data.emails[0].value + '" target="_blank">' + data.emails[0].value + '</a></p><p>' + om.get("orcid.frontend.workspace.ensure_future_access3") + ' <a target="_blank" href="mailto:' + om.get("orcid.frontend.link.email.support") + '">' + om.get("orcid.frontend.link.email.support") + '</a>.</p><button class="btn btn-primary" id="modal-close" ng-click="verifyEmail(' + data.emails[0].value + ')">' + om.get("orcid.frontend.workspace.send_verification") + '</button><a class="cancel-option inner-row" ng-click="closeColorBox()">' + om.get("orcid.frontend.freemarker.btncancel") + '</a></div></div></div>'))(scope),
+                        html : $compile($('#modal-email-unverified-container').html('<div class="lightbox-container" id="modal-email-unverified"><div class="row"><div class="col-md-12 col-xs-12 col-sm-12"><h4>' + om.get("orcid.frontend.workspace.your_primary_email") + '</h4><p>' + om.get("orcid.frontend.workspace.ensure_future_access") + '</p><p>' + om.get("orcid.frontend.workspace.ensure_future_access2") + '<br /><a href="mailto:' + data.emails[0].value + '" target="_blank">' + data.emails[0].value + '</a></p><p>' + om.get("orcid.frontend.workspace.ensure_future_access3") + ' <a target="_blank" href="' + om.get("orcid.frontend.link.url.knowledgebase") + '">' + om.get("orcid.frontend.workspace.ensure_future_access4") + '</a> ' + om.get("orcid.frontend.workspace.ensure_future_access5") + ' <a target="_blank" href="mailto:' + om.get("orcid.frontend.link.email.support") + '">' + om.get("orcid.frontend.link.email.support") + '</a>.</p><button class="btn btn-primary" id="modal-close" ng-click="verifyEmail()">' + om.get("orcid.frontend.workspace.send_verification") + '</button><a class="cancel-option inner-row" ng-click="closeColorBox()">' + om.get("orcid.frontend.freemarker.btncancel") + '</a></div></div></div>'))(scope),
                         escKey: true,
                         overlayClose: true,
                         transition: 'fade',
@@ -11756,12 +11759,12 @@ orcidNgModule.directive(
                 $.colorbox.resize({height:"250px", width:"500px"});
             }
 
-            var verifyEmail = function( email ){
+            var verifyEmail = function( scope ){
                 var colorboxHtml = null;
                 $.ajax({
                     url: getBaseUri() + '/account/verifyEmail.json',
                     type: 'get',
-                    data:  { "email": email },
+                    data:  { "email": emailVerifiedObj.emails[0].value },
                     contentType: 'application/json;charset=UTF-8',
                     dataType: 'json',
                     success: function(data) {
