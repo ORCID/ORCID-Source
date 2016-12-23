60e61def8c859644d3ea8f68b9af03513a5e3d82
Manuel <ak.abaddon@gmail.com>
2016-12-23 15:58:32 -0600

    Updating directives to separate template from bussiness logic emailUnverified

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/directives/modalEmailUnVerified/modalEmailUnVerified.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/directives/modalEmailUnVerified/modalEmailUnVerified.js
@@ -26,17 +26,34 @@ orcidNgModule.directive(
         '$rootScope',
         '$timeout',
         function( $compile, $rootScope, $timeout ) {
+            $scope.content = {
+                btncancel : om.get("orcid.frontend.freemarker.btncancel"),
+                emailValue : "",
+                ensure_future_access : om.get("orcid.frontend.workspace.ensure_future_access"),
+                ensure_future_access2 : om.get("orcid.frontend.workspace.ensure_future_access2"),
+                ensure_future_access3 : om.get("orcid.frontend.workspace.ensure_future_access3"),
+                ensure_future_access4 : om.get("orcid.frontend.workspace.ensure_future_access4"),
+                ensure_future_access5 : om.get("orcid.frontend.workspace.ensure_future_access5"),
+                knowledgebase : om.get("orcid.frontend.link.url.knowledgebase"), 
+                send_verification : om.get("orcid.frontend.workspace.send_verification"),
+                support : om.get("orcid.frontend.link.email.support"),
+                your_primary_email : om.get("orcid.frontend.workspace.your_primary_email")
+            };
 
             var closeModal = function(){
                 $.colorbox.remove();
                 $('modal-email-un-verified').html('<div id="modal-email-unverified-container"></div>');
-            }
+            };
+
 
             var openModal = function( scope, data ){
+                content.emailValue = emailVerifiedObj.emails[0].value;
                 emailVerifiedObj = data;
+
                 $.colorbox(
                     {
-                        html : $compile($('#modal-email-unverified-container').html('<div class="lightbox-container" id="modal-email-unverified"><div class="row"><div class="col-md-12 col-xs-12 col-sm-12"><h4>' + om.get("orcid.frontend.workspace.your_primary_email") + '</h4><p>' + om.get("orcid.frontend.workspace.ensure_future_access") + '</p><p>' + om.get("orcid.frontend.workspace.ensure_future_access2") + '<br /><a href="mailto:' + data.emails[0].value + '" target="_blank">' + data.emails[0].value + '</a></p><p>' + om.get("orcid.frontend.workspace.ensure_future_access3") + ' <a target="_blank" href="' + om.get("orcid.frontend.link.url.knowledgebase") + '">' + om.get("orcid.frontend.workspace.ensure_future_access4") + '</a> ' + om.get("orcid.frontend.workspace.ensure_future_access5") + ' <a target="_blank" href="mailto:' + om.get("orcid.frontend.link.email.support") + '">' + om.get("orcid.frontend.link.email.support") + '</a>.</p><button class="btn btn-primary" id="modal-close" ng-click="verifyEmail()">' + om.get("orcid.frontend.workspace.send_verification") + '</button><a class="cancel-option inner-row" ng-click="closeColorBox()">' + om.get("orcid.frontend.freemarker.btncancel") + '</a></div></div></div>'))(scope),
+                        //html : $compile( $('#modal-email-unverified-container').html() )(scope),
+                        html : $('#modal-email-unverified-container').html(),
                         escKey: true,
                         overlayClose: true,
                         transition: 'fade',
@@ -45,7 +62,7 @@ orcidNgModule.directive(
                     }
                 );
                 $.colorbox.resize({height:"250px", width:"500px"});
-            }
+            };
 
             var verifyEmail = function( scope ){
                 var colorboxHtml = null;
@@ -74,7 +91,7 @@ orcidNgModule.directive(
                     scrolling: false
                 });
                 $.colorbox.resize({height:"200px", width:"500px"});
-            }
+            };
 
             function link( scope, element, attrs ) {
 
@@ -102,11 +119,11 @@ orcidNgModule.directive(
                     }
 
                 );
-            }
+            };
 
             return {
                 link: link,
-                template: '<div id="modal-email-unverified-container"></div>',
+                templateUrl: 'modalEmailUnVerified.html',
                 transclude: true
             };
         }
