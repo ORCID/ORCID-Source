d58a909549385edde90f800a5e4158e0ef7a42c1
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-01-12 16:38:40 -0600

    Fix unit test

--- orcid-api-web/src/test/java/org/orcid/api/memberV2/server/delegator/MemberV2ApiServiceDelegator_ReadRecordActivitiesPersonAndPersonalDetailsTest.java
+++ orcid-api-web/src/test/java/org/orcid/api/memberV2/server/delegator/MemberV2ApiServiceDelegator_ReadRecordActivitiesPersonAndPersonalDetailsTest.java
@@ -33,12 +33,15 @@ import javax.annotation.Resource;
 import javax.ws.rs.core.Response;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.orcid.api.common.util.ActivityUtils;
 import org.orcid.core.exception.OrcidUnauthorizedException;
+import org.orcid.core.manager.AddressManager;
 import org.orcid.core.utils.SecurityContextTestUtils;
 import org.orcid.jaxb.model.common_rc4.Iso3166Country;
 import org.orcid.jaxb.model.common_rc4.LastModifiedDate;
@@ -90,6 +93,7 @@ import org.orcid.jaxb.model.record_rc4.WorkType;
 import org.orcid.persistence.aop.ProfileLastModifiedAspect;
 import org.orcid.test.DBUnitTest;
 import org.orcid.test.OrcidJUnit4ClassRunner;
+import org.orcid.test.TargetProxyHelper;
 import org.orcid.test.helper.Utils;
 import org.orcid.utils.DateUtils;
 import org.springframework.test.context.ContextConfiguration;
@@ -111,11 +115,20 @@ public class MemberV2ApiServiceDelegator_ReadRecordActivitiesPersonAndPersonalDe
     @Mock
     private ProfileLastModifiedAspect profileLastModifiedAspect;
 
+    @Resource
+    private AddressManager addressManager;
+    
     @BeforeClass
     public static void initDBUnitData() throws Exception {
         initDBUnitData(DATA_FILES);
     }    
 
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        TargetProxyHelper.injectIntoProxy(addressManager, "profileLastModifiedAspect", profileLastModifiedAspect);
+    }
+    
     @AfterClass
     public static void removeDBUnitData() throws Exception {
         Collections.reverse(DATA_FILES);
