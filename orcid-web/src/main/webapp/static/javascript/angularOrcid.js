1c69857f3bc2c39cc39a39cc8343958c91117083
Manuel Calvo <m.calvo@ost.orcid.org>
2017-01-13 13:07:19 -0600

    Moving ngEnterSubmit directive to its own file

--- orcid-web/src/main/webapp/static/javascript/angularOrcid.js
+++ orcid-web/src/main/webapp/static/javascript/angularOrcid.js
@@ -11746,23 +11746,7 @@
 	    ]
 	);
 
-	/*
-	 * For forms submitted using the default submit function (Scope: document)
-	 * Not necessary to be inside an element, for inputs use ngEnter
-	 */
-	angular.module('orcidApp').directive('ngEnterSubmit', function($document) {
-	    return {
-	        restrict: 'A',
-	        link: function(scope, element, attr) {
-	            $document.bind("keydown keypress", function(event) {
-	                if (event.which === 13) {
-	                   element.submit();
-	                }
-	            });
 
-	        }
-	    };
-	});
 
 
 
@@ -12620,12 +12604,12 @@
 	    return {
 	        restrict: 'A',
 	        link: function(scope, element, attr) {
-	            $(document).unbind("keydown.ngEnterSubmit keypress.ngEnterSubmit");
-	            $document.bind("keydown.ngEnterSubmit keypress.ngEnterSubmit", function(event) {
+	            $document.bind("keydown keypress", function(event) {
 	                if (event.which === 13) {
 	                   element.submit();
 	                }
 	            });
+
 	        }
 	    };
 	});
--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
@@ -11681,23 +11681,7 @@ angular.module('orcidApp').directive(
     ]
 );
 
-/*
- * For forms submitted using the default submit function (Scope: document)
- * Not necessary to be inside an element, for inputs use ngEnter
- */
-angular.module('orcidApp').directive('ngEnterSubmit', function($document) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attr) {
-            $document.bind("keydown keypress", function(event) {
-                if (event.which === 13) {
-                   element.submit();
-                }
-            });
 
-        }
-    };
-});
 
 
 
