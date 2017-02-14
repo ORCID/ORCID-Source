87d133cb8c0a8562edfa1ccbd6737c863f8c10db
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-02-14 16:14:33 -0600

    Fix unit tests

--- orcid-web/src/test/java/org/orcid/frontend/web/controllers/ClaimControllerTest.java
+++ orcid-web/src/test/java/org/orcid/frontend/web/controllers/ClaimControllerTest.java
@@ -25,10 +25,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -39,22 +39,19 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.orcid.core.manager.EmailManager;
 import org.orcid.core.manager.EncryptionManager;
-import org.orcid.core.manager.OrcidProfileManager;
+import org.orcid.core.manager.NotificationManager;
+import org.orcid.core.manager.ProfileEntityCacheManager;
 import org.orcid.core.manager.ProfileEntityManager;
 import org.orcid.frontend.web.forms.EmailAddressForm;
-import org.orcid.jaxb.model.common_v2.Visibility;
-import org.orcid.jaxb.model.message.Claimed;
-import org.orcid.jaxb.model.message.ContactDetails;
-import org.orcid.jaxb.model.message.Email;
 import org.orcid.jaxb.model.common_v2.Locale;
-import org.orcid.jaxb.model.message.OrcidBio;
-import org.orcid.jaxb.model.message.OrcidHistory;
-import org.orcid.jaxb.model.message.OrcidIdentifier;
-import org.orcid.jaxb.model.message.OrcidProfile;
+import org.orcid.jaxb.model.common_v2.Visibility;
+import org.orcid.persistence.jpa.entities.EmailEntity;
+import org.orcid.persistence.jpa.entities.ProfileEntity;
 import org.orcid.pojo.ajaxForm.Checkbox;
 import org.orcid.pojo.ajaxForm.Claim;
 import org.orcid.pojo.ajaxForm.Text;
@@ -73,9 +70,7 @@ import com.google.common.collect.Lists;
 
 @RunWith(OrcidJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:orcid-frontend-web-servlet.xml", "classpath:orcid-core-context.xml" })
-public class ClaimControllerTest extends DBUnitTest {
-    private static final List<String> DATA_FILES = Arrays.asList("/data/EmptyEntityData.xml", "/data/SecurityQuestionEntityData.xml",
-            "/data/SourceClientDetailsEntityData.xml", "/data/ProfileEntityData.xml", "/data/ClientDetailsEntityData.xml", "/data/RecordNameEntityData.xml", "/data/BiographyEntityData.xml");
+public class ClaimControllerTest {
     
     @Resource
     private ClaimController claimController;
@@ -84,31 +79,25 @@ public class ClaimControllerTest extends DBUnitTest {
     private ProfileEntityManager profileEntityManager;
     
     @Mock
-    private OrcidProfileManager orcidProfileManager;
+    private EncryptionManager encryptionManager;
     
     @Mock
-    private EncryptionManager encryptionManager;
+    private ProfileEntityCacheManager profileEntityCacheManager;
     
     @Mock
     private EmailManager emailManager;
     
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        initDBUnitData(DATA_FILES);
-    }
-    
-    @AfterClass
-    public static void afterClass() throws Exception {
-        removeDBUnitData(Lists.reverse(DATA_FILES));
-    }
+    @Mock
+    private NotificationManager notificationManager;
     
     @Before
     public void before() {
         MockitoAnnotations.initMocks(this);        
-        TargetProxyHelper.injectIntoProxy(claimController, "orcidProfileManager", orcidProfileManager);
         TargetProxyHelper.injectIntoProxy(claimController, "encryptionManager", encryptionManager);
         TargetProxyHelper.injectIntoProxy(claimController, "emailManager", emailManager); 
-        TargetProxyHelper.injectIntoProxy(claimController, "profileEntityManager", profileEntityManager);                 
+        TargetProxyHelper.injectIntoProxy(claimController, "profileEntityManager", profileEntityManager);
+        TargetProxyHelper.injectIntoProxy(claimController, "profileEntityCacheManager", profileEntityCacheManager);
+        TargetProxyHelper.injectIntoProxy(claimController, "notificationManager", notificationManager);
     }
     
     @Test
@@ -116,7 +105,7 @@ public class ClaimControllerTest extends DBUnitTest {
         HttpServletRequest servletRequest = mock(HttpServletRequest.class);
         BindingResult bindingResult = mock(BindingResult.class);
         when(bindingResult.hasErrors()).thenReturn(false);
-        when(orcidProfileManager.retrieveOrcidProfileByEmail("billie@holiday.com")).thenReturn(getOrcidToTestClaimResend(true));
+        when(profileEntityCacheManager.retrieve(Matchers.anyString())).thenReturn(getProfileEntityToTestClaimResend(true));
         EmailAddressForm emailAddressForm = new EmailAddressForm();
         //Testing with profile 4444-4444-4444-4446
         emailAddressForm.setUserEmailAddress("billie@holiday.com");
@@ -132,7 +121,8 @@ public class ClaimControllerTest extends DBUnitTest {
         HttpServletRequest servletRequest = mock(HttpServletRequest.class);
         BindingResult bindingResult = mock(BindingResult.class);
         when(bindingResult.hasErrors()).thenReturn(false);
-        when(orcidProfileManager.retrieveOrcidProfileByEmail("billie@holiday.com")).thenReturn(getOrcidToTestClaimResend(false));
+        when(emailManager.findOrcidIdByEmail("billie@holiday.com")).thenReturn("4444-4444-4444-4446");
+        when(profileEntityCacheManager.retrieve(Matchers.anyString())).thenReturn(getProfileEntityToTestClaimResend(false));
         EmailAddressForm emailAddressForm = new EmailAddressForm();
         //Testing with profile 4444-4444-4444-4446
         emailAddressForm.setUserEmailAddress("billie@holiday.com");
@@ -149,15 +139,14 @@ public class ClaimControllerTest extends DBUnitTest {
     public void testClaim() {
         String email = "public_0000-0000-0000-0001@test.orcid.org";
         SecurityContextHolder.getContext().setAuthentication(null);
-        when(orcidProfileManager.retrieveOrcidProfileByEmail(any(String.class))).thenReturn(getOrcidToTestClaim(false));
+        when(profileEntityCacheManager.retrieve(Matchers.anyString())).thenReturn(getProfileEntityToTestClam(false));
         when(encryptionManager.decryptForExternalUse(any(String.class))).thenReturn(email);
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         when(request.getAttribute(DispatcherServlet.LOCALE_RESOLVER_ATTRIBUTE)).thenReturn(null);
         when(request.getLocale()).thenReturn(java.util.Locale.US);
-        HashMap<String, String> data = new HashMap<String, String>();
-        data.put(email, "0000-0000-0000-0001");
-        when(emailManager.findOricdIdsByCommaSeparatedEmails(email)).thenReturn(data);       
+        String orcid = "0000-0000-0000-0001";
+        when(emailManager.findOrcidIdByEmail(email)).thenReturn(orcid);       
         when(profileEntityManager.claimProfileAndUpdatePreferences(any(String.class), any(String.class), any(Locale.class), any(Claim.class))).thenReturn(true);
         
         Claim claim = new Claim();
@@ -181,41 +170,27 @@ public class ClaimControllerTest extends DBUnitTest {
         }
     }
     
-    private OrcidProfile getOrcidToTestClaimResend(boolean claimed) {
-        OrcidProfile orcidProfile = new OrcidProfile();
-        OrcidIdentifier orcid = new OrcidIdentifier("0000-0000-0000-000X");        
-        orcidProfile.setOrcidIdentifier(orcid);
-        OrcidHistory orcidHistory = new OrcidHistory();
-        orcidHistory.setClaimed(new Claimed(claimed));
-        orcidProfile.setOrcidHistory(orcidHistory);
-        OrcidBio orcidBio = new OrcidBio();
-        ContactDetails contactDetails = new ContactDetails();
-        List<Email> emails = new ArrayList<Email>();
-        Email email = new Email("billie@holiday.com");
+    private ProfileEntity getProfileEntityToTestClaimResend(boolean claimed) {
+        ProfileEntity entity = new ProfileEntity();
+        entity.setId("0000-0000-0000-000X");
+        entity.setClaimed(claimed);
+        Set<EmailEntity> emails = new HashSet<EmailEntity>();
+        EmailEntity email = new EmailEntity();
+        email.setId("billie@holiday.com");
         email.setPrimary(true);
-        emails.add(email);
-        contactDetails.setEmail(emails);
-        orcidBio.setContactDetails(contactDetails);
-        orcidProfile.setOrcidBio(orcidBio);
-        return orcidProfile;
-    }    
+        entity.setEmails(emails);
+        return entity;
+    }
     
-    private OrcidProfile getOrcidToTestClaim(boolean claimed) {
-        OrcidProfile orcidProfile = new OrcidProfile();
-        OrcidIdentifier orcid = new OrcidIdentifier("0000-0000-0000-0001");        
-        orcidProfile.setOrcidIdentifier(orcid);
-        OrcidHistory orcidHistory = new OrcidHistory();
-        orcidHistory.setClaimed(new Claimed(claimed));
-        orcidProfile.setOrcidHistory(orcidHistory);
-        OrcidBio orcidBio = new OrcidBio();
-        ContactDetails contactDetails = new ContactDetails();
-        List<Email> emails = new ArrayList<Email>();
-        Email email = new Email("public_0000-0000-0000-0001@test.orcid.org");
+    private ProfileEntity getProfileEntityToTestClam(boolean claimed) {
+        ProfileEntity entity = new ProfileEntity();
+        entity.setId("0000-0000-0000-0001");
+        entity.setClaimed(claimed);
+        Set<EmailEntity> emails = new HashSet<EmailEntity>();
+        EmailEntity email = new EmailEntity();
+        email.setId("public_0000-0000-0000-0001@test.orcid.org");
         email.setPrimary(true);
-        emails.add(email);
-        contactDetails.setEmail(emails);
-        orcidBio.setContactDetails(contactDetails);
-        orcidProfile.setOrcidBio(orcidBio);
-        return orcidProfile;
+        entity.setEmails(emails);
+        return entity;
     }
 }
