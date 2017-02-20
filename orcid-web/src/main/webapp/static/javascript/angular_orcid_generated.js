7983e41ada926dd8e7394d5a53d332734e430e53
Will Simpson <w.simpson@orcid.org>
2017-02-20 12:01:40 +0000

    Moved members list service to separate file.

--- orcid-web/src/main/webapp/static/javascript/angular_orcid_generated.js
+++ orcid-web/src/main/webapp/static/javascript/angular_orcid_generated.js
@@ -910,93 +910,6 @@
 	    return serv; 
 	}]);
 
-	angular.module('orcidApp').factory("membersListSrvc", ['$rootScope', function ($rootScope) {
-	    var serv = {
-	        membersList: null,
-	        memberDetails: {},
-	        currentMemberDetails: null,
-	        consortiaList: null,
-	        getMembersList: function() {
-	            $.ajax({
-	                url: getBaseUri() + '/members/members.json',
-	                dataType: 'json',
-	                cache: true,
-	                success: function(data) {
-	                    serv.membersList = data;
-	                    $rootScope.$apply();
-	                }
-	            }).fail(function() {
-	                // something bad is happening!
-	                console.log("error with members list");
-	                serv.feed = [];
-	                $rootScope.$apply();
-	            });
-	        },
-	        getDetails: function(memberId, consortiumLeadId) {
-	            if(serv.memberDetails[memberId] == null){
-	                var url = getBaseUri() + '/members/details.json?memberId=' + encodeURIComponent(memberId);
-	                if(consortiumLeadId != null){
-	                    url += '&consortiumLeadId=' + encodeURIComponent(consortiumLeadId);
-	                }
-	                $.ajax({
-	                    url: url,
-	                    dataType: 'json',
-	                    cache: true,
-	                    success: function(data) {
-	                        serv.memberDetails[memberId] = data;
-	                        $rootScope.$apply();
-	                    }
-	                }).fail(function() {
-	                    // something bad is happening!
-	                    console.log("error with member details");
-	                    serv.feed = [];
-	                    $rootScope.$apply();
-	                });
-	            }
-	        },
-	        getCurrentMemberDetailsBySlug: function(memberSlug) {
-	            if(serv.currentMemberDetails == null){
-	                var url = getBaseUri() + '/members/detailsBySlug.json?memberSlug=' + encodeURIComponent(memberSlug);
-	                $.ajax({
-	                    url: url,
-	                    dataType: 'json',
-	                    cache: true,
-	                    success: function(data) {
-	                        serv.currentMemberDetails = data;
-	                        $rootScope.$apply();
-	                    }
-	                }).fail(function() {
-	                    // something bad is happening!
-	                    console.log("error with member details by slug");
-	                    serv.feed = [];
-	                    $rootScope.$apply();
-	                });
-	            }
-	        },
-	        getConsortiaList: function() {
-	            $.ajax({
-	                url: getBaseUri() + '/consortia/consortia.json',
-	                dataType: 'json',
-	                cache: true,
-	                success: function(data) {
-	                    serv.consortiaList = data;
-	                    $rootScope.$apply();
-	                }
-	            }).fail(function() {
-	                // something bad is happening!
-	                console.log("error with consortia list");
-	                serv.feed = [];
-	                $rootScope.$apply();
-	            });
-	        },
-	        getMemberPageUrl: function(slug) {
-	            return orcidVar.baseUri + '/members/' + slug;
-	        }
-	    };
-
-	    return serv; 
-	}]);
-
 	angular.module('orcidApp').factory("clearMemberListFilterSrvc", ['$rootScope', function ($rootScope) {
 	    return {
 	          clearFilters : function ($scope){
@@ -12216,6 +12129,7 @@
 		"./groupedActivitiesService.js": 33,
 		"./groupedActivitiesUtil.js": 34,
 		"./initialConfigService.js": 35,
+		"./membersListSrvc.js": 40,
 		"./notificationsSrvc.js": 36,
 		"./utilsService.js": 37,
 		"./workspaceSrvc.js": 38
@@ -13306,6 +13220,98 @@
 	webpackContext.resolve = webpackContext;
 	module.exports = webpackContext;
 	webpackContext.id = 39;
+
+
+/***/ },
+/* 40 */
+/***/ function(module, exports) {
+
+	angular.module('orcidApp').factory("membersListSrvc", ['$rootScope', function ($rootScope) {
+	    var serv = {
+	        membersList: null,
+	        memberDetails: {},
+	        currentMemberDetails: null,
+	        consortiaList: null,
+	        getMembersList: function() {
+	            $.ajax({
+	                url: getBaseUri() + '/members/members.json',
+	                dataType: 'json',
+	                cache: true,
+	                success: function(data) {
+	                    serv.membersList = data;
+	                    $rootScope.$apply();
+	                }
+	            }).fail(function() {
+	                // something bad is happening!
+	                console.log("error with members list");
+	                serv.feed = [];
+	                $rootScope.$apply();
+	            });
+	        },
+	        getDetails: function(memberId, consortiumLeadId) {
+	            if(serv.memberDetails[memberId] == null){
+	                var url = getBaseUri() + '/members/details.json?memberId=' + encodeURIComponent(memberId);
+	                if(consortiumLeadId != null){
+	                    url += '&consortiumLeadId=' + encodeURIComponent(consortiumLeadId);
+	                }
+	                $.ajax({
+	                    url: url,
+	                    dataType: 'json',
+	                    cache: true,
+	                    success: function(data) {
+	                        serv.memberDetails[memberId] = data;
+	                        $rootScope.$apply();
+	                    }
+	                }).fail(function() {
+	                    // something bad is happening!
+	                    console.log("error with member details");
+	                    serv.feed = [];
+	                    $rootScope.$apply();
+	                });
+	            }
+	        },
+	        getCurrentMemberDetailsBySlug: function(memberSlug) {
+	            if(serv.currentMemberDetails == null){
+	                var url = getBaseUri() + '/members/detailsBySlug.json?memberSlug=' + encodeURIComponent(memberSlug);
+	                $.ajax({
+	                    url: url,
+	                    dataType: 'json',
+	                    cache: true,
+	                    success: function(data) {
+	                        serv.currentMemberDetails = data;
+	                        $rootScope.$apply();
+	                    }
+	                }).fail(function() {
+	                    // something bad is happening!
+	                    console.log("error with member details by slug");
+	                    serv.feed = [];
+	                    $rootScope.$apply();
+	                });
+	            }
+	        },
+	        getConsortiaList: function() {
+	            $.ajax({
+	                url: getBaseUri() + '/consortia/consortia.json',
+	                dataType: 'json',
+	                cache: true,
+	                success: function(data) {
+	                    serv.consortiaList = data;
+	                    $rootScope.$apply();
+	                }
+	            }).fail(function() {
+	                // something bad is happening!
+	                console.log("error with consortia list");
+	                serv.feed = [];
+	                $rootScope.$apply();
+	            });
+	        },
+	        getMemberPageUrl: function(slug) {
+	            return orcidVar.baseUri + '/members/' + slug;
+	        }
+	    };
+
+	    return serv; 
+	}]);
 
 
 /***/ }
