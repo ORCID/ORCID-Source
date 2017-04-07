38e559ce8e10b13887b4a48958a00fd33991a38b
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-04-07 17:49:15 -0600

    Unit tests for GivenPermissionToManager

--- orcid-core/src/test/java/org/orcid/core/manager/GivenPermissionToManagerTest.java
+++ orcid-core/src/test/java/org/orcid/core/manager/GivenPermissionToManagerTest.java
@@ -16,24 +16,106 @@
  */
 package org.orcid.core.manager;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.orcid.core.BaseTest;
+import org.orcid.persistence.dao.ProfileDao;
+import org.orcid.persistence.jpa.entities.GivenPermissionToEntity;
 
-public class GivenPermissionToManagerTest {
+public class GivenPermissionToManagerTest extends BaseTest {
+
+    private static final List<String> DATA_FILES = Arrays.asList("/data/SecurityQuestionEntityData.xml", "/data/SourceClientDetailsEntityData.xml",
+            "/data/ProfileEntityData.xml", "/data/ClientDetailsEntityData.xml", "/data/RecordNameEntityData.xml");
+
+    private static final String GIVER = "0000-0000-0000-0006";
+    private static final String RECEIVER = "0000-0000-0000-0003";
+
+    @Resource
+    private GivenPermissionToManager givenPermissionToManager;
+
+    @Resource
+    private ProfileDao profileDao;
+
+    @BeforeClass
+    public static void initDBUnitData() throws Exception {
+        initDBUnitData(DATA_FILES);
+    }
+
+    @AfterClass
+    public static void removeDBUnitData() throws Exception {
+        List<String> reversedDataFiles = new ArrayList<String>(DATA_FILES);
+        Collections.reverse(reversedDataFiles);
+        removeDBUnitData(reversedDataFiles);
+    }
 
     @Test
     public void testFindByGiverAndReceiverOrcid() {
-        fail();
+        GivenPermissionToEntity entity = givenPermissionToManager.findByGiverAndReceiverOrcid(GIVER, RECEIVER);
+        assertNotNull(entity);
+        assertEquals(GIVER, entity.getGiver());
+        assertEquals(RECEIVER, entity.getReceiver().getId());
     }
-    
+
     @Test
     public void testRemove() {
-        fail();
+        // Create one
+        givenPermissionToManager.create(RECEIVER, GIVER);
+        // Find it
+        GivenPermissionToEntity entity = givenPermissionToManager.findByGiverAndReceiverOrcid(RECEIVER, GIVER);
+        assertNotNull(entity);
+        assertEquals(RECEIVER, entity.getGiver());
+        assertEquals(GIVER, entity.getReceiver().getId());
+
+        Date rLastModifiedBefore = profileDao.find(RECEIVER).getLastModified();
+        Date gLastModifiedBefore = profileDao.find(GIVER).getLastModified();
+
+        // Delete it
+        givenPermissionToManager.remove(RECEIVER, GIVER);
+        // Verify it was deleted
+        entity = givenPermissionToManager.findByGiverAndReceiverOrcid(RECEIVER, GIVER);
+        assertNull(entity);
+
+        Date rLastModifiedAfter = profileDao.find(RECEIVER).getLastModified();
+        Date gLastModifiedAfter = profileDao.find(GIVER).getLastModified();
+
+        assertTrue(rLastModifiedAfter.after(rLastModifiedBefore));
+        assertTrue(gLastModifiedAfter.after(gLastModifiedBefore));
     }
-    
+
     @Test
     public void testCreate() {
-        fail();
+        Date rLastModifiedBefore = profileDao.find(RECEIVER).getLastModified();
+        Date gLastModifiedBefore = profileDao.find(GIVER).getLastModified();
+
+        // Create one
+        givenPermissionToManager.create(RECEIVER, GIVER);
+
+        GivenPermissionToEntity entity = givenPermissionToManager.findByGiverAndReceiverOrcid(RECEIVER, GIVER);
+        assertNotNull(entity);
+        assertEquals(RECEIVER, entity.getGiver());
+        assertEquals(GIVER, entity.getReceiver().getId());
+
+        Date rLastModifiedAfter = profileDao.find(RECEIVER).getLastModified();
+        Date gLastModifiedAfter = profileDao.find(GIVER).getLastModified();
+
+        assertTrue(rLastModifiedAfter.after(rLastModifiedBefore));
+        assertTrue(gLastModifiedAfter.after(gLastModifiedBefore));
+
+        // Delete it
+        givenPermissionToManager.remove(RECEIVER, GIVER);
     }
 }
