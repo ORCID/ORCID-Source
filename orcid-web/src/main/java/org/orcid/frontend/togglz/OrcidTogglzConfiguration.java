565dc784284b62862b904ae2e94a6b5d2ec630b8
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-04-24 17:08:37 -0600

    Enable togglz admin console for admin users and give an example

--- orcid-web/src/main/java/org/orcid/frontend/togglz/OrcidTogglzConfiguration.java
+++ orcid-web/src/main/java/org/orcid/frontend/togglz/OrcidTogglzConfiguration.java
@@ -19,27 +19,33 @@ package org.orcid.frontend.togglz;
 import java.io.File;
 import java.io.IOException;
 
+import org.orcid.core.oauth.OrcidProfileUserDetails;
+import org.orcid.jaxb.model.message.OrcidType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.togglz.core.Feature;
 import org.togglz.core.manager.TogglzConfig;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.repository.file.FileBasedStateRepository;
+import org.togglz.core.user.FeatureUser;
+import org.togglz.core.user.SimpleFeatureUser;
 import org.togglz.core.user.UserProvider;
-import org.togglz.servlet.user.ServletUserProvider;
 
 @Component
 public class OrcidTogglzConfiguration implements TogglzConfig {
 
     @Value("${org.orcid.frontend.togglz.config:classpath:/features.properties}")
     private String togglzConfigFilePath;
-    
+
     @Autowired
     private ResourceLoader resourceLoader;
-    
+
     @Override
     public Class<? extends Feature> getFeatureClass() {
         return Features.class;
@@ -47,15 +53,14 @@ public class OrcidTogglzConfiguration implements TogglzConfig {
 
     @Override
     public StateRepository getStateRepository() {
-        Resource r = resourceLoader.getResource(togglzConfigFilePath);        
-        
-        File configFile = null; 
+        Resource r = resourceLoader.getResource(togglzConfigFilePath);
+        File configFile = null;
         try {
             configFile = r.getFile();
-        } catch(IOException ioe){
-            
+        } catch (IOException e) {
+
         }
-        if(configFile == null || !configFile.exists()) {
+        if (configFile == null || !configFile.exists()) {
             throw new IllegalArgumentException("Unable to find config file " + togglzConfigFilePath);
         }
         return new FileBasedStateRepository(configFile);
@@ -63,7 +68,26 @@ public class OrcidTogglzConfiguration implements TogglzConfig {
 
     @Override
     public UserProvider getUserProvider() {
-        return new ServletUserProvider("admin");
+        return new UserProvider() {
+            @Override
+            public FeatureUser getCurrentUser() {
+                boolean isAdmin = false;
+                String userOrcid = null;
+                SecurityContext context = SecurityContextHolder.getContext();
+                if (context != null && context.getAuthentication() != null) {
+                    Authentication authentication = context.getAuthentication();
+                    if (authentication != null) {
+                        Object principal = authentication.getPrincipal();
+                        if (principal instanceof OrcidProfileUserDetails) {
+                            OrcidProfileUserDetails userDetails = (OrcidProfileUserDetails) principal;
+                            isAdmin = OrcidType.ADMIN.equals(userDetails.getOrcidType());
+                            userOrcid = userDetails.getOrcid();
+                        }
+                    }
+                }
+                return new SimpleFeatureUser(userOrcid, isAdmin);
+            }
+        };
     }
 
 }
