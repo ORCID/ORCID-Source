b3695093c2a6f53d87e80301e75557df8a30d034
Jeff P <jeff@siccr.com>
2017-05-10 15:43:43 -0600

    use webpack conf per environment

---
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/qa.webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./require.js",
+    module: {
+        loaders: [
+            { 
+                test: /\.ts$/, 
+                loader: 'ts-loader' 
+            }
+        ]
+    },
+    output: {
+        path: __dirname,
+        filename: "../angular_orcid_generated.js"
+    },
+    plugins: [
+    ],
+    resolve: {
+        alias: {
+            "@angular/upgrade/static": "@angular/upgrade/bundles/upgrade-static.umd.js"
+        }
+    },
+    watch: false
+};
