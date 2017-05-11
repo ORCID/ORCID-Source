97524c9e0fc36c6bbfd813486deaf0f4ab1d3672
Manuel Calvo <m.calvo@ost.orcid.org>
2017-05-11 08:17:42 -0600

    Moving remaining filters to its own files, moving more controllers, updating require for work on ng2 migration

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/require.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/require.js
@@ -7,10 +7,12 @@ function requireAll(requireContext) {
 require('./app/angularOrcidOriginal.js');
 //require('./app/main.ts');
 requireAll(require.context("./app/controllers", true, /^\.\/.*\.js$/));
-requireAll(require.context("./app/controllers", true, /^\.\/.*\.ts$/));
+//requireAll(require.context("./app/controllers", true, /^\.\/.*\.ts$/));
 requireAll(require.context("./app/directives", true, /^\.\/.*\.js$/));
-requireAll(require.context("./app/directives", true, /^\.\/.*\.ts$/));
+//requireAll(require.context("./app/directives", true, /^\.\/.*\.ts$/));
+requireAll(require.context("./app/filters", true, /^\.\/.*\.js$/));
+//requireAll(require.context("./app/filters", true, /^\.\/.*\.ts$/));
 requireAll(require.context("./app/modules", true, /^\.\/.*\.js$/));
 //requireAll(require.context("./app/modules", true, /^\.\/.*\.ts$/));
 requireAll(require.context("./app/services", true, /^\.\/.*\.js$/));
-requireAll(require.context("./app/services", true, /^\.\/.*\.ts$/));
+//requireAll(require.context("./app/services", true, /^\.\/.*\.ts$/));
