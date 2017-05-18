f2177a84d6b7af12217d8517024f8c05a14d7d22
Tom <tom@Toms-MacBook-Pro-2.local>
2017-05-18 15:23:59 +0100

    added configurable jwks file for openid signing.

--- orcid-core/src/main/java/org/orcid/core/oauth/openid/OpenIDConnectKeyService.java
+++ orcid-core/src/main/java/org/orcid/core/oauth/openid/OpenIDConnectKeyService.java
@@ -1,11 +1,15 @@
 package org.orcid.core.oauth.openid;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.text.ParseException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.nimbusds.jose.JOSEException;
@@ -14,6 +18,7 @@ import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
@@ -22,33 +27,46 @@ import com.nimbusds.jwt.SignedJWT;
 @Component
 public class OpenIDConnectKeyService {
 
-    private final KeyPair kp;
     private final String keyID = "OpenIDConnectKey1";
-    private final JWK publicJWK;
-    //private final JWK privateJWK;
+    private final RSAKey publicJWK;
+    private final RSAKey privateJWK;
     private final JWSAlgorithm defaultAlg = JWSAlgorithm.RS256;
-    
-    
-    /** Generate a random key for now
+        
+    /** Use a configured key ${org.orcid.openid.jwks_location} or generate a random key
+     * 
+     * New keys can be generated using this: https://mkjwk.org/ or a command line tool found here: https://connect2id.com/products/nimbus-jose-jwt/generator
      * @throws NoSuchAlgorithmException
+     * @throws ParseException 
+     * @throws IOException 
      */
-    public OpenIDConnectKeyService() throws NoSuchAlgorithmException{
-        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
-        keyGenerator.initialize(1024);
-        kp = keyGenerator.genKeyPair();
-     
+    public OpenIDConnectKeyService(@Value("${org.orcid.openid.jwks_location}") String jwksLocation) throws NoSuchAlgorithmException, IOException, ParseException{
+        if (jwksLocation !=null && !jwksLocation.isEmpty()){
+            //use a configured key.
+            JWKSet keys = JWKSet.load(new File(jwksLocation));
+            privateJWK = (RSAKey) keys.getKeyByKeyId(keyID);
+            publicJWK = privateJWK.toPublicJWK();
+            
+        }else{
+            //generate a random key.  Fine for testing.
+            KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+            keyGenerator.initialize(1024);
+            KeyPair kp = keyGenerator.genKeyPair();     
+            privateJWK = new RSAKey.Builder((RSAPublicKey)kp.getPublic())
+                    .privateKey((RSAPrivateKey)kp.getPrivate())
+                    .keyUse(KeyUse.SIGNATURE)
+                    .keyID(getDefaultKeyID())
+                    .algorithm(defaultAlg)
+                    .build();
+            
+            publicJWK = privateJWK.toPublicJWK();
+            /*new RSAKey.Builder((RSAPublicKey)kp.getPublic())
+                    .keyID(getDefaultKeyID())
+                    .keyUse(KeyUse.SIGNATURE)
+                    .algorithm(defaultAlg)
+                    .build();*/
+        }
+
         
-        publicJWK = new RSAKey.Builder((RSAPublicKey)kp.getPublic())
-                .keyID(getDefaultKeyID())
-                .keyUse(KeyUse.SIGNATURE)
-                /*We could also support HMAC*/
-                .algorithm(defaultAlg)
-                .build();
-        
-        /*privateJWK = new RSAKey.Builder((RSAPublicKey)kp.getPublic())
-                .privateKey((RSAPrivateKey)kp.getPrivate())
-                .keyID(getDefaultKeyID())
-                .build();*/
     }
     
     /** Get the private key for signing
@@ -57,7 +75,7 @@ public class OpenIDConnectKeyService {
      * @throws JOSEException 
      */
     public SignedJWT sign(JWTClaimsSet claims) throws JOSEException{
-        JWSSigner signer = new RSASSASigner(kp.getPrivate());
+        JWSSigner signer = new RSASSASigner(privateJWK);
         JWSHeader.Builder head = new JWSHeader.Builder(defaultAlg);
         head.keyID(getDefaultKeyID());
         SignedJWT signedJWT = new SignedJWT(head.build(), claims);
