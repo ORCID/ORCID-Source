c52ea90a050d1443afea56483fc5d8c905858e79
Jean Helie <jean@semmle.com>
2017-05-26 09:45:01 +0100

    fix 3 lgtm.com alerts: comparison using == and != operators only test for reference equality

--- orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/WorkEntityDisplayIndexComparatorDesc.java
+++ orcid-persistence/src/main/java/org/orcid/persistence/jpa/entities/WorkEntityDisplayIndexComparatorDesc.java
@@ -31,7 +31,7 @@ public class WorkEntityDisplayIndexComparatorDesc<T> implements Comparator<WorkE
     public int compare(WorkEntity o1, WorkEntity o2) {
         Long index = o1.getDisplayIndex();
         Long otherIndex = o2.getDisplayIndex();
-        if (index == otherIndex) return o2.compareTo(o1);
+        if (index == null? otherIndex == null : index.equals(otherIndex)) return o2.compareTo(o1);
         if (index == null) return 1;
         if (otherIndex == null) return -1;
         return otherIndex.compareTo(index);
