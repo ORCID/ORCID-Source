4dee9a7db731ec5d2d0102e5bc5700456d3b5be9
Will Simpson <w.simpson@orcid.org>
2017-05-29 14:08:43 +0100

    Set source on group record after conversion to entity (to fix null source).

--- orcid-core/src/main/java/org/orcid/core/manager/impl/GroupIdRecordManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/GroupIdRecordManagerImpl.java
@@ -55,17 +55,14 @@ public class GroupIdRecordManagerImpl extends GroupIdRecordManagerReadOnlyImpl i
         SourceEntity sourceEntity = sourceManager.retrieveSourceEntity();
         activityValidator.validateGroupIdRecord(groupIdRecord, true, sourceEntity);
         validateDuplicate(groupIdRecord);
-        if (sourceEntity != null) {
-            Source source = new Source();
-            if (sourceEntity.getSourceClient() != null) {
-                source.setSourceClientId(new SourceClientId(sourceEntity.getSourceClient().getClientId()));
-            } else if (sourceEntity.getSourceProfile() != null) {
-                source.setSourceOrcid(new SourceOrcid(sourceEntity.getSourceProfile().getId()));
-            }
-            groupIdRecord.setSource(source);
-        }
-
         GroupIdRecordEntity entity = jpaJaxbGroupIdRecordAdapter.toGroupIdRecordEntity(groupIdRecord);
+        if (sourceEntity != null) {
+            if (sourceEntity.getSourceClient() != null) {
+                entity.setClientSourceId(sourceEntity.getSourceClient().getClientId());
+            } else if (sourceEntity.getSourceProfile() != null) {
+                entity.setSourceId(sourceEntity.getSourceProfile().getId());
+            }
+        }
         groupIdRecordDao.persist(entity);
         return jpaJaxbGroupIdRecordAdapter.toGroupIdRecord(entity);
     }
