a1262f786d0434e59c99945bf084253db561f503
Manuel Calvo <m.calvo@ost.orcid.org>
2017-06-02 13:13:32 -0600

    Ng2 migration: removing scripts from script ftl to load them only once and improve load times, updating polyfills

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/ng1_app.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/ng1_app.ts
@@ -5,18 +5,23 @@
 
 import * as angular from 'angular'
 import * as vcRecaptcha from 'angular-recaptcha'
+import * as ngCookies from 'angular-cookies'
+import * as ngSanitize from 'angular-sanitize'
+import typeahead from 'angular-ui-bootstrap/src/typeahead/index-nocss.js';
 
 
 //import { EmailFrequencyCtrl } from './../controllers/EmailFrequencyCtrl.ts' 
 
+
+//need to import ui.bootstrap and ui.multiselect correctly
 export const orcidApp = angular.module(
     'orcidApp', 
     [
-        'ngCookies',
-        'ngSanitize', 
-        'ui.multiselect', 
+        ngCookies,
+        ngSanitize, 
         vcRecaptcha,
-        'ui.bootstrap'//,
+        //'ui.multiselect', 
+        //typeahead//,
         //'EmailFrequencyCtrl'
         //EmailFrequencyModule.name
     ]
