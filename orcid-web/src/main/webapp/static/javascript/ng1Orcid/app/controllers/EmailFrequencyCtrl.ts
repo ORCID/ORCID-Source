0a9f73b881d5ac26398aab7dc54a41ac262ddad8
Manuel Calvo <m.calvo@ost.orcid.org>
2017-06-13 15:49:22 -0600

    Ng2Migration: Formatting EmailFrequencyCtrl

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/controllers/EmailFrequencyCtrl.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/controllers/EmailFrequencyCtrl.ts
@@ -3,22 +3,22 @@ import {NgModule} from '@angular/core';
 
 // This is the Angular 1 part of the module
 export const EmailFrequencyCtrl = angular.module('orcidApp').controller(
-	'EmailFrequencyCtrl',
-	[
-		'$scope', 
-		'$compile', 
-		'emailSrvc', 
-		'prefsSrvc', 
-		function (
-			$scope, 
-			$compile, 
-			emailSrvc, 
-			prefsSrvc
-		) {
-    		$scope.prefsSrvc = prefsSrvc;
-    		$scope.emailSrvc = emailSrvc;
-    	}
-	]
+    'EmailFrequencyCtrl',
+    [
+        '$scope', 
+        '$compile', 
+        'emailSrvc', 
+        'prefsSrvc', 
+        function (
+            $scope, 
+            $compile, 
+            emailSrvc, 
+            prefsSrvc
+        ) {
+            $scope.prefsSrvc = prefsSrvc;
+            $scope.emailSrvc = emailSrvc;
+        }
+    ]
 );
 
 // This is the Angular 2 part of the module
