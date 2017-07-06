607b5d94aab9a168daf7056d12ab9d131b28d16e
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-07-06 17:40:29 -0600

    Unit testing DAO's

--- orcid-persistence/src/test/java/org/orcid/persistence/dao/WorkDaoTest.java
+++ orcid-persistence/src/test/java/org/orcid/persistence/dao/WorkDaoTest.java
@@ -16,8 +16,10 @@
  */
 package org.orcid.persistence.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.Arrays;
@@ -35,19 +37,17 @@ import org.orcid.persistence.jpa.entities.ProfileEntity;
 import org.orcid.persistence.jpa.entities.WorkEntity;
 import org.orcid.test.DBUnitTest;
 import org.orcid.test.OrcidJUnit4ClassRunner;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(OrcidJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:orcid-persistence-context.xml" })
 public class WorkDaoTest extends DBUnitTest {
 
-    private static String USER_ORCID="4444-4444-4444-4441";
+    private static String USER_ORCID = "0000-0000-0000-0003";
+    private static String OTHER_USER_ORCID = "4444-4444-4444-4443";
     
-    @Resource
-    private WorkDao workDao;
+    @Resource(name = "workDao")
+    private WorkDao dao;
 
     @BeforeClass
     public static void initDBUnitData() throws Exception {
@@ -57,12 +57,10 @@ public class WorkDaoTest extends DBUnitTest {
 
     @AfterClass
     public static void removeDBUnitData() throws Exception {
-        removeDBUnitData(Arrays.asList("/data/WorksEntityData.xml", "/data/ProfileEntityData.xml", "/data/SecurityQuestionEntityData.xml"));
+        removeDBUnitData(Arrays.asList("/data/WorksEntityData.xml", "/data/ProfileEntityData.xml", "/data/SourceClientDetailsEntityData.xml", "/data/SecurityQuestionEntityData.xml"));
     }
 
     @Test
-    @Rollback(true)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void testAddWork() {
         String title = "New Work";
         String subtitle = "Subtitle";
@@ -86,11 +84,30 @@ public class WorkDaoTest extends DBUnitTest {
         assertNull(work.getId());
 
         try {
-            work = workDao.addWork(work);
+            work = dao.addWork(work);
         } catch (Exception e) {
             fail();
         }
 
         assertNotNull(work.getId());
+    }
+    
+    @Test
+    public void removeAllWorksTest() {
+        long initialNumber = dao.countAll();
+        long elementThatBelogsToUser = dao.findWorks(USER_ORCID, 0L).size();
+        long otherUserElements = dao.findWorks(OTHER_USER_ORCID, 0L).size();
+        assertTrue(elementThatBelogsToUser > 0);
+        assertTrue(elementThatBelogsToUser < initialNumber);
+        assertEquals(3, otherUserElements);
+        //Remove all elements that belongs to USER_ORCID
+        dao.removeWorks(USER_ORCID);
+        
+        long finalNumberOfElements = dao.countAll();
+        long finalNumberOfOtherUserElements = dao.findWorks(OTHER_USER_ORCID, 1L).size();
+        long finalNumberOfElementsThatBelogsToUser = dao.findWorks(USER_ORCID, 1L).size();
+        assertEquals(0, finalNumberOfElementsThatBelogsToUser);
+        assertEquals(otherUserElements, finalNumberOfOtherUserElements);
+        assertEquals((initialNumber - elementThatBelogsToUser), finalNumberOfElements);
     }
 }
