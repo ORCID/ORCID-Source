475431287b43f2a049179739643c4113ac1f8c9b
Manuel Calvo <m.calvo@ost.orcid.org>
2017-07-11 12:04:09 -0600

    Ng2 migration: updating code to be ts compiler ready

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
@@ -274,61 +274,6 @@ angular.module('orcidApp').controller('externalConsortiumCtrl',['$scope', '$comp
 
 
 
-
-
-
-angular.module('orcidApp').controller('removeSecQuestionCtrl',['$scope','$compile', function ($scope,$compile) {
-    $scope.showSection = false;
-    $scope.orcidOrEmail = '';
-    $scope.result= '';
-
-    $scope.toggleSection = function(){
-        $scope.showSection = !$scope.showSection;
-        $('#remove_security_question_section').toggle();
-    };
-
-    $scope.removeSecurityQuestion = function() {
-        $.ajax({
-            url: getBaseUri()+'/admin-actions/remove-security-question.json',
-            type: 'POST',
-            data: $scope.orcidOrEmail,
-            contentType: 'application/json;charset=UTF-8',
-            dataType: 'text',
-            success: function(data){
-                $scope.$apply(function(){
-                    $scope.result=data;
-                    $scope.orcid = '';
-                });
-                $scope.closeModal();
-            }
-        }).fail(function(error) {
-            // something bad is happening!
-            console.log("Error generating random string");
-        });
-    };
-
-    $scope.confirmRemoveSecurityQuestion = function(){
-        if($scope.orcid != '') {
-            $.colorbox({
-                html : $compile($('#confirm-remove-security-question').html())($scope),
-                    scrolling: true,
-                    onLoad: function() {
-                    $('#cboxClose').remove();
-                },
-                scrolling: true
-            });
-
-            $.colorbox.resize({width:"450px" , height:"150px"});
-        }
-    };
-
-    $scope.closeModal = function() {
-        $scope.orcidOrEmail = '';
-        $scope.result= '';
-        $.colorbox.close();
-    };
-}]);
-
 angular.module('orcidApp').controller('profileLockingCtrl', ['$scope', '$compile', function($scope, $compile){
     $scope.orcidToLock = '';
     $scope.orcidToUnlock = '';
