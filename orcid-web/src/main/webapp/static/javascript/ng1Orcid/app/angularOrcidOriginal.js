0e369d4d859d9015fa1bbd9cd3872846f438bd9e
rcpeters <info@rcpeters.com>
2017-07-12 14:53:14 -0700

    migrate LoginLayoutController.ts to it's own file

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
@@ -3812,70 +3812,7 @@ angular.module('orcidApp').controller('OauthAuthorizationController',['$scope', 
     $scope.loadRequestInfoForm();          
 }]);
 
-angular.module('orcidApp').controller('LoginLayoutController',['$scope', function ($scope){
-    
-    $scope.personalLogin = true; // Flag to show or not Personal or
-                                    // Institution Account Login
-    $scope.scriptsInjected = false; // Flag to show or not the spinner
-    $scope.counter = 0; // To hide the spinner when the second script has been
-                        // loaded, not the first one.
-    $scope.showDeactivatedError = false;
-    $scope.showReactivationSent = false;
 
-
-    $scope.showPersonalLogin = function () {        
-        $scope.personalLogin = true;    
-    };
-    
-    $scope.showInstitutionLogin = function () {
-        $scope.personalLogin = false; // Hide Personal Login
-        
-        if(!$scope.scriptsInjected){ // If shibboleth scripts haven't been
-                                        // loaded yet.
-            $scope.addScript('/static/javascript/shibboleth-embedded-ds/1.1.0/idpselect_config.js', function(){
-                $scope.addScript('/static/javascript/shibboleth-embedded-ds/1.1.0/idpselect.js', function(){
-                    $scope.scriptsInjected = true;
-                    $scope.$apply();
-                    addShibbolethGa($scope.gaString);
-                });
-            });
-        };
-    };
-    
-    $scope.addScript = function(url, onLoadFunction){        
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.src = getBaseUri() + url + '?v=' + orcidVar.version;
-        script.onload =  onLoadFunction;
-        head.appendChild(script); // Inject the script
-    };
-    
-    $scope.loginSocial = function(idp) {
-        orcidGA.gaPush(['send', 'event', 'RegGrowth', 'Sign-In-Submit-Social', idp]);
-        return false;
-    };
-    
-    $scope.showDeactivationError = function() {
-        $scope.showDeactivatedError = true;
-        $scope.showReactivationSent = false;
-        $scope.$apply();
-    };
-
-    $scope.sendReactivationEmail = function () {
-       $scope.showDeactivatedError = false;
-       $scope.showReactivationSent = true;
-       $.ajax({
-           url: getBaseUri() + '/sendReactivation.json',
-           type: "POST",
-           data: { email: $('#userId').val() },
-           dataType: 'json',
-       }).fail(function(){
-       // something bad is happening!
-           console.log("error sending reactivation email");
-       });
-   };
-    
-}]);
 
 angular.module('orcidApp').controller('LinkAccountController',['$scope', 'discoSrvc', function ($scope, discoSrvc){
     
@@ -3945,7 +3882,6 @@ angular.module('orcidApp').directive('bindHtmlCompile', ['$compile', function ($
         }
     };
 }]);
-
 
 angular.module('orcidApp').directive('scroll', function () {
     return {
