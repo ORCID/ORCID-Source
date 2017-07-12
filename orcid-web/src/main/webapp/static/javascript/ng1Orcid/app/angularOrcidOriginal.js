bce1fb5040fda0ec1f067b3299eda0fe44a3cbe7
Liz Krznarich <ejkrznarich@gmail.com>
2017-07-12 11:49:07 -0400

    migrate internalConsortiumCtrl to typescript

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/angularOrcidOriginal.js
@@ -28,6 +28,7 @@
 *******************************************************************************/
 
 
+alert('It works');
 
 angular.module('orcidApp').controller('SearchCtrl',['$scope', '$compile', function ($scope, $compile){
     $scope.hasErrors = false;
@@ -1477,78 +1478,6 @@ angular.module('orcidApp').controller('manageMembersCtrl',['$scope', '$compile',
     $scope.selectAll = function($event){
         $event.target.select();
     };
-}]);
-
-/**
- * Internal consortium controller
- */
-angular.module('orcidApp').controller('internalConsortiumCtrl',['$scope', '$compile', function manageConsortiumCtrl($scope, $compile) {    
-    $scope.showFindModal = false;
-    $scope.consortium = null;
-
-    $scope.toggleFindConsortiumModal = function() {
-        $scope.showFindModal = !$scope.showFindModal;
-    };
-    
-    /**
-     * FIND
-     */
-    $scope.findConsortium = function() {
-        $.ajax({
-            url: getBaseUri()+'/manage-members/find-consortium.json?id=' + encodeURIComponent($scope.salesForceId),
-            type: 'GET',
-            dataType: 'json',
-            success: function(data){
-                $scope.consortium = data;
-                $scope.$apply();
-            }
-        }).fail(function(error) {
-            // something bad is happening!
-            console.log("Error finding the consortium");
-        });
-    };
-    
-    $scope.confirmUpdateConsortium = function() {
-        $.colorbox({
-            html : $compile($('#confirm-modal-consortium').html())($scope),
-                scrolling: true,
-                onLoad: function() {
-                $('#cboxClose').remove();
-            },
-            scrolling: true
-        });
-
-        $.colorbox.resize({width:"450px" , height:"175px"});
-    };
-    
-    $scope.updateConsortium = function() {
-        $.ajax({
-            url: getBaseUri()+'/manage-members/update-consortium.json',
-            contentType: 'application/json;charset=UTF-8',
-            type: 'POST',
-            dataType: 'json',
-            data: angular.toJson($scope.consortium),
-            success: function(data){
-                $scope.$apply(function(){
-                    if(data.errors.length == 0){
-                        $scope.consortium = null;
-                        $scope.success_edit_member_message = om.get('manage_member.edit_member.success');
-                    } else {
-                        $scope.consortium = data;
-                    }
-                });
-                $scope.closeModal();
-            }
-        }).fail(function(error) {
-            // something bad is happening!
-            console.log("Error updating the consortium");
-        });
-    };
-    
-    $scope.closeModal = function() {
-        $.colorbox.close();
-    };
-    
 }]);
 
 /**
