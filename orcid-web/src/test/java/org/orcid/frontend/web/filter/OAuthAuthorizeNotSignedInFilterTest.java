7f7065d58a9a82154519127aa75b0565fe7caa24
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-07-20 11:42:16 -0600

    Fix unit tests

--- orcid-web/src/test/java/org/orcid/frontend/web/filter/OAuthAuthorizeNotSignedInFilterTest.java
+++ orcid-web/src/test/java/org/orcid/frontend/web/filter/OAuthAuthorizeNotSignedInFilterTest.java
@@ -16,6 +16,10 @@
  */
 package org.orcid.frontend.web.filter;
 
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
@@ -30,17 +34,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.orcid.core.constants.OrcidOauth2Constants;
 import org.orcid.core.manager.impl.OrcidUrlManager;
 import org.orcid.core.oauth.OrcidProfileUserDetails;
+import org.orcid.core.togglz.Features;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
-
-import static org.mockito.Mockito.*;
+import org.togglz.junit.TogglzRule;
 
 public class OAuthAuthorizeNotSignedInFilterTest {
 
@@ -71,6 +77,9 @@ public class OAuthAuthorizeNotSignedInFilterTest {
     @Mock
     OrcidProfileUserDetails orcidProfileUserDetails;
 
+    @Rule
+    public TogglzRule togglzRule = TogglzRule.allDisabled(Features.class);
+    
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
@@ -80,6 +89,9 @@ public class OAuthAuthorizeNotSignedInFilterTest {
         when(request.getParameterMap()).thenReturn(new HashMap<String, String[]>());
         when(request.getScheme()).thenReturn("i hate you with all my heart spring mvc");
         when(request.getRequestURL()).thenReturn(new StringBuffer("really, we should break up"));
+        
+        // Disable all features by default
+        togglzRule.disableAll();
     }
 
     @Test
@@ -154,5 +166,50 @@ public class OAuthAuthorizeNotSignedInFilterTest {
         verify(response, never()).sendRedirect(Mockito.anyString());
         verify(chain).doFilter(Mockito.any(), Mockito.any());
     }
+    
+    @Test
+    public void oauth2ScreensFeatureEnabledTest() throws IOException, ServletException {
+        when(request.getContextPath()).thenReturn("http://test.com");
+        when(request.getRequestURI()).thenReturn("http://test.com/oauth/authorize");
+        when(request.getQueryString()).thenReturn("test_param=param");
+        when(request.getSession()).thenReturn(session);
+        when(request.getSession(false)).thenReturn(session);
+        when(session.getAttribute("SPRING_SECURITY_CONTEXT")).thenReturn(context);
 
+        togglzRule.enable(Features.OAUTH_2SCREENS);
+        oaFilter.doFilter((ServletRequest) request, (ServletResponse) response, chain);        
+        
+        verify(response).sendRedirect("http://test.com/signin?oauth&test_param=param");
+        verify(chain, never()).doFilter(Mockito.any(), Mockito.any());
+    }
+    
+    @Test
+    public void oauth2ScreensFeatureDisabledTest() throws IOException, ServletException {
+        when(request.getContextPath()).thenReturn("http://test.com");
+        when(request.getRequestURI()).thenReturn("http://test.com/oauth/authorize");
+        when(request.getQueryString()).thenReturn("test_param=param");
+        when(request.getSession()).thenReturn(session);
+        when(request.getSession(false)).thenReturn(session);
+        when(session.getAttribute("SPRING_SECURITY_CONTEXT")).thenReturn(context);
+        
+        oaFilter.doFilter((ServletRequest) request, (ServletResponse) response, chain);
+        
+        verify(response).sendRedirect("http://test.com/oauth/signin?test_param=param");
+        verify(chain, never()).doFilter(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    public void oauth2ScreensFeatureFlagUsedTest() throws IOException, ServletException {
+        when(request.getContextPath()).thenReturn("http://test.com");
+        when(request.getRequestURI()).thenReturn("http://test.com/oauth/authorize");
+        when(request.getQueryString()).thenReturn("test_param=param&" + OrcidOauth2Constants.OAUTH_2SCREENS);
+        when(request.getSession()).thenReturn(session);
+        when(request.getSession(false)).thenReturn(session);
+        when(session.getAttribute("SPRING_SECURITY_CONTEXT")).thenReturn(context);
+        
+        oaFilter.doFilter((ServletRequest) request, (ServletResponse) response, chain);
+        
+        verify(response).sendRedirect("http://test.com/signin?oauth&test_param=param&" + OrcidOauth2Constants.OAUTH_2SCREENS);
+        verify(chain, never()).doFilter(Mockito.any(), Mockito.any());
+    }
 }
