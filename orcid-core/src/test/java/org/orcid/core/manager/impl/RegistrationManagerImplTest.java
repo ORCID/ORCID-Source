22bd6d5a82b3d37f46c2ae372fd563681f9a4a77
Liz Krznarich <ejkrznarich@gmail.com>
2017-08-16 20:19:39 -0500

    add tests for additional emails

--- orcid-core/src/test/java/org/orcid/core/manager/impl/RegistrationManagerImplTest.java
+++ orcid-core/src/test/java/org/orcid/core/manager/impl/RegistrationManagerImplTest.java
@@ -26,8 +26,10 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -97,6 +99,24 @@ public class RegistrationManagerImplTest extends DBUnitTest {
         assertNotNull(map);
         assertEquals(userOrcid, map.get(email));
     }
+    
+    @Test
+    public void testCreateMinimalRegistrationMultipleEmails() {
+        String email = "new_user_" + System.currentTimeMillis() + "@test.orcid.org";
+        String additionalEmail = "new_user_addl_" + System.currentTimeMillis() + "@test.orcid.org";
+        List<Text> emailsAdditionalList = new ArrayList<Text>();
+        Text emailAdditional = new Text();
+        emailAdditional.setValue(additionalEmail);
+        emailsAdditionalList.add(emailAdditional);
+        Registration form = createRegistrationFormMultipleEmails(email, emailsAdditionalList, true);        
+        String userOrcid = registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
+        assertNotNull(userOrcid);
+        assertTrue(OrcidStringUtils.isValidOrcid(userOrcid));
+        Map<String, String> map = emailManager.findOricdIdsByCommaSeparatedEmails(email + "," + additionalEmail);
+        assertNotNull(map);
+        assertEquals(userOrcid, map.get(email));
+        assertEquals(userOrcid, map.get(additionalEmail));
+    }
 
     @Test
     public void testCreateMinimalRegistrationWithExistingClaimedEmail() {
@@ -109,6 +129,32 @@ public class RegistrationManagerImplTest extends DBUnitTest {
         
         //Then try to create it again
         form = createRegistrationForm(email, true);  
+        try {
+            registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
+            fail();
+        } catch(InvalidRequestException e) {
+            assertEquals("Unable to register user due: Email " + email + " already exists and is claimed, so, it can't be used again", e.getMessage());
+        } catch(Exception e) {
+            fail();
+        }        
+    }
+    
+    @Test
+    public void testCreateMinimalRegistrationWithExistingClaimedEmailAdditional() {
+        //Create the user
+        String email = "new_user_" + System.currentTimeMillis() + "@test.orcid.org";
+        Registration form = createRegistrationForm(email, true);        
+        String userOrcid = registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
+        assertNotNull(userOrcid);
+        assertTrue(OrcidStringUtils.isValidOrcid(userOrcid));
+        
+        //Then try to create it again, with additional email set to primary email of existing user
+        String email2 = "new_user2_" + System.currentTimeMillis() + "@test.orcid.org";
+        List<Text> emailsAdditionalList = new ArrayList<Text>();
+        Text emailAdditional = new Text();
+        emailAdditional.setValue(email);
+        emailsAdditionalList.add(emailAdditional);
+        form = createRegistrationFormMultipleEmails(email2, emailsAdditionalList, true);  
         try {
             registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
             fail();
@@ -133,6 +179,34 @@ public class RegistrationManagerImplTest extends DBUnitTest {
         
         try {
             Registration form = createRegistrationForm(email, true);
+            registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
+            fail();
+        } catch(InvalidRequestException e) {
+            assertEquals("Unable to register user due: Autodeprecate is not enabled for " + email, e.getMessage());
+        } catch(Exception e) {
+            fail();
+        }     
+    }
+    
+    @Test
+    public void testCreateMinimalRegistrationWithExistingUnclaimedEmailAdditionalNotAutoDeprecatable() {
+        //Create the user, but set it as unclaimed
+        String email = "new_user_" + System.currentTimeMillis() + "@test.orcid.org";
+                
+        //Create a record by a member
+        OrcidProfile orcidProfile = createBasicProfile(email, false, CLIENT_ID_AUTODEPRECATE_DISABLED);
+        orcidProfile = orcidProfileManager.createOrcidProfile(orcidProfile, true, true);
+        assertNotNull(orcidProfile);
+        assertNotNull(orcidProfile.getOrcidIdentifier());
+        assertNotNull(orcidProfile.getOrcidIdentifier().getPath());
+        
+        try {
+            String email2 = "new_user2_" + System.currentTimeMillis() + "@test.orcid.org";
+            List<Text> emailsAdditionalList = new ArrayList<Text>();
+            Text emailAdditional = new Text();
+            emailAdditional.setValue(email);
+            emailsAdditionalList.add(emailAdditional);
+            Registration form = createRegistrationFormMultipleEmails(email2, emailsAdditionalList, true);  
             registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
             fail();
         } catch(InvalidRequestException e) {
@@ -172,6 +246,92 @@ public class RegistrationManagerImplTest extends DBUnitTest {
     }
     
     @Test
+    public void testCreateMinimalRegistrationWithExistingEmailAdditionalThatCanBeAutoDeprecated() {
+        //Create the user, but set it as unclaimed
+        String email = "new_user_" + System.currentTimeMillis() + "@test.orcid.org";
+        
+        //Create a record by a member
+        OrcidProfile orcidProfile = createBasicProfile(email, false, CLIENT_ID_AUTODEPRECATE_ENABLED);
+        orcidProfile = orcidProfileManager.createOrcidProfile(orcidProfile, true, true);
+        assertNotNull(orcidProfile);
+        assertNotNull(orcidProfile.getOrcidIdentifier());
+        assertNotNull(orcidProfile.getOrcidIdentifier().getPath());
+        String orcidBefore = orcidProfile.getOrcidIdentifier().getPath();
+        
+        Map<String, String> map1 = emailManager.findOricdIdsByCommaSeparatedEmails(email);
+        assertNotNull(map1);
+        assertEquals(orcidBefore, map1.get(email));  
+        
+        //Then try to create it again, with additional email set to primary email of existing user
+        String email2 = "new_user2_" + System.currentTimeMillis() + "@test.orcid.org";
+        List<Text> emailsAdditionalList = new ArrayList<Text>();
+        Text emailAdditional = new Text();
+        emailAdditional.setValue(email);
+        emailsAdditionalList.add(emailAdditional);
+        Registration form = createRegistrationFormMultipleEmails(email2, emailsAdditionalList, true);
+        String orcidAfter = registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
+        assertTrue(OrcidStringUtils.isValidOrcid(orcidAfter));
+        
+        assertThat(orcidAfter, is(not(equalTo(orcidBefore))));
+        
+        Map<String, String> map2 = emailManager.findOricdIdsByCommaSeparatedEmails(email + "," + email2);
+        assertNotNull(map2);
+        assertEquals(orcidAfter, map2.get(email));  
+        assertEquals(orcidAfter, map2.get(email2));
+    }
+    
+    @Test
+    public void testCreateMinimalRegistrationWithExistingTwoEmailsAdditionalThatCanBeAutoDeprecated() {
+        //Create a user, but set it as unclaimed
+        String email = "new_user_" + System.currentTimeMillis() + "@test.orcid.org";
+        
+        //Create a record by a member
+        OrcidProfile orcidProfile = createBasicProfile(email, false, CLIENT_ID_AUTODEPRECATE_ENABLED);
+        orcidProfile = orcidProfileManager.createOrcidProfile(orcidProfile, true, true);
+        assertNotNull(orcidProfile);
+        assertNotNull(orcidProfile.getOrcidIdentifier());
+        assertNotNull(orcidProfile.getOrcidIdentifier().getPath());
+        String orcidBefore = orcidProfile.getOrcidIdentifier().getPath();
+        
+        Map<String, String> map1 = emailManager.findOricdIdsByCommaSeparatedEmails(email);
+        assertNotNull(map1);
+        assertEquals(orcidBefore, map1.get(email));
+        
+        //Create another user, but set it as unclaimed
+        String email2 = "new_user2_" + System.currentTimeMillis() + "@test.orcid.org";
+        
+        //Create another record by a member
+        OrcidProfile orcidProfile2 = createBasicProfile(email2, false, CLIENT_ID_AUTODEPRECATE_ENABLED);
+        orcidProfile2 = orcidProfileManager.createOrcidProfile(orcidProfile2, true, true);
+        assertNotNull(orcidProfile2);
+        assertNotNull(orcidProfile.getOrcidIdentifier());
+        assertNotNull(orcidProfile2.getOrcidIdentifier().getPath());
+        String orcidBefore2 = orcidProfile2.getOrcidIdentifier().getPath();
+        
+        Map<String, String> map2 = emailManager.findOricdIdsByCommaSeparatedEmails(email2);
+        assertNotNull(map2);
+        assertEquals(orcidBefore2, map2.get(email2)); 
+        
+        try {
+            String email3 = "new_user3_" + System.currentTimeMillis() + "@test.orcid.org";
+            List<Text> emailsAdditionalList = new ArrayList<Text>();
+            Text emailAdditional = new Text();
+            emailAdditional.setValue(email);
+            emailsAdditionalList.add(emailAdditional);
+            Text emailAdditional2 = new Text();
+            emailAdditional2.setValue(email2);
+            emailsAdditionalList.add(emailAdditional2);
+            Registration form = createRegistrationFormMultipleEmails(email3, emailsAdditionalList, true);
+            registrationManager.createMinimalRegistration(form, true, java.util.Locale.ENGLISH, "0.0.0.0");
+            fail();
+        } catch(InvalidRequestException e) {
+            assertEquals("Unable to register user due: More than 2 duplicate emails", e.getMessage());
+        } catch(Exception e) {
+            fail();
+        } 
+    }
+    
+    @Test
     public void testPasswordIsCommon() {
         assertTrue(registrationManager.passwordIsCommon("baseball"));
         assertTrue(registrationManager.passwordIsCommon("dragon"));
@@ -189,6 +349,17 @@ public class RegistrationManagerImplTest extends DBUnitTest {
         Registration registration = new Registration();
         registration.setPassword(Text.valueOf("password"));
         registration.setEmail(Text.valueOf(email));
+        registration.setFamilyNames(Text.valueOf("User"));
+        registration.setGivenNames(Text.valueOf("New"));
+        registration.setCreationType(Text.valueOf(CreationMethod.DIRECT.value()));                       
+        return registration;
+    }
+    
+    private Registration createRegistrationFormMultipleEmails(String email, List<Text> emailsAdditionalList, boolean claimed) {
+        Registration registration = new Registration();
+        registration.setPassword(Text.valueOf("password"));
+        registration.setEmail(Text.valueOf(email));
+        registration.setEmailsAdditional(emailsAdditionalList);
         registration.setFamilyNames(Text.valueOf("User"));
         registration.setGivenNames(Text.valueOf("New"));
         registration.setCreationType(Text.valueOf(CreationMethod.DIRECT.value()));                       
