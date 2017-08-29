e7b21a1b3306757a741cac4a0e6b846f098188f6
Liz Krznarich <ejkrznarich@gmail.com>
2017-08-29 16:38:46 -0500

    add function to update default visibility

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/worksPrivacyPreferences/worksPrivacyPreferences.component.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/worksPrivacyPreferences/worksPrivacyPreferences.component.ts
@@ -5,12 +5,13 @@ declare var orcidVar: any;
 import * as angular from 'angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common'; 
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/switchMap';
 
 import { PrefsSrvc } from './../../services/prefs.service.ts';
 import { Preferences } from './../../services/preferences';
+import { CommonSrvc } from './../../services/common.service.ts';
 
 //Ng1 hybrid syntax
 /*worksPrivacyPreferencesCtrl {
@@ -21,25 +22,44 @@ export const worksPrivacyPreferencesCmp = {
     controllerAs: 'ctrl'
 };*/
 
-import { Component, Inject, Input, OnInit } from '@angular/core'; 
+import { AfterViewInit, Component, Directive, Inject, Injector, Input, ViewChild, ElementRef } from '@angular/core'; 
 @Component({
     selector: 'works-privacy-preferences-ng2',
     template:  scriptTmpl("works-privacy-preferences-ng2-template"),
-    providers: [PrefsSrvc]
+    providers: [PrefsSrvc, CommonSrvc]
 })
 export class WorksPrivacyPreferencesComponent implements OnInit {
+    private response: any;
     preferences: Preferences[];
     privacyHelp: any;
     showElement: any;
+    developer_tools_enabled: boolean
+    send_member_update_requests: boolean
+    send_orcid_news: boolean
+    send_change_notifications: boolean
+    email_frequency: string;
+    default_visibility: string;
+    notifications_enabled: boolean
+    send_administrative_change_notifications: boolean
     
     constructor(
 
-        private prefsSrvc: PrefsSrvc
+        private prefsSrvc: PrefsSrvc,
+        private commonSrvc: CommonSrvc,
+        
 
     ) {
 
         this.privacyHelp = {};
         this.showElement = {};
+        this.developer_tools_enabled = false;
+        this.send_member_update_requests = false;
+        this.send_orcid_news = false;
+        this.send_change_notifications = false;
+        this.email_frequency = '';
+        this.default_visibility = '';
+        this.notifications_enabled = false;
+        this.send_administrative_change_notifications = false;
     }
 
     /*hideTooltip(el): void {
@@ -54,12 +74,25 @@ export class WorksPrivacyPreferencesComponent implements OnInit {
         if (document.documentElement.className.indexOf('no-touch')  == -1 ) {
             this.privacyHelp[key]=!this.privacyHelp[key];
         }
-    };
-
-    updateActivitiesVisibilityDefault(priv, $event): void {
-        this.prefsSrvc.prefs['default_visibility'] = priv;        
-        this.prefsSrvc.updateDefaultVisibility();        
     };*/
+
+    updateActivitiesVisibilityDefault(priv: string, $event: any): void {
+        this.preferences['default_visibility'] = priv;      
+        this.prefsSrvc.updateDefaultVisibility(this.preferences).subscribe(
+            (response) => {
+                this.response = response;
+                console.log(this.preferences);
+                let preferences_parsed = null;
+                preferences_parsed = JSON.parse(JSON.stringify(this.preferences, null, 2));
+                console.log("preferences_parsed", preferences_parsed);
+                this.default_visibility = preferences_parsed.default_visibility;
+            },
+            (err) => {
+                console.log(err);
+            },
+            () => {}
+        );       
+    };
 
     getPreferences(): void {
         this.prefsSrvc.getPreferences().subscribe(
@@ -68,16 +101,7 @@ export class WorksPrivacyPreferencesComponent implements OnInit {
                 this.preferences = preferences;
                 preferences_parsed = JSON.parse(JSON.stringify(this.preferences, null, 2));
                 console.log("preferences_parsed", preferences_parsed);
-
-                /*this.description = collection_parsed.form.description;
-                this.fullOrcidId = collection_parsed.owner.fullOrcidId;
-                this.orcid = collection_parsed.owner.orcid;
-                this.title = collection_parsed.form.title;
-                this.username = collection_parsed.owner.name;
-
-                if ( this.description.length > 0 && this.title.length > 0 ) {
-                    this.formEmptyOnLoad = false;
-                }*/
+                this.default_visibility = preferences_parsed.default_visibility;
             }
         );
     }
--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/services/prefs.service.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/services/prefs.service.ts
@@ -1,7 +1,9 @@
+declare var getBaseUri: any;
+
 import { Injectable } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { CookieXSRFStrategy, HttpModule, XSRFStrategy } from '@angular/http';
 import { JsonpModule } from '@angular/http';
-import { Http, Response, Jsonp } from '@angular/http';
+import { Headers, Http, Response, RequestOptions, Jsonp } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
@@ -9,10 +11,16 @@ import { Preferences } from './preferences';
 
 @Injectable()
 export class PrefsSrvc {
+    
+    saved: boolean;
 
     constructor(
         private http: Http,
-        private jsonp: Jsonp) { }
+        private jsonp: Jsonp) {
+
+            this.saved = false;
+
+         }
 
     private handleError (error: Response | any) {
         let errMsg: string;
@@ -30,4 +38,44 @@ export class PrefsSrvc {
     getPreferences(): Observable<Preferences[]> {
         return this.http.get( 'account/preferences.json' ).map(( res: Response ) => res.json()).catch(this.handleError);
     }
+
+    //update email frequency
+
+    //update notification prefs
+
+    //update default visibility
+
+    updateDefaultVisibility( data: any ): Observable<any> {
+        const headers = new Headers({ 'Content-Type': 'application/json;charset=UTF-8', 'Accept': 'text/plain'});
+        const options = new RequestOptions({ headers: headers });
+        return this.http.post(
+            getBaseUri() + '/account/default_visibility.json',
+            data.default_visibility,
+            options
+        ).map(( res: Response ) => res.text()).catch(this.handleError);
+        
+
+    }
+
+    /*updateDefaultVisibility: function() {
+            $.ajax({
+                url: getBaseUri() + '/account/default_visibility.json',
+                type: 'POST',
+                data: serv.prefs['default_visibility'],
+                contentType: 'application/json;charset=UTF-8',
+                dataType: 'text',
+                success: function(data) {                    
+                    serv.saved = true;
+                    $rootScope.$apply();
+                }
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+                console.log(errorThrown);
+                console.log(jqXHR);
+                // something bad is happening!
+                console.log("error with prefs");
+            });
+        }*/ 
+
+    //clear message
 }
