ba2e31c53690f16898a6bd867772a8cc255fbe8d
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-09-26 14:08:01 -0600

    Adding ar and cs languages plus the dev languaes as well

--- orcid-web/src/main/java/org/orcid/frontend/spring/i18n/OrcidCookieLocaleResolver.java
+++ orcid-web/src/main/java/org/orcid/frontend/spring/i18n/OrcidCookieLocaleResolver.java
@@ -30,8 +30,9 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 public class OrcidCookieLocaleResolver extends CookieLocaleResolver {
 
-    public static final List<Locale> availableLocales = Arrays.asList(Locale.ENGLISH, new Locale("es"), Locale.FRENCH, Locale.ITALIAN, Locale.JAPANESE, Locale.KOREAN,
+    public static final List<Locale> availableLocales = Arrays.asList(new Locale("ar"), new Locale("cs"), Locale.ENGLISH, new Locale("es"), Locale.FRENCH, Locale.ITALIAN, Locale.JAPANESE, Locale.KOREAN,
             new Locale("pt"), new Locale("ru"), Locale.SIMPLIFIED_CHINESE, Locale.TRADITIONAL_CHINESE);
+    public static final List<Locale> devLocales = Arrays.asList(new Locale("lr"), new Locale("rl"), new Locale("xx"));
 
     @Override
     public void setLocaleContext(HttpServletRequest request, HttpServletResponse response, LocaleContext localeContext) {
@@ -45,7 +46,7 @@ public class OrcidCookieLocaleResolver extends CookieLocaleResolver {
 
             if (!availableLocales.contains(locale)) {
                 Locale justLang = new Locale(locale.getLanguage());
-                if (availableLocales.contains(justLang)) {
+                if (availableLocales.contains(justLang) || devLocales.contains(justLang)) {
                     locale = justLang;
                 } else {
                     locale = Locale.ENGLISH;
