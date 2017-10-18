fd998d4a472b8d44e59c62de3c07c18d9617ccdd
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-10-18 17:22:33 -0600

    Working on create member

--- orcid-core/src/main/java/org/orcid/core/manager/impl/MembersManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/MembersManagerImpl.java
@@ -16,13 +16,18 @@
  */
 package org.orcid.core.manager.impl;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
 
+import org.orcid.core.constants.DefaultPreferences;
 import org.orcid.core.exception.OrcidClientGroupManagementException;
 import org.orcid.core.locale.LocaleManager;
 import org.orcid.core.manager.ClientDetailsManager;
@@ -31,19 +36,29 @@ import org.orcid.core.manager.EmailManager;
 import org.orcid.core.manager.EncryptionManager;
 import org.orcid.core.manager.MembersManager;
 import org.orcid.core.manager.OrcidClientGroupManager;
+import org.orcid.core.manager.OrcidGenerationManager;
 import org.orcid.core.manager.ProfileEntityCacheManager;
 import org.orcid.core.manager.ProfileEntityManager;
 import org.orcid.core.manager.ThirdPartyLinkManager;
 import org.orcid.core.manager.read_only.ClientManagerReadOnly;
+import org.orcid.core.security.OrcidWebRole;
 import org.orcid.jaxb.model.client_v2.Client;
 import org.orcid.jaxb.model.clientgroup.MemberType;
 import org.orcid.jaxb.model.clientgroup.OrcidClient;
 import org.orcid.jaxb.model.clientgroup.OrcidClientGroup;
+import org.orcid.jaxb.model.common_v2.OrcidType;
+import org.orcid.jaxb.model.common_v2.Visibility;
+import org.orcid.jaxb.model.message.CreationMethod;
 import org.orcid.jaxb.model.message.ErrorDesc;
 import org.orcid.persistence.dao.ProfileDao;
+import org.orcid.persistence.jpa.entities.EmailEntity;
+import org.orcid.persistence.jpa.entities.OrcidGrantedAuthority;
 import org.orcid.persistence.jpa.entities.ProfileEntity;
+import org.orcid.persistence.jpa.entities.RecordNameEntity;
 import org.orcid.pojo.ajaxForm.Member;
 import org.orcid.pojo.ajaxForm.PojoUtil;
+import org.orcid.pojo.ajaxForm.Registration;
+import org.orcid.pojo.ajaxForm.Text;
 import org.orcid.utils.OrcidStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -86,13 +101,17 @@ public class MembersManagerImpl implements MembersManager {
     @Resource
     private ClientManagerReadOnly clientManagerReadOnly;
     
+    @Resource
+    private OrcidGenerationManager orcidGenerationManager;
+    
     @Override
-    public Member createMember(String memberName, String email, String salesforceId) {
-        //TODO: Refactor 
-        OrcidClientGroup orcidClientGroup = newMember.toOrcidClientGroup();
-        orcidClientGroup = orcidClientGroupManager.createGroup(orcidClientGroup);
-        newMember.setGroupOrcid(Text.valueOf(orcidClientGroup.getGroupOrcid()));
-        return newMember;
+    public Member createMember(String memberName, String email, String salesforceId, ) {
+        String memberOrcidId = createMemberProfile(memberName, email, salesforceId); 
+        Member member = new Member();
+        member.setEmail(Text.valueOf(email));
+        member.setGroupName(Text.valueOf(memberName));
+        member.setGroupOrcid(Text.valueOf(memberOrcidId));
+        
     }
 
     @Override
@@ -178,5 +197,83 @@ public class MembersManagerImpl implements MembersManager {
         thirdPartyLinkManager.updateDatabaseCacheVersion();
         // Evict current cache
         thirdPartyLinkManager.evictAll();
+    }
+    
+    
+    
+    
+    
+    
+    private String createMemberProfile(String memberName, String email, String salesforceId) {
+        Date now = new Date();
+        String orcid = orcidGenerationManager.createNewOrcid();
+        ProfileEntity newRecord = new ProfileEntity();
+        newRecord.setId(orcid);
+        
+        try {
+            newRecord.setHashedOrcid(encryptionManager.sha256Hash(orcid));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        
+        newRecord.setOrcidType(OrcidType.GROUP);
+        newRecord.setDateCreated(now);
+        newRecord.setLastModified(now);
+        newRecord.setSubmissionDate(now);
+        newRecord.setClaimed(true);
+        newRecord.setEnableDeveloperTools(false);
+        newRecord.setRecordLocked(false);
+        newRecord.setReviewed(false);
+        newRecord.setEnableNotifications(DefaultPreferences.NOTIFICATIONS_ENABLED);
+        newRecord.setUsedRecaptchaOnRegistration(false);
+        newRecord.setSendEmailFrequencyDays(Float.valueOf(DefaultPreferences.SEND_EMAIL_FREQUENCY_DAYS));
+        newRecord.setSendMemberUpdateRequests(DefaultPreferences.SEND_MEMBER_UPDATE_REQUESTS);
+        
+        newRecord.setCreationMethod(CreationMethod.DIRECT.value());
+        newRecord.setSendChangeNotifications(false);
+        newRecord.setSendOrcidNews(false);
+        newRecord.setLocale(org.orcid.jaxb.model.common_v2.Locale.EN);
+        // Visibility defaults
+        newRecord.setActivitiesVisibilityDefault(Visibility.PRIVATE);
+        
+        // No password
+        newRecord.setEncryptedPassword(null);
+
+        // Set primary email
+        EmailEntity emailEntity = new EmailEntity();
+        emailEntity.setId(email);
+        emailEntity.setProfile(newRecord);
+        emailEntity.setPrimary(true);
+        emailEntity.setCurrent(true);
+        emailEntity.setVerified(true);
+        // Email is private by default
+        emailEntity.setVisibility(Visibility.PRIVATE);
+        emailEntity.setSourceId(orcid);
+        Set<EmailEntity> emails = new HashSet<>();
+        emails.add(emailEntity);
+        //Add all emails to record
+        newRecord.setEmails(emails);
+
+        // Set the name
+        RecordNameEntity recordNameEntity = new RecordNameEntity();
+        recordNameEntity.setDateCreated(now);
+        recordNameEntity.setLastModified(now);
+        recordNameEntity.setProfile(newRecord);
+        // Name is public by default
+        recordNameEntity.setVisibility(Visibility.PUBLIC);
+        recordNameEntity.setCreditName(memberName);
+        newRecord.setRecordNameEntity(recordNameEntity);
+
+        // Set authority
+        OrcidGrantedAuthority authority = new OrcidGrantedAuthority();
+        authority.setProfileEntity(newRecord);
+        authority.setAuthority(OrcidWebRole.ROLE_GROUP.getAuthority());
+        Set<OrcidGrantedAuthority> authorities = new HashSet<OrcidGrantedAuthority>(1);
+        authorities.add(authority);
+        newRecord.setAuthorities(authorities);
+
+        profileDao.persist(newRecord);
+        profileDao.flush();
+        return orcid;
     }
 }
