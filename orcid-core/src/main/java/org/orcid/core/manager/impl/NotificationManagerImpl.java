e40955d18436e7138879c2b2ff574c8af1ae8e7b
Liz Krznarich <ejkrznarich@gmail.com>
2017-10-26 11:41:05 -0500

    remove unused import

--- orcid-core/src/main/java/org/orcid/core/manager/impl/NotificationManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/NotificationManagerImpl.java
@@ -101,7 +101,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.MessageSource;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 /**
  * @author Will Simpson
