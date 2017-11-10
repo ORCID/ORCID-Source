f8214295772030c50cd4d32bde3a442793d8d392
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-11-10 14:08:04 -0600

    Remove OpenEntityInView filter from our web project

--- orcid-web/src/main/java/org/orcid/frontend/spring/OrcidAuthenticationProvider.java
+++ orcid-web/src/main/java/org/orcid/frontend/spring/OrcidAuthenticationProvider.java
@@ -16,24 +16,25 @@
  */
 package org.orcid.frontend.spring;
 
-import java.util.Set;
-
 import javax.annotation.Resource;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jboss.aerogear.security.otp.Totp;
 import org.orcid.core.manager.BackupCodeManager;
 import org.orcid.core.manager.EncryptionManager;
+import org.orcid.core.manager.ProfileEntityCacheManager;
+import org.orcid.core.manager.SlackManager;
 import org.orcid.core.oauth.OrcidProfileUserDetails;
 import org.orcid.frontend.web.exception.Bad2FARecoveryCodeException;
 import org.orcid.frontend.web.exception.Bad2FAVerificationCodeException;
 import org.orcid.frontend.web.exception.VerificationCodeFor2FARequiredException;
 import org.orcid.jaxb.model.v3.dev1.common.OrcidType;
 import org.orcid.persistence.dao.EmailDao;
-import org.orcid.persistence.dao.ProfileDao;
 import org.orcid.persistence.jpa.entities.EmailEntity;
 import org.orcid.persistence.jpa.entities.ProfileEntity;
 import org.orcid.utils.OrcidStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -42,17 +43,22 @@ import org.springframework.security.core.AuthenticationException;
 
 public class OrcidAuthenticationProvider extends DaoAuthenticationProvider {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrcidAuthenticationProvider.class);
+    
     @Resource
-    private ProfileDao profileDao;
+    private ProfileEntityCacheManager profileEntityCacheManager;
 
     @Resource
-    private EmailDao emailDao;
+    private EmailDao emailDaoReadOnly;
     
     @Resource
     private EncryptionManager encryptionManager;
     
     @Resource
     private BackupCodeManager backupCodeManager;
+    
+    @Resource
+    private SlackManager slackManager;
 
     @Override
     public Authentication authenticate(Authentication auth) throws AuthenticationException {
@@ -91,24 +97,26 @@ public class OrcidAuthenticationProvider extends DaoAuthenticationProvider {
     
     private OrcidProfileUserDetails toOrcidProfileUserDetails(ProfileEntity profileEntity) {
         String orcid = profileEntity.getId();
-        Set<EmailEntity> emails = profileEntity.getEmails();
         
-        for (EmailEntity email : emails) {
-            if (email.getPrimary()) {
-                OrcidType orcidType = OrcidType.valueOf(profileEntity.getOrcidType().name());
-                return new OrcidProfileUserDetails(orcid, email.getId(), profileEntity.getPassword(), orcidType);
-            }
-        }
-        return null;
+        try {
+            EmailEntity primaryEmail = emailDaoReadOnly.findPrimaryEmail(orcid);
+            OrcidType orcidType = OrcidType.valueOf(profileEntity.getOrcidType().name());
+            return new OrcidProfileUserDetails(orcid, primaryEmail.getId(), profileEntity.getPassword(), orcidType);                    
+        } catch(javax.persistence.NoResultException nre) {
+            String message = String.format("User with orcid %s have no primary email", orcid);
+            LOGGER.error(message);
+            slackManager.sendSystemAlert(message);
+            throw nre;
+        }                
     }
 
     private ProfileEntity getProfileEntity(String username) {
         ProfileEntity profile = null;
         if (!StringUtils.isEmpty(username)) {
             if (OrcidStringUtils.isValidOrcid(username)) {
-                profile = profileDao.find(username);
+                profile = profileEntityCacheManager.retrieve(username);
             } else {
-                EmailEntity emailEntity = emailDao.findCaseInsensitive(username);
+                EmailEntity emailEntity = emailDaoReadOnly.findCaseInsensitive(username);
                 if (emailEntity != null) {
                     profile = emailEntity.getProfile();
                 }
