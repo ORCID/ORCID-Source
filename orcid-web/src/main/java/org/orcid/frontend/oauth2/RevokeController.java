f6a084865cb5f31025d37d93421593cf0e1776e2
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-11-14 10:47:03 -0600

    Testing

--- orcid-web/src/main/java/org/orcid/frontend/oauth2/RevokeController.java
+++ orcid-web/src/main/java/org/orcid/frontend/oauth2/RevokeController.java
@@ -27,8 +27,6 @@ import org.orcid.pojo.ajaxForm.PojoUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
