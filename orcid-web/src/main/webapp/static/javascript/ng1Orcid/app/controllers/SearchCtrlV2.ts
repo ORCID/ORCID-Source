3900d0b0eb3673b47bf935ac1f46e587b782fb6f
Liz Krznarich <ejkrznarich@gmail.com>
2017-11-14 10:20:14 -0500

    only show each affiliation org once

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/controllers/SearchCtrlV2.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/controllers/SearchCtrlV2.ts
@@ -176,13 +176,16 @@ export const SearchCtrlV2 = angular.module('orcidApp').controller(
                         success: function(data) {
                             if(data.employments){
                                 for(var i in data.employments['employment-summary']){
-                                    result['affiliations'].push(data.employments['employment-summary'][i]['organization']['name']);
+                                    if (result['affiliations'].indexOf(data.employments['employment-summary'][i]['organization']['name']) < 0){
+                                        result['affiliations'].push(data.employments['employment-summary'][i]['organization']['name']);
+                                    }
                                 }
                             }
                             if(data.educations){
                                 for(var i in data.educations['education-summary']){
-                                    result['affiliations'].push(data.educations['education-summary'][i]['organization']['name']);
-
+                                    if (result['affiliations'].indexOf(data.educations['education-summary'][i]['organization']['name']) < 0){
+                                        result['affiliations'].push(data.educations['education-summary'][i]['organization']['name']);
+                                    }
                                 }
                             }
                         }
@@ -191,12 +194,7 @@ export const SearchCtrlV2 = angular.module('orcidApp').controller(
                         console.log("error getting name for " + orcid);
                     });  
                 } 
-
-                /*if (result['given-names']) {
-                    name = result['given-names'];
-                } else {
-                    name = "";
-                }*/            
+        
                 return result['affiliations'].join(", "); 
             };
 
