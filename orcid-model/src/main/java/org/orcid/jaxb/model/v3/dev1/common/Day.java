7654a87be45375202c187346dd8d589f130dd6f2
George Nash <bobcaprice@hotmail.com>
2017-12-01 18:32:20 +0100

    Fixed FuzzyDate comparison bug caused by null months / days

--- orcid-model/src/main/java/org/orcid/jaxb/model/v3/dev1/common/Day.java
+++ orcid-model/src/main/java/org/orcid/jaxb/model/v3/dev1/common/Day.java
@@ -46,7 +46,7 @@ import java.io.Serializable;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType( propOrder = { "value" })
 @XmlRootElement(name = "day")
-public class Day implements Serializable, Comparable<Day> {
+public class Day implements Serializable {
 
     /**
      * 
