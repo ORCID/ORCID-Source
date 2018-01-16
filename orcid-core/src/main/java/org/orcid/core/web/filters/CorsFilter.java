26f5a8d6fc7a26b10936a86e8d51da2093752a9c
Liz Krznarich <ejkrznarich@gmail.com>
2018-01-16 13:33:39 -0600

    allow CSRF token from ORCID domains in api CORS filter

--- orcid-core/src/main/java/org/orcid/core/web/filters/CorsFilter.java
+++ orcid-core/src/main/java/org/orcid/core/web/filters/CorsFilter.java
@@ -24,6 +24,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.orcid.pojo.ajaxForm.PojoUtil;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -40,12 +41,19 @@ public class CorsFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+	    
 		response.addHeader("Access-Control-Allow-Origin", "*");
+		
 		if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
 			// CORS "pre-flight" request
 			response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-			response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept");
+			if(crossDomainWebManger.allowed(request)) {
+	                    response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept,x-csrf-token");
+	                }else{
+	                    response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept");
+	                }
 		}
+		
 		filterChain.doFilter(request, response);
 	}
 
