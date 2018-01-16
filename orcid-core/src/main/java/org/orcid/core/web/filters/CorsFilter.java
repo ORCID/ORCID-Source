36b5afdd12afd5018fc4a59fb43742870f2fc12c
Liz Krznarich <ejkrznarich@gmail.com>
2018-01-16 13:35:26 -0600

    removed unused import and extra space

--- orcid-core/src/main/java/org/orcid/core/web/filters/CorsFilter.java
+++ orcid-core/src/main/java/org/orcid/core/web/filters/CorsFilter.java
@@ -24,7 +24,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.orcid.pojo.ajaxForm.PojoUtil;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -41,9 +40,7 @@ public class CorsFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-	    
 		response.addHeader("Access-Control-Allow-Origin", "*");
-		
 		if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
 			// CORS "pre-flight" request
 			response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -53,7 +50,6 @@ public class CorsFilter extends OncePerRequestFilter {
 	                    response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept");
 	                }
 		}
-		
 		filterChain.doFilter(request, response);
 	}
 
