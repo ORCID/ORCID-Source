33cc12e4de46bcd3becf0b088e6accd238f24f7c
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-02-15 17:16:29 -0600

    All new aff types displays well

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/ng1_app.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/ng1_app.ts
@@ -15,9 +15,6 @@ import 'angular-route'
 import { AffiliationModule } 
     from './affiliation/affiliation.ts';
 
-import { AffiliationFormModule } 
-    from './affiliation/affiliationForm.ts';
-
 import { AlsoKnownAsModule } 
     from './alsoKnownAs/alsoKnownAs.ts';
 
@@ -119,7 +116,6 @@ export const orcidApp = angular.module(
         vcRecaptcha,
         uibootstraptypeahead,
         AffiliationModule.name,
-        AffiliationFormModule.name,
         AlsoKnownAsModule.name,
         AlsoKnownAsFormModule.name,
         BiographyModule.name,
