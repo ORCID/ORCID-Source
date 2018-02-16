bcbd805d4d959a548a970dd8faad4659d45129c2
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-02-16 15:11:32 -0600

    Format and remove unused imports

--- orcid-api-common/src/main/java/org/orcid/api/common/filter/Disable12ApiFilter.java
+++ orcid-api-common/src/main/java/org/orcid/api/common/filter/Disable12ApiFilter.java
@@ -25,9 +25,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.MediaType;
 
-import org.orcid.core.api.OrcidApiConstants;
 import org.orcid.core.manager.impl.OrcidUrlManager;
 import org.orcid.core.togglz.Features;
 import org.orcid.core.utils.JsonUtils;
@@ -37,20 +35,19 @@ import org.orcid.pojo.ajaxForm.PojoUtil;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class Disable12ApiFilter extends OncePerRequestFilter {
-
 	private static final String API_12_version = "1.2";
 	private static final Pattern VERSION_PATTERN = Pattern.compile("/v(\\d.*?)/");
-
 	private final String JSON_ERROR_MESSAGE;
-	private final String XML_ERROR_MESSAGE;	
-	
+	private final String XML_ERROR_MESSAGE;
+
 	{
 		OrcidMessage error = new OrcidMessage();
-		error.setErrorDesc(new ErrorDesc("API 1.2 is disabled, please upgrade to the 2.0 API https://members.orcid.org/api/news/xsd-20-update"));				
+		error.setErrorDesc(new ErrorDesc(
+				"API 1.2 is disabled, please upgrade to the 2.0 API https://members.orcid.org/api/news/xsd-20-update"));
 		JSON_ERROR_MESSAGE = JsonUtils.convertToJsonString(error);
 		XML_ERROR_MESSAGE = error.toXmlString();
 	}
-	
+
 	@Resource
 	protected OrcidUrlManager orcidUrlManager;
 
@@ -76,11 +73,11 @@ public class Disable12ApiFilter extends OncePerRequestFilter {
 			} else if (version.equals(API_12_version)) {
 				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 				String accept = request.getHeader("Accept") == null ? null : request.getHeader("Accept").toLowerCase();
-				if(accept.contains("json")) {
+				if (accept.contains("json")) {
 					response.getWriter().println(JSON_ERROR_MESSAGE);
 				} else {
 					response.getWriter().println(XML_ERROR_MESSAGE);
-				}								
+				}
 			} else {
 				filterChain.doFilter(request, response);
 			}
