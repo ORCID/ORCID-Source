8f7dbfa7a5c5d5b2bbca588bb2c7a703d2acf59a
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-02-16 15:09:21 -0600

    Two new Togglz to disable 1.2 API on the pub and members API

---
+++ orcid-api-common/src/main/java/org/orcid/api/common/filter/Disable12ApiFilter.java
@@ -0,0 +1,91 @@
+/**
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+package org.orcid.api.common.filter;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.Resource;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+
+import org.orcid.core.api.OrcidApiConstants;
+import org.orcid.core.manager.impl.OrcidUrlManager;
+import org.orcid.core.togglz.Features;
+import org.orcid.core.utils.JsonUtils;
+import org.orcid.jaxb.model.message.ErrorDesc;
+import org.orcid.jaxb.model.message.OrcidMessage;
+import org.orcid.pojo.ajaxForm.PojoUtil;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class Disable12ApiFilter extends OncePerRequestFilter {
+
+	private static final String API_12_version = "1.2";
+	private static final Pattern VERSION_PATTERN = Pattern.compile("/v(\\d.*?)/");
+
+	private final String JSON_ERROR_MESSAGE;
+	private final String XML_ERROR_MESSAGE;	
+	
+	{
+		OrcidMessage error = new OrcidMessage();
+		error.setErrorDesc(new ErrorDesc("API 1.2 is disabled, please upgrade to the 2.0 API https://members.orcid.org/api/news/xsd-20-update"));				
+		JSON_ERROR_MESSAGE = JsonUtils.convertToJsonString(error);
+		XML_ERROR_MESSAGE = error.toXmlString();
+	}
+	
+	@Resource
+	protected OrcidUrlManager orcidUrlManager;
+
+	protected Features feature;
+
+	public void setFeature(Features f) {
+		this.feature = f;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		if (feature.isActive()) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			String path = httpRequest.getServletPath();
+			Matcher matcher = VERSION_PATTERN.matcher(path);
+			String version = null;
+			if (matcher.lookingAt()) {
+				version = matcher.group(1);
+			}
+			if (PojoUtil.isEmpty(version)) {
+				filterChain.doFilter(request, response);
+			} else if (version.equals(API_12_version)) {
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+				String accept = request.getHeader("Accept") == null ? null : request.getHeader("Accept").toLowerCase();
+				if(accept.contains("json")) {
+					response.getWriter().println(JSON_ERROR_MESSAGE);
+				} else {
+					response.getWriter().println(XML_ERROR_MESSAGE);
+				}								
+			} else {
+				filterChain.doFilter(request, response);
+			}
+		} else {
+			filterChain.doFilter(request, response);
+		}
+	}
+}
