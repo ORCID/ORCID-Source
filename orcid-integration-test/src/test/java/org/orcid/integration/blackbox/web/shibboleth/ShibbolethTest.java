fbcc47f43eb481ebc7a2428a5de383df7743cd1b
Will Simpson <w.simpson@orcid.org>
2018-03-14 18:16:56 +0000

    Improvemed code that waits until angular is ready.

--- orcid-integration-test/src/test/java/org/orcid/integration/blackbox/web/shibboleth/ShibbolethTest.java
+++ orcid-integration-test/src/test/java/org/orcid/integration/blackbox/web/shibboleth/ShibbolethTest.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -81,6 +82,7 @@ public class ShibbolethTest {
         // Marionette does not allow untrusted certs yet
         options.setCapability(FirefoxDriver.MARIONETTE, false);
         WebDriver webDriver = new FirefoxDriver(options);
+        webDriver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
         return webDriver;
     }
 
