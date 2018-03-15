17ad279f599f1080d842ca25ba9b2cca9cf1c037
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-03-15 11:16:42 -0600

    Small change

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/GetAllMyDataController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/GetAllMyDataController.java
@@ -2,6 +2,7 @@ package org.orcid.frontend.web.controllers;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -12,8 +13,10 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
+import org.orcid.core.manager.WorkEntityCacheManager;
 import org.orcid.core.manager.v3.read_only.PersonDetailsManagerReadOnly;
 import org.orcid.jaxb.model.v3.dev1.record.Person;
+import org.orcid.persistence.jpa.entities.WorkLastModifiedEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +28,17 @@ public class GetAllMyDataController extends BaseController {
     @Resource(name = "personDetailsManagerV3")
     private PersonDetailsManagerReadOnly personDetailsManager;
     
+    @Resource
+    private WorkEntityCacheManager workEntityCacheManager;
+    
     @GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM)
     public void getMyData(HttpServletResponse response) throws JAXBException, IOException {
         String currentUserOrcid = getCurrentUserOrcid();
         String fileName = currentUserOrcid + ".zip";
         Person person = personDetailsManager.getPersonDetails(currentUserOrcid);
+        
+        
+        
         JAXBContext context = JAXBContext.newInstance(person.getClass());
         
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -49,4 +58,13 @@ public class GetAllMyDataController extends BaseController {
         response.getOutputStream().write(result.toByteArray());
         response.flushBuffer();             
     }
+        
+    
+    private void generateWorksData(String orcid, Long profileLastModified) {
+        List<WorkLastModifiedEntity> elements = workEntityCacheManager.retrieveWorkLastModifiedList(orcid, profileLastModified);
+        for(WorkLastModifiedEntity entity : elements) {
+            entity.getId()
+        }
+    }
+    
 }
