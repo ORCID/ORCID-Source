978dcb56c85899ebc8d2f75eba17a31796f45236
George Nash <bobcaprice@hotmail.com>
2018-03-20 15:33:54 +0100

    Added client name to output

--- orcid-core/src/main/java/org/orcid/core/cli/logs/AnalysisResults.java
+++ orcid-core/src/main/java/org/orcid/core/cli/logs/AnalysisResults.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.orcid.core.utils.JsonUtils;
+import org.orcid.persistence.dao.ClientDetailsDao;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -17,36 +18,47 @@ public class AnalysisResults implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private long hitsAnalysed;
-    
+
     private List<ClientStats> clientResults = new ArrayList<>();
-  
+
     @JsonIgnore
     private Map<String, ClientStats> statsByClient = new HashMap<>();
-    
+
     @JsonIgnore
     private OutputStream outputStream;
-    
+
+    @JsonIgnore
+    private ClientDetailsDao clientDetailsDao;
+
     public void setOutputStream(OutputStream outputStream) {
         this.outputStream = outputStream;
     }
-    
+
+    public void setClientDetailsDao(ClientDetailsDao clientDetailsDao) {
+        this.clientDetailsDao = clientDetailsDao;
+    }
+
     public void record(String clientDetailsId, ApiLog apiLog) {
         hitsAnalysed++;
-        
+
         ClientStats stats = statsByClient.get(clientDetailsId);
         if (stats == null) {
             stats = new ClientStats();
             stats.setClientDetailsId(clientDetailsId);
+
+            if (!ApiAccessLogsAnalyser.UNKNOWN_CLIENT.equals(clientDetailsId)) {
+                stats.setClientName(clientDetailsDao.getMemberName(clientDetailsId));
+            }
         }
         stats.recordVersionHit(apiLog.getVersion());
         stats.incrementTotalHits();
         statsByClient.put(clientDetailsId, stats);
     }
-    
+
     public long getHitsAnalysed() {
         return hitsAnalysed;
     }
-    
+
     public List<ClientStats> getClientResults() {
         return clientResults;
     }
@@ -55,9 +67,9 @@ public class AnalysisResults implements Serializable {
         for (String clientId : statsByClient.keySet()) {
             clientResults.add(statsByClient.get(clientId));
         }
-        
+
         outputStream.write(JsonUtils.convertToJsonString(this).getBytes());
         outputStream.close();
     }
-    
+
 }
