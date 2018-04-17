432c3394520af15e717c1d73aff1c990090be4db
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-04-17 15:42:11 -0600

    Create a thread pool on every listener, so, we can handle how many threads are processed at the same time

--- orcid-activities-indexer/src/main/java/org/orcid/activitiesindexer/listener/UpdatedOrcidWorker.java
+++ orcid-activities-indexer/src/main/java/org/orcid/activitiesindexer/listener/UpdatedOrcidWorker.java
@@ -16,12 +16,16 @@
  */
 package org.orcid.activitiesindexer.listener;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.annotation.Resource;
 
 import org.orcid.activitiesindexer.s3.S3MessageProcessor;
 import org.orcid.utils.listener.LastModifiedMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.google.common.cache.RemovalListener;
@@ -35,15 +39,23 @@ public class UpdatedOrcidWorker implements RemovalListener<String, LastModifiedM
     @Resource
     private S3MessageProcessor s3Processor;
 
+    private final ExecutorService executor;
+
+    public UpdatedOrcidWorker(@Value("${org.orcid.message-listener.updated_orcid.threads:5}") Integer maxThreads) {
+        executor = Executors.newFixedThreadPool(maxThreads);
+    }
+    
     /**
      * Fires when the queue evicts after an inactivity period.
      * Populates the Amazon S3 buckets and updates solr index
      */
     public void onRemoval(RemovalNotification<String, LastModifiedMessage> removal) {
         if (removal.wasEvicted()) {
-            LastModifiedMessage m = removal.getValue();
-            LOG.info("Removing " + removal.getKey() + " from UpdatedOrcidCacheQueue '" + m.getLastUpdated() + "' Removal cause " + removal.getCause() );            
-            s3Processor.accept(m);
+            executor.submit(() -> {
+                LastModifiedMessage m = removal.getValue();
+                LOG.info("Removing " + removal.getKey() + " from UpdatedOrcidCacheQueue '" + m.getLastUpdated() + "' Removal cause " + removal.getCause() );            
+                s3Processor.accept(m);
+            });            
         }
     }
 }
