fbba22f88f7f5475ecd7d77df012d23dd3a61940
Jeff P <jeff@siccr.com>
2018-07-10 17:58:40 -0600

    add maven profile production mode NG

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/webpack.config.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/webpack.config.js
@@ -3,37 +3,35 @@ var webpack = require('webpack');
 module.exports = {
     context: __dirname + "/",
     entry: "./require.js",
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [
+        {
+            test: /\.ts$/, 
+            use: [
             {
-                test: /\.ts$/, 
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
+                loader: 'ts-loader'
             }
+            ]
+        }
         ]
     },
     output: {
         path: __dirname + '/',
         filename: "angular_orcid_generated.js"
     },
-    plugins: [
-        new webpack.DefinePlugin(
-            {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-                'process.env': {
-                    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-                }
-            }
-        )
-    ],
+    /*plugins: [
+    new webpack.DefinePlugin({
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+                             'process.env':{
+                                 'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+                             }
+    })
+    ],*/
     resolve: {
         alias: {
             "@angular/upgrade/static": "@angular/upgrade/bundles/upgrade-static.umd.js"
         }
     },
-    watch: true
-};
+    watch: false
+}
