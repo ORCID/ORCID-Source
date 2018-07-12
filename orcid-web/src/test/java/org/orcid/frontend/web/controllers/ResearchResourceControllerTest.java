ce532bedf08512e3042cb3dba30a003ad0c4920f
Tom Demeranville <tom@demeranville.com>
2018-07-12 11:22:51 +0100

    added empty test

--- orcid-web/src/test/java/org/orcid/frontend/web/controllers/ResearchResourceControllerTest.java
+++ orcid-web/src/test/java/org/orcid/frontend/web/controllers/ResearchResourceControllerTest.java
@@ -1,5 +1,86 @@
 package org.orcid.frontend.web.controllers;
 
-public class ResearchResourceControllerTest {
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.orcid.core.manager.OrcidProfileManager;
+import org.orcid.frontend.web.util.BaseControllerTest;
+import org.orcid.test.OrcidJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.google.common.collect.Lists;
+
+@RunWith(OrcidJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath:orcid-core-context.xml", "classpath:orcid-frontend-web-servlet.xml", "classpath:statistics-core-context.xml" })
+public class ResearchResourceControllerTest extends BaseControllerTest{
+
+    private static final List<String> DATA_FILES = Arrays.asList("/data/EmptyEntityData.xml", "/data/SecurityQuestionEntityData.xml",
+            "/data/SourceClientDetailsEntityData.xml", "/data/ProfileEntityData.xml", "/data/WorksEntityData.xml", "/data/ClientDetailsEntityData.xml",
+            "/data/Oauth2TokenDetailsData.xml", "/data/WebhookEntityData.xml", "/data/RecordNameEntityData.xml");
+
+    @Resource
+    private ResearchResourcesController controller;
+    
+    @Resource
+    protected OrcidProfileManager orcidProfileManager;
+
+    @Captor
+    private ArgumentCaptor<List<Long>> idsCaptor;
+    
+    @Before
+    public void init() {
+        orcidProfileManager.updateLastModifiedDate("4444-4444-4444-4446");
+        assertNotNull(controller);
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        initDBUnitData(DATA_FILES);
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        removeDBUnitData(Lists.reverse(DATA_FILES));
+    }
+    
+    @Test
+    public void testReadPage(){
+        
+    }
+    
+    @Test
+    public void testReadOne(){
+        
+    }
+
+    @Test
+    public void testUpdateVis(){
+        
+    }
+
+    @Test
+    public void testPrivacy(){
+        
+    }
+
+   
+    @Test
+    public void testDeleteOne(){
+        
+    }
+
 
 }
