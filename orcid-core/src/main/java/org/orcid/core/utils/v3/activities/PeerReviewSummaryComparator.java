b6632b14d9c29ee646adf74fe94b7710ea0e49c2
George Nash <bobcaprice@hotmail.com>
2018-07-18 11:47:29 +0200

    Fixed tests

--- orcid-core/src/main/java/org/orcid/core/utils/v3/activities/PeerReviewSummaryComparator.java
+++ orcid-core/src/main/java/org/orcid/core/utils/v3/activities/PeerReviewSummaryComparator.java
@@ -8,6 +8,17 @@ public class PeerReviewSummaryComparator implements Comparator<PeerReviewSummary
 
     @Override
     public int compare(PeerReviewSummary x, PeerReviewSummary y) {
+        if (x.getCompletionDate() == null && y.getCompletionDate() == null) {
+            return 0;
+        }
+        
+        if (x.getCompletionDate() == null) {
+            return -1;
+        }
+        
+        if (y.getCompletionDate() == null) {
+            return 1;
+        }
         return x.getCompletionDate().compareTo(y.getCompletionDate());
     }
     
