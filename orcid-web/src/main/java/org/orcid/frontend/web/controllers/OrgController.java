e19d28f21fb4444ab63ef2a07ace1d01dddefed0
Tom Demeranville <tom@demeranville.com>
2018-07-23 14:08:57 +0100

    added /orgs/disambiguated/{type}/{value} to retrive org info

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/OrgController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/OrgController.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
+import org.orcid.core.manager.OrgDisambiguatedManager;
 import org.orcid.core.manager.v3.OrgManager;
+import org.orcid.pojo.OrgDisambiguated;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = { "/orgs" })
@@ -17,6 +25,9 @@ public class OrgController {
     @Resource(name = "orgManagerV3")
     private OrgManager orgManager;
 
+    @Resource
+    private OrgDisambiguatedManager orgDisambiguatedManager;
+    
     @RequestMapping(value = "ambiguous", method = RequestMethod.GET, produces = "text/csv")
     public void getAmbiguousOrgs(HttpServletResponse response) throws IOException {
         response.setContentType("text/csv");
@@ -29,6 +40,14 @@ public class OrgController {
         response.setContentType("text/csv");
         response.addHeader("Content-Disposition", "attachment; filename=\"disambiguated_orgs.csv\"");
         orgManager.writeDisambiguatedOrgs(response.getWriter());
+    }
+    
+    @RequestMapping(value = "/disambiguated/{idType}/{idValue}", method = RequestMethod.GET)
+    public ResponseEntity<OrgDisambiguated> getDisambiguatedOrg( @PathVariable("idValue") String idValue, @PathVariable("idType") String idType){
+        OrgDisambiguated org = orgDisambiguatedManager.findInDB(idValue,idType);
+        if (org == null)
+            return new ResponseEntity<OrgDisambiguated>(HttpStatus.NOT_FOUND);
+        return  new ResponseEntity<OrgDisambiguated>(org, HttpStatus.OK);
     }
 
 }
