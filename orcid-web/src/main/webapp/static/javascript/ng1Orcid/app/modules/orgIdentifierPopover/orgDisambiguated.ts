ed46e887a392bcb7418a182436181bd3ff1f8242
Liz Krznarich <ejkrznarich@gmail.com>
2018-07-26 09:40:13 -0500

    add OrgDisambgiated class

---
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/orgIdentifierPopover/orgDisambiguated.ts
@@ -0,0 +1,13 @@
+export class OrgDisambiguated {
+    city: string;
+    country: string;
+    countryForDisplay: string;
+    disambiguatedAffiliationIdentifier: string;
+    orgDisambiguatedExternalIdentifiers: any;
+    orgType: string;
+    region: string;
+    sourceId: string;
+    sourceType: string;
+    url: string;
+    value: string;
+}
