8f841dabbf5852ec38e0f1aa5112d260db82a3e7
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-07-27 16:26:01 -0600

    On 1.2 PUT, load all affiliations, including the new aff types, so, they don't get deleted by the orphan removal

--- orcid-core/src/main/java/org/orcid/core/manager/LoadOptions.java
+++ orcid-core/src/main/java/org/orcid/core/manager/LoadOptions.java
@@ -6,16 +6,27 @@ public class LoadOptions {
     public static final LoadOptions BIO_ONLY = new LoadOptions(true, false, false);
     public static final LoadOptions BIO_AND_INTERNAL_ONLY = new LoadOptions(true, false, true);
     public static final LoadOptions INTERNAL_ONLY = new LoadOptions(false, false, true);
-
+    public static final LoadOptions ALL_WITH_NEW_AFFILIATION_TYPES = new LoadOptions(true, true, true, true);
+    
     private boolean loadBio;
     private boolean loadActivities;
     private boolean loadInternal;
+    private boolean loadNewAffiliationTypes;
 
     public LoadOptions(boolean loadBio, boolean loadActivities, boolean loadInternal) {
         super();
         this.loadBio = loadBio;
         this.loadActivities = loadActivities;
         this.loadInternal = loadInternal;
+        this.loadNewAffiliationTypes = false;
+    }
+    
+    private LoadOptions(boolean loadBio, boolean loadActivities, boolean loadInternal, boolean loadNewAffiliationTypes) {
+        super();
+        this.loadBio = loadBio;
+        this.loadActivities = loadActivities;
+        this.loadInternal = loadInternal;
+        this.loadNewAffiliationTypes = loadNewAffiliationTypes;
     }
 
     public boolean isLoadBio() {
@@ -41,6 +52,10 @@ public class LoadOptions {
     public void setLoadInternal(boolean loadInternal) {
         this.loadInternal = loadInternal;
     }
+    
+    public boolean isLoadNewAffiliationTypes() {
+        return loadNewAffiliationTypes;
+    }
 
     @Override
     public int hashCode() {
@@ -49,6 +64,7 @@ public class LoadOptions {
         result = prime * result + (loadActivities ? 1231 : 1237);
         result = prime * result + (loadBio ? 1231 : 1237);
         result = prime * result + (loadInternal ? 1231 : 1237);
+        result = prime * result + (loadNewAffiliationTypes ? 1231 : 1237);
         return result;
     }
 
@@ -67,6 +83,8 @@ public class LoadOptions {
             return false;
         if (loadInternal != other.loadInternal)
             return false;
+        if (loadNewAffiliationTypes != other.loadNewAffiliationTypes)
+            return false;        
         return true;
     }
 
