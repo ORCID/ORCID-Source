9ffc85f26096cbb47cdd0cc31abadceacd7ee077
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-08-08 13:59:23 -0600

    Fix unit tests

--- orcid-core/src/test/java/org/orcid/core/security/OrcidUserDetailsServiceTest.java
+++ orcid-core/src/test/java/org/orcid/core/security/OrcidUserDetailsServiceTest.java
@@ -9,6 +9,8 @@ import static org.mockito.Mockito.when;
 
 import java.util.Date;
 
+import javax.annotation.Resource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -16,8 +18,9 @@ import org.mockito.MockitoAnnotations;
 import org.orcid.core.manager.OrcidSecurityManager;
 import org.orcid.core.manager.SalesForceManager;
 import org.orcid.core.manager.SlackManager;
+import org.orcid.core.manager.v3.read_only.EmailManagerReadOnly;
 import org.orcid.jaxb.model.common_v2.OrcidType;
-import org.orcid.persistence.dao.EmailDao;
+import org.orcid.jaxb.model.v3.rc1.record.Email;
 import org.orcid.persistence.dao.ProfileDao;
 import org.orcid.persistence.jpa.entities.EmailEntity;
 import org.orcid.persistence.jpa.entities.ProfileEntity;
@@ -33,8 +36,8 @@ public class OrcidUserDetailsServiceTest {
     private ProfileDao profileDao;
 
     @Mock
-    private EmailDao emailDao;
-
+    protected EmailManagerReadOnly emailManagerReadOnly;
+    
     @Mock
     private OrcidSecurityManager securityMgr;
 
@@ -54,17 +57,24 @@ public class OrcidUserDetailsServiceTest {
     public void before() {
         MockitoAnnotations.initMocks(this);
         TargetProxyHelper.injectIntoProxy(service, "profileDao", profileDao);
-        TargetProxyHelper.injectIntoProxy(service, "emailDao", emailDao);
         TargetProxyHelper.injectIntoProxy(service, "securityMgr", securityMgr);
         TargetProxyHelper.injectIntoProxy(service, "slackManager", slackManager);
         TargetProxyHelper.injectIntoProxy(service, "salesForceManager", salesForceManager);
-
-        when(emailDao.findCaseInsensitive(anyString())).thenReturn(null);
-        when(emailDao.findCaseInsensitive(EMAIL)).thenReturn(getEmailEntity(getProfileEntity()));
-        when(emailDao.findPrimaryEmail(ORCID)).thenReturn(getEmailEntity(getProfileEntity()));
-
+        TargetProxyHelper.injectIntoProxy(service, "emailManagerReadOnly", emailManagerReadOnly);
+        
         when(profileDao.find(anyString())).thenReturn(null);
         when(profileDao.find(ORCID)).thenReturn(getProfileEntity());
+        
+        EmailEntity emailEntity = new EmailEntity();
+        emailEntity.setEmail(EMAIL);
+        emailEntity.setProfile(new ProfileEntity(ORCID));
+        
+        Email email = new Email();
+        email.setEmail(EMAIL);
+        
+        when(emailManagerReadOnly.findOrcidIdByEmail(anyString())).thenReturn(null);
+        when(emailManagerReadOnly.findOrcidIdByEmail(EMAIL)).thenReturn(ORCID);
+        when(emailManagerReadOnly.findPrimaryEmail(ORCID)).thenReturn(email);       
     }
 
     @Test
