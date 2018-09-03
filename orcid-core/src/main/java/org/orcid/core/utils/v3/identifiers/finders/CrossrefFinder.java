84c420d47549a058b9b70213de20109500e2d8d4
Tom Demeranville <tom@demeranville.com>
2018-09-03 14:37:30 +0100

    added crossref finder

--- orcid-core/src/main/java/org/orcid/core/utils/v3/identifiers/finders/CrossrefFinder.java
+++ orcid-core/src/main/java/org/orcid/core/utils/v3/identifiers/finders/CrossrefFinder.java
@@ -1,5 +1,77 @@
 package org.orcid.core.utils.v3.identifiers.finders;
 
-public class CrossrefFinder {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.orcid.core.utils.v3.identifiers.PIDResolverCache;
+import org.orcid.core.utils.v3.identifiers.normalizers.DOINormalizer;
+import org.orcid.jaxb.model.v3.rc1.common.TransientNonEmptyString;
+import org.orcid.jaxb.model.v3.rc1.record.ExternalID;
+import org.orcid.jaxb.model.v3.rc1.record.ExternalIDs;
+import org.orcid.pojo.FindMyStuffResult;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CrossrefFinder implements Finder{
+
+    @Resource
+    DOINormalizer norm;
+
+    @Resource
+    PIDResolverCache cache;
+
+    private static final String serviceName = "crossref";
+    private static final String metadataEndpoint = "https://search.crossref.org/dois?q=";
+
+    @Override
+    public FindMyStuffResult find(String orcid, ExternalIDs existingIDs) {
+        FindMyStuffResult result = new FindMyStuffResult();
+        result.setServiceName(getServiceName());
+        try {
+            InputStream is = cache.get(metadataEndpoint + orcid, "application/json");
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<CrossrefSearchResult> crResult = objectMapper.readValue(is, new TypeReference<List<CrossrefSearchResult>>(){});
+            for (CrossrefSearchResult w : crResult) {
+                if (!existingIDs.getExternalIdentifier().contains(w.getExternalID(norm))) {
+                    result.getResults().add(new FindMyStuffResult.Result(w.doi, "doi", w.title));
+                }
+            }
+        } catch (MalformedURLException e) {
+            // do nothing
+        } catch (IOException e) {
+            // do nothing
+        }
+        return result;
+    }
+
+    @Override
+    public String getServiceName() {
+        return serviceName;
+    }
+    
+    //MODELS
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CrossrefSearchResult {
+        public String doi;
+        public String title;
+
+        public CrossrefSearchResult() {
+        };
+        
+        public ExternalID getExternalID(DOINormalizer norm) {
+            ExternalID eid = new ExternalID();
+            eid.setType("doi");
+            eid.setValue(doi);
+            eid.setNormalized(new TransientNonEmptyString(norm.normalise("doi", doi)));
+            return eid;
+        }
+    }
+    
 }
---
+++ orcid-core/src/test/java/org/orcid/core/utils/v3/identifiers/finders/CrossrefFinderTest.java
@@ -0,0 +1,110 @@
+package org.orcid.core.utils.v3.identifiers.finders;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.annotation.Resource;
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.orcid.core.utils.ArXivResolverTest;
+import org.orcid.core.utils.v3.identifiers.PIDNormalizationService;
+import org.orcid.core.utils.v3.identifiers.PIDResolverCache;
+import org.orcid.core.utils.v3.identifiers.finders.DataciteFinder;
+import org.orcid.core.utils.v3.identifiers.normalizers.DOINormalizer;
+import org.orcid.jaxb.model.v3.rc1.common.TransientNonEmptyString;
+import org.orcid.jaxb.model.v3.rc1.record.ExternalID;
+import org.orcid.jaxb.model.v3.rc1.record.ExternalIDs;
+import org.orcid.pojo.FindMyStuffResult;
+import org.orcid.test.OrcidJUnit4ClassRunner;
+import org.orcid.test.TargetProxyHelper;
+import org.springframework.test.context.ContextConfiguration;
+
+public class CrossrefFinderTest {
+
+    @Mock
+    DOINormalizer norm;
+
+    @Mock
+    PIDResolverCache cache;
+    
+    CrossrefFinder finder = new CrossrefFinder();
+    
+    @Before
+    public void setUp() throws IOException {
+        MockitoAnnotations.initMocks(this);
+        TargetProxyHelper.injectIntoProxy(finder, "norm", norm);
+        TargetProxyHelper.injectIntoProxy(finder, "cache", cache);
+
+        when(norm.normalise(eq("doi"), eq("http://dx.doi.org/10.7287/peerj.preprints.26505"))).thenReturn("10.7287/peerj.preprints.26505");
+        when(norm.normalise(eq("doi"), eq("http://dx.doi.org/10.7287/peerj.preprints.26505v1"))).thenReturn("10.7287/peerj.preprints.26505v1");
+        when(norm.normalise(eq("doi"), eq("http://dx.doi.org/10.1101/097196"))).thenReturn("10.1101/097196");
+        when(cache.isHttp200(anyString())).thenReturn(true);
+
+        when(cache.get("https://search.crossref.org/dois?q=0000-0003-1419-2405", "application/json")).thenAnswer(new Answer<InputStream>() {
+
+            @Override
+            public InputStream answer(InvocationOnMock invocation) throws Throwable {
+                return CrossrefFinderTest.class.getResourceAsStream("/examples/works/finder/crossref.json");
+            }
+
+        });
+    }
+    
+    
+    @Test
+    public void testSimple(){
+        FindMyStuffResult result = finder.find("0000-0003-1419-2405", new ExternalIDs());
+        assertEquals("crossref",result.getServiceName());
+        assertEquals(3,result.getResults().size());
+    }
+    
+    @Test
+    public void testAlreadyHaveOne(){
+        ExternalID id = new ExternalID();
+        id.setType("doi");
+        id.setValue("10.7287/peerj.preprints.26505");
+        id.setNormalized(new TransientNonEmptyString("10.7287/peerj.preprints.26505"));
+        ExternalIDs ids = new ExternalIDs();
+        ids.getExternalIdentifier().add(id);
+        
+        FindMyStuffResult result = finder.find("0000-0003-1419-2405", ids);
+        assertEquals("crossref",result.getServiceName());
+        assertEquals(2,result.getResults().size());
+    }
+    
+    @Test
+    public void testAlreadyHaveAll(){
+        ExternalID id = new ExternalID();
+        id.setType("doi");
+        id.setValue("10.7287/peerj.preprints.26505");
+        id.setNormalized(new TransientNonEmptyString("10.7287/peerj.preprints.26505"));
+        ExternalID id2 = new ExternalID();
+        id2.setType("doi");
+        id2.setValue("https://doi.org/10.7287/peerj.preprints.26505v1");
+        id2.setNormalized(new TransientNonEmptyString("10.7287/peerj.preprints.26505v1"));
+        ExternalID id3 = new ExternalID();
+        id3.setType("doi");
+        id3.setValue("http://dx.doi.org/10.1101/097196");
+        id3.setNormalized(new TransientNonEmptyString("10.1101/097196"));
+        
+        ExternalIDs ids = new ExternalIDs();
+        ids.getExternalIdentifier().add(id);
+        ids.getExternalIdentifier().add(id2);
+        ids.getExternalIdentifier().add(id3);
+        FindMyStuffResult result = finder.find("0000-0003-1419-2405", ids);
+        assertEquals("crossref",result.getServiceName());
+        assertEquals(0,result.getResults().size());
+    }
+}
