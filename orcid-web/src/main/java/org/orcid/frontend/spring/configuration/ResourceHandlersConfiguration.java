c6346ebbbec9e05686afc0a8e72c2f615dd7ab90
Will Simpson <w.simpson@orcid.org>
2018-09-06 13:25:44 +0100

    Added config option to disable static resource caching.

--- orcid-web/src/main/java/org/orcid/frontend/spring/configuration/ResourceHandlersConfiguration.java
+++ orcid-web/src/main/java/org/orcid/frontend/spring/configuration/ResourceHandlersConfiguration.java
@@ -1,6 +1,7 @@
 package org.orcid.frontend.spring.configuration;
 
 import org.orcid.utils.ReleaseNameUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,10 +13,14 @@ import org.springframework.web.servlet.resource.VersionResourceResolver;
 @EnableWebMvc
 public class ResourceHandlersConfiguration extends WebMvcConfigurerAdapter {
 
+    @Value("${org.orcid.frontend.static.resource.cache.enabled:true}")
+    private boolean cacheResources;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String releaseVersion = ReleaseNameUtils.getReleaseName();
         VersionResourceResolver versionResourceResolver = new VersionResourceResolver().addVersionStrategy(new FixedVersionStrategy(releaseVersion), "/**");
-        registry.addResourceHandler("/static/**").addResourceLocations("/static/").setCachePeriod(1206297260).resourceChain(true).addResolver(versionResourceResolver);
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/").setCachePeriod(1206297260).resourceChain(cacheResources)
+                .addResolver(versionResourceResolver);
     }
 }
