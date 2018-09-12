f9fe9076a2932744d66fc6ac7691eb73ed34a085
Tom Demeranville <tom@demeranville.com>
2018-09-12 13:16:33 +0100

    added ORCID to the user agent string, sorted crossref results by most recent

--- orcid-core/src/main/java/org/orcid/core/utils/v3/identifiers/PIDResolverCache.java
+++ orcid-core/src/main/java/org/orcid/core/utils/v3/identifiers/PIDResolverCache.java
@@ -111,6 +111,8 @@ public class PIDResolverCache {
     
     public InputStream get(String url, String accept) throws IOException {
         HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        System.out.println(con.getRequestProperty("User-Agent"));
+        con.setRequestProperty("User-Agent", con.getRequestProperty("User-Agent")+ " (orcid.org)");
         con.addRequestProperty("Accept", accept);
         con.setRequestMethod("GET");
         con.setInstanceFollowRedirects(true);
