2c7d29a84a58625fd9072eca1a89acb1df5301b9
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-09-12 15:34:27 -0600

    Still working on it

--- orcid-core/src/main/java/org/orcid/core/utils/v3/identifiers/resolvers/PubMedResolver.java
+++ orcid-core/src/main/java/org/orcid/core/utils/v3/identifiers/resolvers/PubMedResolver.java
@@ -15,20 +15,15 @@ import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.orcid.core.exception.UnexpectedResponseCodeException;
-import org.orcid.core.locale.LocaleManager;
 import org.orcid.core.manager.IdentifierTypeManager;
 import org.orcid.core.utils.v3.identifiers.PIDNormalizationService;
 import org.orcid.core.utils.v3.identifiers.PIDResolverCache;
-import org.orcid.jaxb.model.v3.rc1.common.Subtitle;
 import org.orcid.jaxb.model.v3.rc1.common.Title;
 import org.orcid.jaxb.model.v3.rc1.common.Url;
 import org.orcid.jaxb.model.v3.rc1.record.ExternalID;
-import org.orcid.jaxb.model.v3.rc1.record.ExternalIDs;
 import org.orcid.jaxb.model.v3.rc1.record.Relationship;
 import org.orcid.jaxb.model.v3.rc1.record.Work;
 import org.orcid.jaxb.model.v3.rc1.record.WorkTitle;
-import org.orcid.jaxb.model.v3.rc1.record.WorkType;
-import org.orcid.pojo.IdentifierType;
 import org.orcid.pojo.PIDResolutionResult;
 import org.orcid.pojo.ajaxForm.PojoUtil;
 import org.springframework.stereotype.Component;
@@ -41,16 +36,13 @@ public class PubMedResolver implements LinkResolver, MetadataResolver {
 
     @Resource
     PIDResolverCache cache;
-    
+
     @Resource
     private IdentifierTypeManager identifierTypeManager;
-    
-    @Resource
-    protected LocaleManager localeManager;
 
     List<String> types = Lists.newArrayList("pmc", "pmid");
-    
-    private String metadataEndpoint = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary?db={db}&id={id}&retmode=json";        
+
+    private String metadataEndpoint = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary?db={db}&id={id}&retmode=json";
 
     @Override
     public List<String> canHandle() {
@@ -58,8 +50,8 @@ public class PubMedResolver implements LinkResolver, MetadataResolver {
     }
 
     /**
-     * Checks for a http 200 
-     * normalizing the value and creating a URL using the resolution prefix
+     * Checks for a http 200 normalizing the value and creating a URL using the
+     * resolution prefix
      * 
      */
     @Override
@@ -69,32 +61,32 @@ public class PubMedResolver implements LinkResolver, MetadataResolver {
 
         String normUrl = normalizationService.generateNormalisedURL(apiTypeName, value);
         if (!StringUtils.isEmpty(normUrl)) {
-            if (cache.isHttp200(normUrl)){
-                return new PIDResolutionResult(true,true,true,normUrl);                
-            }else{
-                return new PIDResolutionResult(false,true,true,null);
+            if (cache.isHttp200(normUrl)) {
+                return new PIDResolutionResult(true, true, true, normUrl);
+            } else {
+                return new PIDResolutionResult(false, true, true, null);
             }
         }
-        
-        return new PIDResolutionResult(false,false,true,null);//unreachable?        
+
+        return new PIDResolutionResult(false, false, true, null);// unreachable?
     }
-    
+
     @Override
     public Work resolveMetadata(String apiTypeName, String value) {
         PIDResolutionResult rr = this.resolve(apiTypeName, value);
         if (!rr.isResolved())
             return null;
-        
+
         try {
             String endpoint = null;
-            
-            if(value.startsWith("PMC") || value.startsWith("pmc")) {
+
+            if (value.startsWith("PMC") || value.startsWith("pmc")) {
                 endpoint = metadataEndpoint.replace("{id}", value.substring(3));
             } else {
                 endpoint = metadataEndpoint.replace("{id}", value);
             }
-            
-            if(apiTypeName.equals("pmid")) {
+
+            if (apiTypeName.equals("pmid")) {
                 endpoint = endpoint.replace("{db}", "pubmed");
             } else {
                 endpoint = endpoint.replace("{db}", "pmc");
@@ -119,84 +111,58 @@ public class PubMedResolver implements LinkResolver, MetadataResolver {
         } catch (IOException | JSONException e) {
             return null;
         }
-        return null;                
+        return null;
     }
-    
+
     private Work getWork(JSONObject json) throws JSONException {
         Work result = new Work();
-        
+
         JSONObject results = json.getJSONObject("result");
-        
-        if(results != null) {
-            JSONArray uids = json.getJSONArray("uids");
-            String id1 = uids.getString(0);
-        }
-        
-        
-        WorkTitle workTitle = new WorkTitle();
-        if(json.has("title")) {
-            workTitle.setTitle(new Title(json.getString("title")));            
-        } 
-        
-        result.setWorkTitle(workTitle);
-        
-        if(json.has("URL")) {
-            result.setUrl(new Url(json.getString("URL")));
-        }
-        
-        // Populate other external identifiers
-        result.setWorkExternalIdentifiers(new ExternalIDs());
-        if(json.has("DOI")) {
-            String doi = json.getString("DOI");
-            ExternalID extId = new ExternalID();
-            extId.setType("DOI");
-            extId.setRelationship(Relationship.SELF);
-            extId.setValue(doi);
-            IdentifierType idType = identifierTypeManager.fetchIdentifierTypeByDatabaseName("DOI", localeManager.getLocale());
-            if(idType != null && !PojoUtil.isEmpty(idType.getResolutionPrefix())) {
-                extId.setUrl(new Url(idType.getResolutionPrefix() + doi));
+
+        if (results != null) {
+            JSONArray uids = results.getJSONArray("uids");
+            String elementId = uids.getString(0);
+            JSONObject metadata = results.getJSONObject(elementId);
+
+            WorkTitle workTitle = new WorkTitle();
+            if (metadata.has("title")) {
+                workTitle.setTitle(new Title(metadata.getString("title")));
             }
-            result.getWorkExternalIdentifiers().getExternalIdentifier().add(extId);
-        }
-        if(json.has("ISBN")) {
-            try {
-                JSONArray isbns = json.getJSONArray("ISBN");
-                for(int i = 0; i < isbns.length(); i++) {
-                    String isbn = isbns.getString(i);
+            result.setWorkTitle(workTitle);
+
+            if (metadata.has("fulljournalname")) {
+                result.setJournalTitle(new Title(metadata.getString("fulljournalname")));
+            }
+
+            if (metadata.has("articleids")) {
+                JSONArray extIdsMetadata = metadata.getJSONArray("articleids");
+                for (int i = 0; i < extIdsMetadata.length(); i++) {
+                    JSONObject extIdMetadata = extIdsMetadata.getJSONObject(i);
+                    String type = extIdMetadata.getString("idtype");
+                    String value = extIdMetadata.getString("value");
                     ExternalID extId = new ExternalID();
-                    extId.setType("ISBN");
                     extId.setRelationship(Relationship.SELF);
-                    extId.setValue(isbn);
-                    IdentifierType idType = identifierTypeManager.fetchIdentifierTypeByDatabaseName("ISBN", localeManager.getLocale());
-                    if(idType != null && !PojoUtil.isEmpty(idType.getResolutionPrefix())) {
+                    switch (type) {
+                    case "doi":
+                        extId.setType("DOI");
+                        break;
+                    case "pmid":
+                        extId.setType("PMID");
+                        break;
+                    case "pmcid":
+                        extId.setType("PMC");
+                        break;
+                    }
+                    extId.setValue(value);
+                    if (idType != null && !PojoUtil.isEmpty(idType.getResolutionPrefix())) {
                         extId.setUrl(new Url(idType.getResolutionPrefix() + isbn));
                     }
+
                     result.getWorkExternalIdentifiers().getExternalIdentifier().add(extId);
                 }
-            } catch(Exception e) {
-                
             }
         }
-        if(json.has("ISSN")) {
-            try {
-                JSONArray isbns = json.getJSONArray("ISSN");
-                for(int i = 0; i < isbns.length(); i++) {
-                    String isbn = isbns.getString(i);
-                    ExternalID extId = new ExternalID();
-                    extId.setType("ISSN");
-                    extId.setRelationship(Relationship.SELF);
-                    extId.setValue(isbn);
-                    IdentifierType idType = identifierTypeManager.fetchIdentifierTypeByDatabaseName("ISSN", localeManager.getLocale());
-                    if(idType != null && !PojoUtil.isEmpty(idType.getResolutionPrefix())) {
-                        extId.setUrl(new Url(idType.getResolutionPrefix() + isbn));
-                    }
-                    result.getWorkExternalIdentifiers().getExternalIdentifier().add(extId);
-                }
-            } catch(Exception e) {
-                
-            }
-        }        
-              
+
         return result;
     }
 
