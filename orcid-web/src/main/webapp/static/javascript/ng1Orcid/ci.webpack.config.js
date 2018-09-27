c939b9c3d733ae652ded09379070f690840d8101
Liz Krznarich <ejkrznarich@gmail.com>
2018-09-27 17:44:08 -0500

    uglify angular_orcid_generated but don't mangle

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/ci.webpack.config.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/ci.webpack.config.js
@@ -1,9 +1,10 @@
 var webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     context: __dirname + "/",
     entry: "./require.js",
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [
             {
@@ -19,6 +20,19 @@ module.exports = {
     output: {
         path: __dirname + '/',
         filename: "angular_orcid_generated.js"
+    },
+    optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            uglifyOptions: {
+                mangle: false,
+                compress: true,
+                comments: false
+
+            }
+            
+          })
+        ]
     },
     plugins: [
         function()
