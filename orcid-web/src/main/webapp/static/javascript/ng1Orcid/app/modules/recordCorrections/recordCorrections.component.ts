aa5658c82602939402c8aa20de0e44bf9cdc0188
leomendoza123 <leomendoza123@gmail.com>
2018-10-04 16:00:23 -0600

    recordCorrections reformats new ts files

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/recordCorrections/recordCorrections.component.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/recordCorrections/recordCorrections.component.ts
@@ -1,80 +1,68 @@
-//Import all the angular components
-
-import { NgForOf, NgIf }
-    from '@angular/common';
-
-import { AfterViewInit, Component, OnDestroy, OnInit }
-    from '@angular/core';
-
-import { Observable, Subject, Subscription }
-    from 'rxjs';
-import { takeUntil }
-    from 'rxjs/operators';
-
-import { CommonService }
-    from '../../shared/common.service.ts';
-
-import { GenericService }
-    from '../../shared/generic.service.ts';
+import { NgForOf, NgIf } from "@angular/common";
+import { AfterViewInit, Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subject, Subscription } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { CommonService } from "../../shared/common.service.ts";
+import { GenericService } from "../../shared/generic.service.ts";
 
 @Component({
-    selector: 'record-corrections-ng2',
-    template: scriptTmpl("record-corrections-ng2-template")
+  selector: "record-corrections-ng2",
+  template: scriptTmpl("record-corrections-ng2-template")
 })
-export class RecordCorrectionsComponent implements AfterViewInit, OnDestroy, OnInit {
-    private ngUnsubscribe: Subject<void> = new Subject<void>();
-    nextPageUrl = '/record-corrections/next';
-    previousPageUrl = '/record-corrections/previous';
-    currentPage
+export class RecordCorrectionsComponent
+  implements AfterViewInit, OnDestroy, OnInit {
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+  nextPageUrl = "/record-corrections/next";
+  previousPageUrl = "/record-corrections/previous";
+  currentPage;
 
-    constructor(
-        private commonSrvc: CommonService,
-        private genericService: GenericService
-    ) {
-    }
+  constructor(
+    private commonSrvc: CommonService,
+    private genericService: GenericService
+  ) {}
 
+  ngAfterViewInit() {}
 
-    //Default init functions provided by Angular Core
-    ngAfterViewInit() {
-        //Fire functions AFTER the view inited. Useful when DOM is required or access children directives
-    };
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
-    ngOnDestroy() {
-        this.ngUnsubscribe.next();
-        this.ngUnsubscribe.complete();
-    };
+  ngOnInit() {
+    this.nextPage();
+  }
 
-    ngOnInit() {
-        this.nextPage();
-    };
+  nextPage() {
+    this.genericService
+      .getData(
+        this.nextPageUrl +
+          (this.currentPage ? "/" + this.currentPage.lastElementId : "")
+      )
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+        data => {
+          this.currentPage = data;
+        },
+        error => {
+          console.log("error fetching next page");
+        }
+      );
+  }
 
-    nextPage() {
-        this.genericService.getData(this.nextPageUrl + (this.currentPage ? "/" + this.currentPage.lastElementId : ""))
-            .pipe(
-                takeUntil(this.ngUnsubscribe)
-            )
-            .subscribe(
-                data => {
-                    this.currentPage = data;
-                },
-                error => {
-                    console.log("error fetching next page");
-                }
-            );
-    }
-
-    previousPage() {
-        this.genericService.getData(this.previousPageUrl + (this.currentPage ? "/" + this.currentPage.firstElementId : ""))
-            .pipe(
-                takeUntil(this.ngUnsubscribe)
-            )
-            .subscribe(
-                data => {
-                    this.currentPage = data;
-                },
-                error => {
-                    console.log("error fetching previous page");
-                }
-            );
-    }
+  previousPage() {
+    this.genericService
+      .getData(
+        this.previousPageUrl +
+          (this.currentPage ? "/" + this.currentPage.firstElementId : "")
+      )
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+        data => {
+          this.currentPage = data;
+        },
+        error => {
+          console.log("error fetching previous page");
+        }
+      );
+  }
 }
--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/recordCorrections/recordCorrections.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/recordCorrections/recordCorrections.ts
@@ -1,51 +1,28 @@
-import * as angular 
-    from 'angular';
-
-import { Directive, NgModule } 
-    from '@angular/core';
-
-import { downgradeComponent } 
-    from '@angular/upgrade/static';
-
-//User generated
-import { RecordCorrectionsComponent } 
-    from './recordCorrections.component.ts';
-
-import { CommonNg2Module }
-    from './../common/common.ts';
+import * as angular from "angular";
+import { Directive, NgModule } from "@angular/core";
+import { downgradeComponent } from "@angular/upgrade/static";
+import { RecordCorrectionsComponent } from "./recordCorrections.component.ts";
+import { CommonNg2Module } from "./../common/common.ts";
 
 // This is the Angular 1 part of the module
 export const RecordCorrectionsModule = angular.module(
-    'RecordCorrectionsModule', 
-    []
+  "RecordCorrectionsModule",
+  []
 );
 
 // This is the Angular 2 part of the module
-@NgModule(
-    {
-        imports: [
-            CommonNg2Module
-        ],
-        declarations: [ 
-            RecordCorrectionsComponent
-        ],
-        entryComponents: [ 
-            RecordCorrectionsComponent 
-        ],
-        providers: [
-            
-        ]
-    }
-)
+@NgModule({
+  imports: [CommonNg2Module],
+  declarations: [RecordCorrectionsComponent],
+  entryComponents: [RecordCorrectionsComponent],
+  providers: []
+})
 export class RecordCorrectionsNg2Module {}
 
 // components migrated to angular 2 should be downgraded here
 //Must convert as much as possible of our code to directives
-RecordCorrectionsModule.directive(
-    'recordCorrectionsNg2', 
-    <any>downgradeComponent(
-        {
-            component: RecordCorrectionsComponent,
-        }
-    )
-);
+RecordCorrectionsModule.directive("recordCorrectionsNg2", <any>(
+  downgradeComponent({
+    component: RecordCorrectionsComponent
+  })
+));
