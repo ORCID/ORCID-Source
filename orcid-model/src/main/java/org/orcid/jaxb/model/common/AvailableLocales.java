00f40401e4c46b4c944134207f472b7c88151ff1
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-11-10 11:28:22 -0600

    Initial commit

---
+++ orcid-model/src/main/java/org/orcid/jaxb/model/common/AvailableLocales.java
@@ -0,0 +1,58 @@
+package org.orcid.jaxb.model.common;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "locale")
+@XmlEnum
+public enum AvailableLocales {
+    @XmlEnumValue("ar")
+    AR("ar"),
+    @XmlEnumValue("en")
+    EN("en"),
+    @XmlEnumValue("es")
+    ES("es"),
+    @XmlEnumValue("fr")
+    FR("fr"),
+    @XmlEnumValue("it")
+    IT("it"),
+    @XmlEnumValue("ja")
+    JA("ja"),
+    @XmlEnumValue("ko")
+    KO("ko"),
+    @XmlEnumValue("pt")
+    PT("pt"),
+    @XmlEnumValue("ru")
+    RU("ru"),
+    @XmlEnumValue("zh_CN")
+    ZH_CN("zh_CN"),
+    @XmlEnumValue("zh_TW")
+    ZH_TW("zh_TW"),
+    @XmlEnumValue("xx")
+    XX("xx");
+    private final String value;
+
+    AvailableLocales(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+    
+    public static AvailableLocales fromValue(String v) {
+        for (AvailableLocales c : AvailableLocales.values()) {
+            if (v.startsWith(c.value)) {
+                return c;
+            }
+        }
+        // if we don't support the specified language return english
+        return AvailableLocales.EN;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
