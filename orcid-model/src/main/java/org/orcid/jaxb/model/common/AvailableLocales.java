cfa0f6621f35bb2bd6b8ee0a4c81d2915940c5ee
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2018-11-12 09:10:56 -0600

    Adding javadoc to AvailableLocales

--- orcid-model/src/main/java/org/orcid/jaxb/model/common/AvailableLocales.java
+++ orcid-model/src/main/java/org/orcid/jaxb/model/common/AvailableLocales.java
@@ -6,31 +6,46 @@ import javax.xml.bind.annotation.XmlType;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+/**
+ * List of available locales in the ORCID registry.
+ * 
+ * <p>
+ * This enum defines the list of available locale in the ORCID registry
+ * </p>
+ * 
+ * <p>
+ * Besides of defining the enum value here, the developer need to generate the
+ * translation files with the help of transifex, see <a href=
+ * "https://github.com/ORCID/ORCID-Source/blob/master/orcid-core/src/main/resources/i18n/README.md">README.md</a>
+ * and update the list of avaliable locales in <a href=
+ * "https://github.com/ORCID/ORCID-Source/blob/master/SUPPORTED_LOCALES.md">SUPPORTED_LOCALES.md</a>.
+ * </p>
+ */
 @XmlType(name = "locale")
 @XmlEnum
 public enum AvailableLocales {
     @XmlEnumValue("ar")
-    AR("ar"),
+    AR("ar"), 
     @XmlEnumValue("en")
-    EN("en"),
+    EN("en"), 
     @XmlEnumValue("es")
-    ES("es"),
+    ES("es"), 
     @XmlEnumValue("fr")
-    FR("fr"),
+    FR("fr"), 
     @XmlEnumValue("it")
-    IT("it"),
+    IT("it"), 
     @XmlEnumValue("ja")
-    JA("ja"),
+    JA("ja"), 
     @XmlEnumValue("ko")
-    KO("ko"),
+    KO("ko"), 
     @XmlEnumValue("pt")
-    PT("pt"),
+    PT("pt"), 
     @XmlEnumValue("ru")
-    RU("ru"),
+    RU("ru"), 
     @XmlEnumValue("zh_CN")
-    ZH_CN("zh_CN"),
+    ZH_CN("zh_CN"), 
     @XmlEnumValue("zh_TW")
-    ZH_TW("zh_TW"),
+    ZH_TW("zh_TW"), 
     @XmlEnumValue("xx")
     XX("xx");
     private final String value;
@@ -42,12 +57,12 @@ public enum AvailableLocales {
     public String value() {
         return value;
     }
-    
+
     @JsonValue
     public String jsonValue() {
         return this.name();
     }
-    
+
     public static AvailableLocales fromValue(String v) {
         for (AvailableLocales c : AvailableLocales.values()) {
             if (v.startsWith(c.value)) {
