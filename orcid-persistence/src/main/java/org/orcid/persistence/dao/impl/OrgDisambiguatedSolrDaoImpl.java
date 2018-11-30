80dfb54d2bcd795760bfb1484dacf5ea2f2e0530
George Nash <bobcaprice@hotmail.com>
2018-11-30 16:59:39 +0100

    Adjusted sort order for orgs solr query

--- orcid-persistence/src/main/java/org/orcid/persistence/dao/impl/OrgDisambiguatedSolrDaoImpl.java
+++ orcid-persistence/src/main/java/org/orcid/persistence/dao/impl/OrgDisambiguatedSolrDaoImpl.java
@@ -12,7 +12,6 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.params.SolrParams;
 import org.orcid.persistence.dao.OrgDisambiguatedSolrDao;
 import org.orcid.utils.solr.entities.OrgDisambiguatedSolrDocument;
 import org.orcid.utils.solr.entities.SolrConstants;
@@ -71,16 +70,20 @@ public class OrgDisambiguatedSolrDaoImpl implements OrgDisambiguatedSolrDao {
 
     @Override
     public List<OrgDisambiguatedSolrDocument> getOrgs(String searchTerm, int firstResult, int maxResult, boolean fundersOnly) {
-        SolrQuery query = new SolrQuery();
+        StringBuilder queryString = new StringBuilder("{!edismax qf='org-disambiguated-name^50.0 text^1.0' pf='org-disambiguated-name^50.0' mm=1 ");
+        queryString.append("sort='score desc, ");
+        queryString.append(SolrConstants.ORG_CHOSEN_BY_MEMBER).append(" desc, ");
+        queryString.append("org-disambiguated-popularity desc'}");
+        queryString.append(searchTerm).append("*");
+        
         if (fundersOnly) {
-            query.setQuery(
-                    "{!edismax qf='org-disambiguated-name^50.0 text^1.0' pf='org-disambiguated-name^50.0' mm=1 sort='score desc, org-disambiguated-popularity desc'}"
-                            + searchTerm + "* AND is-funding-org:true").setFields("*");
-        } else {
-            query.setQuery(
-                    "{!edismax qf='org-disambiguated-name^50.0 text^1.0' pf='org-disambiguated-name^50.0' mm=1 sort='score desc, org-disambiguated-popularity desc'}"
-                            + searchTerm + "*").setFields("*");
+            queryString.append(" AND is-funding-org:true");
         }
+
+        SolrQuery query = new SolrQuery();
+        query.setQuery(queryString.toString());
+        query.setFields("*");
+        
         try {
             QueryResponse queryResponse = solrServerReadOnly.query(query);
             return queryResponse.getBeans(OrgDisambiguatedSolrDocument.class);
