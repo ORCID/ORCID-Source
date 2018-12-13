864493ca491d6b00dec0b460f0cb2abe4b5dc83f
lizkrznarich <ejkrznarich@gmail.com>
2018-12-13 08:11:17 -0500

    remove ng1 code and references to it

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/filters/formatBibtexOutput.js
+++
@@ -1,6 +0,0 @@
-angular.module('orcidApp').filter('formatBibtexOutput', function () {
-    return function (text) {
-        var str = text.replace(/[\-?_?]/, ' ');
-        return str.toUpperCase();
-    };
-});
