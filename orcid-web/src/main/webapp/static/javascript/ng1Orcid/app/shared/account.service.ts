5e830dd19474582471f7ee5eeb95276fa6e0295b
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-01-16 12:04:23 -0600

    Remove csrf header from the custom headers in several services

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/shared/account.service.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/shared/account.service.ts
@@ -24,8 +24,7 @@ export class AccountService {
         this.headers = new HttpHeaders(
             {
                 'Access-Control-Allow-Origin':'*',
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': document.querySelector("meta[name='_csrf']").getAttribute("content")
+                'Content-Type': 'application/json'
             }
         );
     }
