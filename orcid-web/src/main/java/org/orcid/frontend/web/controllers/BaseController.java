c354bd8abefc5564e1cd2f5545f9ff4900b63b65
George Nash <bobcaprice@hotmail.com>
2019-01-28 16:02:36 +0100

    Improved url validation such that path is now url encoded

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/BaseController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/BaseController.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -667,27 +672,38 @@ public class BaseController {
         validateUrl(url, "common.invalid_url");
     }
     
-    protected void validateUrl(Text url, String errorCode) {
-        // Clear previous errors
-        url.setErrors(new ArrayList<String>());
-        if (!PojoUtil.isEmpty(url.getValue())) {
-            // trim if required
-            if (!url.getValue().equals(url.getValue().trim()))
-                url.setValue(url.getValue().trim());
+    protected void validateUrl(Text urlString, String errorCode) {
+        urlString.setErrors(new ArrayList<String>());
+        if (!PojoUtil.isEmpty(urlString.getValue())) {
+            urlString.setValue(urlString.getValue().trim());
+            validateNoLongerThan(SiteConstants.URL_MAX_LENGTH, urlString);
 
-            // check length
-            validateNoLongerThan(SiteConstants.URL_MAX_LENGTH, url);
 
             // add protocol if missing
-            if (!urlValidator.isValid(url.getValue())) {
-                String tempUrl = "http://" + url.getValue();
-                // test validity again
-                if (urlValidator.isValid(tempUrl))
-                    url.setValue("http://" + url.getValue());
-                else
-                    setError(url, errorCode);
+            boolean valid = false;
+            try {
+                valid = urlValidator.isValid(encodeUrl(urlString.getValue()));
+                if (!valid) {
+                    String tempUrl = encodeUrl("http://" + urlString.getValue());
+                    // test validity again
+                    valid = urlValidator.isValid(tempUrl);
+                    if (valid) {
+                        urlString.setValue("http://" + urlString.getValue());
+                    } 
+                }
+            } catch (Exception e) {
+            }
+
+            if (!valid) {
+                setError(urlString, errorCode);
             }
         }
+    }
+    
+    private String encodeUrl(String urlString) throws MalformedURLException, URISyntaxException {
+        URL url = new URL(urlString);
+        URI encoded = new URI(url.getProtocol(), url.getHost(), url.getPath(), null);
+        return encoded.toASCIIString();
     }
     
     /**
