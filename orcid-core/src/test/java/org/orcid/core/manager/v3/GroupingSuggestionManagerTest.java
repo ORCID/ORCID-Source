83e38b677ded5bf9e9789a6cf93c108a2a7a8e8c
George Nash <bobcaprice@hotmail.com>
2019-02-07 15:43:32 +0100

    Fixed test

--- orcid-core/src/test/java/org/orcid/core/manager/v3/GroupingSuggestionManagerTest.java
+++ orcid-core/src/test/java/org/orcid/core/manager/v3/GroupingSuggestionManagerTest.java
@@ -3,6 +3,7 @@ package org.orcid.core.manager.v3;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
@@ -82,7 +83,7 @@ public class GroupingSuggestionManagerTest {
         groupingSuggestionManager.cacheGroupingSuggestions("orcid", Arrays.asList(first, second, third));
         
         List<WorkGroupingSuggestion> retrieved = suggestionListCaptor.getValue();
-        assertNull(retrieved);
+        assertTrue(retrieved.isEmpty());
     }
     
     @Test
