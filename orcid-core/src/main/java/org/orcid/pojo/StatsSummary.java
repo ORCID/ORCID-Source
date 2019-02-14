7eb7ed04ee159353071f85ab8dfb1ef9307a4397
George Nash <bobcaprice@hotmail.com>
2019-02-14 22:51:00 +0700

    Added endpoint for new statistics angular component to request statistics, rather than putting them into model

---
+++ orcid-core/src/main/java/org/orcid/pojo/StatsSummary.java
@@ -0,0 +1,195 @@
+package org.orcid.pojo;
+
+public class StatsSummary {
+
+    private String statisticsDate;
+
+    private Long liveIds;
+    
+    private Long idsWithExternalId;
+    
+    private Long idsWithEducation;
+    
+    private Long numEducations;
+    
+    private Long educationUniqueOrgs;
+    
+    private Long idsWithEmployment;
+    
+    private Long numEmployments;
+    
+    private Long employmentUniqueOrgs;
+    
+    private Long idsWithFunding;
+    
+    private Long numFundings;
+    
+    private Long fundingUniqueOrgs;
+    
+    private Long idsWithPeerReview;
+    
+    private Long numPeerReviews;
+    
+    private Long idsWithPersonId;
+    
+    private Long numPersonIds;
+    
+    private Long idsWithWork;
+    
+    private Long numWorks;
+    
+    private Long uniqueDois;
+
+    public String getStatisticsDate() {
+        return statisticsDate;
+    }
+
+    public void setStatisticsDate(String statisticsDate) {
+        this.statisticsDate = statisticsDate;
+    }
+
+    public Long getLiveIds() {
+        return liveIds;
+    }
+
+    public void setLiveIds(Long liveIds) {
+        this.liveIds = liveIds;
+    }
+
+    public Long getIdsWithExternalId() {
+        return idsWithExternalId;
+    }
+
+    public void setIdsWithExternalId(Long idsWithExternalId) {
+        this.idsWithExternalId = idsWithExternalId;
+    }
+
+    public Long getIdsWithEducation() {
+        return idsWithEducation;
+    }
+
+    public void setIdsWithEducation(Long idsWithEducation) {
+        this.idsWithEducation = idsWithEducation;
+    }
+
+    public Long getNumEducations() {
+        return numEducations;
+    }
+
+    public void setNumEducations(Long numEducations) {
+        this.numEducations = numEducations;
+    }
+
+    public Long getEducationUniqueOrgs() {
+        return educationUniqueOrgs;
+    }
+
+    public void setEducationUniqueOrgs(Long educationUniqueOrgs) {
+        this.educationUniqueOrgs = educationUniqueOrgs;
+    }
+
+    public Long getIdsWithEmployment() {
+        return idsWithEmployment;
+    }
+
+    public void setIdsWithEmployment(Long idsWithEmployment) {
+        this.idsWithEmployment = idsWithEmployment;
+    }
+
+    public Long getNumEmployments() {
+        return numEmployments;
+    }
+
+    public void setNumEmployments(Long numEmployments) {
+        this.numEmployments = numEmployments;
+    }
+
+    public Long getEmploymentUniqueOrgs() {
+        return employmentUniqueOrgs;
+    }
+
+    public void setEmploymentUniqueOrgs(Long employmentUniqueOrgs) {
+        this.employmentUniqueOrgs = employmentUniqueOrgs;
+    }
+
+    public Long getIdsWithFunding() {
+        return idsWithFunding;
+    }
+
+    public void setIdsWithFunding(Long idsWithFunding) {
+        this.idsWithFunding = idsWithFunding;
+    }
+
+    public Long getNumFundings() {
+        return numFundings;
+    }
+
+    public void setNumFundings(Long numFundings) {
+        this.numFundings = numFundings;
+    }
+
+    public Long getFundingUniqueOrgs() {
+        return fundingUniqueOrgs;
+    }
+
+    public void setFundingUniqueOrgs(Long fundingUniqueOrgs) {
+        this.fundingUniqueOrgs = fundingUniqueOrgs;
+    }
+
+    public Long getIdsWithPeerReview() {
+        return idsWithPeerReview;
+    }
+
+    public void setIdsWithPeerReview(Long idsWithPeerReview) {
+        this.idsWithPeerReview = idsWithPeerReview;
+    }
+
+    public Long getNumPeerReviews() {
+        return numPeerReviews;
+    }
+
+    public void setNumPeerReviews(Long numPeerReviews) {
+        this.numPeerReviews = numPeerReviews;
+    }
+
+    public Long getIdsWithPersonId() {
+        return idsWithPersonId;
+    }
+
+    public void setIdsWithPersonId(Long idsWithPersonId) {
+        this.idsWithPersonId = idsWithPersonId;
+    }
+
+    public Long getNumPersonIds() {
+        return numPersonIds;
+    }
+
+    public void setNumPersonIds(Long numPersonIds) {
+        this.numPersonIds = numPersonIds;
+    }
+
+    public Long getIdsWithWork() {
+        return idsWithWork;
+    }
+
+    public void setIdsWithWork(Long idsWithWorks) {
+        this.idsWithWork = idsWithWorks;
+    }
+
+    public Long getNumWorks() {
+        return numWorks;
+    }
+
+    public void setNumWorks(Long numWorks) {
+        this.numWorks = numWorks;
+    }
+
+    public Long getUniqueDois() {
+        return uniqueDois;
+    }
+
+    public void setUniqueDois(Long uniqueDois) {
+        this.uniqueDois = uniqueDois;
+    }
+    
+}
--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/StatisticsController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/StatisticsController.java
@@ -2,18 +2,18 @@ package org.orcid.frontend.web.controllers;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 import org.orcid.core.manager.impl.StatisticsCacheManager;
 import org.orcid.jaxb.model.statistics.StatisticsSummary;
+import org.orcid.pojo.StatsSummary;
 import org.springframework.context.MessageSource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,36 +34,47 @@ public class StatisticsController extends BaseController {
     @RequestMapping
     public ModelAndView getStatistics() {        
         ModelAndView mav = new ModelAndView("statistics");
-        Map<String, Long> statisticsMap = null;
-                
-        StatisticsSummary statisticsSummary = statisticsCacheManager.retrieve();
-        
-        if(statisticsSummary != null) {
-        	statisticsMap = statisticsSummary.getStatistics();
-        	mav.addObject("statistics_date", formatStatisticsDate(statisticsSummary.getDate()));
-        }
-        if(statisticsMap == null) {
-        	statisticsMap = new HashMap<String, Long>();
-        }
-        mav.addObject("statistics", statisticsMap);
-        
         return mav;
     }
     
+    @RequestMapping(value = "/statistics.json", method = RequestMethod.GET)
+    public @ResponseBody StatsSummary getStatsSummary() {
+        StatisticsSummary statisticsSummary = statisticsCacheManager.retrieve();
+        StatsSummary statsSummary = new StatsSummary();
+        if (statisticsSummary != null) {
+            mapProperties(statisticsSummary.getStatistics(), statsSummary);
+            statsSummary.setStatisticsDate(formatStatisticsDate(statisticsSummary.getDate()));
+        }
+        return statsSummary;
+    }
+    
+    private void mapProperties(Map<String, Long> statistics, StatsSummary statsSummary) {
+        statsSummary.setLiveIds(statistics.get("liveIds"));
+        statsSummary.setIdsWithExternalId(statistics.get("idsWithExternalId"));
+        statsSummary.setIdsWithEducation(statistics.get("idsWithEducation"));
+        statsSummary.setNumEducations(statistics.get("education"));
+        statsSummary.setEducationUniqueOrgs(statistics.get("educationUniqueOrg"));
+        statsSummary.setIdsWithEmployment(statistics.get("idsWithEmployment"));
+        statsSummary.setNumEmployments(statistics.get("employment"));
+        statsSummary.setEmploymentUniqueOrgs(statistics.get("employmentUniqueOrg"));
+        statsSummary.setIdsWithFunding(statistics.get("idsWithFunding"));
+        statsSummary.setNumFundings(statistics.get("funding"));
+        statsSummary.setFundingUniqueOrgs(statistics.get("fundingUniqueOrg"));
+        statsSummary.setIdsWithPeerReview(statistics.get("idsWithPeerReview"));
+        statsSummary.setNumPeerReviews(statistics.get("peerReview"));
+        statsSummary.setIdsWithPersonId(statistics.get("idsWithPersonId"));
+        statsSummary.setNumPersonIds(statistics.get("personId"));
+        statsSummary.setIdsWithWork(statistics.get("idsWithWork"));
+        statsSummary.setNumWorks(statistics.get("work"));
+        statsSummary.setUniqueDois(statistics.get("uniqueDois"));
+    }
+
     /**
      * Formats the date when the statistic was added
      * */
     private String formatStatisticsDate(Date date){
         SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd"); 
         return dt.format(date);
-    }
-    
-    /** 
-     * @return the total amount of Live iDs  
-     * */
-    @RequestMapping(value = "/liveids.json")    
-    public @ResponseBody String getLiveIdsAmount(HttpServletRequest request) {        
-        return statisticsCacheManager.retrieveLiveIds(request.getLocale());
     }
     
 }
