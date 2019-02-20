0ddcf9dde882ec95e99b69c84d95cf1a7c8bc50b
George Nash <bobcaprice@hotmail.com>
2019-02-20 09:56:51 +0700

    Formatting

--- orcid-core/src/main/java/org/orcid/pojo/ajaxForm/ExternalIdentifierForm.java
+++ orcid-core/src/main/java/org/orcid/pojo/ajaxForm/ExternalIdentifierForm.java
@@ -15,11 +15,11 @@ public class ExternalIdentifierForm extends VisibilityForm implements ErrorsInte
     private String reference;
     private String url;
     private String source;
-    private String sourceName;    
+    private String sourceName;
     private Long displayIndex;
     private String putCode;
     private Date createdDate;
-	private Date lastModified;
+    private Date lastModified;
 
     public static ExternalIdentifierForm valueOf(PersonExternalIdentifier extId) {
         if (extId == null)
@@ -47,25 +47,25 @@ public class ExternalIdentifierForm extends VisibilityForm implements ErrorsInte
         } else {
             form.setDisplayIndex(Long.valueOf(0L));
         }
-        
+
         // Set created date
         form.setCreatedDate(Date.valueOf(extId.getCreatedDate()));
-        //Set last modified
-        form.setLastModified(Date.valueOf(extId.getLastModifiedDate()));                
-        
+        // Set last modified
+        form.setLastModified(Date.valueOf(extId.getLastModifiedDate()));
+
         return form;
     }
-    
+
     public PersonExternalIdentifier toPersonExternalIdentifier() {
         PersonExternalIdentifier result = new PersonExternalIdentifier();
-        if(putCode != null) {
+        if (putCode != null) {
             result.setPutCode(Long.valueOf(putCode));
         }
         result.setDisplayIndex(displayIndex);
-        if(visibility != null && visibility.getVisibility() != null) {
+        if (visibility != null && visibility.getVisibility() != null) {
             result.setVisibility(org.orcid.jaxb.model.v3.rc2.common.Visibility.fromValue(visibility.getVisibility().value()));
         }
-        if(url != null) {
+        if (url != null) {
             result.setUrl(new Url(url));
         }
         result.setType(commonName);
@@ -136,19 +136,20 @@ public class ExternalIdentifierForm extends VisibilityForm implements ErrorsInte
     public void setDisplayIndex(Long displayIndex) {
         this.displayIndex = displayIndex;
     }
+
     public Date getCreatedDate() {
-		return createdDate;
-	}
+        return createdDate;
+    }
 
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 
-	public Date getLastModified() {
-		return lastModified;
-	}
+    public Date getLastModified() {
+        return lastModified;
+    }
 
-	public void setLastModified(Date lastModified) {
-		this.lastModified = lastModified;
-	}
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
 }
