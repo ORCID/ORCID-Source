f4920b281350e659d79c41b5bfb1901589ba516c
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-02-27 10:30:11 -0600

    Features keep being static

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/shared/features.service.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/shared/features.service.ts
@@ -14,27 +14,10 @@ export class FeaturesService {
     private done = false;
     
      constructor(private commonSrvc: CommonService){
-         /*
-         this.commonSrvc.configInfo$
-         .subscribe(
-             data => {
-                 this.features = data.messages;  
-                 this.done = true;
-             },
-             error => {
-                 console.log('features.service.ts: unable to fetch configInfo', error);                
-             } 
-         );
-         */
+         
     }
     
     isFeatureEnabled(featureName: string) : boolean {
-        /*         
-         while(!done) {
-            console.log('Awaiting for features');
-            window.setTimeout(10)
-        }
-        */
         if (this.features[featureName]) {
             return true;
         }
