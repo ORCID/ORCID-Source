fb180f348fafac4db1292a7c1e19956245c85d59
leomendoza123 <leomendoza123@gmail.com>
2019-02-28 13:40:54 -0600

    revert 'Build landing page with static templates'

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/webpack.config.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/webpack.config.js
@@ -1,7 +1,4 @@
 var webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-
 
 module.exports = {
     context: __dirname + "/",
@@ -26,28 +23,7 @@ module.exports = {
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
         }
     }),
-    new HtmlWebpackPlugin({
-        template: __dirname + '/app/templates/index.html',
-        output: __dirname + '/dist',
-        // inject: 'head'
-    }),
-    new CopyPlugin([
-        { from: '../../css/**/*.css', to: './ng1Orcid//dist' },
-        { from: '../../fonts/**/*', to: './ng1Orcid//dist' },
-        { from: '../../img/**/*', to: './ng1Orcid//dist' },
-        { from: '../../twitter-bootstrap/**/*', to: './ng1Orcid/dist' },
-        {from: '../jquery/**/*', to: './javascript/' },
-        {from: '../jquery-migrate/**/*', to: './javascript/' },
-        {from: '../jqueryui/**/*', to: './javascript/' },
-        {from: '../typeahead/**/*', to: './javascript/' },
-        {from: '../plugins.js', to: './javascript/' },
-        {from: '../orcid.js', to: './javascript/' },
-        {from: '../script.js', to: './javascript/' },
-
-
-
-      ]),
-     ],
+    ],
     resolve: {
         alias: {
             "@angular/upgrade/static": "@angular/upgrade/bundles/upgrade-static.umd.js"
