1030b5e3441da7fb15b88bc7f58ab51173427b81
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-03-27 14:07:22 -0600

    Token endpoint back

--- orcid-api-web/src/main/java/org/orcid/api/member/common/OrcidApiCommonEndpoints.java
+++ orcid-api-web/src/main/java/org/orcid/api/member/common/OrcidApiCommonEndpoints.java
@@ -1,20 +1,14 @@
-package T2OrcidApiServiceImplBase;
+package org.orcid.api.member.common;
 
 import static org.orcid.api.common.T2OrcidApiService.OAUTH_TOKEN;
-import static org.orcid.api.common.T2OrcidApiService.OAUTH_REVOKE;
+import static org.orcid.core.api.OrcidApiConstants.ORCID_JSON;
+import static org.orcid.core.api.OrcidApiConstants.ORCID_XML;
+import static org.orcid.core.api.OrcidApiConstants.PROFILE_GET_PATH;
+import static org.orcid.core.api.OrcidApiConstants.PROFILE_POST_PATH;
+import static org.orcid.core.api.OrcidApiConstants.VND_ORCID_JSON;
 import static org.orcid.core.api.OrcidApiConstants.VND_ORCID_XML;
 
 import java.net.URI;
-import java.security.NoSuchAlgorithmException;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.orcid.core.api.OrcidApiConstants.VND_ORCID_JSON;
-import static org.orcid.core.api.OrcidApiConstants.ORCID_JSON;
-import static org.orcid.core.api.OrcidApiConstants.ORCID_XML;
-import static org.orcid.core.api.OrcidApiConstants.PROFILE_POST_PATH;
-import static org.orcid.core.api.OrcidApiConstants.PROFILE_GET_PATH;
 
 import javax.annotation.Resource;
 import javax.ws.rs.Consumes;
@@ -31,55 +25,14 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.lang.StringUtils;
-import org.hibernate.exception.ConstraintViolationException;
-import org.orcid.core.adapter.Jaxb2JpaAdapter;
-import org.orcid.core.adapter.JpaJaxbEntityAdapter;
 import org.orcid.core.exception.OrcidBadRequestException;
+import org.orcid.core.locale.LocaleManager;
+import org.orcid.core.manager.APIRecordCreateManager;
 import org.orcid.core.oauth.OrcidClientCredentialEndPointDelegator;
-import org.orcid.core.security.OrcidWebRole;
-import org.orcid.core.security.visibility.OrcidVisibilityDefaults;
 import org.orcid.core.security.visibility.aop.AccessControl;
-import org.orcid.core.togglz.Features;
-import org.orcid.jaxb.model.common.OrcidType;
-import org.orcid.jaxb.model.message.Affiliation;
-import org.orcid.jaxb.model.message.CreationMethod;
-import org.orcid.jaxb.model.message.Email;
-import org.orcid.jaxb.model.message.ExternalIdentifier;
-import org.orcid.jaxb.model.message.Funding;
-import org.orcid.jaxb.model.message.Keyword;
-import org.orcid.jaxb.model.message.Keywords;
-import org.orcid.jaxb.model.message.OrcidActivities;
-import org.orcid.jaxb.model.message.OrcidBio;
-import org.orcid.jaxb.model.message.OrcidHistory;
 import org.orcid.jaxb.model.message.OrcidMessage;
 import org.orcid.jaxb.model.message.OrcidProfile;
-import org.orcid.jaxb.model.message.OrcidWork;
-import org.orcid.jaxb.model.message.OtherName;
-import org.orcid.jaxb.model.message.ResearcherUrl;
-import org.orcid.jaxb.model.message.ResearcherUrls;
 import org.orcid.jaxb.model.message.ScopePathType;
-import org.orcid.jaxb.model.message.Source;
-import org.orcid.jaxb.model.message.SourceClientId;
-import org.orcid.jaxb.model.message.SourceName;
-import org.orcid.jaxb.model.message.SubmissionDate;
-import org.orcid.jaxb.model.message.Visibility;
-import org.orcid.jaxb.model.message.Work;
-import org.orcid.persistence.dao.ProfileDao;
-import org.orcid.persistence.jpa.entities.ClientDetailsEntity;
-import org.orcid.persistence.jpa.entities.OrcidGrantedAuthority;
-import org.orcid.persistence.jpa.entities.ProfileEntity;
-import org.orcid.utils.DateUtils;
-import org.orcid.utils.OrcidStringUtils;
-import org.springframework.dao.DataAccessException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.OAuth2Request;
-import org.orcid.core.locale.LocaleManager;
-import org.orcid.core.manager.EncryptionManager;
-import org.orcid.core.manager.LoadOptions;
-import org.orcid.core.manager.OrcidGenerationManager;
-import org.orcid.core.manager.v3.NotificationManager;
 
 @Path("/")
 public class OrcidApiCommonEndpoints {
@@ -93,6 +46,9 @@ public class OrcidApiCommonEndpoints {
     @Resource
     private LocaleManager localeManager;
 
+    @Resource
+    private APIRecordCreateManager apiRecordCreateManager;
+    
     /**
      * 
      * @param formParams
@@ -121,12 +77,12 @@ public class OrcidApiCommonEndpoints {
     public Response createProfile(OrcidMessage orcidMessage) {
         // TODO: we should remove this ASAP
         OrcidProfile orcidProfile = apiRecordCreateManager.createProfile(orcidMessage);
-        return createProfile(uriInfo, orcidMessage);
+        return getCreatedResponse(orcidProfile);
     }
 
-    private Response getCreatedResponse(UriInfo uriInfo, String requested, OrcidProfile profile) {
+    private Response getCreatedResponse(OrcidProfile profile) {
         if (profile != null && profile.getOrcidIdentifier() != null) {
-            URI uri = uriInfo.getBaseUriBuilder().path("/").path(requested).build(profile.getOrcidIdentifier().getPath());
+            URI uri = uriInfo.getBaseUriBuilder().path("/").path(PROFILE_GET_PATH).build(profile.getOrcidIdentifier().getPath());
             return Response.created(uri).build();
         } else {
             throw new OrcidBadRequestException(localeManager.resolveMessage("apiError.badrequest_findorcid.exception"));
