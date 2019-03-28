16f988d9c853aa7f93ed4fe91ddeba089ab1b3b1
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-03-28 10:52:34 -0600

    Put the token endpoint in the orcid-api-common project, so, the members and pub API can share the code

---
+++ orcid-api-common/src/main/java/org/orcid/api/common/OrcidApiCommonEndpoints.java
@@ -0,0 +1,50 @@
+package org.orcid.api.common;
+
+import static org.orcid.api.common.T2OrcidApiService.OAUTH_TOKEN;
+
+import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.commons.lang.StringUtils;
+import org.orcid.core.locale.LocaleManager;
+import org.orcid.core.oauth.OrcidClientCredentialEndPointDelegator;
+import org.springframework.stereotype.Component;
+
+@Path("/")
+@Component
+public class OrcidApiCommonEndpoints {
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Resource
+    private OrcidClientCredentialEndPointDelegator orcidClientCredentialEndPointDelegator;
+
+    @Resource
+    private LocaleManager localeManager;
+    
+    /**
+     * 
+     * @param formParams
+     * @return
+     */
+    @POST
+    @Path(OAUTH_TOKEN)
+    @Produces(value = { MediaType.APPLICATION_JSON })
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response obtainOauth2TokenPost(@HeaderParam("Authorization") @DefaultValue(StringUtils.EMPTY) String authorization, @FormParam("grant_type") String grantType,
+            MultivaluedMap<String, String> formParams) {
+        return orcidClientCredentialEndPointDelegator.obtainOauth2Token(authorization, formParams);
+    }    
+}
--- orcid-core/src/main/java/org/orcid/core/manager/APIRecordCreateManager.java
+++
@@ -1,9 +0,0 @@
-package org.orcid.core.manager;
-
-import org.orcid.jaxb.model.message.OrcidMessage;
-import org.orcid.jaxb.model.message.OrcidProfile;
-
-@Deprecated
-public interface APIRecordCreateManager {
-    OrcidProfile createProfile(OrcidMessage orcidMessage);
-}
