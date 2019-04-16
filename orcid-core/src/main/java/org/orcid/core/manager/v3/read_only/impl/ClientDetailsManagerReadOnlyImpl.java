ed212fa3bcf55bf23b704fb677563c00eafb72b5
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-04-16 13:10:03 -0600

    Create client summary endpoint

--- orcid-core/src/main/java/org/orcid/core/manager/v3/read_only/impl/ClientDetailsManagerReadOnlyImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/v3/read_only/impl/ClientDetailsManagerReadOnlyImpl.java
@@ -12,6 +12,7 @@ import org.orcid.core.adapter.v3.JpaJaxbClientAdapter;
 import org.orcid.core.manager.EncryptionManager;
 import org.orcid.core.manager.v3.read_only.ClientDetailsManagerReadOnly;
 import org.orcid.jaxb.model.v3.rc2.client.Client;
+import org.orcid.jaxb.model.v3.rc2.client.ClientSummary;
 import org.orcid.persistence.dao.ClientDetailsDao;
 import org.orcid.persistence.dao.ClientRedirectDao;
 import org.orcid.persistence.dao.ClientSecretDao;
@@ -199,6 +200,13 @@ public class ClientDetailsManagerReadOnlyImpl implements ClientDetailsManagerRea
         Date lastModified = clientDetailsDao.getLastModified(clientId);
         ClientDetailsEntity clientDetailsEntity = clientDetailsDao.findByClientId(clientId, lastModified.getTime());
         return jpaJaxbClientAdapter.toClient(clientDetailsEntity);
+    }
+
+    @Override
+    public ClientSummary getClientSummary(String clientId) {
+        Date lastModified = clientDetailsDao.getLastModified(clientId);
+        ClientDetailsEntity clientDetailsEntity = clientDetailsDao.findByClientId(clientId, lastModified.getTime());
+        return jpaJaxbClientAdapter.toClientSummary(clientDetailsEntity);
     }
         
 }
