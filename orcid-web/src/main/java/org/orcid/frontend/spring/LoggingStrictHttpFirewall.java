2e1931485bbabb8035c8ff3b90a5e3d80ecd192b
lizkrznarich <ejkrznarich@gmail.com>
2019-04-16 10:18:45 -0500

    format code

--- orcid-web/src/main/java/org/orcid/frontend/spring/LoggingStrictHttpFirewall.java
+++ orcid-web/src/main/java/org/orcid/frontend/spring/LoggingStrictHttpFirewall.java
@@ -9,23 +9,25 @@ import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 public class LoggingStrictHttpFirewall extends StrictHttpFirewall {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingStrictHttpFirewall.class);
 
-    public LoggingStrictHttpFirewall()
-    {
+    public LoggingStrictHttpFirewall() {
         super();
         return;
     }
 
     /**
-     * Provides the request object which will be passed through the filter chain.
+     * Provides the request object which will be passed through the filter
+     * chain.
      *
-     * @returns A FirewalledRequest (required by the HttpFirewall interface) which
-     *          inconveniently breaks the general contract of ServletFilter because
-     *          we can't upcast this to an HttpServletRequest. This prevents us
-     *          from re-wrapping this using an HttpServletRequestWrapper.
-     * @throws RequestRejectedException if the request should be rejected immediately.
+     * @returns A FirewalledRequest (required by the HttpFirewall interface)
+     *          which inconveniently breaks the general contract of
+     *          ServletFilter because we can't upcast this to an
+     *          HttpServletRequest. This prevents us from re-wrapping this using
+     *          an HttpServletRequestWrapper.
+     * @throws RequestRejectedException
+     *             if the request should be rejected immediately.
      */
     @Override
     public FirewalledRequest getFirewalledRequest(final HttpServletRequest request) throws RequestRejectedException {
@@ -33,14 +35,13 @@ public class LoggingStrictHttpFirewall extends StrictHttpFirewall {
             return super.getFirewalledRequest(request);
         } catch (RequestRejectedException ex) {
             LOGGER.warn("Intercepted RequestBlockedException: " + "Request URL: " + request.getRequestURL().toString());
-            // Wrap in a new RequestRejectedException with request metadata and a shallower stack trace.
-            throw new RequestRejectedException(ex.getMessage() + ".\n Request URL: " + request.getRequestURL().toString())
-            {
+            // Wrap in a new RequestRejectedException with request metadata and
+            // a shallower stack trace.
+            throw new RequestRejectedException(ex.getMessage() + ".\n Request URL: " + request.getRequestURL().toString()) {
                 private static final long serialVersionUID = 1L;
 
                 @Override
-                public synchronized Throwable fillInStackTrace()
-                {
+                public synchronized Throwable fillInStackTrace() {
                     return this; // suppress the stack trace.
                 }
             };
@@ -48,18 +49,19 @@ public class LoggingStrictHttpFirewall extends StrictHttpFirewall {
     }
 
     /**
-     * Provides the response which will be passed through the filter chain.
-     * This method isn't extensible because the request may already be committed.
-     * Furthermore, this is only invoked for requests that were not blocked, so we can't
-     * control the status or response for blocked requests here.
+     * Provides the response which will be passed through the filter chain. This
+     * method isn't extensible because the request may already be committed.
+     * Furthermore, this is only invoked for requests that were not blocked, so
+     * we can't control the status or response for blocked requests here.
      *
-     * @param response The original HttpServletResponse.
+     * @param response
+     *            The original HttpServletResponse.
      * @return the original response or a replacement/wrapper.
      */
     @Override
-    public HttpServletResponse getFirewalledResponse(final HttpServletResponse response)
-    {
-        // Note: The FirewalledResponse class is not accessible outside the package.
+    public HttpServletResponse getFirewalledResponse(final HttpServletResponse response) {
+        // Note: The FirewalledResponse class is not accessible outside the
+        // package.
         return super.getFirewalledResponse(response);
     }
 
