4a197dfe40e5ae331db5508dba1537be14ea5faa
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-04-16 13:33:30 -0600

    Fix unit test

--- orcid-pub-web/src/test/java/org/orcid/api/publicV3/server/PublicV3ApiServiceDelegatorTest.java
+++ orcid-pub-web/src/test/java/org/orcid/api/publicV3/server/PublicV3ApiServiceDelegatorTest.java
@@ -51,6 +51,7 @@ import org.orcid.jaxb.model.common.WorkType;
 import org.orcid.jaxb.model.message.CreationMethod;
 import org.orcid.jaxb.model.message.ScopePathType;
 import org.orcid.jaxb.model.v3.rc2.client.Client;
+import org.orcid.jaxb.model.v3.rc2.client.ClientSummary;
 import org.orcid.jaxb.model.v3.rc2.common.OrcidIdentifier;
 import org.orcid.jaxb.model.v3.rc2.common.Visibility;
 import org.orcid.jaxb.model.v3.rc2.error.OrcidError;
@@ -1249,14 +1250,14 @@ public class PublicV3ApiServiceDelegatorTest extends DBUnitTest {
     }
 
     @Test
-    public void testViewClient() {
+    public void testViewClientSummary() {
         Response response = serviceDelegator.viewClient("APP-6666666666666666");
         assertNotNull(response.getEntity());
-        assertTrue(response.getEntity() instanceof Client);
+        assertTrue(response.getEntity() instanceof ClientSummary);
 
-        Client client = (Client) response.getEntity();
-        assertEquals("Source Client 2", client.getName());
-        assertEquals("A test source client", client.getDescription());
+        ClientSummary summary = (ClientSummary) response.getEntity();
+        assertEquals("Source Client 2", summary.getName());
+        assertEquals("A test source client", summary.getDescription());
     }
 
     // Distinction
