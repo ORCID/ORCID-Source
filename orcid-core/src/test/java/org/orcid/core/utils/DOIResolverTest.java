7f8884650ce45a88814edf440c194e870378d883
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-05-23 10:32:29 -0600

    Journal title could come in 'container-title' or 'container-title-short' fields

--- orcid-core/src/test/java/org/orcid/core/utils/DOIResolverTest.java
+++ orcid-core/src/test/java/org/orcid/core/utils/DOIResolverTest.java
@@ -124,60 +124,76 @@ public class DOIResolverTest {
             }
 
         });
+        
+        when(cache.get("https://doi.org/10.000/0000.0002", "application/vnd.citationstyles.csl+json")).thenAnswer(new Answer<InputStream>() {
+
+            @Override
+            public InputStream answer(InvocationOnMock invocation) throws Throwable {
+                return DOIResolverTest.class.getResourceAsStream("/examples/works/form_autofill/doi-journal-title-in-container-title.json");
+            }
+
+        });
+        
+        when(cache.get("https://doi.org/10.000/0000.0003", "application/vnd.citationstyles.csl+json")).thenAnswer(new Answer<InputStream>() {
+
+            @Override
+            public InputStream answer(InvocationOnMock invocation) throws Throwable {
+                return DOIResolverTest.class.getResourceAsStream("/examples/works/form_autofill/doi-journal-title-in-container-title-short.json");
+            }
+
+        });
     }
 
     @Test
     public void resolveMetadataTest() {
         Work work = resolver.resolveMetadata("doi", "10.000/0000.0000");
-        assertNotNull(work);
-        assertEquals(WorkType.JOURNAL_ARTICLE, work.getWorkType());
-        assertEquals("DOI title", work.getWorkTitle().getTitle().getContent());
-        assertEquals("Subtitle", work.getWorkTitle().getSubtitle().getContent());
         assertEquals("Journal title", work.getJournalTitle().getContent());
-        assertEquals("This is a description", work.getShortDescription());
-        assertEquals("2018", work.getPublicationDate().getYear().getValue());
-        assertEquals("12", work.getPublicationDate().getMonth().getValue());
-        assertEquals("31", work.getPublicationDate().getDay().getValue());
-        assertEquals("http://dx.doi.org/10.000/0000.0000", work.getUrl().getValue());
-        
-        assertEquals(5, work.getExternalIdentifiers().getExternalIdentifier().size());
-        assertEquals("doi", work.getExternalIdentifiers().getExternalIdentifier().get(0).getType());
-        assertEquals("https://doi.org/10.000/0000.0000", work.getExternalIdentifiers().getExternalIdentifier().get(0).getUrl().getValue());
-        assertEquals("10.000/0000.0000", work.getExternalIdentifiers().getExternalIdentifier().get(0).getValue());
-        assertEquals(Relationship.SELF, work.getExternalIdentifiers().getExternalIdentifier().get(0).getRelationship());
-        
-        assertEquals("isbn", work.getExternalIdentifiers().getExternalIdentifier().get(1).getType());
-        assertEquals("https://www.worldcat.org/isbn/ISBN-0000-0000", work.getExternalIdentifiers().getExternalIdentifier().get(1).getUrl().getValue());
-        assertEquals("ISBN-0000-0000", work.getExternalIdentifiers().getExternalIdentifier().get(1).getValue());
-        assertEquals(Relationship.SELF, work.getExternalIdentifiers().getExternalIdentifier().get(1).getRelationship());
-    
-        assertEquals("isbn", work.getExternalIdentifiers().getExternalIdentifier().get(2).getType());
-        assertEquals("https://www.worldcat.org/isbn/ISBN-0000-0001", work.getExternalIdentifiers().getExternalIdentifier().get(2).getUrl().getValue());
-        assertEquals("ISBN-0000-0001", work.getExternalIdentifiers().getExternalIdentifier().get(2).getValue());
-        assertEquals(Relationship.SELF, work.getExternalIdentifiers().getExternalIdentifier().get(2).getRelationship());
-    
-        assertEquals("issn", work.getExternalIdentifiers().getExternalIdentifier().get(3).getType());
-        assertEquals("https://www.test.org/issn/ISSN-0000-0000", work.getExternalIdentifiers().getExternalIdentifier().get(3).getUrl().getValue());
-        assertEquals("ISSN-0000-0000", work.getExternalIdentifiers().getExternalIdentifier().get(3).getValue());
-        assertEquals(Relationship.PART_OF, work.getExternalIdentifiers().getExternalIdentifier().get(3).getRelationship());
-    
-        assertEquals("issn", work.getExternalIdentifiers().getExternalIdentifier().get(4).getType());
-        assertEquals("https://www.test.org/issn/ISSN-0000-0001", work.getExternalIdentifiers().getExternalIdentifier().get(4).getUrl().getValue());
-        assertEquals("ISSN-0000-0001", work.getExternalIdentifiers().getExternalIdentifier().get(4).getValue());
-        assertEquals(Relationship.PART_OF, work.getExternalIdentifiers().getExternalIdentifier().get(4).getRelationship());
+        verifyDoi(work, true);        
     }
     
     /** checks published date populated by 'issued' field */
     @Test
     public void resolveMetadataTestNoPublishedDate() {
         Work work = resolver.resolveMetadata("doi", "10.000/0000.0001");
+        assertNotNull(work.getPublicationDate());
+        assertEquals("2018", work.getPublicationDate().getYear().getValue());
+        assertEquals("01", work.getPublicationDate().getMonth().getValue());
+        assertEquals("01", work.getPublicationDate().getDay().getValue());
+        verifyDoi(work, false);        
+    }
+    
+    /** 
+     * checks journal title is populated by 'container-title' field 
+     * */
+    @Test
+    public void resolveMetadataJournalTitleInContainerTitleTest() {
+        Work work = resolver.resolveMetadata("doi", "10.000/0000.0002");
+        assertEquals("journal-title-in-container-title", work.getJournalTitle().getContent());
+        verifyDoi(work, true);
+    } 
+    
+    /** 
+     * checks journal title is populated by 'container-title-short' field 
+     * */
+    @Test
+    public void resolveMetadataJournalTitleInContainerTitleShortTest() {
+        Work work = resolver.resolveMetadata("doi", "10.000/0000.0003");
+        assertEquals("journal-title-in-container-title-short", work.getJournalTitle().getContent());
+        verifyDoi(work, true);
+    }
+    
+    private void verifyDoi(Work work, boolean checkPublicationDate) {
         assertNotNull(work);
         assertEquals(WorkType.JOURNAL_ARTICLE, work.getWorkType());
         assertEquals("DOI title", work.getWorkTitle().getTitle().getContent());
-        assertEquals("Subtitle", work.getWorkTitle().getSubtitle().getContent());
-        assertEquals("Journal title", work.getJournalTitle().getContent());
+        assertEquals("Subtitle", work.getWorkTitle().getSubtitle().getContent());        
         assertEquals("This is a description", work.getShortDescription());
-        assertEquals("http://dx.doi.org/10.000/0000.0000", work.getUrl().getValue());
+        if(checkPublicationDate) {
+            assertEquals("2018", work.getPublicationDate().getYear().getValue());
+            assertEquals("12", work.getPublicationDate().getMonth().getValue());
+            assertEquals("31", work.getPublicationDate().getDay().getValue());
+            assertEquals("http://dx.doi.org/10.000/0000.0000", work.getUrl().getValue());
+        }
         
         assertEquals(5, work.getExternalIdentifiers().getExternalIdentifier().size());
         assertEquals("doi", work.getExternalIdentifiers().getExternalIdentifier().get(0).getType());
@@ -204,10 +220,5 @@ public class DOIResolverTest {
         assertEquals("https://www.test.org/issn/ISSN-0000-0001", work.getExternalIdentifiers().getExternalIdentifier().get(4).getUrl().getValue());
         assertEquals("ISSN-0000-0001", work.getExternalIdentifiers().getExternalIdentifier().get(4).getValue());
         assertEquals(Relationship.PART_OF, work.getExternalIdentifiers().getExternalIdentifier().get(4).getRelationship());
-        
-        assertNotNull(work.getPublicationDate());
-        assertEquals("2018", work.getPublicationDate().getYear().getValue());
-        assertEquals("01", work.getPublicationDate().getMonth().getValue());
-        assertEquals("01", work.getPublicationDate().getDay().getValue());
     }
 }
