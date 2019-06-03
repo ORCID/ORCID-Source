e5a2f7529774bcf5013dc93ca721a9eeb4a1e066
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2019-06-03 15:42:40 -0600

    Create a custom request cache so we cache only certain urls

---
+++ orcid-web/src/main/java/org/orcid/frontend/spring/configuration/OrcidRequestCache.java
@@ -0,0 +1,21 @@
+package org.orcid.frontend.spring.configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.orcid.core.manager.impl.OrcidUrlManager;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.stereotype.Service;
+
+@Service("orcidRequestCache")
+public class OrcidRequestCache extends HttpSessionRequestCache {
+    @Override
+    public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+        String requestUrl = OrcidUrlManager.getPathWithoutContextPath(request);
+        
+        // Save the request just in case it matches the SAVED_REQUEST_PATTERN pattern
+        if(OrcidUrlManager.SAVED_REQUEST_PATTERN.matcher(requestUrl).find()) {
+            super.saveRequest(request, response);
+        }        
+    }
+}
