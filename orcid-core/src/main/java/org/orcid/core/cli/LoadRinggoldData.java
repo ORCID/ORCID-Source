5dca7fe78e2a167315db2d4a1129347272b6f53c
George Nash <bobcaprice@hotmail.com>
2019-07-30 12:23:48 +0200

    Import script no longer looks for null regions

--- orcid-core/src/main/java/org/orcid/core/cli/LoadRinggoldData.java
+++ orcid-core/src/main/java/org/orcid/core/cli/LoadRinggoldData.java
@@ -474,7 +474,7 @@ public class LoadRinggoldData {
             LOGGER.info("Processing organization {} for {}", name, disambiguatedEntity.getId());
             String city = altName.get("city").asText();
             Iso3166Country country = Iso3166Country.fromValue(altName.get("country").asText());
-            OrgEntity existingOrg = orgDao.findByNameCityRegionCountryAndType(name, city, null, country.name(), "RINGGOLD");
+            OrgEntity existingOrg = orgDao.findByNameCityRegionCountryAndType(name, city, "", country.name(), "RINGGOLD");
             if (existingOrg != null) {
                 if (existingOrg.getOrgDisambiguated() == null) {
                     existingOrg.setOrgDisambiguated(disambiguatedEntity);
