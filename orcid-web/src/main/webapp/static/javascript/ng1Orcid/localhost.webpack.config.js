b96e3fed17545d39f17b09b44fef546ff5fb2882
leomendoza123 <leomendoza123@gmail.com>
2019-10-14 11:46:39 -0600

    Support sass on every webpack built environment

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/localhost.webpack.config.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/localhost.webpack.config.js
@@ -10,11 +10,8 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
           'style-loader',
-          // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
           'sass-loader',
         ],
       },
