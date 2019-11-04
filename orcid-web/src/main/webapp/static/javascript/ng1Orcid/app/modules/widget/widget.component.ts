a7c1d346fa236d92cc7a952777a34446b79ad105
lizkrznarich <e.krznarich@orcid.org>
2019-11-04 14:47:30 -0500

    add rel=me to share my iD on other sites widget code

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/widget/widget.component.ts
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/app/modules/widget/widget.component.ts
@@ -21,7 +21,7 @@ export class WidgetComponent {
         this.hash = orcidVar.orcidIdHash.substr(0, 6);
         this.showCode = false;
         this.domain = getBaseUriHttps();    
-        this.widgetURLND = '<div itemscope itemtype="https://schema.org/Person"><a itemprop="sameAs" content="'+ getBaseUri() + '/' + orcidVar.orcidId + '" href="'+ getBaseUri() + '/' + orcidVar.orcidId + '" target="orcid.widget" rel="noopener noreferrer" style="vertical-align:top;"><img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon">' + this.domain + '/' + orcidVar.orcidId + '</a></div>';
+        this.widgetURLND = '<div itemscope itemtype="https://schema.org/Person"><a itemprop="sameAs" content="'+ getBaseUri() + '/' + orcidVar.orcidId + '" href="'+ getBaseUri() + '/' + orcidVar.orcidId + '" target="orcid.widget" rel="me noopener noreferrer" style="vertical-align:top;"><img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style="width:1em;margin-right:.5em;" alt="ORCID iD icon">' + this.domain + '/' + orcidVar.orcidId + '</a></div>';
      
     } 
 
