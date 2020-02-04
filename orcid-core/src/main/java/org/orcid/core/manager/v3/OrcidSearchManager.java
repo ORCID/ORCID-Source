fb3b1fd3ef92955671f9abdf0ef081de35c7685c
George Nash <bobcaprice@hotmail.com>
2020-02-04 14:35:19 -0400

    Changed max and default number of rows for search to 1000

--- orcid-core/src/main/java/org/orcid/core/manager/v3/OrcidSearchManager.java
+++ orcid-core/src/main/java/org/orcid/core/manager/v3/OrcidSearchManager.java
@@ -19,11 +19,11 @@ import org.orcid.jaxb.model.v3.release.search.expanded.ExpandedSearch;
  */
 public interface OrcidSearchManager {
 
-    static final int DEFAULT_SEARCH_ROWS = 100;
+    static final int DEFAULT_SEARCH_ROWS = 1000;
     
     static final int MAX_SEARCH_START = 10000;      
 
-    static final int MAX_SEARCH_ROWS = 200; 
+    static final int MAX_SEARCH_ROWS = 1000; 
     
     Search findOrcidIds(Map<String, List<String>> queryParameters);
     
