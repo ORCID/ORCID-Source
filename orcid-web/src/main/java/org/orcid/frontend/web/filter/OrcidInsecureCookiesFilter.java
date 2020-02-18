438168b87f5087797c313961585d4b5da343b473
Angel <a.montenegro@orcid.org>
2020-02-18 14:42:58 -0600

    orcid-web cant see orcid-core elements

--- orcid-web/src/main/java/org/orcid/frontend/web/filter/OrcidInsecureCookiesFilter.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/filter/OrcidInsecureCookiesFilter.java
@@ -9,11 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.orcid.core.manager.InternalSSOManager;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class OrcidInsecureCookiesFilter extends OncePerRequestFilter {
 
+    private static final String ORCID_TOKEN_COOKIE_NAME = "orcid_token";
+    private static final String JSESSIONID_COOKIE_NAME = "JSESSIONID";
     private static final String CSRF_COOKIE_NAME = "XSRF-TOKEN";
     private static final String LOCALE_COOKIE_NAME = "locale_v3";
 
@@ -24,7 +25,7 @@ public class OrcidInsecureCookiesFilter extends OncePerRequestFilter {
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 String cookieName = cookie.getName();
-                if (InternalSSOManager.COOKIE_NAME.equals(cookieName) || InternalSSOManager.JSESSIONID.equals(cookieName) || CSRF_COOKIE_NAME.equals(cookieName)
+                if (ORCID_TOKEN_COOKIE_NAME.equals(cookieName) || JSESSIONID_COOKIE_NAME.equals(cookieName) || CSRF_COOKIE_NAME.equals(cookieName)
                         || LOCALE_COOKIE_NAME.equals(cookieName)) {
                     if (!cookie.getSecure()) {
                         cookie.setMaxAge(0);
