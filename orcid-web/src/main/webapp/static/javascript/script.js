ead33262bc45b05b876c6a41cfd68687bc7c7b32
leomendoza123 <leomendoza123@gmail.com>
2020-03-06 14:58:43 -0600

    Add unsuported banner with regexp

--- orcid-web/src/main/webapp/static/javascript/script.js
+++ orcid-web/src/main/webapp/static/javascript/script.js
@@ -18,6 +18,7 @@
 /*
  * 1 - Utility functions 
  */
+
 function scriptTmpl(elemId) {
     var elt = document.getElementById( elemId );
     var viewdef = "";
@@ -442,25 +443,14 @@ $(function() {
     var uaParser = new UAParser()
     var uaResult = uaParser.getResult()
     
+
+    // The following regexp is auto generated on the orcid-angular
+    // And should be maintained by replacing it for the most recently updated version on 
+    // https://github.com/ORCID/orcid-angular/tree/master/src/app/cdk/platform-info/browserlist.regexp.ts
+    var BROWSERLIST_REGEXP = /((CPU[ +]OS|iPhone[ +]OS|CPU[ +]iPhone|CPU IPhone OS)[ +]+(12[_\.]2|12[_\.]([3-9]|\d{2,})|12[_\.]4|12[_\.]([5-9]|\d{2,})|(1[3-9]|[2-9]\d|\d{3,})[_\.]\d+|13[_\.]0|13[_\.]([1-9]|\d{2,})|(1[4-9]|[2-9]\d|\d{3,})[_\.]\d+)(?:[_\.]\d+)?)|(Edge\/(79|([8-9]\d|\d{3,}))(?:\.\d+)?)|(HeadlessChrome((?:\/79\.\d+\.\d+)?|(?:\/([8-9]\d|\d{3,})\.\d+\.\d+)?))|((Chromium|Chrome)\/(79|([8-9]\d|\d{3,}))\.\d+(?:\.\d+)?)|(Version\/(12\.0|12\.([1-9]|\d{2,})|(1[3-9]|[2-9]\d|\d{3,})\.\d+|13\.0|13\.([1-9]|\d{2,})|(1[4-9]|[2-9]\d|\d{3,})\.\d+)(?:\.\d+)?.*Safari\/)|(Trident\/7\.0)|(Firefox\/(68|(69|[7-9]\d|\d{3,})|72|(7[3-9]|[8-9]\d|\d{3,}))\.\d+\.\d+)|(Firefox\/(68|(69|[7-9]\d|\d{3,})|72|(7[3-9]|[8-9]\d|\d{3,}))\.\d+(pre|[ab]\d+[a-z]*)?)|(([MS]?IE) (11|(1[2-9]|[2-9]\d|\d{3,}))\.\d+)/
     var oldBrowserFlag = false;
-    // IE 10
-    if(uaResult.browser.name.toLowerCase()=="ie" && parseInt(uaResult.browser.version, 10) < 11){
-        oldBrowserFlag = true;
-    // edge
-    } else if(uaResult.browser.name.toLowerCase()=="edge" && parseInt(uaResult.browser.version, 10) < 14){
-        oldBrowserFlag = true;
-    // chrome 55
-    } else if((uaResult.browser.name.toLowerCase().indexOf("chrom") >= 0) && parseInt(uaResult.browser.version, 10) < 55){
-        oldBrowserFlag = true;
-    // firefox 50
-    } else if((uaResult.browser.name.toLowerCase()=="mozilla" || uaResult.browser.name.toLowerCase()=="firefox") && parseInt(uaResult.browser.version, 10) < 50){
-        oldBrowserFlag = true;
-    // opera 42
-    } else if(uaResult.browser.name.toLowerCase()=="opera" && parseInt(uaResult.browser.version, 10) < 42){
-        oldBrowserFlag = true;
-    // safari 10
-    } else if((uaResult.browser.name.toLowerCase().indexOf("safari") >= 0) && parseInt(uaResult.browser.version, 10) < 9){
-        oldBrowserFlag = true;
+    if (!BROWSERLIST_REGEXP.test(navigator.userAgent)) {
+        oldBrowserFlag = true
     }
 
     if (oldBrowserFlag && location == parent.location) {
