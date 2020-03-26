2d4835763c12913dc20bd1ba7af8df177f83fd05
Camelia-Orcid <c.dumitru@orcid.org>
2020-03-26 12:46:10 +0000

    Fix for: Affiliations allow dates with no month if the day is set to 01

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/BaseWorkspaceController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/BaseWorkspaceController.java
@@ -139,6 +139,7 @@ public class BaseWorkspaceController extends BaseController {
                         .parseStrict().toFormatter() };
         String dateString = date.getYear();
         
+        //If the month is empty and day provided is an invalid date
         if(StringUtils.isBlank(date.getMonth())) {
             if(!StringUtils.isBlank(date.getDay()))
             {
