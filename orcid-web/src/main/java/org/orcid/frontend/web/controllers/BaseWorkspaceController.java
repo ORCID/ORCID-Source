325d260e2b4888c5eda8718514870819205cffb2
Camelia-Orcid <c.dumitru@orcid.org>
2020-04-02 09:40:17 +0100

    Fix for the case when year not provided only month and day

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/BaseWorkspaceController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/BaseWorkspaceController.java
@@ -132,13 +132,20 @@ public class BaseWorkspaceController extends BaseController {
                 new DateTimeFormatterBuilder().appendPattern("yyyyMM").parseDefaulting(ChronoField.DAY_OF_MONTH, 1).toFormatter(),
                 new DateTimeFormatterBuilder().appendPattern("yyyyMMdd").parseStrict().toFormatter() };
         String dateString = date.getYear();
-
+        System.out.println("Get Date: " + date.toString());
         // If the month is empty and day provided is an invalid date
         if (StringUtils.isBlank(date.getMonth())) {
             if (!StringUtils.isBlank(date.getDay())) {
                 return false;
             }
-        } else {
+        }
+        else if (StringUtils.isBlank(date.getYear())) {
+            System.out.println("Get Year: " + date.getYear());
+            if (!StringUtils.isBlank(date.getDay()) && !StringUtils.isBlank(date.getMonth())) {
+                return false;
+            }
+        }
+        else {
             dateString += date.getMonth();
             if (!StringUtils.isBlank(date.getDay())) {
                 dateString += date.getDay();
