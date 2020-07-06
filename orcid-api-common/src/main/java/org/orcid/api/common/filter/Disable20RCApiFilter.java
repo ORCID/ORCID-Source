33072c4c0707202fe25cbbf2718b39bebd04354f
Angel <a.montenegro@orcid.org>
2020-07-06 11:30:27 -0600

    Formatting the code

--- orcid-api-common/src/main/java/org/orcid/api/common/filter/Disable20RCApiFilter.java
+++ orcid-api-common/src/main/java/org/orcid/api/common/filter/Disable20RCApiFilter.java
@@ -13,6 +13,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
 import org.orcid.core.locale.LocaleManager;
+import org.orcid.core.manager.impl.OrcidUrlManager;
 import org.orcid.core.togglz.Features;
 import org.orcid.core.utils.JsonUtils;
 import org.orcid.core.web.filters.ApiVersionFilter;
@@ -24,24 +25,24 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 public class Disable20RCApiFilter extends OncePerRequestFilter {
     private static final Logger LOGGER = LoggerFactory.getLogger(Disable20RCApiFilter.class);
-    
+
     private static final String API_20_RC = "2.0_rc";
-    
+
     private String JSON_RESPONSE;
-    
+
     private String XML_RESPONSE;
-    
+
     @Resource
     private LocaleManager localeManager;
-    
+
     private String getXmlResponse() throws JAXBException {
-        if(XML_RESPONSE == null) {
+        if (XML_RESPONSE == null) {
             OrcidError error = new OrcidError();
             error.setDeveloperMessage(localeManager.resolveMessage("apiError.9056.developerMessage"));
             error.setUserMessage(localeManager.resolveMessage("apiError.9056.userMessage"));
             error.setResponseCode(HttpServletResponse.SC_MOVED_PERMANENTLY);
             error.setErrorCode(9056);
-            
+
             JAXBContext context = JAXBContext.newInstance(error.getClass());
             StringWriter writer = new StringWriter();
             Marshaller marshaller = context.createMarshaller();
@@ -51,40 +52,51 @@ public class Disable20RCApiFilter extends OncePerRequestFilter {
         }
         return XML_RESPONSE;
     }
-    
+
     private String getJsonResponse() {
-        if(JSON_RESPONSE == null) {
+        if (JSON_RESPONSE == null) {
             OrcidError error = new OrcidError();
             error.setDeveloperMessage(localeManager.resolveMessage("apiError.9056.developerMessage"));
             error.setUserMessage(localeManager.resolveMessage("apiError.9056.userMessage"));
             error.setResponseCode(HttpServletResponse.SC_MOVED_PERMANENTLY);
             error.setErrorCode(9056);
-            JSON_RESPONSE = JsonUtils.convertToJsonString(error);            
+            JSON_RESPONSE = JsonUtils.convertToJsonString(error);
         }
         return JSON_RESPONSE;
     }
-    
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String version = (String) request.getAttribute(ApiVersionFilter.API_VERSION_REQUEST_ATTRIBUTE_NAME);
         if (PojoUtil.isEmpty(version)) {
             filterChain.doFilter(request, response);
-        } else if (version.startsWith(API_20_RC) && Features.V2_DISABLE_RELEASE_CANDIDATES.isActive()) {            
-                response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-                String accept = request.getHeader("Accept") == null ? null : request.getHeader("Accept").toLowerCase();
-                if (accept.contains("json")) {
-                    response.getWriter().println(getJsonResponse());
-                } else {
-                    try {
-                        response.getWriter().println(getXmlResponse());
-                    } catch(Exception e) {
-                        LOGGER.error("Unable to generate XML message", e);
-                        response.getWriter().println(localeManager.resolveMessage("apiError.9056.developerMessage"));
-                    }
-                }
+        } else if (version.startsWith(API_20_RC) && Features.V2_DISABLE_RELEASE_CANDIDATES.isActive()) {
+            String v20Location;
+            String requestUri = request.getRequestURI();
             
+            if(requestUri.startsWith("https://localhost")) {
+                v20Location = requestUri.replaceFirst("_(rc1|rc2|rc3|rc4)", "");                  
+            } else {
+                String path = OrcidUrlManager.getPathWithoutContextPath(request);
+                v20Location = path.replaceFirst("_(rc1|rc2|rc3|rc4)", "");                
+            }
+            LOGGER.info("Redirecting request '{}' to '{}'", requestUri, v20Location);
+            response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+            response.setHeader("Location", v20Location);
+            String accept = request.getHeader("Accept") == null ? null : request.getHeader("Accept").toLowerCase();
+            if (accept.contains("json")) {
+                response.getWriter().println(getJsonResponse());
+            } else {
+                try {
+                    response.getWriter().println(getXmlResponse());
+                } catch (Exception e) {
+                    LOGGER.error("Unable to generate XML message", e);
+                    response.getWriter().println(localeManager.resolveMessage("apiError.9056.developerMessage"));
+                }
+            }
+
         } else {
             filterChain.doFilter(request, response);
         }
-    }    
+    }
 }
