3a815a999e37c1adfc0057270caa6f920f08755b
Daniel Palafox <dp852@hotmail.com>
2020-09-30 16:46:52 -0500

    add missing parameters

--- orcid-web/src/main/java/org/orcid/frontend/oauth2/OauthController.java
+++ orcid-web/src/main/java/org/orcid/frontend/oauth2/OauthController.java
@@ -121,6 +121,18 @@ public class OauthController {
 
         if(request.getSession() != null && request.getSession().getAttribute(OauthHelper.REQUEST_INFO_FORM) != null) {
             requestInfoForm = oauthHelper.setUserRequestInfoForm((RequestInfoForm) request.getSession().getAttribute(OauthHelper.REQUEST_INFO_FORM));
+            if (requestInfoForm.getUserOrcid() != null) {
+                if (requestParameters.isEmpty()) {
+                    String url =  URLDecoder.decode((String) request.getSession().getAttribute(OrcidOauth2Constants.OAUTH_QUERY_STRING), "UTF-8").trim();
+                    String[] pairs = url.split("&");
+                    for (int i = 0; i < pairs.length; i++) {
+                        String pair = pairs[i];
+                        String[] keyValue = pair.split("=");
+                        requestParameters.put(keyValue[0], keyValue[1]);
+                    }
+                }
+                setAuthorizationRequest(request, model, requestParameters, sessionStatus, principal, requestInfoForm);
+            }
         }
         request.getSession().setAttribute(OauthHelper.REQUEST_INFO_FORM, requestInfoForm);
         return requestInfoForm;
