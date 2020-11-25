6ed19cb130027af01a3f8e34099b7b311ea932f2
Angel <a.montenegro@orcid.org>
2020-11-25 14:01:51 -0600

    Remove unused imports

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/AdminController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/AdminController.java
@@ -16,8 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.orcid.core.exception.ClientAlreadyActiveException;
-import org.orcid.core.exception.ClientAlreadyDeactivatedException;
 import org.orcid.core.manager.AdminManager;
 import org.orcid.core.manager.ProfileEntityCacheManager;
 import org.orcid.core.manager.v3.ClientDetailsManager;
@@ -34,7 +32,6 @@ import org.orcid.password.constants.OrcidPasswordConstants;
 import org.orcid.persistence.jpa.entities.ProfileEntity;
 import org.orcid.pojo.AdminChangePassword;
 import org.orcid.pojo.AdminDelegatesRequest;
-import org.orcid.pojo.ClientActivationRequest;
 import org.orcid.pojo.LockAccounts;
 import org.orcid.pojo.ProfileDeprecationRequest;
 import org.orcid.pojo.ProfileDetails;
