a9dd39919a989a337a26fb22ee871ffea7f7b17c
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2020-11-25 18:56:51 -0600

    Update the total count endpoint

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/NotificationController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/NotificationController.java
@@ -144,9 +144,11 @@ public class NotificationController extends BaseController {
     }
 
     @RequestMapping("/totalCount.json")
-    public @ResponseBody int getTotalCountJson(@RequestParam(value = "archived", defaultValue = "false") boolean archived) {
+    public @ResponseBody ResponseEntity<?> getTotalCountJson() {
         String currentOrcid = getCurrentUserOrcid();
-        return notificationManager.getTotalCount(currentOrcid, archived);
+        int allNotifications = notificationManager.getTotalCount(currentOrcid, true);
+        int notArchivedCount = notificationManager.getTotalCount(currentOrcid, false);
+        return ResponseEntity.ok("{\"all\":" + allNotifications + ",\"nonArchived\":" + notArchivedCount + "}");
     }
 
     @RequestMapping(value = "{id}/read.json")
