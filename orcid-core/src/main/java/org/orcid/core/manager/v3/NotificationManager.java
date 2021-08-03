de42ac6d11803882196692ba425c389c84ae142d
andrej romanov <50377758+auumgn@users.noreply.github.com>
2021-08-03 16:00:14 +0300

    remove unused emails
    api_record_creation_email.ftl
    api_record_creation_email_html.ftl
    claim_reminder_email.ftl
    claim_reminder_email_html.ftl
    new_claim_reminder_email.ftl
    new_claim_reminder_email_html.ftl

--- orcid-core/src/main/java/org/orcid/core/manager/v3/NotificationManager.java
+++ orcid-core/src/main/java/org/orcid/core/manager/v3/NotificationManager.java
@@ -44,13 +44,9 @@ public interface NotificationManager {
 
     void sendOrcidDeactivateEmail(String userOrcid);
 
-    void sendOrcidLockedEmail(String orcidToLock);
-
-    void sendApiRecordCreationEmail(String toEmail, String orcid);        
+    void sendOrcidLockedEmail(String orcidToLock);  
 
     void sendEmailAddressChangedNotification(String currentUserOrcid, String newEmail, String oldEmail);
-
-    void sendClaimReminderEmail(String userOrcid, int daysUntilActivation, String email);
 
     void sendDelegationRequestEmail(String managedOrcid, String trustedOrcid, String link);
 
