0fc8ec066e7a688d9fcc44907ee76d9bc15bebd0
andrej romanov <50377758+auumgn@users.noreply.github.com>
2021-08-17 20:21:09 +0300

    adjust test

--- orcid-persistence/src/test/java/org/orcid/persistence/dao/ClientSecretDaoTest.java
+++ orcid-persistence/src/test/java/org/orcid/persistence/dao/ClientSecretDaoTest.java
@@ -15,26 +15,22 @@ import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.orcid.persistence.jpa.entities.ClientDetailsEntity;
 import org.orcid.persistence.jpa.entities.ClientSecretEntity;
 import org.orcid.test.DBUnitTest;
 import org.orcid.test.OrcidJUnit4ClassRunner;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(OrcidJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:orcid-persistence-context.xml" })
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ClientSecretDaoTest extends DBUnitTest {
     private static String CLIENT_ID = "APP-5555555555555555";
+    private static String CLIENT_ID_TWO = "APP-5555555555555556";
     private static String CLIENT_SECRET = "DhkFj5EI0qp6GsUKi55Vja+h+bsaKpBt";
-    private static String CONDITION = String.format("(client_details_id = '%1$s' and client_secret = '%2$s')", CLIENT_ID, CLIENT_SECRET);
+    private static String CONDITION = String.format("(client_details_id = '%1$s' and client_secret = '%2$s') or (client_details_id = '%3$s' and client_secret = '%2$s')", CLIENT_ID, CLIENT_SECRET, CLIENT_ID_TWO);
 
     @Resource
     ClientSecretDao clientSecretDao;
-
-    @Resource
-    ClientDetailsDao clientDetailsDao;
 
     @BeforeClass
     public static void initDBUnitData() throws Exception {
@@ -47,17 +43,15 @@ public class ClientSecretDaoTest extends DBUnitTest {
     }
 
     @Test
-    @Transactional
     public void testGetNonPrimaryKeys() {
         List<ClientSecretEntity> nonPrimaryKeys = clientSecretDao.getNonPrimaryKeys();
         assertEquals(nonPrimaryKeys.size(), 2);
-        ClientSecretEntity secret = nonPrimaryKeys.get(0);
-        ClientDetailsEntity client = secret.getClientDetailsEntity();
-        assertNotNull(secret.getDateCreated());
-        assertNotNull(secret.getLastModified());
-        assertEquals(secret.getDateCreated(), secret.getLastModified());
-        assertEquals(secret.getClientSecret(), CLIENT_SECRET);
-        assertEquals(client.getClientId(), CLIENT_ID);
+        for (ClientSecretEntity secret : nonPrimaryKeys) {           
+            assertNotNull(secret.getDateCreated());
+            assertNotNull(secret.getLastModified());
+            assertEquals(secret.getDateCreated(), secret.getLastModified());
+            assertEquals(secret.getClientSecret(), CLIENT_SECRET);
+        }
     }
 
     @Test
