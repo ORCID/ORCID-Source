24f1f7355c272ce7d14a71ecb65de0f4cfb41eb4
Angel <a.montenegro@orcid.org>
2021-12-23 11:34:46 -0600

    Wrong if condition, changed to an OR

--- orcid-core/src/main/java/org/orcid/core/oauth/service/OrcidRandomValueTokenServicesImpl.java
+++ orcid-core/src/main/java/org/orcid/core/oauth/service/OrcidRandomValueTokenServicesImpl.java
@@ -266,7 +266,7 @@ public class OrcidRandomValueTokenServicesImpl extends DefaultTokenServices impl
         if (Features.ALLOW_DELETE_WITH_REVOKED_TOKENS.isActive() && RequestMethod.DELETE.name().equals(attr.getRequest().getMethod())) {
             OrcidOauth2TokenDetail orcidAccessToken = orcidTokenStore.readOrcidOauth2TokenDetail(accessTokenValue);
             String revokeReason = orcidAccessToken.getRevokeReason();
-            if (!PojoUtil.isEmpty(revokeReason) && RevokeReason.USER_REVOKED.equals(RevokeReason.valueOf(revokeReason))) {
+            if (PojoUtil.isEmpty(revokeReason) || RevokeReason.USER_REVOKED.equals(RevokeReason.valueOf(revokeReason))) {
                 OAuth2AccessToken accessToken = orcidTokenStore.readEvenDisabledAccessToken(accessTokenValue);
                 validateTokenExpirationAndClientStatus(accessToken, accessTokenValue);
                 return orcidTokenStore.readAuthenticationEvenOnDisabledTokens(accessTokenValue);
