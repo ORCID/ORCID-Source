1d3215d3f17364c3ab7cc7d1cca1e45709fd1e1d
andrej romanov <50377758+auumgn@users.noreply.github.com>
2022-02-14 13:03:42 +0200

    update max char limit to 256

--- orcid-web/src/main/java/org/orcid/frontend/web/util/PasswordConstants.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/util/PasswordConstants.java
@@ -2,8 +2,8 @@ package org.orcid.frontend.web.util;
 
 public class PasswordConstants {
 
-    public static final String ORCID_PASSWORD_REGEX = "^(?=.*?\\d)(?=.*?\\D)(?=.*?\\w).{8,64}$";
+    public static final String ORCID_PASSWORD_REGEX = "^(?=.*?\\d)(?=.*?\\D)(?=.*?\\w).{8,256}$";
 
-    public static final String ORCID_PASSWORD_CHARACTER_LIMIT = "^.{8,64}$";
+    public static final String ORCID_PASSWORD_CHARACTER_LIMIT = "^.{8,256}$";
     
 }
