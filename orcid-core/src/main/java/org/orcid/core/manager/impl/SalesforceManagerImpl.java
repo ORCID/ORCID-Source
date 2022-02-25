06b0cde17838dfb070a2c5259f955614f367b741
amontenegro <a.montenegro@orcid.org>
2022-02-24 21:04:39 -0600

    More progress

--- orcid-core/src/main/java/org/orcid/core/manager/impl/SalesforceManagerImpl.java
+++ orcid-core/src/main/java/org/orcid/core/manager/impl/SalesforceManagerImpl.java
@@ -25,18 +25,18 @@ import org.springframework.stereotype.Component;
 public class SalesforceManagerImpl implements SalesforceManager {
 
 	@Resource
-    private SalesForceAdapter salesForceAdapter;
-	
+	private SalesForceAdapter salesForceAdapter;
+
 	@Resource
 	private SalesforceMicroserviceClient client;
-	
+
 	public List<Member> retrieveMembers() {
 		List<Member> members = new ArrayList<Member>();
-		
+
 		try {
 			String membersListString = client.retrieveMembers();
-			JSONObject membersListJsonObject = new JSONObject(membersListString);			            
-			members = salesForceAdapter.createMembersListFromJson(membersListJsonObject);            			
+			JSONObject membersListJsonObject = new JSONObject(membersListString);
+			members = salesForceAdapter.createMembersListFromJson(membersListJsonObject);
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,7 +44,7 @@ public class SalesforceManagerImpl implements SalesforceManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return members;
 	}
 
@@ -54,57 +54,68 @@ public class SalesforceManagerImpl implements SalesforceManager {
 		try {
 			String memberDetailsString = client.retrieveMemberDetails(SlugUtils.extractIdFromSlug(memberSlug));
 			JSONObject memberDetailsJsonObject = new JSONObject(memberDetailsString);
-			
+
 			// Set parent org name
-			String parentOrgName = memberDetailsJsonObject.has("parentOrgName") ? memberDetailsJsonObject.getString("parentOrgName") : null;
+			String parentOrgName = memberDetailsJsonObject.has("parentOrgName")
+					? memberDetailsJsonObject.getString("parentOrgName")
+					: null;
 			details.setParentOrgName(parentOrgName);
-			
-			if(memberDetailsJsonObject.has("member")) {
+
+			if (memberDetailsJsonObject.has("member")) {
 				JSONObject memberJsonObject = memberDetailsJsonObject.getJSONObject("member");
 				// Set parent slug
-				String consortiumLeadId = memberJsonObject.has("Consortium_Lead__c") ? memberJsonObject.getString("Consortium_Lead__c") : null; 
+				String consortiumLeadId = memberJsonObject.has("Consortium_Lead__c")
+						? memberJsonObject.getString("Consortium_Lead__c")
+						: null;
 				details.setParentOrgSlug(SlugUtils.createSlug(consortiumLeadId, parentOrgName));
-				// Set member details 
-				Member member= salesForceAdapter.createMemberFromSalesForceRecord(memberJsonObject);
+				// Set member details
+				Member member = salesForceAdapter.createMemberFromSalesForceRecord(memberJsonObject);
 				details.setMember(member);
 			}
-			
-			if(memberDetailsJsonObject.has("integrations")) {
+
+			if (memberDetailsJsonObject.has("integrations")) {
 				JSONArray integrations = memberDetailsJsonObject.getJSONArray("integrations");
-				if(integrations != null && integrations.length() > 0) {
+				if (integrations != null && integrations.length() > 0) {
 					List<Integration> integrationList = salesForceAdapter.createIntegrationsList(integrations);
 					details.setIntegrations(integrationList);
+				} else {
+					details.setIntegrations(List.of());
 				}
 			} else {
 				details.setIntegrations(List.of());
 			}
-			
-			if(memberDetailsJsonObject.has("consortiumOpportunities")) {
+
+			if (memberDetailsJsonObject.has("consortiumOpportunities")) {
 				JSONArray consortiumOpportunities = memberDetailsJsonObject.getJSONArray("consortiumOpportunities");
-				if(consortiumOpportunities != null && consortiumOpportunities.length() > 0) {
-					List<Opportunity> opportunityList = salesForceAdapter.createOpportunitiesList(consortiumOpportunities);
+				if (consortiumOpportunities != null && consortiumOpportunities.length() > 0) {
+					List<Opportunity> opportunityList = salesForceAdapter
+							.createOpportunitiesList(consortiumOpportunities);
 					// Create the sub members list
 					List<SubMember> subMembers = opportunityList.stream().map(o -> {
-		                SubMember subMember = new SubMember();
-		                subMember.setOpportunity(o);
-		                subMember.setSlug(SlugUtils.createSlug(o.getTargetAccountId(), o.getAccountName()));
-		                return subMember;
-		            }).collect(Collectors.toList());
+						SubMember subMember = new SubMember();
+						subMember.setOpportunity(o);
+						subMember.setSlug(SlugUtils.createSlug(o.getTargetAccountId(), o.getAccountName()));
+						return subMember;
+					}).collect(Collectors.toList());
 					details.setSubMembers(subMembers);
+				} else {
+					details.setSubMembers(List.of());
 				}
 			} else {
 				details.setSubMembers(List.of());
 			}
-			
-			if(memberDetailsJsonObject.has("contacts")) {
+
+			if (memberDetailsJsonObject.has("contacts")) {
 				JSONArray contacts = memberDetailsJsonObject.getJSONArray("contacts");
-				if(contacts != null && contacts.length() > 0) {
-					//TODO
+				if (contacts != null && contacts.length() > 0) {
+					// TODO
+				} else {
+					details.setContacts(List.of());
 				}
 			} else {
 				details.setContacts(List.of());
 			}
-			
+
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -114,5 +125,5 @@ public class SalesforceManagerImpl implements SalesforceManager {
 		}
 		return details;
 	}
-	
+
 }
