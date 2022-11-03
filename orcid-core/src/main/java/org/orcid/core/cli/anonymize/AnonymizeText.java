e57ea424140e8b0670cbc7bc118aad54a5600bf6
c.dumitru@orcid.org <c.dumitru@orcid.org>
2022-11-03 23:11:21 +0000

    Fixed compilation error due to packaging changes related  to jersey

--- orcid-core/src/main/java/org/orcid/core/cli/anonymize/AnonymizeText.java
+++ orcid-core/src/main/java/org/orcid/core/cli/anonymize/AnonymizeText.java
@@ -18,7 +18,7 @@ import org.orcid.jaxb.model.v3.release.record.ExternalIDs;
 import org.orcid.jaxb.model.v3.release.record.WorkContributors;
 import org.orcid.pojo.ajaxForm.ActivityExternalIdentifier;
 import org.orcid.pojo.ajaxForm.PojoUtil;
-import org.orcid.utils.OrcidStringUtils;
+import org.orcid.core.utils.OrcidStringUtils;
 
 public class AnonymizeText {
 
