112716919b013d118a6b90fd9376936040f522e9
amontenegro <a.montenegro@orcid.org>
2023-02-02 14:34:12 -0600

    Rollback SSL context

--- orcid-web/src/main/java/org/orcid/frontend/salesforce/client/SalesforceMicroserviceClientImpl.java
+++ orcid-web/src/main/java/org/orcid/frontend/salesforce/client/SalesforceMicroserviceClientImpl.java
@@ -5,14 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.time.Duration;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -30,31 +23,13 @@ public class SalesforceMicroserviceClientImpl implements SalesforceMicroserviceC
     private final String consortiaListEndpoint;
 
     public SalesforceMicroserviceClientImpl(@Value("${org.orcid.microservice.gateway.url}") String gatewayUrl,
-            @Value("${org.orcid.salesforce.api.timeout:5000}") Integer timeout) throws NoSuchAlgorithmException, KeyManagementException {
+            @Value("${org.orcid.salesforce.api.timeout:5000}") Integer timeout) {
         membersListEndpoint = gatewayUrl + "/members/list";
         memberDetailsEndpoint = gatewayUrl + "/member/%s/details";
         consortiaListEndpoint = gatewayUrl + "/consortia/list";
-        
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustAllCerts, new SecureRandom());
-        
-        httpClient = HttpClient.newBuilder().sslContext(sslContext).version(HttpClient.Version.HTTP_2).connectTimeout(Duration.ofSeconds(timeout)).build();
+        httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).connectTimeout(Duration.ofSeconds(timeout)).build();
     }
-    
-    private static TrustManager[] trustAllCerts = new TrustManager[]{
-            new X509TrustManager() {
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-                public void checkClientTrusted(
-                    java.security.cert.X509Certificate[] certs, String authType) {
-                }
-                public void checkServerTrusted(
-                    java.security.cert.X509Certificate[] certs, String authType) {
-                }
-            }
-        };
-    
+
     @Override
     public String retrieveMembers() throws IOException, InterruptedException {
         // TODO: Handle exceptions
@@ -73,9 +48,9 @@ public class SalesforceMicroserviceClientImpl implements SalesforceMicroserviceC
         return doGetRequest(consortiaListEndpoint);
     }
     
-    private String doGetRequest(String url) throws IOException, InterruptedException {        
+    private String doGetRequest(String url) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).setHeader("Authorization", "Bearer " + salesforceReadToken).build();
-        
+
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         // TODO: Handle exceptions
         return response.body();
