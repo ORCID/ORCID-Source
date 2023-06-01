f5cf80934b5c662e4b120fc8c45352365c645417
amontenegro <a.montenegro@orcid.org>
2023-06-01 09:41:24 -0600

    Redirect to the root endpoint with path /oauth/token

--- orcid-pub-web/src/main/java/org/orcid/api/filters/PutAuthTokenActionFilter.java
+++ orcid-pub-web/src/main/java/org/orcid/api/filters/PutAuthTokenActionFilter.java
@@ -6,21 +6,15 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 
 import org.orcid.core.togglz.Features;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import liquibase.repackaged.org.apache.commons.lang3.StringUtils;
 
 @Provider
 @Component
@@ -43,7 +37,7 @@ public class PutAuthTokenActionFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (Features.REDIRECT_PUT_TOKEN_ENDPOINT.isActive() && request.getRequestURI().contains(OAUTH_TOKEN_PATH)) {
             response.setStatus(httpRedirectCode);
-            response.setHeader("Location", rootLocation);
+            response.setHeader("Location", rootLocation + OAUTH_TOKEN_PATH);
             LOGGER.debug("Redirecting PUT token request to root");
         }
         else {
