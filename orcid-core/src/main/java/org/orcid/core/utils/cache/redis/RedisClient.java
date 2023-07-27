3b409817d1fa8e9a4de49c596b8f5fa29d1c1cc4
amontenegro <a.montenegro@orcid.org>
2023-07-27 16:31:49 -0600

    Add TLS

--- orcid-core/src/main/java/org/orcid/core/utils/cache/redis/RedisClient.java
+++ orcid-core/src/main/java/org/orcid/core/utils/cache/redis/RedisClient.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.DefaultRedisCredentials;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.RedisCredentials;
+import redis.clients.jedis.RedisCredentialsProvider;
 import redis.clients.jedis.params.SetParams;
 
 public class RedisClient {
@@ -15,43 +18,26 @@ public class RedisClient {
     private static final Logger LOG = LoggerFactory.getLogger(RedisClient.class);
     
     private static final int DEFAULT_CACHE_EXPIRY = 60;
-    private static final int DEFAULT_TIMEOUT = 3000;
+    private static final int DEFAULT_TIMEOUT = 5000;
     
-    private String redisHost;
-    private int redisPort;
-    private int cacheExpiryInSecs;
-    private int timeoutInMillis;
     private JedisPool pool;
     private SetParams setParams;
-
-    public RedisClient(String redisHost, int redisPort) {
-        this.redisHost = redisHost;
-        this.redisPort = redisPort;
-        // Default values
-        this.cacheExpiryInSecs = DEFAULT_CACHE_EXPIRY;
-        this.timeoutInMillis = DEFAULT_TIMEOUT;
-        init();
+    
+    public RedisClient(String redisHost, int redisPort, String password) {
+        init(redisHost, redisPort, password, DEFAULT_CACHE_EXPIRY, DEFAULT_TIMEOUT);
     }
 
-    public RedisClient(String redisHost, int redisPort, int cacheExpiryInSecs) {
-        this.redisHost = redisHost;
-        this.redisPort = redisPort;
-        this.cacheExpiryInSecs = cacheExpiryInSecs;
-        this.timeoutInMillis = DEFAULT_TIMEOUT;
-        init();
+    public RedisClient(String redisHost, int redisPort, String password, int cacheExpiryInSecs) {
+        init(redisHost, redisPort, password, cacheExpiryInSecs, DEFAULT_TIMEOUT);
     }
 
-    public RedisClient(String redisHost, int redisPort, int cacheExpiryInSecs, int clientTimeoutInMillis) {
-        this.redisHost = redisHost;
-        this.redisPort = redisPort;
-        this.cacheExpiryInSecs = cacheExpiryInSecs;
-        this.timeoutInMillis = clientTimeoutInMillis;
-        init();
+    public RedisClient(String redisHost, int redisPort, String password, int cacheExpiryInSecs, int clientTimeoutInMillis) {
+        init(redisHost, redisPort, password, cacheExpiryInSecs, clientTimeoutInMillis);
     }
 
-    private void init() {
+    private void init(String redisHost, int redisPort, String password, int cacheExpiryInSecs, int timeoutInMillis) {
         JedisClientConfig config = DefaultJedisClientConfig.builder().connectionTimeoutMillis(timeoutInMillis).timeoutMillis(timeoutInMillis)
-                .socketTimeoutMillis(timeoutInMillis).build();
+                .socketTimeoutMillis(timeoutInMillis).password(password).ssl(true).build();        
         pool = new JedisPool(new HostAndPort(redisHost, redisPort), config);
         setParams = new SetParams().ex(cacheExpiryInSecs);
     }
@@ -70,4 +56,15 @@ public class RedisClient {
             return jedis.get(key);
         }
     }    
+    
+    //Trying to test TLS
+    public static void main(String [] args) {
+        RedisClient c = new RedisClient("xxx", 6379, "xxxxx");
+        c.set("name", "angelito");
+        c.set("name2", "angelito2");
+        c.set("name3", "angelito3");
+        System.out.println(c.get("name"));
+        System.out.println(c.get("name2"));
+        System.out.println(c.get("name3"));
+    }
 }
