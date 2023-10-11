85d55766ae8b93a1116399453fd8a958db7ab7f2
amontenegro <a.montenegro@orcid.org>
2023-10-11 15:50:08 -0600

    Domain endpoing

---
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/EmailDomainController.java
@@ -0,0 +1,37 @@
+package org.orcid.frontend.web.controllers;
+
+import javax.annotation.Resource;
+import javax.ws.rs.core.MediaType;
+
+import org.orcid.core.common.manager.EmailDomainManager;
+import org.orcid.persistence.jpa.entities.EmailDomainEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.net.InternetDomainName;
+
+@Controller("emailDomainController")
+@RequestMapping(value = { "/email-domain" })
+public class EmailDomainController {
+    
+    @Resource
+    private EmailDomainManager emailDomainManager;
+    
+    @RequestMapping(value = "/find-category", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+    public @ResponseBody String findCategory(@RequestParam("domain") String domain) {
+        
+        if(domain == null || domain.length() > 64 || !InternetDomainName.isValid(domain)) {
+            return "{'error':'domain lenght too long or invalid'}";
+        }        
+                
+        EmailDomainEntity ede = emailDomainManager.findByEmailDoman(domain);
+        if(ede == null) {
+            return "{'category':'" + EmailDomainEntity.DomainCategory.UNDEFINED.name() + "'}";
+        } else {
+            return "{'category':'" + ede.getCategory().name() + "'}";
+        }
+    }
+}
