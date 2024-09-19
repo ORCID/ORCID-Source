afd82390e928050402e337ed04c28a873a0bc1b1
amontenegro <a.montenegro@orcid.org>
2024-09-19 09:39:01 -0600

    This should be rolled back

---
+++ orcid-web/src/main/java/org/orcid/frontend/spring/configuration/OrcidBeanClassLoaderAware.java
@@ -0,0 +1,35 @@
+package org.orcid.frontend.spring.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+
+@Configuration
+public class OrcidBeanClassLoaderAware implements BeanClassLoaderAware {
+    private ClassLoader loader;
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer(objectMapper());
+    }
+
+    /**
+     * Customized {@link ObjectMapper} to add mix-in for class that doesn't have default
+     * constructors
+     * @return the {@link ObjectMapper} to use
+     */
+    private ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
+        return mapper;
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.loader = classLoader;
+    }
+}
