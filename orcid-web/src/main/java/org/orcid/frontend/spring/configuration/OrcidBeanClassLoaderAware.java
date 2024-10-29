3d522133036c92167332b1be49570024d997e276
amontenegro <a.montenegro@orcid.org>
2024-10-29 15:20:23 -0600

    Add mixin for string array

--- orcid-web/src/main/java/org/orcid/frontend/spring/configuration/OrcidBeanClassLoaderAware.java
+++ orcid-web/src/main/java/org/orcid/frontend/spring/configuration/OrcidBeanClassLoaderAware.java
@@ -1,11 +1,14 @@
 package org.orcid.frontend.spring.configuration;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.CoreJackson2Module;
 import org.springframework.security.jackson2.SecurityJackson2Modules;
 
 @Configuration
@@ -25,11 +28,22 @@ public class OrcidBeanClassLoaderAware implements BeanClassLoaderAware {
     private ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
+        mapper.registerModules(new CoreJackson2Module());
+        mapper.addMixIn(String[].class, StringArrayMixin.class);
         return mapper;
     }
 
     @Override
     public void setBeanClassLoader(ClassLoader classLoader) {
         this.loader = classLoader;
+    }
+
+    @JsonTypeInfo(
+            use = JsonTypeInfo.Id.CLASS
+    )
+    abstract class StringArrayMixin {
+        @JsonCreator
+        StringArrayMixin(String[] array) {
+        }
     }
 }
