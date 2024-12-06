34085f5efb8e9b1d8cd60f28e7e94070ac4aa3c5
Giles Westwood <giles.westwood@gmail.com>
2024-12-06 17:59:37 +0000

    adding in haprouter as default postgres connection

--- docker-entrypoint-initdb.d/1-dev-users.sql
+++ docker-entrypoint-initdb.d/1-dev-users.sql
@@ -2,4 +2,5 @@ CREATE USER orcid WITH PASSWORD 'orcid';
 CREATE USER statistics WITH PASSWORD 'statistics';
 CREATE USER orcidro WITH PASSWORD 'orcidro';
 CREATE USER dw_user WITH PASSWORD 'dw_user';
+CREATE USER pgc WITH PASSWORD 'pgc';
 
