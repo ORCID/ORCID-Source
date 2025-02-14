8917d04f9aba3490e76fa710887c78176590aeaf
Giles Westwood <giles.westwood@gmail.com>
2025-02-14 11:47:37 +0000

    feat: unique log location for each tomcat

--- docker-up.sh
+++ docker-up.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
+mkdir -p /opt/docker/logs/reg-ui
 
 docker compose --profile db up -d --remove-orphans
 
-sleep 20
+echo "sleeping 5s"
+sleep 5
 
 docker compose --profile dev up -d --remove-orphans
