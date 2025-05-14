46aadc24bb34edfe4e104367828cdabdac20e0f8
amontenegro <a.montenegro@orcid.org>
2025-05-14 14:57:36 -0600

    More logs

--- orcid-core/src/main/java/org/orcid/core/common/util/AuthenticationUtils.java
+++ orcid-core/src/main/java/org/orcid/core/common/util/AuthenticationUtils.java
@@ -2,6 +2,8 @@ package org.orcid.core.common.util;
 
 import org.apache.commons.lang3.StringUtils;
 import org.orcid.core.security.OrcidRoles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -17,6 +19,8 @@ import org.springframework.security.web.authentication.switchuser.SwitchUserGran
 import java.util.Collection;
 
 public class AuthenticationUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationUtils.class);
 
     public static final GrantedAuthority adminAuthority = new SimpleGrantedAuthority(OrcidRoles.ROLE_ADMIN.name());
 
@@ -85,7 +89,15 @@ public class AuthenticationUtils {
                     if (authority instanceof SwitchUserGrantedAuthority) {
                         SwitchUserGrantedAuthority suga = (SwitchUserGrantedAuthority) authority;
                         Authentication sourceAuthentication = suga.getSource();
+
+                        LOGGER.trace("isDelegatedByAnAdmin");
+                        LOGGER.trace("Authentication: {}", sourceAuthentication);
+                        LOGGER.trace("Authentication type: {}", sourceAuthentication.getClass().getName());
+                        LOGGER.trace("User Details type: {}", sourceAuthentication.getDetails().getClass());
+
                         if (sourceAuthentication instanceof UsernamePasswordAuthenticationToken && sourceAuthentication.getDetails() instanceof UserDetails) {
+                            LOGGER.trace("Authorities: {}", ((UserDetails) sourceAuthentication.getDetails()).getAuthorities());
+                            LOGGER.trace("Admin authority: {}", adminAuthority);
                             return ((UserDetails) sourceAuthentication.getDetails()).getAuthorities().contains(adminAuthority);
                         }
                     }
