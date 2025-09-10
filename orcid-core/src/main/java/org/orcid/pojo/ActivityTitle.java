cf6722d33211a748a206f2eb0bd505eb2524854c
Camelia Dumitru <62257307+Camelia-Orcid@users.noreply.github.com>
2025-09-10 21:07:11 +0100

    ${PD-2040} Added extra fields needed by UI to the pojo used for work title search (#7362)
    * Added extra fields as needed by the frontend
    
    * added search works title to feature to the frontend security
    
    * Remove security intercept url that is not requiered
    
    ---------
    
    Co-authored-by: amontenegro <a.montenegro@orcid.org>

--- orcid-core/src/main/java/org/orcid/pojo/ActivityTitle.java
+++ orcid-core/src/main/java/org/orcid/pojo/ActivityTitle.java
@@ -10,6 +10,11 @@ public class ActivityTitle {
     private boolean isDefault;
     private boolean isFeatured;
     private boolean isPublic;
+    private String publicationYear;
+    private String publicationMonth;
+    private String publicationDay;
+    private String workType;
+    private String journalTitle;
 
     public long getPutCode() {
         return putCode;
@@ -60,4 +65,43 @@ public class ActivityTitle {
         this.isPublic = isPublic;
     }
 
+    public String getJournalTitle() {
+        return journalTitle;
+    }
+
+    public void setJournalTitle(String journalTitle) {
+        this.journalTitle = journalTitle;
+    }
+
+    public String getWorkType() {
+        return workType;
+    }
+
+    public void setWorkType(String workType) {
+        this.workType = workType;
+    }
+
+    public String getPublicationDay() {
+        return publicationDay;
+    }
+
+    public void setPublicationDay(String publicationDay) {
+        this.publicationDay = publicationDay;
+    }
+
+    public String getPublicationMonth() {
+        return publicationMonth;
+    }
+
+    public void setPublicationMonth(String publicationMonth) {
+        this.publicationMonth = publicationMonth;
+    }
+
+    public String getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(String publicationYear) {
+        this.publicationYear = publicationYear;
+    }
 }
