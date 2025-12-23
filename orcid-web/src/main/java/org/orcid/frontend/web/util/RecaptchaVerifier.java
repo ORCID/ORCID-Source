919adc83a16b6d5b31139b7ab2d3793223d51db3
amontenegro <a.montenegro@orcid.org>
2025-12-23 13:32:26 -0600

    Small refactoring

--- orcid-web/src/main/java/org/orcid/frontend/web/util/RecaptchaVerifier.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/util/RecaptchaVerifier.java
@@ -2,6 +2,7 @@ package org.orcid.frontend.web.util;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
@@ -14,14 +15,14 @@ import org.slf4j.LoggerFactory;
  * Adapted from
  * http://www.journaldev.com/7133/how-to-integrate-google-recaptcha-
  * in-java-web-application Not great code, but I think it will work.
- * 
+ *
  * @author tom
  *
  */
 public class RecaptchaVerifier {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RecaptchaVerifier.class);
-    
+
     private final static String USER_AGENT = "Mozilla/5.0";
     private final String url;
     private final String secret;
@@ -38,43 +39,45 @@ public class RecaptchaVerifier {
         }
 
         try {
-            URL obj = new URL(this.url);
-            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
-            // add request header
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-            String postParams = "secret=" + this.secret + "&response=" + gRecaptchaResponse;
-
-            // Send post request
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.writeBytes(postParams);
-            wr.flush();
-            wr.close();
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            BufferedReader in = getBufferedReader(gRecaptchaResponse);
             String inputLine;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
 
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
-            
-            String responseStr = response.toString();
-            boolean verified = responseStr.contains("true");
-            
-            if (!verified) {
-                LOGGER.warn("Recaptcha verification failed: response does not contain 'true'. Response: {}", responseStr);
+
+            if(response.toString().contains("true")){
+                return true;
+            } else {
+                LOGGER.warn("Recaptcha verification failed: Response: {}", response.toString());
+                return false;
             }
-            
-            return verified;
         } catch (Exception e) {
             LOGGER.error("Recaptcha verification error", e);
             return false;
         }
+    }
+
+    private BufferedReader getBufferedReader(String gRecaptchaResponse) throws IOException {
+        URL obj = new URL(this.url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        // add request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String postParams = "secret=" + this.secret + "&response=" + gRecaptchaResponse;
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(postParams);
+        wr.flush();
+        wr.close();
+
+        return new BufferedReader(new InputStreamReader(con.getInputStream()));
     }
 }
