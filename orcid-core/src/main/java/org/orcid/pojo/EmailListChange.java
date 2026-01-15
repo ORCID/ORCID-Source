3c80c03dbeb58e2863c4d0d4fd542347469a397e
andrej romanov <50377758+auumgn@users.noreply.github.com>
2026-01-15 18:41:31 +0200

    update email list change message logic
    send on adding new (unverified) emails
    do not send on verification
    do not send on deprecation

--- orcid-core/src/main/java/org/orcid/pojo/EmailListChange.java
+++ orcid-core/src/main/java/org/orcid/pojo/EmailListChange.java
@@ -6,22 +6,11 @@ import java.util.List;
 
 // Used for populating security email templates when changes are made to a person's email list
 public class EmailListChange {
-    // Used during verification
-    private List<String> verifiedEmails = new ArrayList<>();
-
     // Used when emails are removed using the UI
     private List<org.orcid.jaxb.model.v3.release.record.Email> removedEmails = new ArrayList<>();
 
     // Used when emails are transferred to another record via deprecation
-    private List<org.orcid.jaxb.model.v3.release.record.Email> addedEmails = new ArrayList<>();
-
-    public List<String> getVerifiedEmails() {
-        return verifiedEmails;
-    }
-
-    public void setVerifiedEmails(List<String> verifiedEmails) {
-        this.verifiedEmails = verifiedEmails;
-    }
+    private List<org.orcid.pojo.ajaxForm.Email> addedEmails = new ArrayList<>();
 
     public List<org.orcid.jaxb.model.v3.release.record.Email> getRemovedEmails() {
         return removedEmails;
@@ -31,11 +20,11 @@ public class EmailListChange {
         this.removedEmails = removedEmails;
     }
 
-    public List<org.orcid.jaxb.model.v3.release.record.Email> getAddedEmails() {
+    public List<org.orcid.pojo.ajaxForm.Email> getAddedEmails() {
         return addedEmails;
     }
 
-    public void setAddedEmails(List<org.orcid.jaxb.model.v3.release.record.Email> addedEmails) {
+    public void setAddedEmails(List<org.orcid.pojo.ajaxForm.Email> addedEmails) {
         this.addedEmails = addedEmails;
     }
 }
