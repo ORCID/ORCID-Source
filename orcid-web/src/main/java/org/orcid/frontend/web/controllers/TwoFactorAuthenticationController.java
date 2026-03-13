d0c17bb1bde3ee1249c73e4d9476f451a27fa91b
Angel Montenegro <a.montenegro@orcid.org>
2026-03-13 10:04:19 -0600

    Fix headers (#7480)
    Co-authored-by: andrej romanov <50377758+aromanovv@users.noreply.github.com>

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/TwoFactorAuthenticationController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/TwoFactorAuthenticationController.java
@@ -90,8 +90,10 @@ public class TwoFactorAuthenticationController extends BaseController {
         return code;
     }
     
-    @RequestMapping(value = "/qr-code.png", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE, headers = "Cache-Control=no-cache,no-store,must-revalidate")
-    public @ResponseBody byte[] generateQrCode() {
+    @RequestMapping(value = "/qr-code.png", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+    @ResponseBody
+    public byte[] generateQrCode(HttpServletResponse response) {
+        response.addHeader("Cache-Control", "no-cache,no-store,must-revalidate");
         return QRCode.from(twoFactorAuthenticationManager.getQRCode(getCurrentUserOrcid())).withSize(250, 250).stream().toByteArray();
     }
 
