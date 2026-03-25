9844102edcb4aff379b900477b102774d3f9ea6a
andrej romanov <50377758+aromanovv@users.noreply.github.com>
2026-03-25 16:12:22 +0200

    fix id property name (#7490)
    Co-authored-by: andrej romanov <50377758+auumgn@users.noreply.github.com>
    Co-authored-by: Angel Montenegro <a.montenegro@orcid.org>

--- orcid-core/src/main/java/org/orcid/pojo/ManageSocialAccount.java
+++ orcid-core/src/main/java/org/orcid/pojo/ManageSocialAccount.java
@@ -1,24 +1,20 @@
 package org.orcid.pojo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.orcid.persistence.jpa.entities.UserconnectionPK;
-import org.orcid.pojo.ajaxForm.ErrorsInterface;
 
 /**
  * @author Shobhit Tyagi
  */
 public class ManageSocialAccount extends AuthChallenge {
 
-    private UserconnectionPK idToMange;
+    private UserconnectionPK id;
 
-    public UserconnectionPK getIdToManage() {
-        return idToMange;
+    public UserconnectionPK getId() {
+        return id;
     }
 
-    public void setIdToManage(UserconnectionPK idToManage) {
-        this.idToMange = idToManage;
+    public void setId(UserconnectionPK idToManage) {
+        this.id = idToManage;
     }
 
 }
