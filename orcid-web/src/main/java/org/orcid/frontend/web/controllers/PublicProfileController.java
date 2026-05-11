3a2c12359ac549a828359973d087931d38ebf2eb
amontenegro <a.montenegro@orcid.org>
2026-05-11 10:22:02 -0600

    Do the page size check first

--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/PublicProfileController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/PublicProfileController.java
@@ -449,13 +449,13 @@ public class PublicProfileController extends BaseWorkspaceController {
     @RequestMapping(value = "/{orcid:(?:\\d{4}-){3,}\\d{3}[\\dX]}/worksPage.json", method = RequestMethod.GET)
     public @ResponseBody Page<WorkGroup> getWorkGroupsJson(@PathVariable("orcid") String orcid, @RequestParam(value="pageSize", defaultValue = PAGE_SIZE_DEFAULT) int pageSize, @RequestParam("offset") int offset, @RequestParam("sort") String sort,
             @RequestParam("sortAsc") boolean sortAsc) {
+        if(pageSize > maxWorksPerPageForUI) {
+            // If the page size is greater than the max works per page for UI, return an empty page.
+            return new Page<WorkGroup>();
+        }
         try {
             orcidSecurityManager.checkProfile(orcid);
         } catch (Exception e) {
-            return new Page<WorkGroup>();
-        }
-        if(pageSize > maxWorksPerPageForUI) {
-            // If the page size is greater than the max works per page for UI, return an empty page.
             return new Page<WorkGroup>();
         }
         return worksPaginator.getWorksPage(orcid, offset, pageSize, true, sort, sortAsc);
