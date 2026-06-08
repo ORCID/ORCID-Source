cf571df6c098abff83576e848ad64974fc1060c4
Angel Montenegro <a.montenegro@orcid.org>
2026-06-08 13:26:59 -0600

    Pd 5670 (#7529)
    * The problem with Address serialization is fixed
    
    * Unit tests
    
    * Use the RequestContextHolder to get the request attributes

--- orcid-core/src/main/java/org/orcid/core/utils/SourceEntityUtils.java
+++ orcid-core/src/main/java/org/orcid/core/utils/SourceEntityUtils.java
@@ -3,6 +3,7 @@ package org.orcid.core.utils;
 import java.util.Optional;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.orcid.core.manager.ClientDetailsEntityCacheManager;
@@ -18,10 +19,15 @@ import org.orcid.persistence.jpa.entities.OrcidAware;
 import org.orcid.persistence.jpa.entities.ProfileEntity;
 import org.orcid.persistence.jpa.entities.SourceAwareEntity;
 import org.orcid.persistence.jpa.entities.SourceEntity;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class SourceEntityUtils {
 
     public static final String SOURCE_MAP = "sourceMap";
+
+    public static final String DO_NOT_POPULATE_SOURCES = "DO_NOT_POPULATE_SOURCES";
 
     @Resource(name = "recordNameManagerReadOnlyV3")
     private RecordNameManagerReadOnly recordNameManagerReadOnlyV3;
@@ -153,7 +159,7 @@ public class SourceEntityUtils {
         if (s.getSourceOrcid() != null && s.getSourceOrcid().getPath() != null) {
             s.getSourceOrcid().setHost(orcidUrlManager.getBaseHost());
             s.getSourceOrcid().setUri(orcidUrlManager.getBaseUrl() + "/" + s.getSourceOrcid().getPath());
-            String sourceNameValue = sourceNameCacheManager.retrieve(s.getSourceOrcid().getPath());
+            String sourceNameValue = getSourceName(s.getSourceOrcid().getPath());
             if (sourceNameValue != null) {
                 s.setSourceName(new SourceName(sourceNameValue));
             }
@@ -161,7 +167,7 @@ public class SourceEntityUtils {
         if (s.getSourceClientId() != null && s.getSourceClientId().getPath() != null) {
             s.getSourceClientId().setHost(orcidUrlManager.getBaseHost());
             s.getSourceClientId().setUri(orcidUrlManager.getBaseUrl() + "/client/" + s.getSourceClientId().getPath());
-            String sourceNameValue = sourceNameCacheManager.retrieve(s.getSourceClientId().getPath());
+            String sourceNameValue = getSourceName(s.getSourceClientId().getPath());
             if (sourceNameValue != null) {
                 s.setSourceName(new SourceName(sourceNameValue));
             }
@@ -170,7 +176,7 @@ public class SourceEntityUtils {
         if (s.getAssertionOriginOrcid() != null && s.getAssertionOriginOrcid().getPath() != null) {
             s.getAssertionOriginOrcid().setHost(orcidUrlManager.getBaseHost());
             s.getAssertionOriginOrcid().setUri(orcidUrlManager.getBaseUrl() + "/" + s.getAssertionOriginOrcid().getPath());
-            String sourceNameValue = sourceNameCacheManager.retrieve(s.getAssertionOriginOrcid().getPath());
+            String sourceNameValue = getSourceName(s.getAssertionOriginOrcid().getPath());
             if (sourceNameValue != null) {
                 s.setAssertionOriginName(new SourceName(sourceNameValue));
             }
@@ -178,11 +184,22 @@ public class SourceEntityUtils {
         if (s.getAssertionOriginClientId() != null && s.getAssertionOriginClientId().getPath() != null) {
             s.getAssertionOriginClientId().setHost(orcidUrlManager.getBaseHost());
             s.getAssertionOriginClientId().setUri(orcidUrlManager.getBaseUrl() + "/client/" + s.getAssertionOriginClientId().getPath());
-            String sourceNameValue = sourceNameCacheManager.retrieve(s.getAssertionOriginClientId().getPath());
+            String sourceNameValue = getSourceName(s.getAssertionOriginClientId().getPath());
             if (sourceNameValue != null) {
                 s.setAssertionOriginName(new SourceName(sourceNameValue));
             }
         }
+    }
+
+    private String getSourceName(String sourceId) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            Object doNotPopulateSource = attributes.getAttribute(SourceEntityUtils.DO_NOT_POPULATE_SOURCES, RequestAttributes.SCOPE_REQUEST);
+            if(doNotPopulateSource != null) {
+                return null;
+            }
+        }
+        return sourceNameCacheManager.retrieve(sourceId);
     }
 
     // =================================
--- orcid-web/src/main/java/org/orcid/frontend/web/controllers/PublicRecordApiController.java
+++ orcid-web/src/main/java/org/orcid/frontend/web/controllers/PublicRecordApiController.java
@@ -1,10 +1,24 @@
 package org.orcid.frontend.web.controllers;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import org.orcid.api.common.util.v3.PublicRecordUtils;
+import org.orcid.api.publicV3.server.security.PublicAPISecurityManagerV3;
 import org.orcid.core.api.OrcidApiConstants;
+import org.orcid.core.manager.v3.OrcidSecurityManager;
+import org.orcid.core.manager.v3.read_only.AddressManagerReadOnly;
+import org.orcid.core.manager.v3.read_only.RecordManagerReadOnly;
+import org.orcid.core.utils.SourceEntityUtils;
+import org.orcid.jaxb.model.common.Iso3166Country;
+import org.orcid.jaxb.model.v3.release.common.Country;
+import org.orcid.jaxb.model.v3.release.record.Address;
+import org.orcid.jaxb.model.v3.release.record.Addresses;
 import org.orcid.jaxb.model.v3.release.record.Record;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,14 +29,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PublicRecordApiController {
 
-    @Resource
-    private PublicRecordUtils publicRecordUtils;
+    @Resource(name = "recordManagerReadOnlyV3")
+    private RecordManagerReadOnly recordManagerReadOnly;
+
+    @Resource(name = "orcidSecurityManagerV3")
+    private OrcidSecurityManager orcidSecurityManager;
+
+    @Resource(name = "publicAPISecurityManagerV3")
+    private PublicAPISecurityManagerV3 publicAPISecurityManagerV3;
+
+    private final ObjectMapper mapper;
 
     private final boolean filterVersionOfIdentifiers = false;
 
+    public PublicRecordApiController() {
+        this.mapper = new ObjectMapper();
+        mapper.registerModule(new JaxbAnnotationModule());
+    }
+
     @RequestMapping(value = "/{orcid:(?:\\d{4}-){3,}\\d{3}[\\dX]}/record", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
             OrcidApiConstants.ORCID_JSON, OrcidApiConstants.VND_ORCID_JSON })
-    public @ResponseBody Record viewRecord(@PathVariable("orcid") String orcid) {
-        return publicRecordUtils.getPublicRecord(orcid, filterVersionOfIdentifiers);
+    public @ResponseBody String viewRecord(HttpServletRequest request, @PathVariable("orcid") String orcid) throws JsonProcessingException {
+        orcidSecurityManager.checkProfile(orcid);
+        request.setAttribute(SourceEntityUtils.DO_NOT_POPULATE_SOURCES, true);
+        Record publicRecord = recordManagerReadOnly.getPublicRecord(orcid, filterVersionOfIdentifiers);
+        return mapper.writeValueAsString(publicRecord);
     }
 }
