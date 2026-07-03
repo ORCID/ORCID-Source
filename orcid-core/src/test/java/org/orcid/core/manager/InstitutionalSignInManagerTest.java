35dc03955ce2fe34d933afdddce6538054d84bfc
amontenegro <a.montenegro@orcid.org>
2026-07-03 13:49:19 -0600

    Fix unit tests

--- orcid-core/src/test/java/org/orcid/core/manager/InstitutionalSignInManagerTest.java
+++ orcid-core/src/test/java/org/orcid/core/manager/InstitutionalSignInManagerTest.java
@@ -1,10 +1,14 @@
 package org.orcid.core.manager;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -12,19 +16,19 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Resource;
-
 import org.apache.commons.io.IOUtils;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.orcid.core.manager.impl.InstitutionalSignInManagerImpl;
+import org.orcid.core.manager.impl.OrcidUrlManager;
+import org.orcid.core.manager.v3.NotificationManager;
 import org.orcid.core.oauth.OrcidOauth2TokenDetailService;
 import org.orcid.core.utils.JsonUtils;
 import org.orcid.persistence.dao.UserConnectionDao;
@@ -32,12 +36,9 @@ import org.orcid.persistence.jpa.entities.ClientDetailsEntity;
 import org.orcid.persistence.jpa.entities.UserconnectionEntity;
 import org.orcid.pojo.HeaderCheckResult;
 import org.orcid.pojo.HeaderMismatch;
-import org.orcid.test.OrcidJUnit4ClassRunner;
-import org.orcid.test.TargetProxyHelper;
-import org.springframework.test.context.ContextConfiguration;
+import org.orcid.pojo.RemoteUser;
+import org.springframework.test.util.ReflectionTestUtils;
 
-@RunWith(OrcidJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:test-orcid-core-context.xml" })
 public class InstitutionalSignInManagerTest {
 
     private final String userOrcid = "0000-0000-0000-0001";
@@ -55,37 +56,22 @@ public class InstitutionalSignInManagerTest {
     @Mock
     private OrcidOauth2TokenDetailService mock_orcidOauth2TokenDetailService;
 
-    @Resource
-    private UserConnectionDao userConnectionDao;
+    @Mock
+    private OrcidUrlManager mock_orcidUrlManager;
 
-    @Resource
-    private ClientDetailsEntityCacheManager clientDetailsEntityCacheManager;
-
-    @Resource
-    private NotificationManager notificationManager;
-
-    @Resource
-    private OrcidOauth2TokenDetailService orcidOauth2TokenDetailService;
-
-    @Resource
-    private InstitutionalSignInManager institutionalSignInManager;
+    private InstitutionalSignInManagerImpl institutionalSignInManager;
 
     @Before
     public void before() {
         MockitoAnnotations.initMocks(this);
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "userConnectionDao", mock_userConnectionDao);
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "clientDetailsEntityCacheManager", mock_clientDetailsEntityCacheManager);
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "notificationManager", mock_notificationManager);
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "orcidOauth2TokenDetailService", mock_orcidOauth2TokenDetailService);
-    }
-
-    @After
-    public void after() {
-        // Rollback to the original beans
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "userConnectionDao", userConnectionDao);
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "clientDetailsEntityCacheManager", clientDetailsEntityCacheManager);
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "notificationManager", notificationManager);
-        TargetProxyHelper.injectIntoProxy(institutionalSignInManager, "orcidOauth2TokenDetailService", orcidOauth2TokenDetailService);
+        // Use a dummy discoFeedSource to avoid network calls in constructor
+        institutionalSignInManager = new InstitutionalSignInManagerImpl("http://localhost/dummy");
+        
+        ReflectionTestUtils.setField(institutionalSignInManager, "userConnectionDao", mock_userConnectionDao);
+        ReflectionTestUtils.setField(institutionalSignInManager, "clientDetailsEntityCacheManager", mock_clientDetailsEntityCacheManager);
+        ReflectionTestUtils.setField(institutionalSignInManager, "notificationManager", mock_notificationManager);
+        ReflectionTestUtils.setField(institutionalSignInManager, "orcidOauth2TokenDetailService", mock_orcidOauth2TokenDetailService);
+        ReflectionTestUtils.setField(institutionalSignInManager, "orcidUrlManager", mock_orcidUrlManager);
     }
 
     @Test
@@ -212,4 +198,95 @@ public class InstitutionalSignInManagerTest {
         assertEquals("myself@testshib.org;someoneelse@testshib.org", mismatch.getCurrentValue());
     }
 
+    @Test
+    public void testRetrieveRemoteUser() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("persistent-id", "user1");
+        RemoteUser remoteUser = institutionalSignInManager.retrieveRemoteUser(headers);
+        assertNotNull(remoteUser);
+        assertEquals("user1", remoteUser.getUserId());
+        assertEquals("persistent-id", remoteUser.getIdType());
+
+        headers.clear();
+        headers.put("edu-person-unique-id", "user2");
+        remoteUser = institutionalSignInManager.retrieveRemoteUser(headers);
+        assertNotNull(remoteUser);
+        assertEquals("user2", remoteUser.getUserId());
+        assertEquals("edu-person-unique-id", remoteUser.getIdType());
+
+        headers.clear();
+        headers.put("targeted-id-oid", "user3");
+        remoteUser = institutionalSignInManager.retrieveRemoteUser(headers);
+        assertNotNull(remoteUser);
+        assertEquals("user3", remoteUser.getUserId());
+        assertEquals("targeted-id-oid", remoteUser.getIdType());
+
+        headers.clear();
+        headers.put("targeted-id", "user4");
+        remoteUser = institutionalSignInManager.retrieveRemoteUser(headers);
+        assertNotNull(remoteUser);
+        assertEquals("user4", remoteUser.getUserId());
+        assertEquals("targeted-id", remoteUser.getIdType());
+
+        headers.clear();
+        headers.put("subject-id", "user5");
+        remoteUser = institutionalSignInManager.retrieveRemoteUser(headers);
+        assertNotNull(remoteUser);
+        assertEquals("user5", remoteUser.getUserId());
+        assertEquals("subject-id", remoteUser.getIdType());
+
+        headers.clear();
+        assertNull(institutionalSignInManager.retrieveRemoteUser(headers));
+    }
+
+    @Test
+    public void testRetrieveDisplayName() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("eppn", "test@orcid.org");
+        assertEquals("test@orcid.org", institutionalSignInManager.retrieveDisplayName(headers));
+
+        headers.clear();
+        assertNull(institutionalSignInManager.retrieveDisplayName(headers));
+    }
+
+    @Test
+    public void testRetrieveFirstName() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("givenname", "John");
+        assertEquals("John", institutionalSignInManager.retrieveFirstName(headers));
+
+        headers.clear();
+        assertEquals("", institutionalSignInManager.retrieveFirstName(headers));
+    }
+
+    @Test
+    public void testRetrieveLastName() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("sn", "Doe");
+        assertEquals("Doe", institutionalSignInManager.retrieveLastName(headers));
+
+        headers.clear();
+        assertEquals("", institutionalSignInManager.retrieveLastName(headers));
+    }
+
+    @Test
+    public void testExtractFirstWithEscapedSemicolon() {
+        Map<String, String> headers = new HashMap<>();
+        // Shibboleth escapes semicolons with backslash
+        headers.put("persistent-id", "user\\;name;anotheruser");
+        RemoteUser remoteUser = institutionalSignInManager.retrieveRemoteUser(headers);
+        assertNotNull(remoteUser);
+        assertEquals("user;name", remoteUser.getUserId());
+    }
+
+    @Test
+    public void testGetInstitutionName() {
+        // Since we can't easily populate the map without network, we can use reflection to set it for testing
+        Map<String, String> institutionNames = new HashMap<>();
+        institutionNames.put("provider1", "Institution 1");
+        ReflectionTestUtils.setField(institutionalSignInManager, "institutionNames", institutionNames);
+
+        assertEquals("Institution 1", institutionalSignInManager.getInstitutionName("provider1"));
+        assertNull(institutionalSignInManager.getInstitutionName("unknown"));
+    }
 }
